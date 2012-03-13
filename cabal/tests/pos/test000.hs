51249b0a2bdf1f82307fe7f1c80bcee9183ca9a1
Ranjit Jhala <rjhala@goto.ucsd.edu>
2012-03-13 08:55:03 -0700

    simplify test000.hs

--- cabal/tests/pos/test000.hs
+++ cabal/tests/pos/test000.hs
@@ -4,11 +4,10 @@ module Test0 where
 
 import Language.Haskell.Liquid.Prelude
 
-x :: Int
-x = choose 0
+toss :: Bool 
+toss = (choose 0) > 10
 
-baz :: Int -> Bool
-baz gooberding = assert (gooberding >= 0)
+--baz gooberding = assert gooberding
 
-prop_abs ::  Bool
-prop_abs = if x > 0 then baz x else False
+prop_abs :: Bool
+prop_abs = if toss then assert toss else False
