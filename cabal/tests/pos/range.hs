c8bc22166090b56e228ad6549495ce3a11c24d12
niki <niki@niki-laptop>
2012-04-04 11:37:50 -0700

    fix map

--- cabal/tests/pos/range.hs
+++ cabal/tests/pos/range.hs
@@ -15,7 +15,7 @@ m = choose 1
 mymap f [] = []
 mymap f (x:xs) = (f x) : (mymap f xs)
 
-prop_rng1 = mymap (assert . (0 <=)) $ range 0 n
+prop_rng1 = map (assert . (0 <=)) $ range 0 n
 prop_rng2 = mymap (assert . (n <=)) $ range n 100
 prop_rng3 = mymap (assert . (n <=)) $ range n m
 prop_rng4 = mymap (assert . (<= m)) $ range n m 
