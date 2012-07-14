60f3acf1753d82f83c49d42246d6a8da7d05b9aa
rjhala <rjhala@gazpacho-ubuntu.(none)>
2012-07-14 12:05:13 -0700

    List-mini works. hooray

---
+++ benchmarks/ghc-7.4.1/List-mini.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE Trustworthy #-}
+{-# LANGUAGE CPP, MagicHash #-}
+{-# OPTIONS_HADDOCK hide #-}
+
+module GHC.List (
+   mylength
+ ) where
+
+import Data.Maybe
+import GHC.Base
+
+{-@ assert mylength :: forall a. xs:[a] -> {v: Int | v = len(xs)}  @-}
+mylength                  :: [a] -> Int
+mylength l                =  go l 0# 
+--mylength l                =  lenJHALA l 0#
+--  where
+--    lenJHALA :: [a] -> Int# -> Int
+--    lenJHALA []     a# = I# a#
+--    lenJHALA (_:xs) a# = lenJHALA xs (a# +# 1#)
+
+go :: [a] -> Int# -> Int
+go []     a# = I# a#
+go (_:xs) a# = go xs (a# +# 1#) 
