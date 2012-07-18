9752b5ae6bfe84b5f8e0f8b7dbb25a66841c5699
rjhala <rjhala@gazpacho-ubuntu.(none)>
2012-07-18 16:21:23 -0700

    messing with  parser

--- cabal/tests/pos/ite.hs
+++ cabal/tests/pos/ite.hs
@@ -1,8 +1,8 @@
 module Test0 where
 
-{- assert myabs :: x:Int -> {v: Int | ((x > 0) ? (v = x) : (v + x = 0)) } -}
+{- assert myabs :: x:Int -> {v: Int | v = ((x > 0) ? x : (0-x)) } -}
 
-{-@ assert myabs :: x:Int -> {v: Int | (v = (x > 0) ? x : (0-x)) } @-}
+{-@ assert myabs :: x:Int -> {v: Int | ((x > 0) ? (v = x) : (v + x = 0)) } @-}
 myabs :: Int -> Int
 myabs x | x > 0     = x
         | otherwise = (0 - x)
