67715d44e3c45e22bb4b86a09b6864a1a1963426
rjhala <rjhala@gazpacho-ubuntu.(none)>
2012-07-19 22:34:35 -0700

    tweak ofBareType so it also does the expand type business

--- benchmarks/ghc-7.4.1/List-mini.hs
+++ benchmarks/ghc-7.4.1/List-mini.hs
@@ -3,28 +3,36 @@
 {-# OPTIONS_HADDOCK hide #-}
 
 module GHC.List (
-   length
+   foldr1
  ) where
 
 import Data.Maybe
 import GHC.Base
 
-{-@ assert length :: forall a. xs:[a] -> {v: Int | v = len(xs)}  @-}
-length                  :: [a] -> Int
-length l                =  len l 0#
-  where
-    len :: [a] -> Int# -> Int
-    len []     a# = I# a#
-    len (_:xs) a# = len xs (a# +# 1#)
+--{-@ assert length :: forall a. xs:[a] -> {v: Int | v = len(xs)}  @-}
+--length                  :: [a] -> Int
+--length l                =  len l 0#
+--  where
+--    len :: [a] -> Int# -> Int
+--    len []     a# = I# a#
+--    len (_:xs) a# = len xs (a# +# 1#)
+--
+--go :: [a] -> Int# -> Int
+--go []     a# = I# a#
+--go (_:xs) a# = go xs (a# +# 1#) 
 
-go :: [a] -> Int# -> Int
-go []     a# = I# a#
-go (_:xs) a# = go xs (a# +# 1#) 
+{-@ assert errorEmptyList :: {v: String | (0 = 1)} -> a @-}
+errorEmptyList :: String -> a
+errorEmptyList fun =
+  error (prel_list_str ++ fun ++ ": empty list")
 
-{-@ assert scanl        :: forall a, b. (a -> b -> a) -> a -> xs:[b] -> {v: [a] | len(v) = 1 + len(xs) } @-}
-scanl                   :: (a -> b -> a) -> a -> [b] -> [a]
-scanl f q ls            =  q : (case ls of
-                                []   -> []
-                                x:xs -> scanl f (f q x) xs)
+prel_list_str :: String
+prel_list_str = "Prelude."
+
+{-@ assert foldr1       :: forall a. (a -> a -> a) -> xs:{v: [a] | len(v) > 0} -> a @-}
+foldr1                  :: (a -> a -> a) -> [a] -> a
+foldr1 _ [x]            =  x
+foldr1 f (x:xs)         =  f x (foldr1 f xs)
+foldr1 _ []             =  errorEmptyList "foldr1"
 
 
