51880f5ce83dd0a8ecced57f5a5db48576850062
rjhala <rjhala@gazpacho-ubuntu.(none)>
2012-07-20 17:26:04 -0700

    reverting bar.hs

--- cabal/tests/pos/bar.hs
+++ cabal/tests/pos/bar.hs
@@ -4,7 +4,5 @@ import Language.Haskell.Liquid.Prelude
 
 prop = let s = (\x -> x + 1)  $ 3 in 
        let s1 = (\x -> x - 1) $ 4 in
-       assert (s > 3)
-       && 
-       assert (s1 < 4) 
+       assert (s > 3 && s1 < 4) 
 
