e09a8adc1aa5d40125822d6f1b0122b121e99b11
Ranjit Jhala <rjhala@ramen.(none)>
2012-07-22 16:16:58 -0700

    stuff compiles. get null in ghc/List-mini.hs working

--- benchmarks/ghc-7.4.1/List-mini.hs
+++ benchmarks/ghc-7.4.1/List-mini.hs
@@ -12,53 +12,39 @@ import Data.Maybe
 import GHC.Base hiding (assert) 
 import Language.Haskell.Liquid.Prelude (crash)
 
+{-@ assert null :: forall a. xs:[a] -> {v: Bool | ((? v) <=> len(xs) = 0) }  @-}
+null                    :: [a] -> Bool
+null []                 =  True
+null (_:_)              =  False
+
 --{- include <len.hquals> @-}
---
 --{- assert reverse :: xs:[a] -> {v: [a] | len(v) = len(xs)} @-}
 --reverse :: [a] -> [a]
 --reverse l =  rev l []
 --  where rev []     a = a
 --        rev (x:xs) a = rev xs (x:a)
---
---{- assert length :: xs:[a] -> {v: Int | v = len(xs)}  @-}
---length                  :: [a] -> Int
---length l                =  len l 0#
---  where
---    len :: [a] -> Int# -> Int
---    len []     a# = I# a#
---    len (_:xs) a# = len xs (a# +# 1#)
-----
-----go :: [a] -> Int# -> Int
-----go []     a# = I# a#
-----go (_:xs) a# = go xs (a# +# 1#) 
---
 
 {-@ assert errorEmptyList :: {v: String | (0 = 1)} -> a @-}
-errorEmptyList :: String -> a
-errorEmptyList fun =
-  error (prel_list_str ++ fun ++ ": empty list")
 
-prel_list_str :: String
-prel_list_str = "Prelude."
+--errorEmptyList :: String -> a
+--errorEmptyList fun =
+--  error (prel_list_str ++ fun ++ ": empty list")
+--
+--prel_list_str :: String
+--prel_list_str = "Prelude."
 
 {-@ assert foldr1 :: (a -> a -> a) -> xs:{v: [a] | len(v) > 0} -> a @-}
-foldr1            :: (a -> a -> a) -> [a] -> a
-foldr1 _ [x]      =  x
-foldr1 f (x:xs)   =  f x (foldr1 f xs)
-foldr1 _ []       =  errorEmptyList "foldr1"
 
+--foldr1            :: (a -> a -> a) -> [a] -> a
+--foldr1 _ [x]      =  x
+--foldr1 f (x:xs)   =  f x (foldr1 f xs)
+--foldr1 _ []       =  errorEmptyList "foldr1"
 
-foldr1 f arg = case arg of
-                []     -> crash
-                (x:xs) -> case xs of
-                            [] -> x
-                            _  -> f x (foldr1 f xs) 
-
-len(arg) = 1 + len(xs)
-len(arg) >= 1
-
-len(xs) = len(arg) - 1
-len(xs) >= 0
+--foldr1 f arg = case arg of
+--                []     -> crash
+--                (x:xs) -> case xs of
+--                            [] -> x
+--                            _  -> f x (foldr1 f xs) 
 
 
 
