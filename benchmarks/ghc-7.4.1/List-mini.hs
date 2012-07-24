cacb67a92da9a78c0f74239a6f729b7cf24f94c2
Ranjit Jhala <rjhala@ramen.(none)>
2012-07-23 22:53:01 -0700

    get auto-qualgen to generalize sort params polymorphic -- see k_51 vs k_99 in Lit-min.hs.fq

--- benchmarks/ghc-7.4.1/List-mini.hs
+++ benchmarks/ghc-7.4.1/List-mini.hs
@@ -3,8 +3,7 @@
 {-# OPTIONS_HADDOCK hide #-}
 
 module GHC.List (
- -- take, drop, 
- foo
+ take, drop, foo
  ) where
 
 import Data.Maybe
@@ -20,38 +19,36 @@ fooUInt n
   | n >=# 0#  = fooUInt_unsafe n
   | otherwise = liquidAssert False False
 
--- fooUInt_unsafe n = if (n ==# 0#) then True else liquidAssert (n ># 0#) True
-
 fooUInt_unsafe 0# = True
 fooUInt_unsafe n  = liquidAssert (n ># 0#) True     -- GET THIS WORKING
 
-{- assert take  :: n: {v: Int | v >= 0 } -> xs:[a] -> {v:[a] | len(v) = ((len(xs) < n) ? len(xs) : n) } @-}
+{-@ assert take  :: n: {v: Int | v >= 0 } -> xs:[a] -> {v:[a] | len(v) = ((len(xs) < n) ? len(xs) : n) } @-}
 {- INLINE [0] take -}
---take            :: Int -> [a] -> [a]
---take (I# n#) xs = takeUInt n# xs
---
---takeUInt :: Int# -> [b] -> [b]
---takeUInt n xs
---  | n >=# 0#  =  take_unsafe_UInt n xs
---  | otherwise =  liquidAssert False []
---
---take_unsafe_UInt :: Int# -> [b] -> [b]
---take_unsafe_UInt 0#  _     = []
---take_unsafe_UInt mother ls =
---  case ls of
---    []     -> []
---    (x:xs) -> x : take_unsafe_UInt (mother -# 1#) xs
+take            :: Int -> [a] -> [a]
+take (I# n#) xs = takeUInt n# xs
 
-{- assert drop        :: n: {v: Int | v >= 0 } -> xs:[a] -> {v:[a] | len(v) = ((len(xs) <  n) ? 0 : len(xs) - n) } @-}
---drop                   :: Int -> [a] -> [a]
---drop (I# n#) ls
---  | n# <# 0#    = ls
---  | otherwise   = drop# n# ls
---    where
---        drop# :: Int# -> [a] -> [a]
---        drop# 0# xs      = xs
---        drop# _  xs@[]   = xs
---        drop# m# (_:xs)  = drop# (m# -# 1#) xs
+takeUInt :: Int# -> [a] -> [a]
+takeUInt n xs
+  | n >=# 0#  =  take_unsafe_UInt n xs
+  | otherwise =  liquidAssert False []
+
+take_unsafe_UInt :: Int# -> [a] -> [a]
+take_unsafe_UInt 0#  _     = []
+take_unsafe_UInt n ls      =
+  case ls of
+    []     -> []
+    (x:xs) -> x : take_unsafe_UInt (n -# 1#) xs
+
+{-@ assert drop        :: n: {v: Int | v >= 0 } -> xs:[a] -> {v:[a] | len(v) = ((len(xs) <  n) ? 0 : len(xs) - n) } @-}
+drop                   :: Int -> [a] -> [a]
+drop (I# n#) ls
+  | n# <# 0#    = ls
+  | otherwise   = drop# n# ls
+    where
+        drop# :: Int# -> [a] -> [a]
+        drop# 0# xs      = xs
+        drop# _  xs@[]   = xs
+        drop# m# (_:xs)  = drop# (m# -# 1#) xs
 
 
 
