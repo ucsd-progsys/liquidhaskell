f5219237bfbcf81382958ba905ff9f52b6532b55
Ranjit Jhala <rjhala@ramen.(none)>
2012-07-25 17:36:42 -0700

    use mkCoreLets instead of hand-done stitching...

--- benchmarks/ghc-7.4.1/List-mini.hs
+++ benchmarks/ghc-7.4.1/List-mini.hs
@@ -1,52 +1,38 @@
 {-# LANGUAGE Trustworthy #-}
-{-# LANGUAGE CPP, MagicHash #-}
+{-# LANGUAGE CPP,  NoImplicitPrelude, MagicHash #-}
 {-# OPTIONS_HADDOCK hide #-}
 
--- NoImplicitPrelude
-
 module GHC.List (
- foo, mtake
- ) where
+    take0
+  , take
+  ) where
 
 import Data.Maybe
 import GHC.Base hiding (assert) 
 import Language.Haskell.Liquid.Prelude (liquidAssert, liquidError) 
 
-{-@ assert mtake  :: n: {v: Int | 0 <= v} -> [a] -> {v:[a] | (len(v) = n)} @-}
-mtake          :: Int -> [a] -> [a]
--- OK mtake n xs = if n == 0 then [] else case xs of z:zs -> z : mtake (n-1) zs
+{-@ assert take0  :: n: {v: Int | 0 <= v} -> [a] -> {v:[a] | (len(v) = n)} @-}
+take0 :: Int -> [a] -> [a]
+take0 (I# n#) xs = take_unsafe_UInt0 n# xs
 
-mtake 0 _      = []
-mtake n (x:xs) = x : (mtake ((liquidAssert (n /= 0) n) - 1) xs)
---OK: mtake n (x:xs) = x : (take (n-1) xs)
+take_unsafe_UInt0 :: Int# -> [a] -> [a]
+take_unsafe_UInt0 0#  _     = []
+take_unsafe_UInt0 n  (x:xs) = x : take_unsafe_UInt0 (n -# 1#) xs
 
-{- assert take  :: n: {v: Int | v >= 0 } -> xs:[a] -> {v:[a] | len(v) = ((len(xs) < n) ? len(xs) : n) } @-}
+{-@ assert take  :: n: {v: Int | v >= 0 } -> xs:[a] -> {v:[a] | len(v) = ((len(xs) < n) ? len(xs) : n) } @-}
+take (I# n#) xs = takeUInt n# xs
 
-{-@ assert foo :: Int -> Int @-}
-foo :: Int -> Int
-foo 0 = 0
-foo n = liquidAssert (n /= 0) (n - 1)
+takeUInt :: Int# -> [a] -> [a]
+takeUInt n xs
+  | n >=# 0#  =  take_unsafe_UInt n xs
+  | otherwise =  liquidAssert False []
 
-{- INLINE [0] take -}
---take            :: Int -> [a] -> [a]
---take (I# n#) xs = take_unsafe_UInt n# xs
----- take (I# n#) xs = takeUInt n# xs
---
-----takeUInt :: Int# -> [a] -> [a]
-----takeUInt n xs
-----  | n >=# 0#  =  take_unsafe_UInt n xs
-----  | otherwise =  liquidAssert False []
---
---take_unsafe_UInt :: Int# -> [a] -> [a]
---take_unsafe_UInt 0#  _     = []
---take_unsafe_UInt n ls      =
---  case ls of
---    -- []     -> []
---    (x:xs) -> x : take_unsafe_UInt (n -# 1#) xs
-
-
-
-
+take_unsafe_UInt :: Int# -> [a] -> [a]
+take_unsafe_UInt 0#  _     = []
+take_unsafe_UInt n ls      =
+  case ls of
+    []     -> []
+    (x:xs) -> x : take_unsafe_UInt (n -# 1#) xs
 
 
 
