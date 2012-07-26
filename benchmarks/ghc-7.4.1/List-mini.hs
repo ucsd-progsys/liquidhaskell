49cf7be312251061ff9e6bf0c31bb6afa6d4fef3
Ranjit Jhala <rjhala@ramen.(none)>
2012-07-25 17:59:00 -0700

    dsfsdf

--- benchmarks/ghc-7.4.1/List-mini.hs
+++ benchmarks/ghc-7.4.1/List-mini.hs
@@ -11,6 +11,7 @@ import Data.Maybe
 import GHC.Base hiding (assert) 
 import Language.Haskell.Liquid.Prelude (liquidAssert, liquidError) 
 
+
 {-@ assert take0  :: n: {v: Int | 0 <= v} -> [a] -> {v:[a] | (len(v) = n)} @-}
 take0 :: Int -> [a] -> [a]
 take0 (I# n#) xs = take_unsafe_UInt0 n# xs
@@ -21,6 +22,7 @@ take_unsafe_UInt0 n  (x:xs) = x : take_unsafe_UInt0 (n -# 1#) xs
 
 {-@ assert take  :: n: {v: Int | v >= 0 } -> xs:[a] -> {v:[a] | len(v) = ((len(xs) < n) ? len(xs) : n) } @-}
 take (I# n#) xs = takeUInt n# xs
+-- take (I# n#) xs = take_unsafe_UInt n# xs
 
 takeUInt :: Int# -> [a] -> [a]
 takeUInt n xs
@@ -33,6 +35,10 @@ take_unsafe_UInt n ls      =
   case ls of
     []     -> []
     (x:xs) -> x : take_unsafe_UInt (n -# 1#) xs
+
+
+
+
 
 
 
