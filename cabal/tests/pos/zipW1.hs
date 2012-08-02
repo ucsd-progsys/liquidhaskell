cdfd117c259dd925acfbc30a98f4390af12131e6
Ranjit Jhala <rjhala@ramen.(none)>
2012-08-01 19:30:20 -0700

    moving all List specs into List.lhs

--- cabal/tests/pos/zipW1.hs
+++ cabal/tests/pos/zipW1.hs
@@ -1,7 +1,5 @@
 module T where
 
-import Language.Haskell.Liquid.Prelude
-
 {-@ assert foo :: (a -> b -> c) -> xs : [a] -> ys:{v:[b] | len(v) = len(xs)} -> {v : [c] | len(v) = len(xs)} @-}
 foo = zipWith
 
