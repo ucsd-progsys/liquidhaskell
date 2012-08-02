8dc3cf4e732992f3984916b5519fd306ec41c954
Ranjit Jhala <rjhala@ramen.(none)>
2012-08-02 09:26:49 -0700

    tweaking div sig in prelude.spec

--- cabal/include/Prelude.spec
+++ cabal/include/Prelude.spec
@@ -2,9 +2,13 @@ module spec Prelude where
 
 import GHC.Base
 
+
 assume GHC.Integer.smallInteger :: x:GHC.Prim.Int# -> {v:Integer | v = (x :: Integer)}
 assume GHC.Num.+                :: (Num a) => x:a -> y:a -> {v:a | v = x + y }
 assume GHC.Num.-                :: (Num a) => x:a -> y:a -> {v:a | v = x - y }
 assume GHC.Num.*                :: (Num a) => x:a -> y:a -> {v:a | (((x >= 0) && (y >= 0)) => (v >= 0)) }
+
 assume GHC.Real.div             :: (Integral a) => x:a -> y:a -> {v:a | v = (x / y) }
+assume GHC.Real./               :: (Fractional a) => x:a -> y:{v:a | v != 0} -> {v: a | v = (x / y) }
+
 assume GHC.Num.fromInteger      :: (Num a) => x:Integer -> {v:a | v = x }
--- cabal/tests/pos/trans.hs
+++ cabal/tests/pos/trans.hs
@@ -3,21 +3,17 @@ module Tx where
 import Language.Haskell.Liquid.Prelude
 
 {-@ assert transpose :: n:{v:Int | v >= 0} 
-                     -> m:{v:Int | v >= 0} 
+                     -> m:{v:Int | v > 0} 
                      -> {v:[{v:[a] | len(v) = n}] | len(v) = m} 
                      -> {v:[{v:[a] | len(v) = m}] | len(v) = n} 
   @-}
 transpose :: Int -> Int -> [[a]] -> [[a]]
 transpose 0 _ _              = []
 transpose n m ((x:xs) : xss) = (x : map head xss) : transpose (n - 1) m (xs : map tail xss)
+transpose n m ([] : _)       = liquidError "transpose1" 
+transpose n m []             = liquidError "transpose2"
 
 -- NEEDS TAGS: map head xss = [ h | (h:_) <- xss]
 -- NEEDS TAGS: map tail xss = [t | (_:t) <- xss]
-
-
-
-
-
-
 
 
