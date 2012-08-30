afdf9bf7a3fbedbddb9016d91cf62d82131b60fc
Ranjit Jhala <rjhala@ramen.(none)>
2012-08-30 04:45:35 -0700

    deptupW works but using the same params is tedious.

--- cabal/tests/neg/deptupW.hs
+++ cabal/tests/neg/deptupW.hs
@@ -5,6 +5,8 @@ import Language.Haskell.Liquid.Prelude
 {-@ data Pair a b <p :: x0:a -> x1:b -> Bool> = P (x :: a) (y :: b<p x>) @-} 
 data Pair a b = P a b
 
+
+-- Names are shifty. I bet this would not work with alpha-renaming.
 {-@ mkP :: forall a <p :: x0:a -> x1:a -> Bool>. x: a -> y: a<p x> -> Pair <p> a a @-}
 mkP :: a -> a -> Pair a a
 mkP x y = error "TBD"
