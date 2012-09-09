58fcde22e1d534fb48b63bf82a0a58770c4af256
niki <niki@nikiL>
2012-09-10 00:48:50 +0200

    alpha rename tests/pos/deptupW.hs

--- cabal/tests/pos/deptupW.hs
+++ cabal/tests/pos/deptupW.hs
@@ -5,7 +5,7 @@ import Language.Haskell.Liquid.Prelude
 {-@ data Pair a b <p :: x0:a -> x1:b -> Bool> = P (x :: a) (y :: b<p x>) @-} 
 data Pair a b = P a b
 
-{-@ mkP :: forall a <p :: x0:a -> x1:a -> Bool>. x: a -> y: a<p x> -> Pair <p> a a @-}
+{-@ mkP :: forall a <q :: y0:a -> y1:a -> Bool>. x: a -> y: a<q x> -> Pair <q> a a @-}
 mkP :: a -> a -> Pair a a
 mkP x y = error "TBD"
 
