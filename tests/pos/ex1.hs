7eb9718428d76b94cfc0f33db30b9f55406232ab
Ranjit Jhala <jhala@cs.ucsd.edu>
2012-09-22 13:39:17 -0700

    new tests for ex

---
+++ tests/pos/ex1.hs
@@ -0,0 +1,22 @@
+module Ex where
+
+-- | Testing "existential-types"
+
+{-@ efoldr :: forall a <p :: x0:L a -> x1:b -> Bool>. 
+                (xs:L a -> x:a -> b <p xs> -> exists [xxs : {v: L a | v = cons(x,xs)}]. b<p xxs>) 
+              -> (exists [x0: {v: L a | v = nil }]. b <p x0>) 
+              -> ys: L a
+              -> b <p ys>
+  @-}
+
+efoldr :: (L a -> a -> b -> b) -> b -> L a -> b
+efoldr op b Nil         = b
+efoldr op b (Cons x xs) = op xs x (efoldr f b xs) 
+
+
+{-@ size :: xs:L a -> {v: Int | v = llen(xs)} @-}
+size :: L a -> Int
+size = efoldr (\_ _ n -> n + 1) 0
+
+
+app xs ys = efoldr (\_ -> Cons) ys xs 
