a5ae2eb1089020d133f2aeb79f015c736bde1568
Niki Vazou <nvazou@bender.(none)>
2012-10-17 17:51:43 -0700

    start db example

---
+++ tests/pos/DataBase.hs
@@ -0,0 +1,25 @@
+module DataBase where
+
+data Rec s v = R (s -> v)
+type DBRec = Rec String Value
+
+get :: String -> DBRec -> Value 
+get i (R a) = a i
+
+set :: String -> Value -> DBRec -> DBRec
+set i x (R a) = R $ \k -> if k == i then x else a k
+
+{-@ empty :: Rec {v:String|0=1} {v : Value | v = BOT} @-}
+empty :: Rec String Value
+empty = R $ const BOT
+
+data Value = BOT
+           | INT Int 
+           | BOOL Bool 
+           | STRING String
+--            | VEC Value Int
+
+
+append a1 a2 = \k -> case a1 k of 
+                     BOT -> a2 k
+                     v   -> v
