a9e43909b2a6ee5a1f1cf68e26fd3c8e3c244c73
rjhala <rjhala@gazpacho-ubuntu.(none)>
2012-10-19 15:05:58 -0700

    first test on sets passes

--- tests/pos/meas9.hs
+++ tests/pos/meas9.hs
@@ -2,7 +2,12 @@ module Meas where
 
 import Data.Set (Set(..))
 
-{-@ myapp :: xs:[a] -> ys:[a] -> {v:[a]| listElts(v) = Set_cup(listElts(xs), listElts(ys))} @-}
+{-@ myid :: xs:[a] -> {v:[a]| listElts(v) = listElts(xs)} @-}
+myid []     = []
+myid (x:xs) = x : myid xs
+
+{-@ myapp :: xs:[a] -> ys:[a] -> {v:[a] | listElts(v) = Set_cup(listElts(xs), listElts(ys))} @-}
+myapp :: [a] -> [a] -> [a]
 myapp []     ys = ys
 myapp (x:xs) ys = x : myapp xs ys
 
