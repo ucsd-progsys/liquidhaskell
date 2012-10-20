818684ca9d5463354372f517dddd5eb3977af7a7
Ranjit Jhala <jhala@cs.ucsd.edu>
2012-10-19 19:27:10 -0700

    wtf meas10, meas11.hs?

--- tests/pos/meas9.hs
+++ tests/pos/meas9.hs
@@ -11,3 +11,12 @@ myapp :: [a] -> [a] -> [a]
 myapp []     ys = ys
 myapp (x:xs) ys = x : myapp xs ys
 
+{-@ myrev :: xs:[a] -> {v:[a]| listElts(v) = listElts(xs)} @-}
+myrev :: [a] -> [a]
+myrev = goRev [] 
+
+{-@ goRev :: xs:[a] -> ys:[a] -> {v:[a] | listElts(v) = Set_cup(listElts(xs), listElts(ys))} @-}
+goRev :: [a] -> [a] -> [a]
+goRev acc []     = acc
+goRev acc (y:ys) = goRev (y:acc) ys
+
