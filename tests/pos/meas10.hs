061681e53aab144c9aa43997e6f1b5937f5f14cc
Ranjit Jhala <jhala@cs.ucsd.edu>
2012-10-19 20:58:18 -0700

    why is meas10 being mean. sigh.

--- tests/pos/meas10.hs
+++ tests/pos/meas10.hs
@@ -4,17 +4,25 @@ import Data.Set (Set(..))
 
 {-@ include <listSet.hquals> @-}
 
-{- myrev :: xs:[a] -> {v:[a]| listElts(v) = listElts(xs)} -}
+{-@ myrev :: xs:[a] -> {v:[a]| listElts(v) = listElts(xs)} @-}
 myrev :: [a] -> [a]
-myrev xs = goRev [] xs 
+myrev xs = goo [] xs 
 --   where go acc []     = acc
 --         go acc (y:ys) = go (y:acc) ys
 
+-- WHY DOES THIS JUST NOT GET ANY MEANINGFUL TYPE?
+goo :: [a] -> [a] -> [a]
+goo acc []     = acc
+goo acc (y:ys) = error "foo" -- goRev (y:acc) ys
 
 -- WORKS WITH EXPLICIT ANNOTATION
--- {- goRev :: xs:[a] -> ys:[a] -> {v:[a] | listElts(v) = Set_cup(listElts(xs), listElts(ys))} @-}
+{-@ goRev :: xs:[a] -> ys:[a] -> {v:[a] | listElts(v) = Set_cup(listElts(xs), listElts(ys))} @-}
 goRev :: [a] -> [a] -> [a]
 goRev acc []     = acc
 goRev acc (y:ys) = goRev (y:acc) ys
 
 
+
+{-@ emptySet :: a -> {v:[b] | Set_emp(listElts(v))} @-}
+emptySet :: a -> [b]
+emptySet x = []
