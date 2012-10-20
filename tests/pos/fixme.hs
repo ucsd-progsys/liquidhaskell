0bf36dc9d2185c2e438a41f8f400fdb4b7374229
Ranjit Jhala <jhala@cs.ucsd.edu>
2012-10-20 11:26:16 -0700

    hmm. sets seems to work magically now.

--- tests/pos/fixme.hs
+++ tests/pos/fixme.hs
@@ -1,5 +1,28 @@
-module ListSort where
+module ListSetTest where
+
+import Data.Set (Set(..))
+
+{-@ include <listSet.hquals> @-}
 
 
-foo :: [Int] -> [Int]
-foo zs = zipWith (+) zs zs
+-- WHY DOES THIS JUST NOT GET ANY MEANINGFUL TYPE?
+{- goo :: xs:[a] 
+       -> ys:[a] 
+       -> {v:[a] | listElts(v) = Set_cup(listElts(xs), listElts(ys))} 
+ @-}
+goo :: [a] -> [a] -> [a]
+goo acc []     = acc
+goo acc (y:ys) = error "foo" -- goRev (y:acc) ys
+
+{-@ choo :: [a] -> [a] @-}
+choo :: [a] -> [a]
+choo xs = goo [] xs
+
+
+
+
+
+
+
+
+
--- tests/pos/meas10.hs
+++ tests/pos/meas10.hs
@@ -6,22 +6,21 @@ import Data.Set (Set(..))
 
 {-@ myrev :: xs:[a] -> {v:[a]| listElts(v) = listElts(xs)} @-}
 myrev :: [a] -> [a]
-myrev xs = goo [] xs 
+myrev xs = goRev [] xs 
 --   where go acc []     = acc
 --         go acc (y:ys) = go (y:acc) ys
 
 -- WHY DOES THIS JUST NOT GET ANY MEANINGFUL TYPE?
+{- goo :: xs:[a] -> ys:[a] -> {v:[a] | listElts(v) = Set_cup(listElts(xs), listElts(ys))} @-}
 goo :: [a] -> [a] -> [a]
 goo acc []     = acc
 goo acc (y:ys) = error "foo" -- goRev (y:acc) ys
 
 -- WORKS WITH EXPLICIT ANNOTATION
-{-@ goRev :: xs:[a] -> ys:[a] -> {v:[a] | listElts(v) = Set_cup(listElts(xs), listElts(ys))} @-}
+{- goRev :: xs:[a] -> ys:[a] -> {v:[a] | listElts(v) = Set_cup(listElts(xs), listElts(ys))} @-}
 goRev :: [a] -> [a] -> [a]
 goRev acc []     = acc
 goRev acc (y:ys) = goRev (y:acc) ys
-
-
 
 {-@ emptySet :: a -> {v:[b] | Set_emp(listElts(v))} @-}
 emptySet :: a -> [b]
