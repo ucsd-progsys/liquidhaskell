fb2b6a7d44fd9c90d3d319fce6b60dc4afa26d6a
Ranjit Jhala <jhala@cs.ucsd.edu>
2012-10-23 12:12:21 -0700

    should have made a branch. tweaking parser

--- tests/pos/lets.hs
+++ tests/pos/lets.hs
@@ -6,10 +6,8 @@ foo :: Int -> (Int, Int)
 foo z = (z, z + 1)
 
 baz :: Int -> (Int, Int)
-baz z = if z < 10 
-          then (0, 1) 
-          else let (i, j) = foo z in
-               (i, j + 1)
+baz z = let (i, j) = foo z 
+        in (i, j + 1)
 
 {-@ prop :: Int -> Bool @-}
 prop x = let (a, b) = baz x in
