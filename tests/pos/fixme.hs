218cbe36c7a04f11ae5f00652330081185ae4d7d
rjhala <rjhala@gazpacho-ubuntu.(none)>
2012-10-26 09:59:20 -0700

    fixme is now a shrunk ListQSort.hs why so many iters?

--- tests/pos/fixme.hs
+++ tests/pos/fixme.hs
@@ -1,13 +1,34 @@
-module Fib where
+module ListSort where
 
-{-@ sumn :: (Int, a, b, c, d) -> ({v: Int | v >= 0}, a, b, c, d) @-}
-sumn :: (Int, a, b, c, d) -> (Int, a, b, c, d)
-sumn (n, x1, x2, x3, x4) 
-  | n <= 0    = (0, x1, x2, x3, x4) 
-  | otherwise = let (z, y1, y2, y3, y4) = sumn (n-1, x1, x2, x3, x4) 
-                in (n + z, y1, y2, y3, y4)
+{-@ type OList a = [a]<{v: a | (v >= fld)}> @-}
 
+{-@ app :: k:a -> OList {v:a | v < k} -> OList {v:a | v >= k} -> OList a @-}
+app :: a -> [a] -> [a] -> [a]
+app = error "HOLE"
+-- app k []     ys = k:ys
+-- app k (x:xs) ys = x:(app k xs ys) 
 
+{- qsort :: (Ord a) => xs:[a] -> OList a @-}
+-- qsort []     = []
+-- qsort (x:xs) = app x (qsort [y | y <- xs, y < x]) (qsort [z | z <- xs, z >= x]) 
+-- qsort (x:xs) = app x (qsort [y | y <- xs, y < x]) (qsort [z | z <- xs, z >= x]) 
 
+{-@ quicksort :: (Ord a) => xs:[a] -> OList a @-}
+quicksort []     = []
+quicksort (x:xs) = app x xsle xsge
+  where xsle = quicksort (takeL x xs)
+        xsge = quicksort (takeGE x xs)
+
+{- takeL :: (Ord a) => x:a -> xs:[a] -> [{v:a | v < x}] @-}
+takeL :: (Ord a) => a -> [a] -> [a]
+-- takeL = error "HOLE"
+takeL x []     = []
+takeL x (y:ys) = if (y<x) then y:(takeL x ys) else takeL x ys
+
+{- takeGE :: (Ord a) => x:a -> xs:[a] -> [{v:a | v >= x}] @-}
+takeGE :: (Ord a) => a -> [a] -> [a]
+-- takeGE = error "HOLE"
+takeGE x []     = []
+takeGE x (y:ys) = if (y>=x) then y:(takeGE x ys) else takeGE x ys
 
 
