d7f3509906c40a5562ac7dc9ccf63de57a1525cc
Ranjit Jhala <jhala@cs.ucsd.edu>
2012-11-07 21:01:03 -0800

    look at tests/pos/fixme.hs

--- tests/pos/fixme.hs
+++ tests/pos/fixme.hs
@@ -1,19 +1,12 @@
-module GhcSort where
+module Foo where
 
-import Language.Haskell.Liquid.Prelude
--- This is SAFE
-{-@ bar :: (Ord a) => a -> a -> {v: Bool | (? v)} @-}
-bar a b
-  = case a `compare` b of
-      GT        -> True
-      otherwise -> liquidAssertB (a <= b)
+{-@  
+data List a <p :: x0:a -> x1:a -> Bool>  
+  = Nil 
+  | Cons (h :: a) (t :: List <p> (a <p h>))
+@-}
 
--- This is UNSAFE:
-{-@ foo :: (Ord a) => a -> a -> {v: Bool | (? v)} @-}
-foo a b 
-  | a `compare` b == GT = True
-  | otherwise           = liquidAssertB (a <= b)
+data List a = Nil | Cons a (List a)
 
-
-
+foo x = 8 
 
