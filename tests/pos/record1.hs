7bee51fe686c9c496bf4c7a80cf7c2935039f82e
Ranjit Jhala <jhala@cs.ucsd.edu>
2012-11-08 09:01:17 -0800

    must make simple tyvar-type substitutions easier and more consistent

---
+++ tests/pos/record1.hs
@@ -0,0 +1,15 @@
+module Data.Map.Base (trim) where
+
+data Map k a  = Tip
+
+{-@ data Map k a <l :: root:k -> k -> Bool>
+         = Tip
+  @-}
+
+{-@ measure isBin :: Map k a -> Bool
+    isBin (Tip)          = false
+  @-}
+
+trim :: Map k a
+trim = error "TODO"
+
