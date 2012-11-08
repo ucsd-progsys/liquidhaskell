48fa4184efcc6ae6434b3d3678efa6e4cf0dea6d
Ranjit Jhala <jhala@cs.ucsd.edu>
2012-11-08 07:57:12 -0800

    bare.hs is a hideous mess.

--- tests/pos/fixme.hs
+++ tests/pos/fixme.hs
@@ -3,10 +3,10 @@ module Foo where
 {-@  
 data List a <p :: x0:a -> x1:a -> Bool>  
   = Nil 
-  | Cons (h :: a) (t :: List <p> (a <p h>))
+  | MYCONOS (h :: a) (t :: List <p> (a <p h>))
 @-}
 
-data List a = Nil | Cons a (List a)
+data List a = Nil | MYCONOS a (List a)
 
 foo x = 8 
 
