4bb6f7e9453676ef6c6c0e848fc5f14a40d812a9
Ranjit Jhala <jhala@cs.ucsd.edu>
2012-11-17 09:33:59 -0800

    self-invariants appear to work

--- tests/pos/maybe4.hs
+++ tests/pos/maybe4.hs
@@ -1,6 +1,6 @@
 module Foo where
 
-{-@ goo   :: lo:{v0a: Maybe {v:a | ((isJust v0a) && (v = (fromJust v0a)))} | (0=0) } 
+{-@ goo   :: lo:{v0a: Maybe {v:a | ((isJust v0a) && (v = (fromJust v0a)))} | true } 
           -> hi:{v0b: Maybe {v:a | ((isJust v0b) && (v = (fromJust v0b)))} | (((isJust(lo) && isJust(v0b)) => (fromJust(v0b) >= fromJust(lo)))) }   
           -> Bool 
   @-}
