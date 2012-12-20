158288720853bc312f62ce177e610c578ded8ad0
Ranjit Jhala <jhala@cs.ucsd.edu>
2012-12-19 20:03:34 -0800

    adding cons case just to test...

--- tests/pos/fixme.hs
+++ tests/pos/fixme.hs
@@ -8,7 +8,7 @@ module Ex where
 -- | Data types ---------------------------------------------------------
 -------------------------------------------------------------------------
 
-data Vec a = Nil 
+data Vec a = Nil | Cons a (Vec a)
 
 {-@ efoldr :: forall b a <p :: x0:Vec a -> x1:b -> Bool>. 
               (exists [zz: {v: Vec a | v = Ex.Nil}]. b <p zz>) 
