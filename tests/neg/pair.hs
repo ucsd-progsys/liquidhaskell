e88fcc523b5ead611024da4f5f7d8d8f33fb97c8
niki <niki@nikiL>
2012-12-21 20:50:36 -0800

    modify neg tests

--- tests/neg/pair.hs
+++ tests/neg/pair.hs
@@ -2,7 +2,7 @@ module Pair where
 
 import Language.Haskell.Liquid.Prelude 
 
-{-@ data Pair a b <p :: a -> b -> Bool> = P (x :: a) (y :: b<p x>) @-} 
+{-@ data Pair a b <p :: a -> b -> Prop> = P (x :: a) (y :: b<p x>) @-} 
 data Pair a b = P a b
 
 incr x = let p = P x ((x+1)) in p
