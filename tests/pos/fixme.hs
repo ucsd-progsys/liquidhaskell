fd7c29578b62e01c6788c8ca3d27f83e05aadfe2
Ranjit Jhala <jhala@cs.ucsd.edu>
2012-12-27 08:30:16 -0800

    why is tests pos fixme not working

--- tests/pos/fixme.hs
+++ tests/pos/fixme.hs
@@ -1,20 +1,34 @@
 module Fixme where
 
-import Prelude hiding (length)
-import Data.Vector
 import Language.Haskell.Liquid.Prelude (liquidAssert)
 
+data Vec a = V a deriving (Eq)
 
-{-@ foo :: x:(Vector Int) 
-        -> y:{v: (Vector Int) | v = x } 
-        -> Int 
-  @-}
-foo     :: Vector Int -> Vector Int -> Int
-foo x y = liquidAssert (x == y) 0
-
+-- this is verified ...
 {-@ bar :: x:Int 
         -> y:{v: Int | v = x } 
         -> Int 
   @-}
 bar     :: Int -> Int -> Int
 bar x y = liquidAssert (x == y) 0
+
+-- and so is this...
+
+{-@ goo :: x:[Int] 
+        -> y:{v: [Int] | v = x } 
+        -> Int 
+  @-}
+goo     :: [Int] -> [Int] -> Int
+goo x y = liquidAssert (x == y) 0
+
+
+-- BUT THIS IS NOT!!
+{-@ foo :: x:(Vec Int) 
+        -> y:{v: (Vec Int) | v = x } 
+        -> Int 
+  @-}
+foo     :: Vec Int -> Vec Int -> Int
+foo x y = liquidAssert (x == y) 0
+
+
+
