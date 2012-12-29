4bacd6505b7f32afe776cb52147e7c9c49b89ee1
niki <niki@nikiL>
2012-12-29 19:40:17 -0300

    silly edit in fixme. verified now

--- tests/pos/fixme.hs
+++ tests/pos/fixme.hs
@@ -23,8 +23,8 @@ goo x y = liquidAssert (x == y) 0
 
 
 -- BUT THIS IS NOT!!
-{-@ foo :: x:(Vec Int) 
-        -> y:{v: (Vec Int) | v = x } 
+{-@ foo :: x: (Vec Int)
+        -> y:{v: Vec Int | v = x } 
         -> Int 
   @-}
 foo     :: Vec Int -> Vec Int -> Int
