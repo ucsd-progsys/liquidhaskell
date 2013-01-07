a677ab37bad2c0ef286799855f8be5ad9b0d5455
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-01-06 18:22:32 -0800

    adding smtlib conversion script

---
+++ external/fixpoint/smtLibConvert.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+FIXPOINT=/home/rjhala/research/liquidhaskell/external/fixpoint/fixpoint.native
+Z3=/home/rjhala/research/csolve/external/z3/bin/z3
+
+for f in $1/*.fq; do 
+  echo "Processing $f ..."; 
+  $FIXPOINT -smtlib $f -out $f.smt 
+  perl -pi -e 's/#/_/g' $f.smt
+  $Z3 $f.smt
+done
+
