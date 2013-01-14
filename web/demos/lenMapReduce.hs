66486e62726aa9762ab2400a1c63a49a859facf3
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-01-13 20:47:28 -0800

    ok, blog3 goes through...

---
+++ web/demos/lenMapReduce.hs
@@ -0,0 +1,154 @@
+module ListLengths where
+
+import Data.Hashable 
+import Prelude hiding (length, map, filter, head, tail, foldl1)
+import Language.Haskell.Liquid.Prelude (liquidError)
+import qualified Data.HashMap.Strict as M
+
+-- liquidError = error
+
+-- | The length of a List
+
+-- measure len :: forall a. [a] -> GHC.Types.Int
+-- len ([])     = 0
+-- len (y:ys)   = 1 + (len ys)
+
+------------------------------------------------------------------------------
+
+-- | Constructing Lists
+
+{-@ xs :: {v:[String] | (len v) = 1 } @-}
+xs = "dog" : []
+
+{-@ ys :: {v:[String] | (len v) = 2 } @-}
+ys = ["cat", "dog"]
+
+{-@ zs :: {v:[String] | (len v) = 3 } @-}
+zs = "hippo" : ys
+
+-- Hover your mouse over the `:` and `[]` above to confirm their types!
+
+-- | Deconstructing Lists
+
+{-@ zs' :: {v:[String] | (len v) = 2 } @-}
+zs' = case zs of
+        h : t -> t
+
+
+-- | Reasoning about Lengths
+
+{-@ length :: xs:[a] -> {v: Int | v = (len xs)} @-}
+length :: [a] -> Int
+length []     = 0
+length (x:xs) = 1 + length xs
+
+
+-- | Some old friends ...
+
+{-@ map      :: (a -> b) -> xs:[a] -> {v:[b] | (len v) = (len xs)} @-}
+map _ []     = []
+map f (x:xs) = (f x) : (map f xs)
+
+{-@ filter :: (a -> Bool) -> xs:[a] -> {v:[a] | (len v) <= (len xs)} @-}
+filter _ []     = []
+filter f (x:xs)
+  | f x         = x : filter f xs
+  | otherwise   = filter f xs
+
+{-@ append :: xs:[a] -> ys:[a] -> {v:[a] | (len v) = (len xs) + (len ys)} @-}
+append [] ys     = ys
+append (x:xs) ys = x : append xs ys
+
+------------------------------------------------------------------------------
+
+-- | Safe head
+
+{-@ head   :: {v:[a] | (len v) > 0} -> a @-}
+head (x:_) = x
+head []    = liquidError "Fear not! 'twill ne'er come to pass"
+
+-- | Safe tail
+
+{-@ tail :: {v:[a] | (len v) > 0} -> [a] @-}
+tail (_:xs) = xs
+tail []     = liquidError "Relaxeth! this too shall ne'er be"
+
+-- | Using head, safely
+
+{-@ eliminateStutter :: (Eq a) => [a] -> [a] @-}
+eliminateStutter xs = map head $ groupEq xs
+
+-- `groupEq` gathers consecutive equal elements in the list into a single non-empty list.
+
+groupEq []     = []
+groupEq (x:xs) = (x:ys) : groupEq zs
+                 where (ys,zs) = span (x ==) xs
+
+-- (Put your mouse over the `groupEq` identifier to see inferred type.)
+
+------------------------------------------------------------------------------
+
+-- | A Simple map-reduce library
+
+mapReduce       :: (Eq k, Hashable k) 
+                => (a -> [(k, v)]) -- ^ key-mapper
+                -> (v -> v -> v)   -- ^ reduction operator
+                -> [a]             -- ^ inputs
+                -> [(k, v)]        -- ^ output key-values
+
+mapReduce f op  = M.toList
+                . reduce op
+                . group
+                . keyMap f
+
+-- | `keyMap` expands a list of inputs into a list of key-value pairs:
+
+keyMap :: (a -> [(k, v)]) -> [a] -> [(k, v)]
+keyMap f xs = concatMap f xs
+
+-- | `group` gathers the key-value pairs into a `Map` from keys to the 
+-- lists of values with that same key.
+
+group     :: (Eq k, Hashable k) 
+          => [(k, a)] -> M.HashMap k [a]
+
+group kvs = foldr (\(k, v) m -> inserts k v m) M.empty kvs
+
+
+-- | `inserts` adds the new value `v` to the list of previously known 
+--  values `lookupDefault [] k m` for the key `k`.
+
+inserts   :: (Eq k, Hashable k) 
+          => k -> a -> M.HashMap k [a] -> M.HashMap k [a]
+
+inserts k v m = M.insert k (v : vs) m
+  where vs    = M.lookupDefault [] k m
+
+
+-- | `reduces` crunches a list of values for a given key in the 
+-- table to a single value
+
+reduce    :: (v -> v -> v) -> M.HashMap k [v] -> M.HashMap k v
+reduce op = M.map (foldl1 op)
+
+
+foldl1 f (x:xs) =  foldl f x xs
+foldl1 _ []     =  liquidError "will. never. happen."
+
+
+-- Now, if we want to compute the frequency of `Char` in a given list of words, we can write:
+
+
+{-@ charFrequency :: [String] -> [(Char, Int)] @-}
+charFrequency     :: [String] -> [(Char, Int)]
+charFrequency     = mapReduce wordChars (+)
+  where wordChars = map (\c -> (c, 1))
+
+
+-- You can take it out for a spin like so:
+
+
+f0 = charFrequency [ "the", "quick" , "brown"
+                   , "fox", "jumped", "over"
+                   , "the", "lazy"  , "cow"   ]
+
