c19fd39c50488de64164cc2b80a3627760694949
niki <niki@nikiL>
2013-01-15 11:50:23 -0800

    update Internal.hs

--- benchmarks/bytestring-0.9.2.1/Data/ByteString/Internal.hs
+++ benchmarks/bytestring-0.9.2.1/Data/ByteString/Internal.hs
@@ -36,20 +36,20 @@ module Data.ByteString.Internal (
        fromForeignPtr,         -- :: ForeignPtr Word8 -> Int -> Int -> ByteString
        toForeignPtr,           -- :: ByteString -> (ForeignPtr Word8, Int, Int)
 
--- LIQUID        -- * Utilities
--- LIQUID        inlinePerformIO,        -- :: IO a -> a
--- LIQUID        nullForeignPtr,         -- :: ForeignPtr Word8
--- LIQUID
--- LIQUID        -- * Standard C Functions
--- LIQUID        c_strlen,               -- :: CString -> IO CInt
--- LIQUID        c_free_finalizer,       -- :: FunPtr (Ptr Word8 -> IO ())
--- LIQUID
--- LIQUID        memchr,                 -- :: Ptr Word8 -> Word8 -> CSize -> IO Ptr Word8
--- LIQUID        memcmp,                 -- :: Ptr Word8 -> Ptr Word8 -> CSize -> IO CInt
--- LIQUID        memcpy,                 -- :: Ptr Word8 -> Ptr Word8 -> CSize -> IO ()
--- LIQUID        memset,                 -- :: Ptr Word8 -> Word8 -> CSize -> IO (Ptr Word8)
--- LIQUID
--- LIQUID        -- * cbits functions
+       -- * Utilities
+       inlinePerformIO,        -- :: IO a -> a
+       nullForeignPtr,         -- :: ForeignPtr Word8
+
+       -- * Standard C Functions
+       c_strlen,               -- :: CString -> IO CInt
+       c_free_finalizer,       -- :: FunPtr (Ptr Word8 -> IO ())
+
+       memchr,                 -- :: Ptr Word8 -> Word8 -> CSize -> IO Ptr Word8
+       memcmp,                 -- :: Ptr Word8 -> Ptr Word8 -> CSize -> IO CInt
+       memcpy,                 -- :: Ptr Word8 -> Ptr Word8 -> CSize -> IO ()
+       memset,                 -- :: Ptr Word8 -> Word8 -> CSize -> IO (Ptr Word8)
+
+       -- * cbits functions
 -- LIQUID        c_reverse,              -- :: Ptr Word8 -> Ptr Word8 -> CInt -> IO ()
 -- LIQUID        c_intersperse,          -- :: Ptr Word8 -> Ptr Word8 -> CInt -> Word8 -> IO ()
 -- LIQUID        c_maximum,              -- :: Ptr Word8 -> CInt -> IO Word8
@@ -59,10 +59,10 @@ module Data.ByteString.Internal (
 -- LIQUID        -- * Internal GHC magic
 -- LIQUID        memcpy_ptr_baoff,       -- :: Ptr a -> RawBuffer -> CInt -> CSize -> IO (Ptr ())
 -- LIQUID#endif
--- LIQUID
--- LIQUID        -- * Chars
--- LIQUID        w2c, c2w, isSpaceWord8, isSpaceChar8
--- LIQUID
+
+       -- * Chars
+       w2c, c2w, isSpaceWord8, isSpaceChar8
+
   ) where
 
 import Foreign.ForeignPtr       (ForeignPtr, withForeignPtr)
@@ -86,6 +86,8 @@ import Data.Data                (Data)
 import Data.Generics            (Data)
 #endif
 -- LIQUID import GHC.Base                 (realWorld#,unsafeChr)
+import GHC.Base                 (unsafeChr) -- LIQUID
+import GHC.Int                  (Int32) -- LIQUID
 #if __GLASGOW_HASKELL__ >= 611
 import GHC.IO                   (IO(IO))
 #else
@@ -116,15 +118,19 @@ import GHC.ForeignPtr           (ForeignPtr(ForeignPtr))
 import Foreign.Ptr              (nullPtr)
 #endif
 
+import Foreign.Ptr              (nullPtr) -- LIQUID
+
 #if __HUGS__
 import Hugs.ForeignPtr          (newForeignPtr_)
 #elif __GLASGOW_HASKELL__<=604
 import Foreign.ForeignPtr       (newForeignPtr_)
 #endif
+import Foreign.ForeignPtr       (newForeignPtr_) -- LIQUID 
 
 -- CFILES stuff is Hugs only
 {-# CFILES cbits/fpstring.c #-}
-
+{-@ embed Int32        as int @-}
+{-@ embed CInt        as int @-}
 -- An alternative to Control.Exception (assert) for nhc98
 #ifdef __NHC__
 #define assert	assertS "__FILE__ : __LINE__"
@@ -132,7 +138,6 @@ assertS :: String -> Bool -> a -> a
 assertS _ True  = id
 assertS s False = error ("assertion failed at "++s)
 #endif
-
 -- -----------------------------------------------------------------------------
 --
 -- Useful macros, until we have bang patterns
@@ -154,50 +159,48 @@ assertS s False = error ("assertion failed at "++s)
 data ByteString = PS {-# UNPACK #-} !(ForeignPtr Word8) -- payload
                      {-# UNPACK #-} !Int                -- offset
                      {-# UNPACK #-} !Int                -- length
-
 -- LIQUID #if defined(__GLASGOW_HASKELL__)
 -- LIQUID     deriving (Data, Typeable)
 -- LIQUID #endif
 
 -- LIQUID instance Show ByteString where
 -- LIQUID     showsPrec p ps r = showsPrec p (unpackWith w2c ps) r
--- LIQUID 
+
 -- LIQUID instance Read ByteString where
 -- LIQUID     readsPrec p str = [ (packWith c2w x, y) | (x, y) <- readsPrec p str ]
--- LIQUID 
--- LIQUID -- | /O(n)/ Converts a 'ByteString' to a '[a]', using a conversion function.
--- LIQUID unpackWith :: (Word8 -> a) -> ByteString -> [a]
--- LIQUID unpackWith _ (PS _  _ 0) = []
--- LIQUID unpackWith k (PS ps s l) = inlinePerformIO $ withForeignPtr ps $ \p ->
--- LIQUID         go (p `plusPtr` s) (l - 1) []
--- LIQUID     where
--- LIQUID         STRICT3(go)
--- LIQUID         go p 0 acc = peek p          >>= \e -> return (k e : acc)
--- LIQUID         go p n acc = peekByteOff p n >>= \e -> go p (n-1) (k e : acc)
--- LIQUID {-# INLINE unpackWith #-}
--- LIQUID 
--- LIQUID -- | /O(n)/ Convert a '[a]' into a 'ByteString' using some
--- LIQUID -- conversion function
--- LIQUID packWith :: (a -> Word8) -> [a] -> ByteString
--- LIQUID packWith k str = unsafeCreate (length str) $ \p -> go p str
--- LIQUID     where
--- LIQUID         STRICT2(go)
--- LIQUID         go _ []     = return ()
--- LIQUID         go p (x:xs) = poke p (k x) >> go (p `plusPtr` 1) xs -- less space than pokeElemOff
--- LIQUID {-# INLINE packWith #-}
--- LIQUID 
--- LIQUID ------------------------------------------------------------------------
--- LIQUID 
--- LIQUID -- | The 0 pointer. Used to indicate the empty Bytestring.
--- LIQUID nullForeignPtr :: ForeignPtr Word8
+
+-- | /O(n)/ Converts a 'ByteString' to a '[a]', using a conversion function.
+unpackWith :: (Word8 -> a) -> ByteString -> [a]
+unpackWith _ (PS _  _ 0) = []
+unpackWith k (PS ps s l) = inlinePerformIO $ withForeignPtr ps $ \p ->
+        go (p `plusPtr` s) (l - 1) []
+    where
+        STRICT3(go)
+        go p 0 acc = peek p          >>= \e -> return (k e : acc)
+        go p n acc = peekByteOff p n >>= \e -> go p (n-1) (k e : acc)
+{-# INLINE unpackWith #-}
+
+-- | /O(n)/ Convert a '[a]' into a 'ByteString' using some
+-- conversion function
+packWith :: (a -> Word8) -> [a] -> ByteString
+packWith k str = unsafeCreate (length str) $ \p -> go p str
+    where
+        STRICT2(go)
+        go _ []     = return ()
+        go p (x:xs) = poke p (k x) >> go (p `plusPtr` 1) xs -- less space than pokeElemOff
+{-# INLINE packWith #-}
+
+------------------------------------------------------------------------
+-- | The 0 pointer. Used to indicate the empty Bytestring.
+nullForeignPtr :: ForeignPtr Word8
 -- LIQUID #ifdef __GLASGOW_HASKELL__
 -- LIQUID nullForeignPtr = ForeignPtr nullAddr# undefined --TODO: should ForeignPtrContents be strict?
 -- LIQUID #else
--- LIQUID nullForeignPtr = unsafePerformIO $ newForeignPtr_ nullPtr
--- LIQUID {-# NOINLINE nullForeignPtr #-}
+nullForeignPtr = unsafePerformIO $ newForeignPtr_ nullPtr
+{-# NOINLINE nullForeignPtr #-}
 -- LIQUID #endif
 -- LIQUID 
--- LIQUID -- ---------------------------------------------------------------------
+-- ---------------------------------------------------------------------
 -- Low level constructors
 
 -- | /O(1)/ Build a ByteString from a ForeignPtr.
@@ -269,8 +272,8 @@ createAndTrim' l f = do
                             memcpy p' (p `plusPtr` off) (fromIntegral l')
                     return $! (ps, res)
 
--- LIQUID -- | Wrapper of 'mallocForeignPtrBytes' with faster implementation for GHC
--- LIQUID --
+-- | Wrapper of 'mallocForeignPtrBytes' with faster implementation for GHC
+--
 mallocByteString :: Int -> IO (ForeignPtr a)
 mallocByteString l = do
 #ifdef __GLASGOW_HASKELL__
@@ -281,48 +284,48 @@ mallocByteString l = do
 {-# INLINE mallocByteString #-}
 
 ------------------------------------------------------------------------
--- LIQUID 
--- LIQUID -- | Conversion between 'Word8' and 'Char'. Should compile to a no-op.
--- LIQUID w2c :: Word8 -> Char
--- LIQUID #if !defined(__GLASGOW_HASKELL__)
--- LIQUID w2c = chr . fromIntegral
--- LIQUID #else
--- LIQUID w2c = unsafeChr . fromIntegral
--- LIQUID #endif
--- LIQUID {-# INLINE w2c #-}
--- LIQUID 
--- LIQUID -- | Unsafe conversion between 'Char' and 'Word8'. This is a no-op and
--- LIQUID -- silently truncates to 8 bits Chars > '\255'. It is provided as
--- LIQUID -- convenience for ByteString construction.
--- LIQUID c2w :: Char -> Word8
--- LIQUID c2w = fromIntegral . ord
--- LIQUID {-# INLINE c2w #-}
--- LIQUID 
--- LIQUID -- | Selects words corresponding to white-space characters in the Latin-1 range
--- LIQUID -- ordered by frequency. 
--- LIQUID isSpaceWord8 :: Word8 -> Bool
--- LIQUID isSpaceWord8 w =
--- LIQUID     w == 0x20 ||
--- LIQUID     w == 0x0A || -- LF, \n
--- LIQUID     w == 0x09 || -- HT, \t
--- LIQUID     w == 0x0C || -- FF, \f
--- LIQUID     w == 0x0D || -- CR, \r
--- LIQUID     w == 0x0B || -- VT, \v
--- LIQUID     w == 0xA0    -- spotted by QC..
--- LIQUID {-# INLINE isSpaceWord8 #-}
--- LIQUID 
--- LIQUID -- | Selects white-space characters in the Latin-1 range
--- LIQUID isSpaceChar8 :: Char -> Bool
--- LIQUID isSpaceChar8 c =
--- LIQUID     c == ' '     ||
--- LIQUID     c == '\t'    ||
--- LIQUID     c == '\n'    ||
--- LIQUID     c == '\r'    ||
--- LIQUID     c == '\f'    ||
--- LIQUID     c == '\v'    ||
--- LIQUID     c == '\xa0'
--- LIQUID {-# INLINE isSpaceChar8 #-}
--- LIQUID 
+
+-- | Conversion between 'Word8' and 'Char'. Should compile to a no-op.
+w2c :: Word8 -> Char
+#if !defined(__GLASGOW_HASKELL__)
+w2c = chr . fromIntegral
+#else
+w2c = unsafeChr . fromIntegral
+#endif
+{-# INLINE w2c #-}
+
+-- | Unsafe conversion between 'Char' and 'Word8'. This is a no-op and
+-- silently truncates to 8 bits Chars > '\255'. It is provided as
+-- convenience for ByteString construction.
+c2w :: Char -> Word8
+c2w = fromIntegral . ord
+{-# INLINE c2w #-}
+
+-- | Selects words corresponding to white-space characters in the Latin-1 range
+-- ordered by frequency. 
+isSpaceWord8 :: Word8 -> Bool
+isSpaceWord8 w =
+    w == 0x20 ||
+    w == 0x0A || -- LF, \n
+    w == 0x09 || -- HT, \t
+    w == 0x0C || -- FF, \f
+    w == 0x0D || -- CR, \r
+    w == 0x0B || -- VT, \v
+    w == 0xA0    -- spotted by QC..
+{-# INLINE isSpaceWord8 #-}
+
+-- | Selects white-space characters in the Latin-1 range
+isSpaceChar8 :: Char -> Bool
+isSpaceChar8 c =
+    c == ' '     ||
+    c == '\t'    ||
+    c == '\n'    ||
+    c == '\r'    ||
+    c == '\f'    ||
+    c == '\v'    ||
+    c == '\xa0'
+{-# INLINE isSpaceChar8 #-}
+
 ------------------------------------------------------------------------
 
 -- | Just like unsafePerformIO, but we inline it. Big performance gains as
@@ -360,48 +363,49 @@ foreign import ccall unsafe "string.h memcmp" memcmp
 
 foreign import ccall unsafe "string.h memcpy" c_memcpy
     :: Ptr Word8 -> Ptr Word8 -> CSize -> IO (Ptr Word8)
-memcpy = undefined
+
+memcpy = undefined -- LIQUID 
 -- LIQUID memcpy :: Ptr Word8 -> Ptr Word8 -> CSize -> IO ()
 -- LIQUID memcpy p q s = c_memcpy p q s >> return ()
--- LIQUID 
--- LIQUID {-
--- LIQUID foreign import ccall unsafe "string.h memmove" c_memmove
--- LIQUID     :: Ptr Word8 -> Ptr Word8 -> CSize -> IO (Ptr Word8)
--- LIQUID 
--- LIQUID memmove :: Ptr Word8 -> Ptr Word8 -> CSize -> IO ()
--- LIQUID memmove p q s = do c_memmove p q s
--- LIQUID                    return ()
--- LIQUID -}
--- LIQUID 
--- LIQUID foreign import ccall unsafe "string.h memset" c_memset
--- LIQUID     :: Ptr Word8 -> CInt -> CSize -> IO (Ptr Word8)
--- LIQUID 
--- LIQUID memset :: Ptr Word8 -> Word8 -> CSize -> IO (Ptr Word8)
--- LIQUID memset p w s = c_memset p (fromIntegral w) s
+
+{-
+foreign import ccall unsafe "string.h memmove" c_memmove
+    :: Ptr Word8 -> Ptr Word8 -> CSize -> IO (Ptr Word8)
+
+memmove :: Ptr Word8 -> Ptr Word8 -> CSize -> IO ()
+memmove p q s = do c_memmove p q s
+                   return ()
+-}
+
+foreign import ccall unsafe "string.h memset" c_memset
+    :: Ptr Word8 -> CInt -> CSize -> IO (Ptr Word8)
+
+memset :: Ptr Word8 -> Word8 -> CSize -> IO (Ptr Word8)
+memset p w s = c_memset p (fromIntegral w) s
 -- ---------------------------------------------------------------------
--- LIQUID --
--- LIQUID -- Uses our C code
--- LIQUID --
--- LIQUID 
--- LIQUID foreign import ccall unsafe "static fpstring.h fps_reverse" c_reverse
--- LIQUID     :: Ptr Word8 -> Ptr Word8 -> CULong -> IO ()
--- LIQUID 
--- LIQUID foreign import ccall unsafe "static fpstring.h fps_intersperse" c_intersperse
--- LIQUID     :: Ptr Word8 -> Ptr Word8 -> CULong -> Word8 -> IO ()
--- LIQUID 
--- LIQUID foreign import ccall unsafe "static fpstring.h fps_maximum" c_maximum
--- LIQUID     :: Ptr Word8 -> CULong -> IO Word8
--- LIQUID 
--- LIQUID foreign import ccall unsafe "static fpstring.h fps_minimum" c_minimum
--- LIQUID     :: Ptr Word8 -> CULong -> IO Word8
--- LIQUID 
--- LIQUID foreign import ccall unsafe "static fpstring.h fps_count" c_count
--- LIQUID     :: Ptr Word8 -> CULong -> Word8 -> IO CULong
--- LIQUID 
--- LIQUID -- ---------------------------------------------------------------------
--- LIQUID -- Internal GHC Haskell magic
--- LIQUID 
--- LIQUID #if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ < 611
--- LIQUID foreign import ccall unsafe "__hscore_memcpy_src_off"
--- LIQUID    memcpy_ptr_baoff :: Ptr a -> RawBuffer -> CInt -> CSize -> IO (Ptr ())
--- LIQUID #endif
+--
+-- Uses our C code
+--
+
+foreign import ccall unsafe "static fpstring.h fps_reverse" c_reverse
+    :: Ptr Word8 -> Ptr Word8 -> CULong -> IO ()
+
+foreign import ccall unsafe "static fpstring.h fps_intersperse" c_intersperse
+    :: Ptr Word8 -> Ptr Word8 -> CULong -> Word8 -> IO ()
+
+foreign import ccall unsafe "static fpstring.h fps_maximum" c_maximum
+    :: Ptr Word8 -> CULong -> IO Word8
+
+foreign import ccall unsafe "static fpstring.h fps_minimum" c_minimum
+    :: Ptr Word8 -> CULong -> IO Word8
+
+foreign import ccall unsafe "static fpstring.h fps_count" c_count
+    :: Ptr Word8 -> CULong -> Word8 -> IO CULong
+
+-- ---------------------------------------------------------------------
+-- Internal GHC Haskell magic
+
+#if defined(__GLASGOW_HASKELL__) && __GLASGOW_HASKELL__ < 611
+foreign import ccall unsafe "__hscore_memcpy_src_off"
+   memcpy_ptr_baoff :: Ptr a -> RawBuffer -> CInt -> CSize -> IO (Ptr ())
+#endif
