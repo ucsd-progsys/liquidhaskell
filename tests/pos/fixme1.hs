adde399637d5521cfa8d7a71db9973e488e1f747
niki <niki@nikiL>
2013-01-29 13:04:47 -0800

    tests

--- tests/pos/fixme1.hs
+++ tests/pos/fixme1.hs
@@ -3,8 +3,8 @@ module Foo where
 import Language.Haskell.Liquid.Prelude
 
 gpp :: [Int] -> [Int]
-gpp [] = []
-gpp (x:xs) = liquidAssert (x>=0) x : gpp xs
+gpp []     = []
+gpp (x:xs) = liquidAssert (x==9) x : gpp xs
 
 decr x = x : decr (x-1)
 xs :: [Int]
