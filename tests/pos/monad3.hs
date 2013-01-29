7b733445232425a77275f6110d45d53b246ed2cb
niki <niki@nikiL>
2013-01-29 12:26:47 -0800

    monad3 safe, add fixme

--- tests/pos/monad3.hs
+++ tests/pos/monad3.hs
@@ -2,12 +2,14 @@ module Foo where
 
 import Language.Haskell.Liquid.Prelude
 
-
+-- gpp :: Monad m => m Int -> m Int
 gpp z = do x <- z
            return $ liquidAssert (x >= 0) (x + 1)
 
 
+incrlist n = n : incrlist (n+1)
+
 xs, ys, zs :: [Int]
-xs = [0..]
+xs = incrlist 0
 ys = gpp xs
 zs = gpp ys
