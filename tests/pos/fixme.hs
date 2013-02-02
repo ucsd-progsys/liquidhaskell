0298287cfe87d90d6ccef26233edbb63093b8c6a
niki <niki@nikiL>
2013-02-02 14:42:44 -0800

    moving tests

--- tests/pos/fixme.hs
+++ tests/pos/fixme.hs
@@ -3,10 +3,10 @@ module Foo where
 import Language.Haskell.Liquid.Prelude
 
 gpp :: [Int] -> [Int]
-gpp [] = []
-gpp (x:xs) = liquidAssert (x>=0) x : gpp xs
+gpp []     = []
+gpp (x:xs) = liquidAssert (x==9) x : gpp xs
 
-
+decr x = x : decr (x-1)
 xs :: [Int]
-xs = [0..]
+xs = decr 0
 ys = gpp xs
