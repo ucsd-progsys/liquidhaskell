0980f9699e2b3791ff64e2cd1302e3d3fe9e2821
niki <niki@nikiL>
2013-02-06 16:50:03 -0800

    more tests

---
+++ tests/pos/state00.hs
@@ -0,0 +1,10 @@
+module StateMonad where
+
+type State = Int
+data ST a b = S (b -> (a, b)) | F a | G (b -> a)
+
+{-@ fresh :: ST {v:Int|v>=0} {v:Int|v>=0} @-}
+fresh :: ST Int Int
+fresh = S $ \n -> (n, n+1)
+
+
