d4303919cc3a66c8faa0a0735eb66d4a41799fd4
niki <niki@nikiL>
2013-02-14 11:41:29 -0800

    hide exists from user

--- Language/Haskell/Liquid/Parse.hs
+++ Language/Haskell/Liquid/Parse.hs
@@ -42,7 +42,7 @@ bareTypeP :: Parser BareType
 bareTypeP   
   =  try bareFunP
  <|> bareAllP
- <|> bareExistsP
+--  <|> bareExistsP
  <|> bareAtomP 
  
 bareArgP 
@@ -80,12 +80,12 @@ bareAtomNoAppP
   =  frefP bbaseNoAppP 
  <|> try (dummyP (bbaseNoAppP <* spaces))
 
-bareExistsP 
-  = do reserved "exists"
-       zs <- brackets $ sepBy1 exBindP comma 
-       dot
-       t  <- bareTypeP
-       return $ foldr (uncurry REx) t zs
+-- bareExistsP 
+--   = do reserved "exists"
+--        zs <- brackets $ sepBy1 exBindP comma 
+--        dot
+--        t  <- bareTypeP
+--        return $ foldr (uncurry REx) t zs
      
 exBindP 
   = xyP binderP colon bareTypeP 
