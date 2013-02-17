cbefa81bb2a49bba70409a12d9dde08ca0f134b1
niki <niki@nikiL>
2013-02-17 12:12:03 -0800

    another problem is state

--- tests/todo/state.hs
+++ tests/todo/state.hs
@@ -24,22 +24,17 @@ seq :: ST s a -> (a -> ST s b) -> ST s b
 m `seq` k = S $ \s -> 
   let (a, s') = apply m s in apply (k a) s'
 
--- The above does is not Safe, because s' is not visible 
--- in the type signature
--- The same pattern exists in function composition:
--- What can we have in the result?
-
-{- foo :: forall <p :: xx:a -> a -> Prop>.
-     (x:a -> a<p x>) -> g:(x:a -> a<p x>) -> y:a -> 
-      a<p ?? >
- @-}
-foo :: (a -> a) -> (a -> a) -> a -> a
-foo f g x =
-  let s' = g x in f s'
-
--- We can not have exists [j:{v:a | v = (g x)}]
-
-
+-- The above test is UNSAFE becase it requires
+-- the S :: x:s -> exists[y:s<p1 x>]. (a, s<p1 y>)
+--
+{-
+seq' :: forall <p1 :: old:s -> s -> Prop>.
+        ST <p1> s a -> (a ->  ST <p1> s b) -> 
+        x:s -> exists[y:s<p1 x>]. (a, s<p1 y>)
+  @-}
+-- seq' :: ST s a -> (a -> ST s b) -> ST s b
+-- m `seq'` k = S $ \s -> 
+--   let (a, s') = apply m s in apply (k a) s'
 
 
 {-@ fresh :: ST <\st -> {v:Int|v=st+1}> Int Int@-}
