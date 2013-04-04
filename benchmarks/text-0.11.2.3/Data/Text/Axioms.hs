d87c583187aa31576eaa4c1b6bc6904f306b44a2
Eric Seidel <gridaphobe@gmail.com>
2013-04-04 11:31:21 -0700

    fix up init

--- benchmarks/text-0.11.2.3/Data/Text/Axioms.hs
+++ benchmarks/text-0.11.2.3/Data/Text/Axioms.hs
@@ -10,6 +10,9 @@ axiom_numchars_append a b t = True
 axiom_numchars_replicate :: Text -> Text -> Bool
 axiom_numchars_replicate t t' = True
 
+axiom_numchars_init :: Text -> Text -> Bool
+axiom_numchars_init t t' = True
+
 axiom_numchars_split :: Text -> Int -> Bool
 axiom_numchars_split t i = True
 
