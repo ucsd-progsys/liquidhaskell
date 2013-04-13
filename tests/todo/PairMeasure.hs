5dd4ed5b7d1bba4a8e87a57d84b32dab68fefe4e
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-04-12 18:27:48 -0700

    chuggity

--- tests/todo/PairMeasure.hs
+++ tests/todo/PairMeasure.hs
@@ -4,7 +4,11 @@ module Foo where
     fst (x, y) = x
   @-}
 
-{-@ type OPList a b = [(a, b)]<\h -> {v: (a, b) | (fst v) >= (fst h)}> @-}
+{-@ type Pair a b = (a, b) @-}
+
+-- {v0: ({v:a | v = (fst v0)}, b) | true}  
+ 
+{-@ type OPList a b = [(Pair a b)]<\h -> {v: (Pair a) | (fst v) >= (fst h)}> @-}
 
 {-@ type OList a    = [a]<\h -> {v: a | (v >= h)}> @-}
 
