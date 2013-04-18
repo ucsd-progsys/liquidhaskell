7ea980f60c59669a9744310c421ac89429b58915
niki <niki@nikiL>
2013-04-17 22:48:12 -0700

    adding tests

---
+++ tests/pos/ListKeys.hs
@@ -0,0 +1,24 @@
+module Foo where
+import Data.Set (Set(..)) 
+
+{-@ measure fst :: (a, b) -> a 
+    fst (x, y) = x
+  @-}
+
+{-@  measure listKeys :: [(k, v)] -> (Set k) 
+    listKeys([])   = {v | (? Set_emp(v))}
+    listKeys(x:xs) = {v | v = (Set_cup (Set_sng (fst x)) (listKeys xs)) }
+
+@-}
+
+
+{-@ getFsts :: ys:[(a, b)] -> {v:[a]|listElts(v) = listKeys(ys)} @-}
+getFsts ::[(a, b)] ->  [a]
+getFsts []           = []
+getFsts ((x, _): xs) = x : getFsts xs
+
+
+
+
+
+
