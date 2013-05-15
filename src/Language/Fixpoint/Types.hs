e8913b3cf98240641774ed13676bfa91d38b2987
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-05-15 13:58:53 -0700

    misc tweaks

--- src/Language/Fixpoint/Types.hs
+++ src/Language/Fixpoint/Types.hs
@@ -51,7 +51,12 @@ module Language.Fixpoint.Types (
   , removeLhsKvars
 
   -- * Environments
-  , SEnv, emptySEnv, fromListSEnv, mapSEnv, insertSEnv, deleteSEnv, memberSEnv, lookupSEnv
+  , SEnv
+  , emptySEnv, toListSEnv, fromListSEnv
+  , mapSEnv
+  , insertSEnv, deleteSEnv, memberSEnv, lookupSEnv
+  , intersectWithSEnv
+
   , FEnv, insertFEnv 
   , IBindEnv, BindId, insertsIBindEnv, deleteIBindEnv, emptyIBindEnv
   , BindEnv, insertBindEnv, emptyBindEnv
@@ -622,16 +627,17 @@ sortedReftValueVariable (RR _ (Reft (v,_))) = v
 ----------------- Environments  -------------------------------
 ---------------------------------------------------------------
 
+toListSEnv              ::  SEnv a -> [(Symbol, a)]
+toListSEnv (SE env)     = M.toList env
 fromListSEnv            ::  [(Symbol, a)] -> SEnv a
 fromListSEnv            = SE . M.fromList
-
 mapSEnv f (SE env)      = SE (fmap f env)
 deleteSEnv x (SE env)   = SE (M.delete x env)
 insertSEnv x y (SE env) = SE (M.insert x y env)
 lookupSEnv x (SE env)   = M.lookup x env
 emptySEnv               = SE M.empty
 memberSEnv x (SE env)   = M.member x env
-
+intersectWithSEnv f (SE m1) (SE m2) = SE (M.intersectionWith f m1 m2)
 
 -- | Functions for Indexed Bind Environment 
 
