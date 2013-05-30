7cba022336d8c978b2c2bb21550dbb28bd97bcac
Eric Seidel <gridaphobe@gmail.com>
2013-05-30 13:41:24 -0700

    no need for SearchAux after restricting qualifier mining

--- benchmarks/text-0.11.2.3/Data/Text/Lazy/Search.hs
+++ benchmarks/text-0.11.2.3/Data/Text/Lazy/Search.hs
@@ -33,7 +33,6 @@ import qualified Data.Text.Array
 import qualified Data.Text.Fusion.Internal
 import qualified Data.Text.Internal
 import Data.Text.Lazy.Internal (foldrChunks)
-import Data.Text.Lazy.SearchAux
 import qualified Data.Word
 import Data.Int (Int32)
 import Language.Haskell.Liquid.Prelude
@@ -164,24 +163,24 @@ advance_scan needle@(Chunk n ns) src ts0 x@(T.Text _ _ l) xs !i !g =
 
 -- | Check whether an attempt to index into the haystack at the
 -- given offset would fail.
-{- lackingHay :: q:{v:Int64 | v >= 0}
+{-@ lackingHay :: q:{v:Int64 | v >= 0}
                -> t:NonEmptyStrict
                -> ts:Data.Text.Lazy.Internal.Text
-               -> {v:Bool | ((Prop v) => (q > ((tlen t) + (ltlen ts))))}
+               -> {v:Bool | ((Prop v) <=> (q > ((tlen t) + (ltlen ts))))}
   @-}
--- lackingHay :: Int64 -> T.Text -> Text -> Bool
--- lackingHay q t ts = lackingHay_go q 0 t ts
+lackingHay :: Int64 -> T.Text -> Text -> Bool
+lackingHay q t ts = lackingHay_go q 0 t ts
 
-{- lackingHay_go :: q:{v:Int64 | v >= 0}
+{-@ lackingHay_go :: q:{v:Int64 | v >= 0}
                -> p:{v:Int64 | v >= 0}
                -> t:NonEmptyStrict
                -> ts:Data.Text.Lazy.Internal.Text
-               -> {v:Bool | ((Prop v) => (q > (p + (tlen t) + (ltlen ts))))}
+               -> {v:Bool | ((Prop v) <=> (q > (p + (tlen t) + (ltlen ts))))}
   @-}
--- lackingHay_go :: Int64 -> Int64 -> T.Text -> Text -> Bool
--- lackingHay_go q p (T.Text _ _ l) Empty = q > (p + fromIntegral l)
--- lackingHay_go q p (T.Text _ _ l) (Chunk r rs) = let p' = p + fromIntegral l
---                                                 in q > p' && lackingHay_go q p' r rs
+lackingHay_go :: Int64 -> Int64 -> T.Text -> Text -> Bool
+lackingHay_go q p (T.Text _ _ l) Empty = q > (p + fromIntegral l)
+lackingHay_go q p (T.Text _ _ l) (Chunk r rs) = let p' = p + fromIntegral l
+                                                in q > p' && lackingHay_go q p' r rs
 
 
 {-@ delta :: nlen:{v:Int64 | v > 1}
@@ -198,9 +197,9 @@ delta nlen skip c z nextInPattern =
     else 1
 
 
--- swizzle w = 1 `shiftL` (fromIntegral w .&. 0x3f)
+swizzle w = 1 `shiftL` (fromIntegral w .&. 0x3f)
 
-{- buildTable :: Word16
+{-@ buildTable :: Word16
                -> nlen:{v:Int64 | v > 1}
                -> ts0:{v:Data.Text.Lazy.Internal.Text | (BtwnI (ltlen v) 0 nlen)}
                -> t:{v:Data.Text.Internal.Text | (BtwnEI (tlen v) 0 nlen)}
@@ -212,21 +211,21 @@ delta nlen skip c z nextInPattern =
                -> {v:Int64 | (Btwn (v) (0) nlen)}
                -> PairS Word64 {v:Int64 | (Btwn (v) (0) nlen)}
   @-}
--- buildTable :: Word16 -> Int64 -> Text -> T.Text -> Text -> Int -> Int64 -> Word64 -> Int64
---            -> PairS Word64 Int64
--- buildTable z nlen ts0 t@(T.Text xarr xoff xlen) xs !i !(g::Int64) !msk !skp =
---     if i >= xlast then case xs of
---                          Empty      -> (msk .|. swizzle z) :*: skp
---                          Chunk y ys -> let msk'             = msk .|. swizzle c
---                                            skp' | c == z    = nlen - g - 2
---                                                 | otherwise = skp
---                                        in buildTable z nlen (Chunk t ts0) y ys 0 g msk' skp'
---     else let msk'             = msk .|. swizzle c
---              skp' | c == z    = nlen - g - 2
---                   | otherwise = skp
---          in buildTable z nlen ts0 t xs (i+1) (g+1) msk' skp'
---   where c = A.unsafeIndex xarr (xoff+i)
---         xlast = xlen - 1
+buildTable :: Word16 -> Int64 -> Text -> T.Text -> Text -> Int -> Int64 -> Word64 -> Int64
+           -> PairS Word64 Int64
+buildTable z nlen ts0 t@(T.Text xarr xoff xlen) xs !i !(g::Int64) !msk !skp =
+    if i >= xlast then case xs of
+                         Empty      -> (msk .|. swizzle z) :*: skp
+                         Chunk y ys -> let msk'             = msk .|. swizzle c
+                                           skp' | c == z    = nlen - g - 2
+                                                | otherwise = skp
+                                       in buildTable z nlen (Chunk t ts0) y ys 0 g msk' skp'
+    else let msk'             = msk .|. swizzle c
+             skp' | c == z    = nlen - g - 2
+                  | otherwise = skp
+         in buildTable z nlen ts0 t xs (i+1) (g+1) msk' skp'
+  where c = A.unsafeIndex xarr (xoff+i)
+        xlast = xlen - 1
 
 
 -- | Fast index into a partly unpacked 'Text'.  We take into account
