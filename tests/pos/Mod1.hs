dac0753823b5f613207ff18b83eeb6c798c31d3b
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-06-06 14:34:25 -0700

    restoring datacon checks

--- tests/pos/Mod1.hs
+++ tests/pos/Mod1.hs
@@ -1,11 +1,19 @@
 module Mod1 where
 
+
+
 data Goo = G Int
+
+{-@ data Goo = G (x :: {v: Int | 0 > true}) @-}
 
 {-@ measure myg :: Mod1.Goo -> Int 
     myg (Mod1.G n) = n
   @-}
  
-{-@ inc :: x:Mod1.Goo -> {v: Mod1.Goo | (myg v) > (myg x)} @-}
+{-@ inc :: x:Goo -> {v: Goo | (myg v) > (myg x)} @-}
 inc (G x) = G (x + 1)
 
+
+{-@ moo :: {v: Int | 0 > true } @-}
+moo :: Int
+moo = 0
