09de4b4b1ccdabdfd7577043222229f8e7d497c2
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-06-06 17:35:33 -0700

    moving back to bytestring

--- tests/pos/Mod2.hs
+++ tests/pos/Mod2.hs
@@ -2,6 +2,8 @@ module Mod2 where
 
 import qualified Mod1 as M
 
-{-@ inc :: x:M.Goo -> {v: M.Goo | (myg v) > (myg x)} @-}
+import qualified Mod1
+
+{-@ inc :: x:Mod1.Goo -> {v: Mod1.Goo | (myg v) > (myg x)} @-}
 inc (M.G x) = M.G (x + 1)
 
