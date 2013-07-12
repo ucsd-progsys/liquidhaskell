e9b3d05161a2531b035c42b6bc4a33b2b9559eaf
Eric Seidel <gridaphobe@gmail.com>
2013-07-11 22:57:43 -0700

    add Decrease/Strict to "other annotations" !!

--- benchmarks/text-0.11.2.3/count.py
+++ benchmarks/text-0.11.2.3/count.py
@@ -10,9 +10,9 @@ fname = sys.argv[1]
 str = (open(fname, 'r')).read()
 
 #measures =   [(str[a.start():(3+string.find(str,"@-}", a.start()))]) for a in list(re.finditer('{-@ measure', str)) ]
-other =   [(str[a.start():(3+string.find(str,"@-}", a.start()))]) for a in list(re.finditer('{-@ (type|measure|data|include|predicate)', str)) ]
+other =   [(str[a.start():(3+string.find(str,"@-}", a.start()))]) for a in list(re.finditer('{-@ (type|measure|data|include|predicate|Decrease|Strict)', str)) ]
 qualifs =   [(str[a.start():(3+string.find(str,"@-}", a.start()))]) for a in list(re.finditer('{-@ qualif', str)) ]
-tyspecs  =   [(str[a.start():(3+string.find(str,"@-}", a.start()))]) for a in list(re.finditer('{-@ (?!(type|measure|data|include|predicate|qualif))', str)) ]
+tyspecs  =   [(str[a.start():(3+string.find(str,"@-}", a.start()))]) for a in list(re.finditer('{-@ (?!(type|measure|data|include|predicate|qualif|Decrease|Strict))', str)) ]
 
 #print measures
 #print tyspecs
