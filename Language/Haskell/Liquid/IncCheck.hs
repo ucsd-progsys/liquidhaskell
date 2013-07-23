2e155934aee440d3c65b83a004a3c57c9fb60a44
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-07-22 20:49:39 -0700

    zig zog

--- Language/Haskell/Liquid/IncCheck.hs
+++ Language/Haskell/Liquid/IncCheck.hs
@@ -5,37 +5,74 @@
 
 module Language.Haskell.Liquid.IncCheck (slice) where
 
-import Control.Applicative ((<$>))
-import Data.Algorithm.Diff
+import            Control.Applicative          ((<$>))
+import            Data.Algorithm.Diff
+import            Name                         (getSrcSpan)
+import            SrcLoc                       (srcSpanFile, srcSpanStartLine, srcSpanStartCol)
+import            CoreSyn                      (bindersOf)
+import qualified  Data.HashSet                 as S    
+import qualified  Data.HashMap.Strict          as M    
+import qualified  Data.List                    as L
 
 slice :: FilePath -> [CoreBind] -> IO [CoreBind] 
 slice target cbs
   = do is    <- changedLines target
-       xs    <- diffVars is defs
-       ys    <- dependentVars deps xs
+       let xs = diffVars is   (coreDefs cbs) 
+       let ys = dependentVars (coreDeps cbs) (S.fromList xs)
        return $ filterBinds cbs ys
-    where 
-       deps   = coreDeps cbs
-       defs   = coreDefs cbs
 
-filterBinds :: [CoreBind] -> [Var] -> [CoreBind]
-filterBinds = error "TODO"
+-------------------------------------------------------------------------
+filterBinds        :: [CoreBind] -> S.Set Var -> [CoreBind]
+-------------------------------------------------------------------------
+filterBinds cbs ys = filter f cbs
+  where 
+    f (NonRec x _) = x `S.member` ys 
+    f (Rec xes)    = any (`S.member` ys) $ fst <$> xes 
 
+-------------------------------------------------------------------------
 coreDefs :: [CoreBind] -> [Def]
-coreDefs = error "TODO"
+-------------------------------------------------------------------------
+coreDefs = mkDefs lxs 
+  where 
+    lxs  = L.sortBy (compare `on` fst) [(line x, x) | x <- xs ]
+    xs   = concatMap bindersOf cbs
+    line = srcSpanStartLine . getSrcSpan 
 
-coreDeps :: [CoreBind] -> Deps
-coreDeps = error "TODO"
+mkDefs []          = []
+mkDefs ((l,x):lxs) = case lxs of
+                       []       -> [D l Nothing x]
+                       (l',_):_ -> (D l (Just l') x) : mkDefs lxs
 
-dependtVars :: Deps -> [Var] -> [Var]
-dependentVars d xs = error "TODO: return (closed) list of binders on which xs depend"
-
-data Def  = D { start :: Int, end :: Int, binder :: Var }
+data Def  = D { start :: Int, end :: Maybe Int, binder :: Var } 
             deriving (Eq, Ord, Show)
+               
+-------------------------------------------------------------------------
+coreDeps  :: [CoreBind] -> Deps
+-------------------------------------------------------------------------
+coreDeps  = M.fromList . map bindDep 
 
-type Deps = M.HashMap Var [Var]
+bindDep b = [(x, ys) | x <- bindersOf b]
+  where 
+    ys    = S.fromList $ freeVars S.empty b
 
+type Deps = M.HashMap Var (S.HashSet Var)
+
+-------------------------------------------------------------------------
+dependentVars :: Deps -> S.Set Var -> S.Set Var
+-------------------------------------------------------------------------
+dependentVars d xs = go S.empty xs
+  where 
+    pre            = unions . fmap deps . S.elems
+    deps x         = M.lookupDefault S.empty x d
+    go seen new 
+      | S.null new = seen
+      | otherwise  = let seen' = S.union seen new
+                         new'  = pre new `S.difference` seen'
+                     in go seen' new'
+
+-------------------------------------------------------------------------
 diffVars :: [Int] -> [Def] -> [Var]
+-------------------------------------------------------------------------
 diffVars lines defs = error "TODO"
 
 -------------------------------------------------------------------------
