fdeaee6b483b5d9270fe0b7820ba42c837cde6f9
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-07-23 14:44:45 -0700

    temp

--- tests/pos/inccheck0.hs
+++ tests/pos/inccheck0.hs
@@ -1,9 +1,21 @@
 module Test0 where
 
-{-@ incr :: x:Int -> {v:Int | v > x} @-}
+{-@ incr :: x:Int -> {v:Int | v = x} @-}
 incr :: Int -> Int
-incr x = x + 1
+incr x = x + z
+  where
+     z = a - b
+     a = 2
+     b = 1
 
-{- decr :: x:Int -> {v:Int | v < x} @-}
--- decr :: Int -> Int
--- decr x = x - 1
+{-@ decr :: x:Int -> {v:Int | v < x} @-}
+decr :: Int -> Int
+decr x = x - 1
+
+{-@ jog :: x:Int -> {v:Int | v = x} @-}
+jog  :: Int -> Int
+jog x = x + z
+  where 
+    z = a - b
+    a = 2
+    b = 2
