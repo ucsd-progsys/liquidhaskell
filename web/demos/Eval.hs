2ffbda7614cbca7e79b34d109bbdeca055dec586
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-07-25 18:25:24 -0700

    wtf is going on with len constraints?

--- web/demos/Eval.hs
+++ web/demos/Eval.hs
@@ -1,7 +1,9 @@
 module Eval (eval) where
 
 import Language.Haskell.Liquid.Prelude (liquidError)
-import Prelude hiding (lookup)
+import Data.Set (Set (..))
+
+{-@ embed Set as Set_Set @-}
 
 type Val  = Int
 
@@ -14,21 +16,28 @@ data Expr = Const Int
 
 type Env  = [(Bndr, Val)]
 
-----------------------------------------------------------------
-lookup                 :: Bndr -> Env -> Val
+------------------------------------------------------------------
+{-@ bob :: x:Bndr -> {v:Env | (Set_mem x (vars v))} -> Val @-}
+bob :: Bndr -> Env -> Val
 ---------------------  -------------------------------------------
-lookup x ((y,v):env)   
+bob x ((y,v):env)   
   | x == y             = v
-  | otherwise          = lookup x env
-lookup x []            = liquidError "Unbound Variable"
+  | otherwise          = bob x env
+bob x []            = liquidError "Unbound Variable"
 
 ----------------------------------------------------------------
-eval :: Env -> Expr -> Val
+{-@ eval :: [(Bndr, Val)] -> Expr -> Val @-}
+eval :: [(Bndr, Val)] -> Expr -> Val
 ----------------------------------------------------------------
 eval env (Const i)     = i
-eval env (Var x)       = lookup x env 
+eval env (Var x)       = bob x env 
 eval env (Plus e1 e2)  = eval env e1 + eval env e2 
 eval env (Let x e1 e2) = eval env' e2 
   where 
     env'               = (x, eval env e1) : env
+
+{-@ measure vars :: Env -> (Set Bndr)
+    vars ([])    = {v | (? (Set_emp v)) }
+    vars (b:env) = {v | v = (Set_cup (Set_sng (fst b)) (vars env))}
+  @-}
 
