b8cbda3257bf4e45c798dd779ff71218935d0b2f
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-07-27 14:03:22 -0700

    asdasd

--- tests/todo/aliasError.hs
+++ tests/todo/aliasError.hs
@@ -1,6 +1,8 @@
 module Foo where
 
--- TODO: Expressions inside applications of type and predicate aliases.
+-- TODO: return a civilized error message about the line number with the
+-- problematic predicate application, instead of just a groan about safeZip
+-- failing.
 
 {-@ predicate Rng Lo V Hi = (Lo <= V && V < Hi) @-}
 
