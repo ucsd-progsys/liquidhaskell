3bfcf9521a6779577a631e9a58d81e1a4ab9cae0
nikivazou <nikivazou@gmail.com>
2013-09-16 13:21:02 +0100

    establish termination

--- tests/pos/MutualRec.hs
+++ tests/pos/MutualRec.hs
@@ -1,7 +1,6 @@
 module MutualRec where
 
 
-
 bin = undefined
 singleton = undefined
 
@@ -15,9 +14,9 @@ fromDistinctAscList xs
                        _ -> error "fromDistinctAscList create"
     create c n xs' = seq nr $ create (createR nr c) nl xs'
       where nl = n `div` 2
-            nr = n - nl - 1
+            nr = n - nl
 
-    createR n c l ((k,x):ys) = create (createB l k x c) n ys
+    createR n c l ((k,x):ys) = create (createB l k x c) (n-1) ys
     createR _ _ _ []         = error "fromDistinctAscList createR []"
     createB l k x c r zs     = c (bin k x l r) zs
 
