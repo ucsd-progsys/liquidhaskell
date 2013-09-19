aa1e0f2bf3a5fcb0b9be9ddf0668d7e4def624b4
nikivazou <nikivazou@gmail.com>
2013-09-19 18:29:33 +0100

    seems to work

--- Language/Haskell/Liquid/Misc.hs
+++ Language/Haskell/Liquid/Misc.hs
@@ -29,3 +29,6 @@ first3M  f (a,b,c) = (,b,c) <$> f a
 second3M f (a,b,c) = (a,,c) <$> f b
 third3M  f (a,b,c) = (a,b,) <$> f c
 
+zip4 (x1:xs1) (x2:xs2) (x3:xs3) (x4:xs4) = (x1, x2, x3, x4) : (zip4 xs1 xs2 xs3 xs4) 
+zip4 _ _ _ _                             = []
+
