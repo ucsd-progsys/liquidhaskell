15315211a4ad3425d2ee99bc71e90471adb0dad7
Eric Seidel <gridaphobe@gmail.com>
2013-10-03 17:22:12 -0700

    add expected constraints and types

--- tests/todo/Class.hs
+++ tests/todo/Class.hs
@@ -11,6 +11,7 @@ class Sized s where
 
 instance Sized [] where
   {-@ instance measure size [] = len @-}
+  -- length <: size[[]/s][len/size]
   size = length
 
 
@@ -19,23 +20,30 @@ class (Sized s) => Indexable s where
   index :: s a -> Int -> a
 
 instance Indexable [] where
+  -- (!!) <: index[[]/s][len/size]
   index = (!!)
 
 
 
+{-@ sum :: Indexable s => s Int -> Int @-}
 sum :: Indexable s => s Int -> Int
 sum xs = go 0
   where
+    -- max :: {v:Int | v = size xs}
     max = size xs
     go i
+                    -- i :: {v:Nat | v < size xs}
       | i < max   = index xs i + go (i+1)
       | otherwise = 0
 
 
+{-@ sumList :: [Int] -> Int @-}
 sumList :: [Int] -> Int
 sumList xs = go 0
   where
+    -- max :: {v:Int | v = len xs}
     max = size xs
     go i
+                    -- i :: {v:Nat | v < len xs}
       | i < max   = index xs i + go (i+1)
       | otherwise = 0
