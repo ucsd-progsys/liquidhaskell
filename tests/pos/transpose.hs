fe537020ac18de729c0dddc58768de00e2e0102a
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-10-10 21:25:22 -0700

    ok, done transposing. sigh.

--- tests/pos/transpose.hs
+++ tests/pos/transpose.hs
@@ -1,73 +1,61 @@
 {-@ LIQUID "--no-termination" @-}
 
-module Tx (transpose) where
+module Tx (transpose, transpose', transpose'') where
 
 import Language.Haskell.Liquid.Prelude
 
-{-@ type Vec a N    = {v:[a] | (len v) = N}       @-}
+-- | Specifying a matrix
 
-{-@ type Grid a Cols Rows = Vec (Vec a Cols) Rows @-}
+{-@ type Matrix a       = {m0:[{v:[a] | (len v) = (cols m0)}] | true } @-}
 
-{- transpose :: n:Nat
-              -> m:{v:Nat | v > 0} 
-              -> {v:[{v:[a] | len(v) = n}] | len(v) = m} 
-              -> {v:[{v:[a] | len(v) = m}] | len(v) = n} 
-  -}
-
-{-@ transpose :: n:Nat -> m:{v:Nat | v > 0}  -> (Grid a n m) -> (Grid a m n) @-}
-transpose     :: Int -> Int -> [[a]] -> [[a]]
-transpose 0 _ _              = []
-transpose n m ((x:xs) : xss) = (x : map head xss) : transpose (n - 1) m (xs : map tail xss)
-transpose n m ([] : _)       = liquidError "transpose1" 
-transpose n m []             = liquidError "transpose2"
-
--- | Transposing a matrix
-
-{-@ type Mat a = {m0:[{v:[a] | (len v) = (cols m0)}] | true } @-}
-
-{-@ type MatC a C = {m0:[{v:[a] | ((len v) = (cols m0) && (len v) = (len C))}] | true } @-}
-
-{-@ tx :: m:{v:(Mat a) | ((len v) > 0)} -> {v:(MatC a m) | (len v) = (cols m)} @-}
-tx :: [[a]] -> [[a]]
-tx ([]:_)       = []
-tx ((x:xs) : xss) = fstRow : restRows' 
-                    where fstRow     = x : map head xss
-                          restRows'  = tx restRows
-                          restRows   = xs : map tail xss
-tx []             = liquidError "tx"
-
--- DEBUG
-{-@ eqLen :: xs:[a] -> yss:[{v:[b] | (len v) = (len xs)}] -> [[b]] @-}
-eqLen xs yss = map (\ys -> liquidAssert (length xs == length ys) ys) yss
-
--- | Specifications 
+{-@ type MatrixCR a C R = {m0:[{v:[a] | (len v) = C}] | (((len m0) = R) && (R > 0 => (cols m0) = C)) } @-}
 
 {-@ measure cols :: [[a]] -> Int
     cols ([])   = 0
     cols (x:xs) = (len x)
   @-}
 
-{-@ measure rows :: [[a]] -> Int
-    rows ([])   = 0
-    rows (x:xs) = 1 + (rows xs)
-  @-}
+-- | A Few Simple Examples (which run VERY slowly)
 
-{-@ type Mat a = {m0:[{v:[a] | (len v) = (cols m0)}] | true } @-}
+{-@ predicate Dim V C R = (((len V) = R) && ((cols V) = C)) @-}
 
-{-@ predicate Dim V R C = (((len V) = R) && ((cols V) = C)) @-}
+{-@ mat_3_2 :: {v: Matrix Int | (Dim v 3 2)} @-}
+mat_3_2 :: [[Int]]
+mat_3_2 = [ [1,2,3]
+          , [4,5,6] ]
+          
+{-@ mat_2_4 :: {v: Matrix Int | (Dim v 2 4)} @-}
+mat_2_4 :: [[Int]]
+mat_2_4 = [ [1,2]
+          , [3,4]
+          , [5,6]
+          , [7,8] ]
 
--- {- mat_2_3 :: {v: (Mat Int) | (Dim v 2 3)} -}
--- mat_2_3 :: [[Int]]
--- mat_2_3 
---   = [ [1,2,3]
---     , [4,5,6] ]
---           
--- {- mat :: (Mat Int) -}
--- mat :: [[Int]]
--- mat 
---   = [ [1,2]
---     , [7,7]
---     , [5,6]
---     , [7,8] ]
+-- | Old fashioned transpose with explicit dimensions
 
+{- transpose :: c:Nat
+             -> r:{v:Nat | v > 0} 
+             -> {v:[{v:[a] | (len v) = c}] | (len v) = r} 
+             -> {v:[{v:[a] | (len v) = r}] | (len v) = c} 
+  -}
+
+{-@ transpose :: c:Nat -> r:{v:Nat | v > 0}  -> (MatrixCR a c r) -> (MatrixCR a r c) @-}
+transpose     :: Int -> Int -> [[a]] -> [[a]]
+transpose 0 _ _              = []
+transpose n m ((x:xs) : xss) = (x : map head xss) : transpose (n - 1) m (xs : map tail xss)
+transpose n m ([] : _)       = liquidError "transpose1" 
+transpose n m []             = liquidError "transpose2"
+
+
+{-@ transpose' :: m:{v:(Matrix a) | ((len v) > 0)} -> (MatrixCR a (len m) (cols m)) @-}
+transpose' :: [[a]] -> [[a]]
+transpose' ([]:_)         = []
+transpose' ((x:xs) : xss) = (x : map head xss) : transpose' (xs : map tail xss)
+transpose' []             = liquidError "transpose'"
+
+-- | A wrapper implementing the explicit transpose using the implicit one
+
+{-@ transpose''     :: c:Nat -> r:{v:Nat | v > 0}  -> (MatrixCR a c r) -> (MatrixCR a r c) @-}
+transpose''         :: Int -> Int -> [[a]] -> [[a]]
+transpose'' n m xss = transpose' xss
 
