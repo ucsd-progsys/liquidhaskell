9dbb1f3b4177cea4a1b62906df5f4ab52b83ccbc
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-10-23 21:34:08 -0700

    shuffling the parser for bareTypeP to parse stuff like tests/pos/tyfam0.hs; separate branch to prevent havoc

--- tests/pos/tyfam0.hs
+++ tests/pos/tyfam0.hs
@@ -1,22 +1,17 @@
-{-# LANGUAGE FlexibleContexts #-}
-
--- ---------------------------------------------------------------------------
--- |
--- Module      : Data.Vector.Algorithms.Common
--- Copyright   : (c) 2008-2011 Dan Doel
--- Maintainer  : Dan Doel
--- Stability   : Experimental
--- Portability : Portable
---
--- Common operations and utility functions for all sorts
-
-module Data.Vector.Algorithms.Common where
-
-import Prelude hiding (read, length)
+module Foo where
 
 import Control.Monad.Primitive
 
--- copyOffset :: (PrimMonad m, MVector v e) => v (PrimState m) e -> a 
-copyOffset :: (PrimMonad m) => (PrimState m) -> a 
-copyOffset = undefined
+import Data.Vector.Generic.Mutable
 
+-- {- copyOffset :: (PrimMonad m, MVector v e)
+--            => v (PrimState m) e -> v (PrimState m) e -> Int -> Int -> Int -> m ()
+--   -}
+-- 
+-- copyOffset :: (PrimMonad m, MVector v e)
+--            => v (PrimState m) e -> v (PrimState m) e -> Int -> Int -> Int -> m ()
+-- copyOffset = undefined
+
+{-@ zog :: (m s a) -> Nat @-}
+zog :: (m s a) -> Int
+zog = undefined
