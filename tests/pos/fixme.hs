23f03449b749e7b061ece61caf2d4e5e6c4e84e4
nikivazou <nikivazou@gmail.com>
2013-11-01 02:02:53 +0000

    verify merge

--- tests/pos/fixme.hs
+++ tests/pos/fixme.hs
@@ -19,3 +19,8 @@ data L a = N | C a (L a)
 reverse :: L a -> L a -> L a
 reverse xs N = xs
 reverse xs (C y ys) = reverse (C y xs) ys
+
+merge :: Ord a => L a -> L a -> L a 
+{-@ merge :: Ord a => xs:L a -> ys:L a -> L a /[(llen xs) + (llen ys)]@-}
+merge (C x xs) (C y ys) | x > y     = C x $ merge xs (C y ys) 
+                        | otherwise = C y $ merge (C x xs) ys
