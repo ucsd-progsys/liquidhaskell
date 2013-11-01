a7eb377ad91581dace01e487643c26d6a4d5e3c2
Eric Seidel <gridaphobe@gmail.com>
2013-11-01 11:29:57 -0700

    update TODO.md

--- tests/pos/Mod2.hs
+++ tests/pos/Mod2.hs
@@ -2,8 +2,6 @@ module Mod2 where
 
 import qualified Mod1 as M
 
-import qualified Mod1
-
-{-@ inc :: x:Mod1.Goo -> {v: Mod1.Goo | (myg v) > (myg x)} @-}
+{-@ inc :: x:M.Goo -> {v: M.Goo | (myg v) > (myg x)} @-}
 inc (M.G x) = M.G (x + 1)
 
