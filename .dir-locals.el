55d783cb915fc5589b9e0c9fed7bc5f6a87ec1c5
Eric Seidel <gridaphobe@gmail.com>
2013-11-04 11:51:52 -0800

    add .dir-locals.el for emacs

---
+++ .dir-locals.el
@@ -0,0 +1,19 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((nil
+  (eval setq default-directory
+        (locate-dominating-file buffer-file-name ".dir-locals.el")))
+ (haskell-mode
+  (eval setq flycheck-haskell-options
+        (append
+         flycheck-haskell-options
+         (list
+          (concat "-i"
+                  (car
+                   (file-expand-wildcards "dist/dist-sandbox*/build/autogen")))
+          "-fforce-recomp"
+          "-package" "hashable-1.1.2.5")))))
+
+
+
