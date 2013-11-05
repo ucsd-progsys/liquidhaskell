1f8fdfbc8be0c3d1403490d4c789f5a5488d1890
nikivazou <nikivazou@gmail.com>
2013-11-05 01:29:08 +0000

    test refactoring

--- tests/pos/GeneralizedTermination.hs
+++ tests/pos/GeneralizedTermination.hs
@@ -1,10 +1,21 @@
-module Reverse where
+module GenTerm where
 
-import Language.Haskell.Liquid.Prelude        
 import Prelude hiding (reverse)
+
+foo :: Int -> Int -> Int
+{-@ foo :: n:Nat -> m:Nat -> Nat /[n+m] @-}
+
+foo n m 
+  | cond 1 = 0
+  | cond 2 && n > 1 = foo (n-1) m
+  | cond 3 && m > 2 = foo (n+1) (m-2)
+ 
+{-@ cond :: Int -> Bool @-}
+cond :: Int -> Bool
+cond _ = undefined
+
+
 data L a = N | C a (L a)
-
-
 
 {-@ data L [llen] @-}
 
