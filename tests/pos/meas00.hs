b29e5782fa584436143787ff54ebb13eb6f4751e
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-11-05 09:35:38 -0800

    asdads

---
+++ tests/pos/meas00.hs
@@ -0,0 +1,14 @@
+module Zog where
+
+import Language.Haskell.Liquid.Prelude
+
+xs :: [Int]
+xs = [1]
+
+poo [] = liquidAssertB False
+
+prop1  = liquidAssertB (poo xs)
+
+{-@ zooper :: {v:[a] | (len v) > 0} -> a @-}
+zooper :: [a] -> a
+zooper = undefined
