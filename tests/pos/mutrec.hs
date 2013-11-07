76288a66f3301a32c007fd33f9d418bc5839adec
Eric Seidel <gridaphobe@gmail.com>
2013-11-06 20:13:54 -0800

    add tests/pos/mutrec.hs and a description of ordering functions with ghost parameters to README.md

---
+++ tests/pos/mutrec.hs
@@ -0,0 +1,12 @@
+module MutRec where
+
+{-@ isEven :: Nat -> {v:Int | v = 0} -> Bool @-}
+{-@ Decrease isEven 1 2 @-}
+isEven :: Int -> Int -> Bool
+isEven 0 _ = True
+isEven n _ = isOdd (n-1) 1
+
+{-@ isOdd :: Nat -> {v:Int | v = 1} -> Bool @-}
+{-@ Decrease isOdd 1 2 @-}
+isOdd :: Int -> Int -> Bool
+isOdd  n _ = not $ isEven n 0
