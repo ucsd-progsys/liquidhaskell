3ef5edbdf0da5804765659a6f0ab8b783ef21fee
nikivazou <nikivazou@gmail.com>
2013-11-07 22:09:10 +0000

    merge proved to terminate

--- benchmarks/vector-algorithms-0.5.4.2/Data/Vector/Algorithms/Merge.hs
+++ benchmarks/vector-algorithms-0.5.4.2/Data/Vector/Algorithms/Merge.hs
@@ -64,37 +64,40 @@ mergeSortWithBuf cmp src  buf = loop (length src) 0 (length src)
         mid = (u + l) `shiftRI` 1
 {-# INLINE mergeSortWithBuf #-}
 
-{-@ Lazy merge @-}  -- TODO: MUTUALREC 
 merge :: (PrimMonad m, MVector v e)
       => Comparison e -> v (PrimState m) e -> v (PrimState m) e
       -> Int -> m ()
 merge cmp src buf mid = do unsafeCopy low lower
                            eLow  <- unsafeRead low  0
                            eHigh <- unsafeRead high 0
-                           loopMerge 0 eLow 0 eHigh 0
+                           loopMerge (length low + length high) 0 0 eLow 0 eHigh 0
  where
  lower = unsafeSlice 0   mid src
  high  = unsafeSlice mid nHi src -- upper
  low   = unsafeSlice 0   mid buf -- tmp
  nHi   = nSrc - mid
  nSrc  = length src 
- 
- wroteHigh iLow eLow iHigh iIns
+
+{-@ Decrease wroteHigh 1 2 @-}
+{-@ Decrease wroteLow 1 2 @-}
+{-@ Decrease loopMerge 1 2 @-}
+
+ wroteHigh d1 (d2::Int) iLow eLow iHigh iIns
    | iHigh >= length high = unsafeCopy (unsafeSlice iIns (length low - iLow) src)
                                        (unsafeSlice iLow (length low - iLow) low)
    | otherwise            = do eHigh <- unsafeRead high iHigh
-                               loopMerge iLow eLow iHigh eHigh iIns
+                               loopMerge d1 0 iLow eLow iHigh eHigh iIns
 
- wroteLow iLow iHigh eHigh iIns
+ wroteLow d1 (d2::Int) iLow iHigh eHigh iIns
    | iLow  >= length low  = return ()
    | otherwise            = do eLow <- unsafeRead low iLow
-                               loopMerge iLow eLow iHigh eHigh iIns
+                               loopMerge d1 0 iLow eLow iHigh eHigh iIns
 
- loopMerge !iLow !eLow !iHigh !eHigh !iIns = case cmp eHigh eLow of
+ loopMerge (d::Int) (d2::Int) !iLow !eLow !iHigh !eHigh !iIns = case cmp eHigh eLow of
      LT -> do unsafeWrite src iIns eHigh
-              wroteHigh iLow eLow (iHigh + 1) (iIns + 1)
+              wroteHigh (d-1) 1 iLow eLow (iHigh + 1) (iIns + 1)
      _  -> do unsafeWrite src iIns eLow
-              wroteLow (iLow + 1) iHigh eHigh (iIns + 1)
+              wroteLow (d-1) 1 (iLow + 1) iHigh eHigh (iIns + 1)
 {-# INLINE merge #-}
 
 threshold :: Int
