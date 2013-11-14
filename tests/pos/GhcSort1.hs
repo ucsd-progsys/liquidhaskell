283d2741206344958d0b163baf47c762a1311776
nikivazou <nikivazou@gmail.com>
2013-11-14 23:23:05 +0000

    only mergeAll is left

--- tests/pos/GhcSort1.hs
+++ tests/pos/GhcSort1.hs
@@ -1,5 +1,4 @@
-{--! run liquid with no-termination -}
-
+{-# Language ScopedTypeVariables #-}
 module ListSort where
 
 import Language.Haskell.Liquid.Prelude -- (liquidAssertB, choose)
@@ -7,21 +6,24 @@ import Language.Haskell.Liquid.Prelude -- (liquidAssertB, choose)
 {-@ type OList a = [a]<{\fld v -> (v >= fld)}> @-}
 {-@ assert sort1 :: (Ord a) => [a] -> OList a  @-}
 sort1 :: (Ord a) => [a] -> [a]
-sort1 = mergeAll . sequences
+sort1 xs = mergeAll  (sequences xs 0)
   where
-    sequences (a:b:xs)
-      | a `compare` b == GT = descending b [a]  xs
-      | otherwise           = ascending  b (a:) xs
-    sequences [x] = [[x]]
-    sequences []  = [[]]
+    {-@ Decrease sequences  1 2 @-}
+    {-@ Decrease descending 3 4 @-}
+    {-@ Decrease ascending  3 4 @-}
+    sequences (a:b:xs) (_::Int)
+      | a `compare` b == GT = descending b [a]  xs 1
+      | otherwise           = ascending  b (a:) xs 1
+    sequences [x] _ = [[x]]
+    sequences []  _ = [[]]
 
-    descending a as (b:bs)
-      | a `compare` b == GT = descending b (a:as) bs
-    descending a as bs      = (a:as): sequences bs
+    descending a as (b:bs) (_::Int)
+      | a `compare` b == GT = descending b (a:as) bs 1
+    descending a as bs _    = (a:as): sequences bs 0
 
-    ascending a as (b:bs)
-      | a `compare` b /= GT = ascending b (\ys -> as (a:ys)) bs
-    ascending a as bs       = as [a]: sequences bs
+    ascending a as (b:bs) (_ :: Int)
+      | a `compare` b /= GT = ascending b (\ys -> as (a:ys)) bs 1
+    ascending a as bs _     = as [a]: sequences bs 0
 
     mergeAll [x] = x
     mergeAll xs  = mergeAll (mergePairs xs)
@@ -32,6 +34,9 @@ sort1 = mergeAll . sequences
 
 -- merge1 needs to be toplevel,
 -- to get applied transformRec tx
+
+{-@ merge1 :: Ord a => xs:[a] -> ys:[a] -> [a] / [(len xs) + (len ys)] @-}
+merge1 :: Ord a => [a] -> [a] -> [a]
 merge1 (a:as') (b:bs')
   | a `compare` b == GT = b:merge1 (a:as')  bs'
   | otherwise           = a:merge1 as' (b:bs')
