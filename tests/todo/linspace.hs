4735f87e4e5729d1d82828218c9c99650673151f
Ranjit Jhala <jhala@cs.ucsd.edu>
2013-11-21 22:13:51 -0800

    much progress on linspace

--- tests/todo/linspace.hs
+++ tests/todo/linspace.hs
@@ -1,75 +1,93 @@
-module LinSpace where
+{-# LANGUAGE ScopedTypeVariables #-}
+{-@ LIQUID "--no-termination" @-}
 
-{-@ measure orthDim :: PVector -> Int 
-    orthDim (PVector v o z) = (dim o)
+
+module LinSpace (dotPV) where
+
+import Language.Haskell.Liquid.Prelude (liquidAssert, liquidError)
+import Prelude hiding (zipWith)
+
+data PVector = PVector { 
+    vec       :: [Integer]      -- vector coordinates
+  , muCoeff   :: [Integer]      -- <vec,bn*>det^2(b[1..n-1]) : ... 
+  , orthSpace :: Space PVector  -- lattice basis
+  } deriving (Show, Eq)
+
+-- Orthogonalized vector bn* and squared lattice determinant 
+-- type Space = Maybe (PVector, Integer) 
+
+data Space a = Null | Real a Integer
+  deriving (Show, Eq)
+
+{-@ measure dim     :: (Space PVector) -> Int 
+    dim (Null)      = 0
+    dim (Real pv n) = 1 + (dim (orthSpace pv))
   @-}
 
-{-@ measure tail :: [a] -> [a]
-    tail (x:xs)  = xs
+{-@ measure vec :: PVector -> [Integer]
+    vec (PVector v m o) = v 
   @-}
 
-{-@ measure dim :: Space -> Int 
-    dim (Null) = 0
-    dim (Real pv n) = 1 + (dim (orthSp pv))
+{-@ measure muCoeff :: PVector -> [Integer]
+    muCoeff (PVector v m o) = m 
   @-}
 
-{-@ measure orthSp :: PVector -> Space
-    orthSp (PVector v m o) = o
+{-@ measure orthSpace :: PVector -> (Space PVector)
+    orthSpace (PVector v m o) = o
   @-}
 
-{-@ measure pvlen :: PVector -> Int
-    pvlen (PVector v m o) = (len v)
+{-@ predicate Inv V        = (dim (orthSpace V)) = (len (muCoeff V)) @-}
+{-@ predicate SameLen  X Y = ((len (vec X))  = (len (vec Y)))  @-}
+{-@ predicate SameOrth X Y = ((orthSpace X) = (orthSpace Y)) @-}
+{-@ type SameSpace X       = {v:PVector | ((Inv v) && (SameLen X v) && (SameOrth X v))} @-}
+
+{-@ invariant {v: PVector | (Inv v) } @-}
+
+{-@ measure isReal     :: (Space a) -> Prop
+    isReal (Null)      = false
+    isReal (Real pv n) = true
   @-}
 
-{-@ predicate SameLen  X Y = ((pvlen X)  = (pvlen Y))  @-}
-{-@ predicate SameOrth X Y = ((orthSp X) = (orthSp Y)) @-}
-{-@ type SameSpace X       = {v:PVector | ((SameLen X v) && (SameOrth X v))} @-}
-
-{-@ measure isReal :: Space -> Prop
-    isReal (Null)       = false
-    isReal (Rreal pv n) = true
-  @-}
-
-{-@ measure spaceVec :: Space -> PVector
+{-@ measure spaceVec     :: (Space PVector) -> PVector
     spaceVec (Real pv n) = pv
   @-}
 
 -- ASSERT: length muCoeff == dim orthSpace
 -- ASSERT: maybe True (\(v,_) -> length vec == length (vec v)) orthSpace 
 
-{-@ predicate OrthMu  Orth Mu  = (dim Orth) = (len Mu) @-}
-
-{-@ predicate OrthVec Orth Vec = ((isReal Orth) => (len Vec) = (pvlen (spaceVec Orth))) @-}
+{-@ type PVectorN N = {v: PVector | (len (vec v)) = N}   @-} 
+{-@ type PVectorP P = {v: PVector | (SameLen v P)}       @-} 
 
 {-@ data PVector = PVector { 
-      vec  :: [Integer]   
-    , mu   :: [Integer] 
-    , orth :: {v: Space | ((OrthMu v mu) && (OrthVec v vec))}
+      vec_  :: [Integer]   
+    , mu_   :: [Integer] 
+    , orth_ :: {v: (Space (PVectorN (len vec_))) | (dim v) = (len mu_)}
     } 
   @-}
 
+
 --------------------
 -- "dim" and "sameSpace" are only used in the ASSERTs
-dim             :: Space -> Int 
-dim Null        = 0
-dim (Real pv _) = 1 + dim (orthSpace pv)
+{-@ dim         :: s:(Space PVector) -> {v:Int | v = (dim s)} @-}
+dim Null        = (0 :: Int)
+dim (Real pv _) = 1 + dim (orthSpace_ pv)
 
 sameSpace :: PVector -> PVector -> Bool
 sameSpace pv1 pv2 = 
   (length (vec pv1) == length (vec pv2)) && (orthSpace pv1 == orthSpace pv2)
+
+
+{-@ muCoeff_   :: p:PVector -> {v:[Integer] | v = (muCoeff p) }            @-}
+muCoeff_ (PVector v m o) = m
+
+{-@ orthSpace_ :: p:PVector -> {v:(Space (PVectorP p)) | v = (orthSpace p)} @-} 
+orthSpace_ (PVector v m o) = o
+
+{-@ vec_       :: p:PVector -> {v:[Integer] | v = (vec p)}                  @-}
+vec_     (PVector v m o) = v
+
 --------------------
 
-data PVector = PVector { 
-    vec       :: [Integer]    -- vector coordinates
-  , muCoeff   :: [Integer]    -- <vec,bn*>det^2(b[1..n-1]) : ... 
-  , orthSpace :: Space        -- lattice basis
-  } deriving (Show, Eq)
-
--- Orthogonalized vector bn* and squared lattice determinant 
--- type Space = Maybe (PVector, Integer) 
-
-data Space = Null | Real PVector Integer
-  deriving (Show, Eq)
 
 -- squared determinant of orthSpace
 detPV :: PVector -> Integer 
@@ -79,73 +97,81 @@ detPV (PVector _ _ (Real _ n)) = n
 
 -- peel off last layer of projection from PVector
 -- ASSERT: dim (orthSpace (liftPV v)) == dim (orthSpace v) - 1
-{-@ liftPV :: pv:PVector -> {v:PVector | ((orthSp v) = (tail (orthSp pv)) && ((orthDim v) = (orthDim pv) - 1))} @-}
-liftPV (PVector v (m:mu) (Real pv _)) = PVector v mu (orthSpace pv)
+{-@ liftPV :: pv:PVector -> {v:(PVectorP pv) | ((orthSpace v) = (orthSpace (spaceVec (orthSpace pv))) && ((dim (orthSpace v)) = (dim (orthSpace pv)) - 1))} @-}
+liftPV (PVector v (m:mu) (Real pv _)) 
+  = PVector v mu (orthSpace_ pv)
+
 
 -- dot product of two vectors
 -- ASSERT: length xs == length ys
 {-@ dot :: (Num a) => xs:[a] -> {v:[a] | (len v) = (len xs)} -> a @-}
 dot xs ys = sum (zipWith (*) xs ys)
 
+
 -- scaled dot product of two projected vectors: output is <v*,w*>det^2(orthSpace) 
-{-@ dotPV :: x:PVector -> (SameSpace x) -> Integer @-}
+{-@ dotPV :: pv1:PVector -> pv2:(SameSpace pv1) -> Integer @-}
 -- ASSERT: (dotPV x y) ==> sameSpace x y
-dotPV (PVector v1 [] Null) (PVector v2 [] Null) = dot v1 v2
-dotPV pv1 pv2 = q 
+dotPV (PVector v1 [] _) (PVector v2 [] _) 
+  = dot v1 v2
+dotPV pv1@(PVector _ mu1 _) pv2@(PVector _ mu2 _) 
+  = liquidAssert (length mu1 == length mu2) q
   where
-    -- RJ: ASSUME: orthSp x = orthSp y => tail (orthSp x) = tail (orthSp y)
     dd          = dotPV (liftPV pv1) (liftPV pv2)
-    Real pv0 rr = orthSpace pv1 -- same as orthSpace v2
-    x           = dd * rr - (head (muCoeff pv1)) * (head (muCoeff pv2))
+    Real pv0 rr = orthSpace_ pv1 -- same as orthSpace v2
+    x           = dd * rr - (head mu1) * (head mu2)
     (q, 0)      = divMod x (detPV pv0) -- check remainder is 0
 
-{-@ (.+.) :: x:PVector -> (SameSpace x) -> PVector @-}
+{-@ (.+.) :: x:PVector -> (SameSpace x) -> (SameSpace x) @-}
 -- ASSERT: (x .+. y) ==> sameSpace x y
-(PVector v1 m1 o) .+. (PVector v2 m2 _) = PVector (zipWith (+) v1 v2) (zipWith (+) m1 m2) o
+(PVector v1 m1 o) .+. (PVector v2 m2 _) = PVector (zipWith  (+) v1 v2) (zipWith (+) m1 m2) o
 
-{-@ (.-.) :: x:PVector -> (SameSpace x) -> PVector @-}
+{-@ (.-.) :: x:PVector -> (SameSpace x) -> (SameSpace x) @-}
 -- ASSERT: (x .-. y) ==> sameSpace x y
-(PVector v1 m1 o) .-. (PVector v2 m2 _) = PVector (zipWith (-) v1 v2) (zipWith (-) m1 m2) o
+(PVector v1 m1 o) .-. (PVector v2 m2 _) = PVector (zipWith  (-) v1 v2) (zipWith (-) m1 m2) o
 
+{-@ (*.) :: Integer -> x:PVector -> (SameSpace x) @-}
 (*.) :: Integer -> PVector -> PVector
-x *. (PVector v m o) = PVector (map (x *) v) (map (x *) m) o
+x *. (PVector v m o) = PVector (map (x *)  v) (map (x *) m) o
 
 -- Some auxiliary functions
 
-space2vec :: Space -> PVector
-space2vec sp@(Real bn r) = PVector (vec bn) (r : muCoeff bn) sp
+{-@ space2vec :: n:Int -> s:(Space (PVectorN n)) -> {v: (PVectorN n) | (orthSpace v) = s} @-}
+space2vec (n :: Int) sp@(Real bn r) = PVector (vec_ bn) (r : muCoeff_ bn) sp
 
-makeSpace :: PVector -> Space
+{-@ makeSpace :: p:PVector -> (Space (PVectorP p)) @-}
 makeSpace pvec = Real pvec (dotPV pvec pvec)
 
-makePVector :: Space -> [Integer] -> PVector
-makePVector Null v = PVector v [] Null 
-makePVector s@(Real s1 _) v = 
-  let v1 = makePVector (orthSpace s1) v
-  in PVector v (dotPV v1 s1 : muCoeff v1) s
+{-@ makePVector :: vs:[Integer] -> s:(Space (PVectorN (len vs))) -> {v: (PVectorN (len vs)) | (orthSpace v) = s} @-}
+makePVector :: [Integer] -> Space PVector -> PVector
+makePVector v s@Null = PVector v [] s 
+makePVector v s@(Real s1 _) = 
+  let v1 = makePVector v (orthSpace_ s1) 
+  in PVector v (dotPV v1 s1 : muCoeff_ v1)  s
 
-gramSchmidt :: [[Integer]] -> Space
-gramSchmidt = foldl (\sp v -> makeSpace (makePVector sp v)) Null 
+-- RANJIT: does this need an input like [],[1],[1,2],[1,2,3],... etc?
+gramSchmidt :: [[Integer]] -> Space PVector
+gramSchmidt = foldl (\sp v -> makeSpace (makePVector v sp)) Null 
 
-getBasis :: Space -> [[Integer]]
+getBasis :: Space PVector -> [[Integer]]
 getBasis s = worker s [] where
   worker Null bs = bs
-  worker (Real pv _) bs = worker (orthSpace pv) (vec pv : bs)
+  worker (Real pv _) bs = worker (orthSpace_ pv) (vec pv : bs)
 
+{-@ qualif EqMu(v:PVector, x:PVector): (len (muCoeff v)) = (len (muCoeff x)) @-}
 
 sizeReduce1 :: PVector -> PVector
-sizeReduce1 pv@(PVector _ (m:_) sn@(Real _ r)) = 
+sizeReduce1 pv@(PVector v (m:_) sn@(Real _ r)) = 
   let c  = div (2*m + r) (2*r) -- division with rounded remainder
-  in pv .-. (c *. space2vec sn)
+      n  = length v    
+  in  pv  .-.  (c *. (space2vec n sn))
 
--- RJ: don't see why this holds without som properties of (.-.) 
 {-@ sizeReduce :: x:PVector -> (SameSpace x) @-}
 sizeReduce pv@(PVector v [] Null) = pv
 -- ASSERT: sameSpace (sizeReduce x) x
 sizeReduce pv@(PVector _ _ sp@(Real _ _)) = 
   let pv1 = sizeReduce1 pv
       (PVector v mu _) = sizeReduce (liftPV pv1)
-  in PVector v (head (muCoeff pv1) : mu) sp
+  in PVector v (head (muCoeff_ pv1) : mu) sp
 
 -- Two example algorithms using the library: enumCVP and lll
 
@@ -153,9 +179,9 @@ enumCVP :: PVector -> Integer -> Maybe [Integer]
 enumCVP (PVector v mu Null) r 
   | dot v v < r = Just v 
   | otherwise   = Nothing 
-enumCVP t@(PVector _ _ (Real bn _)) r = 
-  let t0 = sizeReduce1 t
-      cs = if (head (muCoeff t0) < 0) 
+enumCVP t@(PVector _ (_:_) (Real bn _)) r = 
+  let t0 = sizeReduce1  t
+      cs = if (head (muCoeff_ t0) < 0) 
            then 0 : concat [[x,negate x] | x <- [1,2..]]
            else 0 : concat [[negate x,x] | x <- [1,2..]]
       branch :: (Integer, Maybe [Integer]) -> [PVector] -> (Integer, Maybe [Integer])
@@ -166,17 +192,26 @@ enumCVP t@(PVector _ _ (Real bn _)) r =
           Just w  -> branch (dot w w, Just w) ts
   in snd $ branch (r,Nothing) [liftPV t0 .+. (c *. bn) | c <- cs]
 
+-- make this parametric on n
 {- Fraction free LLL Algorithm with exact arithmetic and delta=99/100 -}
-lll :: [[Integer]] -> [[Integer]]
-lll = getBasis . worker Null Nothing where
-  worker :: Space -> Maybe PVector -> [[Integer]] -> Space
+{-@ lll :: n:Int -> [{v:[Integer]| (len v) = n}] -> [[Integer]] @-}
+lll :: Int -> [[Integer]] -> [[Integer]]
+lll n = getBasis . worker Null Nothing where
+  worker :: Space PVector -> Maybe PVector -> [[Integer]] -> Space PVector
   worker bs Nothing [] = bs
-  worker bs Nothing (v:vs) = worker bs (Just (makePVector bs v)) vs
+  worker bs Nothing (v:vs) = worker bs (Just (makePVector v bs)) vs
   worker Null (Just pv) vs = worker (makeSpace pv) Nothing vs
   worker (Real bn r) (Just pv) vs = 
     let pv1 = sizeReduce pv
         pv2 = liftPV pv1
     in if (100*(dotPV pv2 pv2) < 99*r)
-       then worker (orthSpace bn) (Just pv2) (vec bn : vs)
+       then worker (orthSpace_ bn) (Just pv2) (vec bn : vs)
        else worker (makeSpace pv1) Nothing vs
 
+{-@ type List a N = {v : [a] | (len v) = N} @-}
+
+{-@ zipWith :: (a -> b -> c) -> xs:[a] -> (List b (len xs)) -> (List c (len xs)) @-}
+zipWith f (a:as) (b:bs) = f a b : zipWith f as bs
+zipWith _ [] []         = []
+zipWith _ (_:_) []      = liquidError "Dead Code"
+zipWith _ [] (_:_)      = liquidError "Dead Code"
