54640b01fa71fc1a9a82363d56244d448c260472
nikivazou <nikivazou@gmail.com>
2013-12-09 18:21:43 +0200

    incr4 works

--- tests/pos/State.hs
+++ tests/pos/State.hs
@@ -1,4 +1,8 @@
-module State where
+module State (
+   returnST -- :: a -> ST a s
+ , bindST   -- :: ST a s -> (a -> ST b s) -> ST b s
+ , ST(..)
+ ) where
 
 import Prelude hiding (snd, fst)
 
@@ -7,21 +11,21 @@ data ST a s = S (s -> (a, s))
        = S (ys::(x:s<pre> -> ((a, s)<post>)))
   @-}
 
-{-@ return :: forall <pre :: s -> Prop, post :: a -> s -> Prop>.
+{-@ returnST :: forall <pre :: s -> Prop, post :: a -> s -> Prop>.
                x:a 
            -> ST <{v:s<post x>| true}, post> a s
   @-}
-return :: a -> ST a s
-return x = S $ \s -> (x, s)
+returnST :: a -> ST a s
+returnST x = S $ \s -> (x, s)
 
 
-{-@ bind :: forall <p :: s -> Prop, q :: a -> s -> Prop, r :: b -> s -> Prop>.
+{-@ bindST :: forall <p :: s -> Prop, q :: a -> s -> Prop, r :: b -> s -> Prop>.
             ST <p, q> a s 
          -> (x:a -> ST <{v:s<q x> | true}, r> b s) 
          -> ST <p, r> b s
  @-}
-bind :: ST a s -> (a -> ST b s) -> ST b s
-bind (S m) k = S $ \s -> let (a, s') = m s in apply (k a) s'
+bindST :: ST a s -> (a -> ST b s) -> ST b s
+bindST (S m) k = S $ \s -> let (a, s') = m s in apply (k a) s'
 
 {-@ apply :: forall <p :: s -> Prop, q :: a -> s -> Prop>.
              ST <p, q> a s -> s<p> -> (a, s)<q>
