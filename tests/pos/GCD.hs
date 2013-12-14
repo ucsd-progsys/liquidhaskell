44ccbaafafe5d422ff0b49fd71d3a182de34ae17
nikivazou <nikivazou@gmail.com>
2013-12-14 18:07:11 +0200

    add original gcd

--- tests/pos/GCD.hs
+++ tests/pos/GCD.hs
@@ -1,7 +1,7 @@
 module GCD where
 
 import Prelude hiding (gcd, mod)
-
+import Language.Haskell.Liquid.Prelude
 {-@ mod :: a:Nat -> b:{v:Nat| ((v < a) && (v > 0))} -> {v:Nat | v < b} @-}
 mod :: Int -> Int -> Int
 mod a b | a - b >  b = mod (a - b) b
@@ -12,3 +12,11 @@ mod a b | a - b >  b = mod (a - b) b
 gcd :: Int -> Int -> Int
 gcd a 0 = a
 gcd a b = gcd b (a `mod` b)
+
+{-@ gcd' :: a:Nat -> b:Nat -> Nat / [a, b] @-}
+gcd' :: Int -> Int -> Int
+gcd' a b | a == 0 = b
+         | b == 0 = a
+         | a == b = a
+         | a >  b = gcd' (a - b) b 
+         | a <  b = gcd' a (b - a) 
