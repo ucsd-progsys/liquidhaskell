93ce3050d24146eccc7b02963c6d56305072c8b6
nikivazou <nikivazou@gmail.com>
2014-02-20 16:47:49 -0800

    adding unsound test

---
+++ tests/todo/SubType.hs
@@ -0,0 +1,15 @@
+module HOSubtype where
+
+{-@ generic_accept_stable ::
+                    f:(x:a -> {v:a | (v = x)}) ->
+                    ()
+                    @-}
+generic_accept_stable :: (a -> a) -> ()
+generic_accept_stable pf = ()
+
+{-@ useless_proof :: x:{v:Int | v > 0} -> {v:Int | v > 0} @-}
+useless_proof :: Int -> Int
+useless_proof _ = 5
+
+test :: ()
+test = generic_accept_stable useless_proof
