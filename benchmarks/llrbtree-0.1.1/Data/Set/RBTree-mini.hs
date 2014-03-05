0042dbb206d8cd9c071a4aa1c36a91cdb3f515b8
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-03-04 16:51:42 -0800

    RB-insert works

--- benchmarks/llrbtree-0.1.1/Data/Set/RBTree-mini.hs
+++ benchmarks/llrbtree-0.1.1/Data/Set/RBTree-mini.hs
@@ -25,19 +25,17 @@ turnB :: RBTree a -> RBTree a
 turnB Leaf           = error "turnB"
 turnB (Node _ h l x r) = Node 1 h l x r
 
-{-@ insert' :: (Ord a) => a -> t:(RBT a) -> {v:ARBT a | (isRB v)} @-}
-
--- {v: ARBT a | (((col t) /= 0) => (isRB v))} @-}
+{-@ insert' :: (Ord a) => a -> t:(RBT a) -> {v: ARBT a | (((col t) /= 0) => (isRB v))} @-}
 insert' :: Ord a => a -> RBTree a -> RBTree a
 insert' kx Leaf = Node 0 1 Leaf kx Leaf
 insert' kx s@(Node 1 h l x r) = case compare kx x of
-    LT -> l -- balanceL' h (insert' kx l) x r
-    -- GT -> balanceR' h l x (insert' kx r)
---    EQ -> s
--- insert' kx s@(Node 0 h l x r) = case compare kx x of
---    LT -> Node 0 h (insert' kx l) x r
---    GT -> Node 0 h l x (insert' kx r)
---     EQ -> s
+    LT -> balanceL' h (insert' kx l) x r
+    GT -> balanceR' h l x (insert' kx r)
+    EQ -> s
+insert' kx s@(Node 0 h l x r) = case compare kx x of
+    LT -> Node 0 h (insert' kx l) x r
+    GT -> Node 0 h l x (insert' kx r)
+    EQ -> s
 
 {-@ balanceL' :: Int -> ARBT a -> a -> RBT a -> RBT a @-}
 balanceL' :: BlackHeight -> RBTree a -> a -> RBTree a -> RBTree a
@@ -55,7 +53,7 @@ balanceR' h a x (Node 0 _ (Node 0 _ b y c) z d) =
     Node 0 (h+1) (Node 1 h a x b) y (Node 1 h c z d)
 balanceR' h l x r = Node 1 h l x r
 
-{-@ type RBT a  = {v: (RBTree a) | (isRB v)}  @-}
+{-@ type RBT a  = {v: (RBTree a) | ((isRB v) && (isARB v))}  @-}
 
 {-@ type ARBT a = {v: (RBTree a) | ((isARB v) && (((col v) = 1) => (isRB v)))} @-}
 
@@ -64,9 +62,12 @@ balanceR' h l x r = Node 1 h l x r
     isRB (Node c h l x r)  = ((isRB l) && (isRB r) && ((c == 0) => ((col l) /= 0) && ((col r) /= 0)))
   @-}
 
-{-@ invariant {v: RBTree a | (((col v) = 0) || ((col v) = 1))} @-}
+{-@ thm              :: RBTree a -> {v:RBTree a | ((isRB v) => (isARB v)) } @-}
+thm Leaf             = Leaf
+thm (Node c h l x r) = Node c h (thm l) x (thm r)
 
--- isARB v => (c = R => (col l = R || col r = R))
+{-@ invariant {v: RBTree a | ((isRB v) => (isARB v))} @-}
+
 {-@ measure isARB          :: (RBTree a) -> Prop
     isARB (Leaf)           = false 
     isARB (Node c h l x r) = ((isRB l) && (isRB r))
