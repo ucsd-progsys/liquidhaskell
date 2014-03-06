1016d8ea46351fb4db4489c168be64c2bde1090e
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-03-06 11:13:20 -0800

    onto appel-del

--- benchmarks/llrbtree-0.1.1/Data/Set/RBTree-appel.hs
+++ benchmarks/llrbtree-0.1.1/Data/Set/RBTree-appel.hs
@@ -16,6 +16,90 @@ data Color = B -- ^ Black
 type RBTreeBDel a = (RBTree a, Bool)
 
 
+---------------------------------------------------------------------------
+---------------------------------------------------------------------------
+---------------------------------------------------------------------------
+
+-- Definition isblack t :=
+--  match t with Bk _ _ _ => True | _ => False end.
+--
+-- Lemma append_arb_rb n l r : rbt n l -> rbt n r ->
+--  (arbt n (append l r)) /\
+--  (notred l -> notred r -> rbt n (append l r)).
+-- 
+-- A third approach : Lemma ... with ...
+-- 
+-- Lemma del_arb s x n : rbt (S n) s -> isblack s -> arbt n (del x s)
+-- with del_rb s x n : rbt n s -> notblack s -> rbt n (del x s).
+-- Instance remove_rb s x : Rbt s -> Rbt (remove x s).
+
+
+{-@ del              :: a -> RBT a -> ARBT a @-}
+del x Leaf           = Leaf
+del x (Node _ a y b) = case compare x y of
+   EQ -> append a b 
+   LT -> case a of
+           Node B _ _ _ -> lbalS (del x a) y b
+           _            -> Node R (del x a) y b
+   GT -> case b of
+           Node B _ _ _ -> rbalS a y (del x b)
+           _            -> Node R a y (del x b)
+
+{-@ append :: RBT a -> RBT a -> ARBT a @-}
+append :: RBTree a -> RBTree a -> RBTree a
+append = undefined
+
+{- 
+Fixpoint append (l:tree) : tree -> tree :=
+ match l with
+ | Leaf => fun r => r
+ | Node lc ll lx lr =>
+   fix append_l (r:tree) : tree :=
+   match r with
+   | Leaf => l
+   | Node rc rl rx rr =>
+     match lc, rc with
+     | Red, Red =>
+       let lrl := append lr rl in
+       match lrl with
+       | Rd lr' x rl' => Rd (Rd ll lx lr') x (Rd rl' rx rr)
+       | _ => Rd ll lx (Rd lrl rx rr)
+       end
+     | Black, Black =>
+       let lrl := append lr rl in
+       match lrl with
+       | Rd lr' x rl' => Rd (Bk ll lx lr') x (Bk rl' rx rr)
+       | _ => lbalS ll lx (Bk lrl rx rr)
+       end
+     | Black, Red => Rd (append_l rl) rx rr
+     | Red, Black => Rd ll lx (append lr r)
+     end
+   end
+ end.
+-}
+
+
+
+-- Fixpoint del x t :=
+--  match t with
+--  | Leaf => Leaf
+--  | Node _ a y b =>
+--    match X.compare x y with
+--    | Eq => append a b
+--    | Lt =>
+--      match a with
+--      | Bk _ _ _ => lbalS (del x a) y b
+--      | _ => Rd (del x a) y b
+--      end
+--    | Gt =>
+--      match b with
+--      | Bk _ _ _ => rbalS a y (del x b)
+--      | _ => Rd a y (del x b)
+--      end
+--    end
+--  end.
+
+
 
 ---------------------------------------------------------------------------
 ---------------------------------------------------------------------------
@@ -45,6 +129,33 @@ lbalS l k r                           = Node R l k r
 rbal' l k (Node R b y (Node R c z d)) = Node R (Node B l k b) y (Node B c z d)
 rbal' l k (Node R (Node R b y c) z d) = Node R (Node B l k b) y (Node B c z d)
 rbal' l k r                           = Node B l k r
+
+{-@ rbalS                             :: l:RBT a -> a -> ARBT a -> {v: ARBT a | ((IsB l) => (isRB v))} @-}
+rbalS l k (Node R b y c)              = Node R l k (Node B b y c)
+rbalS (Node B a x b) k r              = let zoo = lbal (Node R a x b) k r in zoo
+rbalS (Node R a x (Node B b y c)) k r = Node R (lbal (turnR' a) x b) y (Node B c k r)
+rbalS l k r                           = Node R l k r
+
+{-@ lbal                              :: ARBT a -> a -> RBT a -> RBT a @-}
+lbal (Node R (Node R a x b) y c) k r  = Node R (Node B a x b) y (Node B c k r)
+lbal (Node R a x (Node R b y c)) k r  = Node R (Node B a x b) y (Node B c k r)
+lbal l k r                            = Node B l k r
+
+
+
+
+-- Definition rbalS l k r :=
+--  match r with
+--  | Rd b y c => Rd l k (Bk b y c)
+--  | _ =>
+--    match l with
+--    | Bk a x b => lbal (Rd a x b) k r
+--    | Rd a x (Bk b y c) => Rd (lbal (makeRed a) x b) y (Bk c k r)
+--    | _ => Rd l k r 
+--    end
+--  end.
+
+
 
 ---------------------------------------------------------------------------
 ---------------------------------------------------------------------------
