08683cedc3966b589f9ec178eee54f02b52ba7be
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-03-05 18:23:24 -0800

    hmm wonder whats happening with deleteMin

--- benchmarks/llrbtree-0.1.1/Data/Set/RBTree-mini-color.hs
+++ benchmarks/llrbtree-0.1.1/Data/Set/RBTree-mini-color.hs
@@ -21,57 +21,47 @@ type RBTreeBDel a = (RBTree a, Bool)
 ---------------------------------------------------------------------------
 
 
-delete :: Ord a => a -> RBTree a -> RBTree a
-delete x t = turnB' s
-  where
-    (s,_) = delete' x t
+-- delete :: Ord a => a -> RBTree a -> RBTree a
+-- delete x t = turnB' s
+--   where
+--     (s,_) = delete' x t
+-- 
+-- delete' :: Ord a => a -> RBTree a -> RBTreeBDel a
+-- delete' _ Leaf = (Leaf, False)
+-- delete' x (Node c h l y r) = case compare x y of
+--     LT -> let (l',d) = delete' x l
+--               t = Node c h l' y r
+--           in if d then unbalancedR c (h-1) l' y r else (t, False)
+--     GT -> let (r',d) = delete' x r
+--               t = Node c h l y r'
+--           in if d then unbalancedL c (h-1) l y r' else (t, False)
+--     EQ -> case r of
+--         Leaf -> if c == B then blackify l else (l, False)
+--         _ -> let ((r',d),m) = deleteMin' r
+--                  t = Node c h l m r'
+--              in if d then unbalancedL c (h-1) l m r' else (t, False)
 
-delete' :: Ord a => a -> RBTree a -> RBTreeBDel a
-delete' _ Leaf = (Leaf, False)
-delete' x (Node c h l y r) = case compare x y of
-    LT -> let (l',d) = delete' x l
-              t = Node c h l' y r
-          in if d then unbalancedR c (h-1) l' y r else (t, False)
-    GT -> let (r',d) = delete' x r
-              t = Node c h l y r'
-          in if d then unbalancedL c (h-1) l y r' else (t, False)
-    EQ -> case r of
-        Leaf -> if c == B then blackify l else (l, False)
-        _ -> let ((r',d),m) = deleteMin' r
-                 t = Node c h l m r'
-             in if d then unbalancedL c (h-1) l m r' else (t, False)
+-- deleteMin :: RBTree a -> RBTree a
+-- deleteMin Leaf = Leaf 
+-- deleteMin t = turnB' s
+--   where
+--     ((s, _), _) = deleteMin' t
 
-deleteMin :: RBTree a -> RBTree a
-deleteMin Leaf = empty
-deleteMin t = turnB' s
-  where
-    ((s, _), _) = deleteMin' t
 
+{-@ deleteMin' :: t:RBT a -> (({v: ARBT a | ((IsB t) => (isRB v))}, Bool), a) @-}
 deleteMin' :: RBTree a -> (RBTreeBDel a, a)
 deleteMin' Leaf                           = error "deleteMin'"
 deleteMin' (Node B _ Leaf x Leaf)         = ((Leaf, True), x)
 deleteMin' (Node B _ Leaf x r@(Node R _ _ _ _)) = ((turnB r, False), x)
 deleteMin' (Node R _ Leaf x r)            = ((r, False), x)
 deleteMin' (Node c h l x r)               = if d then (tD, m) else (tD', m)
-  where
-    ((l',d),m) = deleteMin' l
-    tD  = unbalancedR c (h-1) l' x r
-    tD' = (Node c h l' x r, False)
+ where
+   ((l',d),m) = deleteMin' l
+   tD  = unbalancedR c (h-1) l' x r
+   tD' = (Node c h l' x r, False)
 
 
-turnR :: RBTree a -> RBTree a
-turnR Leaf             = error "turnR"
-turnR (Node _ h l x r) = Node R h l x r
-
-turnB :: RBTree a -> RBTree a
-turnB Leaf           = error "turnB"
-turnB (Node _ h l x r) = Node B h l x r
-
-turnB' :: RBTree a -> RBTree a
-turnB' Leaf             = Leaf
-turnB' (Node _ h l x r) = Node B h l x r
-
-
+{-@ unbalancedL :: Color -> BlackHeight -> RBT a -> a -> RBT a -> RBTB a @-}
 unbalancedL :: Color -> BlackHeight -> RBTree a -> a -> RBTree a -> RBTreeBDel a
 unbalancedL c h l@(Node B _ _ _ _) x r
   = (balanceL B h (turnR l) x r, c == B)
@@ -79,8 +69,10 @@ unbalancedL B h (Node R lh ll lx lr@(Node B _ _ _ _)) x r
   = (Node B lh ll lx (balanceL B h (turnR lr) x r), False)
 unbalancedL _ _ _ _ _ = error "unbalancedL"
 
+
 -- The left tree lacks one Black node
-unbalancedR :: Color -> BlackHeight -> RBTree a -> a -> RBTree a -> (RBTree a, Bool)
+{-@ unbalancedR :: Color -> BlackHeight -> RBT a -> a -> RBT a -> RBTB a @-}
+unbalancedR :: Color -> BlackHeight -> RBTree a -> a -> RBTree a -> RBTreeBDel a
 -- Decreasing one Black node in the right
 unbalancedR c h l x r@(Node B _ _ _ _)
   = (balanceR B h l x (turnR r), c == B)
@@ -89,6 +81,22 @@ unbalancedR B h l x (Node R rh rl@(Node B _ _ _ _) rx rr)
   = (Node B rh (balanceR B h l x (turnR rl)) rx rr, False)
 unbalancedR _ _ _ _ _ = error "unbalancedR"
 
+
+{-@ balanceL :: k:Color -> BlackHeight -> {v:ARBT a | ((Red k) => (IsB v))} -> a -> {v:RBT a | ((Red k) => (IsB v))} -> RBT a @-}
+balanceL B h (Node R _ (Node R _ a x b) y c) z d =
+    Node R (h+1) (Node B h a x b) y (Node B h c z d)
+balanceL B h (Node R _ a x (Node R _ b y c)) z d =
+    Node R (h+1) (Node B h a x b) y (Node B h c z d)
+balanceL k h l x r = Node k h l x r
+
+
+{-@ balanceR :: k:Color -> BlackHeight -> {v:RBT a | ((Red k) => (IsB v))} -> a -> {v:ARBT a | ((Red k) => (IsB v))} -> RBT a @-}
+balanceR :: Color -> BlackHeight -> RBTree a -> a -> RBTree a -> RBTree a
+balanceR B h a x (Node R _ b y (Node R _ c z d)) =
+    Node R (h+1) (Node B h a x b) y (Node B h c z d)
+balanceR B h a x (Node R _ (Node R _ b y c) z d) =
+    Node R (h+1) (Node B h a x b) y (Node B h c z d)
+balanceR k h l x r = Node k h l x r
 
 ---------------------------------------------------------------------------
 ---------------------------------------------------------------------------
@@ -99,9 +107,18 @@ insert :: Ord a => a -> RBTree a -> RBTree a
 insert kx t = turnB (insert' kx t)
 
 {-@ turnB :: ARBT a -> RBT a @-}
-turnB :: RBTree a -> RBTree a
 turnB Leaf           = error "turnB"
 turnB (Node _ h l x r) = Node B h l x r
+
+{-@ turnR :: RBT a -> ARBT a @-}
+turnR Leaf             = error "turnR"
+turnR (Node _ h l x r) = Node R h l x r
+
+{-@ turnB' :: ARBT a -> RBT a @-}
+turnB' Leaf             = Leaf
+turnB' (Node _ h l x r) = Node B h l x r
+
+
 
 {-@ insert' :: (Ord a) => a -> t:RBT a -> {v: ARBT a | ((IsB t) => (isRB v))} @-}
 insert' :: Ord a => a -> RBTree a -> RBTree a
@@ -135,9 +152,14 @@ balanceR' h l x r = Node B h l x r
 ---------------------------------------------------------------------------
 ---------------------------------------------------------------------------
 
-{-@ type RBT a  = {v: (RBTree a) | (isRB v)}  @-}
+{-@ type ARBTB a = (RBT a, Bool) @-}
+{-@ type RBTB a = (RBT a, Bool)  @-}
 
-{-@ type ARBT a = {v: (RBTree a) | (isARB v)} @-}
+{-@ type RBT a  = {v: (RBTree a) | (isRB v)} @-}
+{- type ARBT a = {v: (RBTree a) | ((isARB v) && ((IsB v) => (isRB v)))} -}
+
+
+{-@ type ARBT a = {v: (RBTree a) | (isARB v) } @-}
 
 {-@ measure isRB           :: RBTree a -> Prop
     isRB (Leaf)            = true
@@ -161,10 +183,12 @@ balanceR' h l x r = Node B h l x r
 -- Auxiliary Invariants -------------------------------------------------------
 -------------------------------------------------------------------------------
 
-{-@ invariant {v: RBTree a | ((isRB v) => (isARB v))} @-}
+{-@ predicate Invs V = ((Inv1 V) && (Inv2 V))               @-}
+{-@ predicate Inv1 V = (((isARB V) && (IsB V)) => (isRB V)) @-}
+{-@ predicate Inv2 V = ((isRB v) => (isARB v))              @-}
 
-{-@ inv              :: RBTree a -> {v:RBTree a | ((isRB v) => (isARB v)) } @-}
+{-@ invariant {v: RBTree a | (Invs v)} @-}
+
+{-@ inv              :: RBTree a -> {v:RBTree a | (Invs v)} @-}
 inv Leaf             = Leaf
 inv (Node c h l x r) = Node c h (inv l) x (inv r)
-
-
