2a1f288d2ad58f47dc9b1f6415547985807b47ff
nikivazou <nikivazou@gmail.com>
2014-03-26 12:39:15 -0700

    21 failing tests

--- tests/pos/fixme0.hs
+++ tests/pos/fixme0.hs
@@ -1,21 +1,9 @@
-module Loop (listSum) where
+module ListSort  where
 
-{-@ LIQUID "--no-termination"@-}
+{-@ type COList a = [a]<\fld -> {v:a | v >= fld}>  @-}
+{-@ type OList a = [a]<{\fld v -> (v >= fld)}>  @-}
 
--- listNatSum  :: [Int] -> Int
--- listEvenSum :: [Int] -> Int
--- add         :: Int -> Int -> Int
+{-@ mergesort :: COList a -> OList a @-}
+mergesort :: [a] -> [a]
+mergesort []  = []
 
-{-@ loop :: lo:_ -> {hi:_ | lo <= hi} -> a -> (_ -> a -> a) -> a @-}
-loop :: Int -> Int -> a -> (Int -> a -> a) -> a
-loop lo hi base f = go lo base
-  where 
-    go i acc 
-      | i < hi    = go (i+1) (f i acc)
-      | otherwise = acc
-      
-listSum     :: [Int] -> Int
-listSum xs  = loop 0 n 0 body 
-  where 
-    body    = \i acc -> acc + (xs !! i) -- replace !! with `poo` and its safe? wtf.
-    n       = length xs
