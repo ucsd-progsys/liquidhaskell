9fd916ff8dbb991ff322f98a6a60dd26020a5d71
nikivazou <nikivazou@gmail.com>
2014-04-05 09:31:55 -0700

    fix small bug

--- tests/pos/fixme.hs
+++ tests/pos/fixme.hs
@@ -1,15 +1,36 @@
-module ToyMVar where
+module Fixme where
 
-import Prelude hiding (IO)
-data RealWorld
-data State# s 
 
-data IO a = IO (State# RealWorld -> (State# RealWorld, a))
-{-@ data IO a <p :: State# -> Prop, q :: State# -> a -> Prop>
-      = IO (io :: State# RealWorld -> ((State# RealWorld, a)<q>))
-  @-}
-instance Monad IO where
-  return = returnIO
-returnIO :: a -> IO a
-returnIO x = IO $ \s -> (s, x)
+{-@ LIQUID "--no-termination" @-}
+data TokenType
+-- k_1873[lq_tmp_x1871:=ds_d1gi]  Char vs [Char] !!!
+-- lq_tmp_x1871  goes into g for 901 -- guess from rbsplit
+glue ("`":rest1) =				-- `varid` -> varop
+  case glue rest1 of
+    (qn:"`":rest2) -> ("`"++qn++"`"): glue rest2
+    _             -> "`": glue rest1
+glue (s:ss)       | all (=='-') s && length s >=2	-- eol comment
+                  = (s++concat c): glue rest3
+                  where (c,rest3) = break ('\n'`elem`) ss
+glue ("(":ss) = case rest5 of
+                ")":rest6 {- ds_d1gi : rest6 -} -> ("(" ++ concat tuple ++ ")") : glue rest6
+                -- _         -> "(" : glue ss
+              where (tuple,rest5) = span (==",") ss
+-- glue ("[":"]":ss) = "[]" : glue ss
+-- glue ("\n":"#":ss)= "\n" : ('#':concat line) : glue rest
+--                   where (line,rest) = break ('\n'`elem`) ss
+-- glue (s:ss)       = s: glue ss
+-- glue []           = []
+
+-- Deal with comments.
+nestcomment :: Int -> String -> (String,String)
+nestcomment n ('{':'-':ss) | n>=0 = (("{-"++cs),rm)
+                                  where (cs,rm) = nestcomment (n+1) ss
+-- nestcomment n ('-':'}':ss) | n>0  = (("-}"++cs),rm)
+--                                   where (cs,rm) = nestcomment (n-1) ss
+-- nestcomment n ('-':'}':ss) | n==0 = ("-}",ss)
+-- nestcomment n (s:ss)       | n>=0 = ((s:cs),rm)
+--                                   where (cs,rm) = nestcomment n ss
+-- nestcomment n [] = ([],[])
+
 
