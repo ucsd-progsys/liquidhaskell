a1c47693866d148f968c2da4f76ac7fb6b8c1796
nikivazou <nikivazou@gmail.com>
2014-05-05 20:51:48 -0700

    stack class measure

---
+++ tests/pos/StackClass.hs
@@ -0,0 +1,17 @@
+module SClass where
+
+import qualified Data.Set
+
+data Stack a = S [a]
+
+data Foo a = F {stack :: Stack a}
+
+{-@ class measure elts  :: a -> Data.Set.Set a @-}
+
+{-@ instance measure elts :: Stack a -> (Data.Set.Set a)
+    elts (S xs) = (listElts xs)
+  @-}
+
+{-@ instance measure elts :: Foo a -> (Data.Set.Set a)
+    elts (F st) = (elts st)
+  @-}
