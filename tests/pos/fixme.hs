11dace5e2794243915164527bcabbff9a17cbc27
nikivazou <nikivazou@gmail.com>
2014-05-07 21:15:05 -0700

    allJustList

--- tests/pos/fixme.hs
+++ tests/pos/fixme.hs
@@ -24,5 +24,15 @@ bar (F x [] ) = True
 bar _ = False 
 
 
+{-@ bars :: F <{\v -> (isJust (wp v))}> a b -> {v:Bool | (Prop v) }@-}
+bars :: F a b -> Bool
+bars (F _ xs) = allisJustB xs
+
+{-@ allisJustB :: [{v:B a | (isJust (wp v))} ] -> {v:Bool | (Prop v)} @-}
+allisJustB :: [B a] -> Bool
+allisJustB ((B (Just _)):xs) = allisJustB xs
+allisJustB [] = True
+allisJustB ((B Nothing): xs) = False
+
 isJust (Just x) = True
 isJust _        = False
