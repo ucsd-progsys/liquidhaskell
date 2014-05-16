9fa98f27c092af122a1749cb2eb7a7b886dab492
Eric Seidel <gridaphobe@gmail.com>
2014-05-16 10:40:05 -0700

    split postInst hook into postBuild and postCopy

--- Setup.hs
+++ Setup.hs
@@ -8,27 +8,35 @@ import System.Posix.Env
 import System.Process
 import System.Exit
 
-main         = defaultMainWithHooks fixHooks 
-  where 
-    fixHooks = simpleUserHooks { postInst = buildAndCopyFixpoint } 
-   
-buildAndCopyFixpoint _ _ pkg lbi 
-  = do putStrLn $ "Post Install: " ++ show binDir
-       setEnv "Z3MEM" (show z3mem) True
+main         = defaultMainWithHooks fixHooks
+  where
+    fixHooks = simpleUserHooks { postBuild = buildFixpoint
+                               , postCopy = copyFixpoint
+                               }
+
+buildFixpoint _ _ pkg lbi
+  = do setEnv "Z3MEM" (show z3mem) True
        executeShellCommand "./configure"
        executeShellCommand "./build.sh"
        executeShellCommand "chmod a+x external/fixpoint/fixpoint.native "
-       executeShellCommand $ "cp external/fixpoint/fixpoint.native " ++ binDir
+  where
+    allDirs     = absoluteInstallDirs pkg lbi NoCopyDest
+    binDir      = bindir allDirs ++ "/"
+    flags       = configConfigurationsFlags $ configFlags lbi
+    z3mem       = fromJust $ lookup (FlagName "z3mem") flags
+
+copyFixpoint _ _ pkg lbi
+  = do executeShellCommand $ "cp external/fixpoint/fixpoint.native " ++ binDir
        when z3mem $
          executeShellCommand $ "cp external/z3/lib/libz3.* "         ++ binDir
-  where 
+  where
     allDirs     = absoluteInstallDirs pkg lbi NoCopyDest
     binDir      = bindir allDirs ++ "/"
     flags       = configConfigurationsFlags $ configFlags lbi
     z3mem       = fromJust $ lookup (FlagName "z3mem") flags
 
 executeShellCommand cmd   = putStrLn ("EXEC: " ++ cmd) >> system cmd >>= check
-  where 
+  where
     check (ExitSuccess)   = return ()
-    check (ExitFailure n) = error $ "cmd: " ++ cmd ++ " failure code " ++ show n 
+    check (ExitFailure n) = error $ "cmd: " ++ cmd ++ " failure code " ++ show n
 
