7d965b884b2df98b84d1bd6ceb0c348d031f9190
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-05-21 11:10:41 -0700

    slowly pushing EMsg thing through system...

--- src/Language/Haskell/Liquid/GhcInterface.hs
+++ src/Language/Haskell/Liquid/GhcInterface.hs
@@ -1,5 +1,10 @@
 
-{-# LANGUAGE NoMonomorphismRestriction, TypeSynonymInstances, FlexibleInstances, TupleSections, DeriveDataTypeable, ScopedTypeVariables #-}
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE TypeSynonymInstances      #-} 
+{-# LANGUAGE FlexibleInstances         #-}
+{-# LANGUAGE TupleSections             #-}
+{-# LANGUAGE DeriveDataTypeable        #-}
+{-# LANGUAGE ScopedTypeVariables       #-}
 
 module Language.Haskell.Liquid.GhcInterface (
   
@@ -532,7 +537,7 @@ instance PPrint TargetVars where
 
 -- | Throw a panic exception
 exitWithPanic  :: String -> a 
-exitWithPanic  = Ex.throw . ErrOther . text 
+exitWithPanic  = Ex.throw . ErrOther . EMsg 
 
 -- | Convert a GHC error into one of ours
 instance Result SourceError where 
@@ -541,5 +546,5 @@ instance Result SourceError where
          . bagToList 
          . srcErrorMessages
      
-errMsgErrors e = [ ErrGhc l (pprint e) | l <- errMsgSpans e ] 
+errMsgErrors e = [ ErrGhc l (showEMsg e) | l <- errMsgSpans e ] 
 
