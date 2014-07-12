107957199f10536358a63d823b96cdea048f36e2
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-07-11 17:51:14 -0700

    ok, now contexts are closed. next: pretty-printing contexts

--- src/Language/Haskell/Liquid/Errors.hs
+++ src/Language/Haskell/Liquid/Errors.hs
@@ -1,62 +1,72 @@
 
-{-# LANGUAGE OverloadedStrings  #-}
-{-# LANGUAGE FlexibleInstances  #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE OverloadedStrings #-}
 
--- | This module contains the functions related to @Error@ type, 
+-- | This module contains the functions related to @Error@ type,
 -- in particular, to @tidyError@ using a solution, and @pprint@ errors.
 
 module Language.Haskell.Liquid.Errors (tidyError) where
 
-import Data.Maybe (maybeToList)
-import Data.Monoid                              hiding ((<>))
-import Control.Exception                        (Exception (..)) 
-import Control.Applicative                      ((<$>), (<*>))
-import Text.PrettyPrint.HughesPJ    
-import Data.Aeson    
-import qualified  Data.HashMap.Strict as M
-import SrcLoc                                   (SrcSpan)
-import Language.Fixpoint.Misc
-import Language.Fixpoint.Types
-import Language.Haskell.Liquid.Types
-import Language.Haskell.Liquid.RefType
-import Language.Haskell.Liquid.PrettyPrint
-import Language.Haskell.Liquid.Tidy
+
+import           Control.Applicative                 ((<$>), (<*>))
+import           Control.Exception                   (Exception (..))
+import           Data.Aeson
+import           Data.Hashable
+import qualified Data.HashMap.Strict                 as M
+import qualified Data.HashSet                        as S
+import           Data.List                           (intersperse)
+import           Data.Maybe                          (fromMaybe, maybeToList)
+import           Data.Monoid                         hiding ((<>))
+import           Language.Fixpoint.Misc              hiding (intersperse)
+import           Language.Fixpoint.Types
+import           Language.Haskell.Liquid.PrettyPrint
+import           Language.Haskell.Liquid.RefType
+import           Language.Haskell.Liquid.Tidy
+import           Language.Haskell.Liquid.Types
+import           SrcLoc                              (SrcSpan)
+import           Text.PrettyPrint.HughesPJ
 
 ------------------------------------------------------------------------
 tidyError :: FixSolution -> Error -> Error
 ------------------------------------------------------------------------
 tidyError sol = tidyContext sol
-              . fmap (tidySpecType Full) . applySolution sol 
+              . fmap (tidySpecType Full) . applySolution sol
 
-tidyContext s err@(ErrSubType {}) 
+tidyContext s err@(ErrSubType {})
   = err { ctx = shrinkREnv xs $ ctx err }
-    where 
+    where
       xs = syms tA ++ syms tE
       tA = tact err
-      tE = texp err 
+      tE = texp err
 
-tidyContext _ err 
+tidyContext _ err
   = err
 
-shrinkREnv xs m 
-  = M.fromList $ [(x, t) | x <- xs, t <- maybeToList (M.lookup x m)]
+shrinkREnv xs m = M.fromList $ [(x, t) | x <- xs', t <- maybeToList (M.lookup x m)]
+    where
+      xs'       = expandFix deps xs
+      deps y    = fromMaybe [] $ fmap (syms . rTypeReft) $ M.lookup y m
 
--- HEREHEREHERE
--- 1. apply solution
--- 2. gather and filter binders
--- 3. modify ppError to show context 
+expandFix :: (Eq a, Hashable a) => (a -> [a]) -> [a] -> [a]
+expandFix f xs          = S.toList $ go S.empty xs
+  where
+    go seen []          = seen
+    go seen (x:xs)
+      | x `S.member` seen = go seen xs
+      | otherwise         = go (S.insert x seen) (f x ++ xs)
+
 
 ------------------------------------------------------------------------
 -- | Pretty Printing Error Messages ------------------------------------
 ------------------------------------------------------------------------
 
--- Need to put this here intead of in Types, because it depends on the 
+-- Need to put this here intead of in Types, because it depends on the
 -- printer for SpecTypes, which lives in this module.
 
 instance PPrint Error where
   pprint = ppError . fmap (rtypeDoc Lossy)
-            -- undefined 
-            -- ppError . ppr_rtype 
+            -- undefined
+            -- ppError . ppr_rtype
 
 instance Show Error where
   show = showpp
@@ -71,72 +81,78 @@ ppError :: (PPrint a) => TError a -> Doc
 ppError e = ppError' (pprintE $ errSpan e) e
 pprintE l = pprint l <> text ": Error:"
 
+nests n    = foldr (\d acc -> nest n (d $+$ acc)) empty
+
+sepVcat d ds = vcat $ intersperse d ds
+blankLine    = sizedText 5 " "
 
 ------------------------------------------------------------------------
 ppError' :: (PPrint a) => Doc -> TError a -> Doc
 -----------------------------------------------------------------------
 
 
-ppError' dSp (ErrAssType _ OTerm s r) 
+ppError' dSp (ErrAssType _ OTerm s r)
   = dSp <+> text "Termination Check"
 
-ppError' dSp (ErrAssType _ OInv s r) 
+ppError' dSp (ErrAssType _ OInv s r)
   = dSp <+> text "Invariant Check"
 
-ppError' dSp (ErrSubType _ s c tA tE) 
+ppError' dSp (ErrSubType _ s c tA tE)
   = dSp <+> text "Liquid Type Mismatch"
---     DO NOT DELETE EVER! 
-     $+$ (nest 4 $ text "Required Type:" <+> pprint tE)
-     $+$ (nest 4 $ text "Actual   Type:" <+> pprint tA)
-     $+$ (nest 4 $ text "Context: "      <+> pprint c)
+--      DO NOT DELETE EVER!
+        $+$ sepVcat blankLine
+              [ nests 2 [text "Required Type", pprint tE]
+              , nests 2 [text "Actual Type"  , pprint tA]
+              , nests 2 [text "In Context"   , pprint c ]]
 
-ppError' dSp (ErrParse _ _ e)       
-  = dSp <+> text "Cannot parse specification:" 
+
+ppError' dSp (ErrParse _ _ e)
+  = dSp <+> text "Cannot parse specification:"
     $+$ (nest 4 $ pprint e)
 
-ppError' dSp (ErrTySpec _ v t s)       
+ppError' dSp (ErrTySpec _ v t s)
   = dSp <+> text "Bad Type Specification"
-    $+$ (pprint v <+> dcolon <+> pprint t) 
+    $+$ (pprint v <+> dcolon <+> pprint t)
     $+$ (nest 4 $ pprint s)
 
 ppError' dSp (ErrInvt _ t s)
-  = dSp <+> text "Bad Invariant Specification" 
+  = dSp <+> text "Bad Invariant Specification"
     $+$ (nest 4 $ text "invariant " <+> pprint t $+$ pprint s)
 
 ppError' dSp (ErrIAl _ t s)
-  = dSp <+> text "Bad Using Specification" 
+  = dSp <+> text "Bad Using Specification"
     $+$ (nest 4 $ text "as" <+> pprint t $+$ pprint s)
 
 ppError' dSp (ErrIAlMis _ t1 t2 s)
-  = dSp <+> text "Incompatible Using Specification" 
+  = dSp <+> text "Incompatible Using Specification"
     $+$ (nest 4 $ (text "using" <+> pprint t1 <+> text "as" <+> pprint t2) $+$ pprint s)
 
 ppError' dSp (ErrMeas _ t s)
-  = dSp <+> text "Bad Measure Specification" 
+  = dSp <+> text "Bad Measure Specification"
     $+$ (nest 4 $ text "measure " <+> pprint t $+$ pprint s)
 
 ppError' dSp (ErrDupSpecs _ v ls)
   = dSp <+> text "Multiple Specifications for" <+> pprint v <> colon
-    $+$ (nest 4 $ vcat $ pprint <$> ls) 
+    $+$ (nest 4 $ vcat $ pprint <$> ls)
 
 ppError' dSp (ErrDupAlias _ k v ls)
-  = dSp <+> text "Multiple Declarations! " 
+  = dSp <+> text "Multiple Declarations! "
     $+$ (nest 2 $ text "Multiple Declarations of" <+> pprint k <+> ppVar v $+$ text "Declared at:")
-    <+> (nest 4 $ vcat $ pprint <$> ls) 
+    <+> (nest 4 $ vcat $ pprint <$> ls)
 
-ppError' dSp (ErrGhc _ s)       
+ppError' dSp (ErrGhc _ s)
   = dSp <+> text "GHC Error"
     $+$ (nest 4 $ pprint s)
 
-ppError' dSp (ErrMismatch _ x τ t) 
+ppError' dSp (ErrMismatch _ x τ t)
   = dSp <+> text "Specified Type Does Not Refine Haskell Type for" <+> pprint x
     $+$ text "Haskell:" <+> pprint τ
-    $+$ text "Liquid :" <+> pprint t 
-     
-ppError' dSp (ErrSaved _ s)       
+    $+$ text "Liquid :" <+> pprint t
+
+ppError' dSp (ErrSaved _ s)
   = dSp <+> s
 
-ppError' _ (ErrOther _ s)       
+ppError' _ (ErrOther _ s)
   = text "Panic!" <+> nest 4 (pprint s)
 
 
@@ -148,7 +164,7 @@ instance ToJSON Error where
                     ]
 
 instance FromJSON Error where
-  parseJSON (Object v) = errSaved <$> v .: "pos" 
+  parseJSON (Object v) = errSaved <$> v .: "pos"
                                   <*> v .: "msg"
   parseJSON _          = mempty
 
