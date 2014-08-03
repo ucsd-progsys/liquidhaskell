ac8cd5e99dc46072849b2fcd15bad6a9578c0781
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-08-02 20:59:18 -0700

    more nukage -- most of Predicates.hs is unnecessary

--- src/Language/Haskell/Liquid/Predicates.hs
+++ src/Language/Haskell/Liquid/Predicates.hs
@@ -38,11 +38,9 @@ import           Control.Applicative                  ((<$>))
 --   instantiation in a manner symmetric to vanilla polymorphic
 --   instantiation at type-instantiation sites.
 
-generatePredicates ::  GhcInfo -> ([CoreSyn.Bind CoreBndr], F.SEnv PrType)
--- generatePredicates ::  GhcInfo -> F.SEnv PrType
-generatePredicates info = (cbs', nPd)
+generatePredicates ::  GhcInfo -> (F.SEnv PrType)
+generatePredicates info = nPd
   where
-    cbs'                = addPredApp nPd <$> cbs info
     nPd                 = getNeedPd $ spec info
 
 getNeedPd spec          = F.fromListSEnv bs
@@ -55,78 +53,76 @@ getNeedPd spec          = F.fromListSEnv bs
 dataConPtoPredTy :: TC.DataCon -> DataConP -> PrType
 dataConPtoPredTy dc = fmap ur_pred . dataConPSpecType dc
 
+-- NUKE addPredApp γ (NonRec b e) = NonRec b $ thd3 $ pExpr γ e
+-- NUKE addPredApp γ (Rec ls)     = Rec $ zip xs es'
+-- NUKE   where es' = (thd3. pExpr γ) <$> es
+-- NUKE         (xs, es) = unzip ls
+-- NUKE 
+-- NUKE pExpr γ e
+-- NUKE   | a == 0 && p /= 0 = (0, 0, foldl App e' ps)
+-- NUKE   | otherwise        = (0, p, e')
+-- NUKE  where
+-- NUKE    (a, p, e')        = pExprN γ e
+-- NUKE    ps                = nArgs p -- stringArg . ("p" ++) . show <$> [1 .. p]
+-- NUKE 
+-- NUKE pExprN γ (App e1 e2) =
+-- NUKE   let (_, _, e2') = pExprN γ e2 in
+-- NUKE   if (a1 == 0)
+-- NUKE    then (0, 0, (App (foldl App e1' ps) e2'))
+-- NUKE    else (a1-1, p1, (App e1' e2'))
+-- NUKE  where
+-- NUKE    ps            = nArgs p1
+-- NUKE    (a1, p1, e1') = pExprN γ e1
+-- NUKE 
+-- NUKE pExprN γ (Lam x e) = (0, 0, Lam x e')
+-- NUKE   where (_, _, e') = pExpr γ e
+-- NUKE 
+-- NUKE pExprN γ (Var v) | isSpecialId γ v
+-- NUKE   = (a, p, (Var v))
+-- NUKE     where (a, p) = varPredArgs γ v
+-- NUKE 
+-- NUKE pExprN _ (Var v) = (0, 0, Var v)
+-- NUKE 
+-- NUKE pExprN γ (Let (NonRec x1 e1) e) = (0, 0, Let (NonRec x1 e1') e')
+-- NUKE  where (_, _, e') = pExpr γ e
+-- NUKE        (_, _, e1') = pExpr γ e1
+-- NUKE 
+-- NUKE pExprN γ (Let bds e) = (0, 0, Let bds' e')
+-- NUKE  where (_, _, e') = pExpr γ e
+-- NUKE        bds' = addPredApp γ bds
+-- NUKE pExprN γ (Case e b t es) = (0, 0, Case e' b t (map (pExprNAlt γ ) es))
+-- NUKE   where e' = thd3 $ pExpr γ e
+-- NUKE 
+-- NUKE pExprN γ (Tick n e) = (a, p, Tick n e')
+-- NUKE  where (a, p, e') = pExprN γ e
+-- NUKE 
+-- NUKE pExprN _ e@(Type _) = (0, 0, e)
+-- NUKE pExprN _ e@(Lit _)  = (0, 0, e)
+-- NUKE pExprN _ e = (0, 0, e)
+-- NUKE 
+-- NUKE pExprNAlt γ (x, y, e) = (x, y, e')
+-- NUKE  where e' = thd3 $ pExpr γ e
 
 
-addPredApp γ (NonRec b e) = NonRec b $ thd3 $ pExpr γ e
-addPredApp γ (Rec ls)     = Rec $ zip xs es'
-  where es' = (thd3. pExpr γ) <$> es
-        (xs, es) = unzip ls
+-- NUKE -- | @nArgs@ returns a list of dummy (value) variables that are inserted
+-- NUKE --   at predicate application sites.
+-- NUKE nArgs n     = stringArg . ("p" ++) . show <$> [1 .. n]
 
-pExpr γ e
-  | a == 0 && p /= 0 = (0, 0, foldl App e' ps)
-  | otherwise        = (0, p, e')
- where
-   (a, p, e')        = pExprN γ e
-   ps                = nArgs p -- stringArg . ("p" ++) . show <$> [1 .. p]
+-- NUKE stringArg s = Var $ mkGlobalVar idDet name predType idInfo
+-- NUKE   where
+-- NUKE     idDet   = coVarDetails
+-- NUKE     name    = mkInternalName initTyVarUnique occ noSrcSpan
+-- NUKE     occ     = mkTyVarOcc s
+-- NUKE     idInfo  = vanillaIdInfo
 
-pExprN γ (App e1 e2) =
-  let (_, _, e2') = pExprN γ e2 in
-  if (a1 == 0)
-   then (0, 0, (App (foldl App e1' ps) e2'))
-   else (a1-1, p1, (App e1' e2'))
- where
-   ps            = nArgs p1
-   (a1, p1, e1') = pExprN γ e1
+-- NUKE isSpecialId γ x       = pl /= 0
+-- NUKE   where
+-- NUKE     (_, pl)           = varPredArgs γ x
 
-pExprN γ (Lam x e) = (0, 0, Lam x e')
-  where (_, _, e') = pExpr γ e
-
-pExprN γ (Var v) | isSpecialId γ v
-  = (a, p, (Var v))
-    where (a, p) = varPredArgs γ v
-
-pExprN _ (Var v) = (0, 0, Var v)
-
-pExprN γ (Let (NonRec x1 e1) e) = (0, 0, Let (NonRec x1 e1') e')
- where (_, _, e') = pExpr γ e
-       (_, _, e1') = pExpr γ e1
-
-pExprN γ (Let bds e) = (0, 0, Let bds' e')
- where (_, _, e') = pExpr γ e
-       bds' = addPredApp γ bds
-pExprN γ (Case e b t es) = (0, 0, Case e' b t (map (pExprNAlt γ ) es))
-  where e' = thd3 $ pExpr γ e
-
-pExprN γ (Tick n e) = (a, p, Tick n e')
- where (a, p, e') = pExprN γ e
-
-pExprN _ e@(Type _) = (0, 0, e)
-pExprN _ e@(Lit _)  = (0, 0, e)
-pExprN _ e = (0, 0, e)
-
-pExprNAlt γ (x, y, e) = (x, y, e')
- where e' = thd3 $ pExpr γ e
-
-
--- | @nArgs@ returns a list of dummy (value) variables that are inserted
---   at predicate application sites.
-nArgs n     = stringArg . ("p" ++) . show <$> [1 .. n]
-
-stringArg s = Var $ mkGlobalVar idDet name predType idInfo
-  where
-    idDet   = coVarDetails
-    name    = mkInternalName initTyVarUnique occ noSrcSpan
-    occ     = mkTyVarOcc s
-    idInfo  = vanillaIdInfo
-
-isSpecialId γ x       = pl /= 0
-  where
-    (_, pl)           = varPredArgs γ x
-
-varPredArgs γ x       = varPredArgs_ (F.lookupSEnv (F.symbol x) γ)
-
-varPredArgs_ Nothing  = (0, 0)
-varPredArgs_ (Just t) = (length $ ty_vars trep, length $ ty_preds trep)
-  where
-    trep              = toRTypeRep t
+-- NUKE varPredArgs γ x       = varPredArgs_ (F.lookupSEnv (F.symbol x) γ)
+-- NUKE 
+-- NUKE varPredArgs_ Nothing  = (0, 0)
+-- NUKE varPredArgs_ (Just t) = (length $ ty_vars trep, length $ ty_preds trep)
+-- NUKE   where
+-- NUKE     trep              = toRTypeRep t
 
