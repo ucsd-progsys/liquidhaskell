2d5351bd39facd9f99f93eaf07ae676941026db7
Eric Seidel <gridaphobe@gmail.com>
2014-08-27 12:42:09 -0700

    move termination expression with local vars to separate test

--- tests/todo/LocalTermExpr.hs
+++ tests/todo/LocalTermExpr.hs
@@ -1,12 +1,11 @@
-{-@ LIQUID "--no-termination" @-}
-module LocalHole where
+module LocalTermExpr where
 
 {-@ assume (!!) :: xs:[a] -> {v:Nat | v < len xs} -> a @-}
 
 mysum xs = go 0 0
   where
     n = length xs
-    {-@ go :: i:_ -> _ -> _ @-}
+    {-@ go :: i:_ -> _ -> _ / [n - i] @-}
     go i acc
       | i >= n    = acc
       | otherwise = go (i+1) (acc + xs !! i)
