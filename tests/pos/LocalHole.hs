a8ea1642554940804d33efa1dc9db3d327d18bf2
Eric Seidel <gridaphobe@gmail.com>
2014-08-28 16:27:13 -0700

    check SpecTypes in environment with local binders

--- tests/pos/LocalHole.hs
+++ tests/pos/LocalHole.hs
@@ -6,7 +6,7 @@ module LocalHole where
 mysum xs = go 0 0
   where
     n = length xs
-    {-@ go :: i:_ -> _ -> _ @-}
+    {-@ go :: i:{Nat | i <= n} -> _ -> _ @-}
     go i acc
       | i >= n    = acc
       | otherwise = go (i+1) (acc + xs !! i)
