c840f590683c6d3247f097b8c34598f1f535e815
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-09-04 20:06:45 -0400

    adding Box.hs -- safe duck typing with LH

--- tests/todo/Box.hs
+++ tests/todo/Box.hs
@@ -1,7 +1,6 @@
 module Box (Box, get, put, emp) where
 
 import Data.Dynamic
-import Language.Haskell.Liquid.Prelude
 
 
 -----------------------------------------------------------
@@ -23,16 +22,39 @@ put k v (Box kvs) = Box ((k, toDyn v) : kvs)
 -----------------------------------------------------------
 get :: (Typeable v) => Key -> Box -> v 
 -----------------------------------------------------------
-get k (Box kvs) = fromJust $ fromDynamic =<< lookup k kvs
+get k (Box kvs) = fromJust k $ fromDynamic =<< lookup k kvs
 
 
 -----------------------------------------------------------
 -- | Helpers
 -----------------------------------------------------------
 
-{-@ fromJust      :: {v: Maybe a | isJust v} -> a @-}
-fromJust (Just x) = x
-fromJust Nothing  = safeError "nein"
+{-@ fromJust :: Key -> {v: Maybe a | isJust v} -> a @-}
+fromJust     :: Key -> Maybe a -> a
+fromJust _ (Just x) = x
+fromJust k Nothing  = safeError $ "Error on key: " ++ show k
 
 {-@ safeError :: {v:_ | false} -> a @-}
 safeError = error
+
+-----------------------------------------------------------
+-- | Unit Test 
+-----------------------------------------------------------
+
+b0 :: Box
+b0 = put 0 "Apple"
+   $ put 1 (4.5 :: Double)
+   $ put 2 ([1,2,3] :: [Int])
+   $ emp
+
+okFruit  :: String
+okFruit  = get 0 b0
+
+badFruit :: String
+badFruit = get 1 b0
+
+okPi     :: Double
+okPi     = get 1 b0
+
+badPi    :: Double
+badPi    = get 0 b0
