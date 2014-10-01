ff49faa467ec3cb7ba953bcc1c6a076d22a51d1d
nikivazou <nikivazou@gmail.com>
2014-10-01 14:52:19 -0700

    strengthening lifted types does not work...

--- src/Language/Haskell/Liquid/Bare.hs
+++ src/Language/Haskell/Liquid/Bare.hs
@@ -121,6 +121,7 @@ makeGhcSpec' cfg cbs vars defVars exports specs
          >>= makeGhcSpec2 invs ialias measures su                     
          >>= makeGhcSpec3 datacons tycons embs syms             
          >>= makeGhcSpec4 defVars specs name su 
+         >>= strengthenHaskellMeasures specs
 
 emptySpec     :: Config -> GhcSpec
 emptySpec cfg = SP [] [] [] [] [] [] [] [] [] mempty [] [] [] [] mempty mempty cfg mempty [] mempty 
@@ -161,12 +162,14 @@ makeGhcSpec4 defVars specs name su sp
        texprs' <- mconcat <$> mapM (makeTExpr defVars) specs
        lazies  <- mkThing makeLazy
        lvars'  <- mkThing makeLVar
+       hmeas   <- mkThing makeHMeas
        quals   <- mconcat <$> mapM makeQualifiers specs
        return   $ sp { qualifiers = subst su quals
                      , decr       = decr'
                      , texprs     = texprs'
                      , lvars      = lvars'
-                     , lazy       = lazies }        
+                     , lazy       = lazies 
+                     , tySigs     = strengthenHaskellMeasures hmeas ++ tySigs sp}        
     where
        mkThing mk = S.fromList . mconcat <$> sequence [ mk defVars (m, s) | (m, s) <- specs, m == name ]
 
@@ -218,15 +221,42 @@ makeMeasureDefinition cbs x
     binders (NonRec x _) = [x]
     binders (Rec xes)    = fst <$> xes  
 
-    simplesymbol = symbol . getName
-
     coreToDef' x v def = case (runToLogic $ coreToDef x v def) of 
                            Left x         -> return  x
                            Right (LE str) -> mkError str
 
     mkError str = throwError $ ErrHMeas (sourcePosSrcSpan $ loc x) (val x) (text str)                       
 
+simplesymbol = symbol . getName
 
+
+strengthenHaskellMeasures :: S.HashSet Var -> [(Var, Located SpecType)]
+strengthenHaskellMeasures = undefined
+{-
+strengthenHaskellMeasures :: [(ModName, Ms.BareSpec)] -> GhcSpec -> BareM GhcSpec  -- [(ModName, Ms.BareSpec)] -> GhcSpec -> BareM GhcSpec 
+strengthenHaskellMeasures specs sp
+  = do name <- gets modName
+       let hmeans = S.map val <$> Ms.hmeas <$> L.lookup name specs
+       return $ sp{tySigs = go hmeans <$> tySigs sp}
+  where 
+    go Nothing       (x, t)
+     = traceShow "NOTHING!!!!" (x, t)
+    go (Just hmeans) (x, t) 
+     | S.member (dropModuleNames $ simplesymbol x) hmeans 
+     = traceShow "DID IT" (x, strengthenResult (dummyLoc $ dropModuleNames $ simplesymbol x) <$> t)
+     | otherwise
+     = traceShow ("\nNope: " ++ show (dropModuleNames $ simplesymbol x)) (x, t)
+-}
+
+strengthenResult :: LocSymbol -> SpecType -> SpecType
+strengthenResult f t 
+  = fromRTypeRep $ rep{ty_res = ty_res rep `strengthen` r}
+  where rep = toRTypeRep t
+        r   = U (exprReft (EApp f [EVar x])) mempty mempty
+        x   = safeHead "strengthenResult" $ ty_binds rep
+
+isHaskellMeasure :: LocSymbol -> Var -> Bool
+isHaskellMeasure x v = (val x) == (dropModuleNames (simplesymbol v))
 
 makeMeasureSelectors :: (DataCon, Located DataConP) -> [Measure SpecType DataCon]
 makeMeasureSelectors (dc, (Loc loc (DataConP _ vs _ _ _ xts r))) = go <$> zip (reverse xts) [1..]
@@ -235,7 +265,6 @@ makeMeasureSelectors (dc, (Loc loc (DataConP _ vs _ _ _ xts r))) = go <$> zip (r
         
     dty t         = foldr RAllT  (RFun dummySymbol r (fmap mempty t) mempty) vs
     n             = length xts
-
 
 makePluggedSigs name embs tcEnv exports sigs
   = forM sigs $ \(x,t) -> do
@@ -490,6 +519,7 @@ makeClasses cfg vs (mod, spec) = inModule mod $ mapM mkClass $ Ms.classes spec
 makeHints vs (_, spec) = varSymbols id "Hint" vs $ Ms.decr spec
 makeLVar  vs (_, spec) = fmap fst <$> (varSymbols id "LazyVar" vs $ [(v, ()) | v <- Ms.lvars spec])
 makeLazy  vs (_, spec) = fmap fst <$> (varSymbols id "Lazy" vs $ [(v, ()) | v <- S.toList $ Ms.lazy spec])
+makeHMeas vs (_, spec) = fmap fst <$> (varSymbols id "HMeas" vs $ [(v, ()) | v <- S.toList $ Ms.hmeas spec])
 makeTExpr vs (_, spec) = varSymbols id "TermExpr" vs $ Ms.termexprs spec
 
 varSymbols :: ([Var] -> [Var]) -> Symbol ->  [Var] -> [(LocSymbol, a)] -> BareM [(Var, a)]
