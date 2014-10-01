2fa7f5b28a6b52720a59d4bf7a62b2c267c8915b
nikivazou <nikivazou@gmail.com>
2014-10-01 15:15:38 -0700

    add type to measure haskell functions, if they don't have

--- tests/pos/HaskellMeasure.hs
+++ tests/pos/HaskellMeasure.hs
@@ -9,7 +9,7 @@ llen (x:xs) = 1 + llen xs
 {-@ llen, llllen :: xs:[a] -> {v:Int| (lllen xs) = v} @-}
 
 lllen :: [a] -> Int	
-{-@ lllen :: [a] -> Int	@-}
+{- lllen :: [a] -> Int	@-}
 lllen [] = 0
 lllen (x:xs) = 1 + lllen xs
 
