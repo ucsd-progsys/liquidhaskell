cc91bafa41f9a8dbb1cf4ed3919adeb5b5f9eaff
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-10-07 15:43:33 -0400

    ok, lets push the empty uif and no-warnings

--- tests/todo/nodecr-err.hs
+++ tests/todo/nodecr-err.hs
@@ -4,7 +4,7 @@ import Prelude hiding (map, foldr, foldr1)
 
 data List a = N | C a (List a)
 
--- this should be flagged as an ERROR (UNSAFE) not a warning.
+-- this should be flagged as an ERROR (UNSAFE) not a warning, because no decreasing measure is specified.
 
 map f (N)      = N
 map f (C x xs) = C (f x) (map f xs) 
