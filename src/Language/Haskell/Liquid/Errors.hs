49d755ddca87d41f141e597807134e06bc354ec0
nikivazou <nikivazou@gmail.com>
2014-10-07 18:05:15 -0700

    fix for #251

--- src/Language/Haskell/Liquid/Errors.hs
+++ src/Language/Haskell/Liquid/Errors.hs
@@ -227,6 +227,9 @@ ppError' _ dSp (ErrAliasApp _ n name dl dn)
 ppError' _ dSp (ErrSaved _ s)
   = dSp <+> s
 
+ppError' _ dSp (ErrTermin _ s)
+  = dSp <+> text "Termination Error" $+$ s
+
 ppError' _ _ (ErrOther _ s)
   = text "Panic!" <+> nest 4 (pprint s)
 
