7161f86fe2435e6f4cb0867774207dafc403f4f2
nikivazou <nikivazou@gmail.com>
2014-10-10 07:51:32 -0700

    fix for #254

--- tests/todo/measbug.hs
+++ tests/todo/measbug.hs
@@ -5,8 +5,11 @@ data Heap a   = Empty | Node { pri   :: a
                              , left  :: Heap a
                              , right :: Heap a
                              }
+{-@ data Heap [zoo] @-}
 
+{-@ invariant {v:Heap a| (zoo v) >=0} @-}
 
 {-@ measure zoo @-}
+zoo :: Heap a -> Int
 zoo (Empty)        = 0
 zoo (Node _ _ l r) = 1 + zoo l + zoo r
