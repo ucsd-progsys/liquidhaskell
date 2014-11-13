38dc4d4346bcc93306a18a917d3efacc059ff671
nikivazou <nikivazou@gmail.com>
2014-11-12 18:49:44 -0800

    inline case fail

--- src/Language/Haskell/Liquid/TransformRec.hs
+++ src/Language/Haskell/Liquid/TransformRec.hs
@@ -15,6 +15,7 @@ import           Control.Arrow       (second, (***))
 import           Control.Monad.State
 import           CoreLint
 import           CoreSyn
+import           CoreSubst
 import qualified Data.HashMap.Strict as M
 import           ErrUtils
 import           Id                  (idOccInfo, setIdInfo)
@@ -27,9 +28,12 @@ import           Unique              hiding (deriveUnique)
 import           Var
 import           Language.Haskell.Liquid.GhcMisc
 import           Language.Haskell.Liquid.Misc (mapSndM)
+import           Language.Fixpoint.Misc       (mapSnd)
 
 import           Data.List                (foldl', isInfixOf)
 import           Control.Applicative      ((<$>))
+
+import qualified Data.List as L
 
 transformRecExpr :: CoreProgram -> CoreProgram
 transformRecExpr cbs
@@ -37,8 +41,36 @@ transformRecExpr cbs
   =  {-trace "new cbs"-} pg 
   | otherwise 
   = error (showPpr pg ++ "Type-check" ++ showSDoc (pprMessageBag e))
-  where pg     = evalState (transPg cbs) initEnv
+  where pg0    = evalState (transPg cbs) initEnv
         (_, e) = lintCoreBindings [] pg
+        pg     = inlineFailCases pg0
+
+
+inlineFailCases pg = substExpr [] su pg'
+  where 
+    (pg', su) = runState act emptySubst
+    act       = mapM (go []) pg 
+    go su (Rec xes)    = Rec (mapSnd (go' su) <$> xes)
+    go su (NonRec x e) = NonRec x (go' su e)
+
+    go' su (App (Var x) _)       | isFailId x, Just e <- getFailExpr x su = e  
+    go' su (Let (NonRec x ex) e) | isFailId x   = go' (addFailExpr x ex su) e
+
+    go' su (App e1 e2)      = App (go' su e1) (go' su e2)
+    go' su (Lam x e)        = Lam x (go' su e)
+    go' su (Let xs e)       = Let (go su xs) (go' su e)
+    go' su (Case e x t alt) = Case (go' su e) x t (goalt su <$> alt) 
+    go' su (Cast e c)       = Cast (go' su e) c
+    go' su (Tick t e)       = Tick t (go' su e)
+    go' su e = e
+
+    goalt su (c, xs, e)     = (c, xs, go' su e)
+
+    isFailId x  = isLocalId x && L.isPrefixOf "fail" (show x)
+    getFailExpr = L.lookup
+
+    addFailExpr x (Lam _ e) su = (x, e):su 
+    addFailExpr x e         _  = error "Internal Error"
 
 isTypeError s | isInfixOf "Non term variable" (showSDoc s) = False
 isTypeError _ = True
