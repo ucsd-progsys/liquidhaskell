1381c294726c69a9eff26be6a8f120ba49fe6a67
Eric Seidel <gridaphobe@gmail.com>
2014-11-17 10:28:23 -0800

    add fixpoint.native-i386-linux

--- Setup.hs
+++ Setup.hs
@@ -4,37 +4,48 @@ import Distribution.PackageDescription
 import Distribution.Simple
 import Distribution.Simple.LocalBuildInfo
 import Distribution.Simple.Setup
+import Distribution.System
+import System.Directory
 import System.Environment
 import System.Exit
+import System.FilePath
 import System.Process
 
-main         = defaultMainWithHooks fixHooks
+main       = defaultMainWithHooks fixHooks
   where
-    fixHooks = simpleUserHooks { postBuild = buildFixpoint
-                               , postCopy = copyFixpoint
-                               , postInst = copyFixpoint
-                               }
+  fixHooks = simpleUserHooks { postBuild = buildFixpoint
+                             , postCopy = copyFixpoint
+                             , postInst = copyFixpoint
+                             }
 
-buildFixpoint _ _ pkg lbi
-  = do setEnv "Z3MEM" (show z3mem)
-       executeShellCommand "./configure"
-       executeShellCommand "./build.sh"
-       executeShellCommand "chmod a+x external/fixpoint/fixpoint.native "
+copyFixpoint _ _ pkg lbi = do
+  copyFile fixpoint bin
   where
-    allDirs     = absoluteInstallDirs pkg lbi NoCopyDest
-    binDir      = bindir allDirs ++ "/"
-    flags       = configConfigurationsFlags $ configFlags lbi
-    z3mem       = fromMaybe False $ lookup (FlagName "z3mem") flags
+  allDirs     = absoluteInstallDirs pkg lbi NoCopyDest
+  bin         = bindir allDirs </> "fixpoint.native"
+  fixpoint    = "external" </> "fixpoint" </> "fixpoint.native"
+             ++ if build then "" else "-" ++ system
+  system      = case hostPlatform lbi of
+                  Platform I386 Linux -> "i386-linux"
+                  Platform X86_64 Linux -> "x86_64-linux"
+                  Platform X86_64 OSX -> "x86_64-darwin"
+                  Platform _      Windows -> "i686-w64-mingw32"
+                  _ -> error "We don't have a prebuilt fixpoint.native for your system, please install with -fbuild-external (requires ocaml)"
+  flags       = configConfigurationsFlags $ configFlags lbi
+  build       = fromMaybe False $ lookup (FlagName "build-external") flags
 
-copyFixpoint _ _ pkg lbi
-  = do executeShellCommand $ "cp external/fixpoint/fixpoint.native " ++ binDir
-       when z3mem $
-         executeShellCommand $ "cp external/z3/lib/libz3.* "         ++ binDir
+
+buildFixpoint _ _ pkg lbi = when build $ do
+  setEnv "Z3MEM" (show z3mem)
+  executeShellCommand "./configure"
+  executeShellCommand "./build.sh"
+  executeShellCommand "chmod a+x external/fixpoint/fixpoint.native "
   where
-    allDirs     = absoluteInstallDirs pkg lbi NoCopyDest
-    binDir      = bindir allDirs ++ "/"
-    flags       = configConfigurationsFlags $ configFlags lbi
-    z3mem       = fromMaybe False $ lookup (FlagName "z3mem") flags
+  allDirs     = absoluteInstallDirs pkg lbi NoCopyDest
+  binDir      = bindir allDirs ++ "/"
+  flags       = configConfigurationsFlags $ configFlags lbi
+  z3mem       = fromMaybe False $ lookup (FlagName "z3mem") flags
+  build       = fromMaybe False $ lookup (FlagName "build-external") flags
 
 executeShellCommand cmd   = putStrLn ("EXEC: " ++ cmd) >> system cmd >>= check
   where
