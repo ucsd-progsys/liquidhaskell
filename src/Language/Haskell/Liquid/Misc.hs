1c12a3f00e0fd0d10173954c803f391d4b7e0def
Eric Seidel <gridaphobe@gmail.com>
2014-11-17 16:35:52 -0800

    ugh, pruneUnsortedReft nonsense

--- src/Language/Haskell/Liquid/Misc.hs
+++ src/Language/Haskell/Liquid/Misc.hs
@@ -56,6 +56,10 @@ getHqBotPath  = getDataFileName $ "include" </> "Bot.hquals"
 maximumWithDefault zero [] = zero
 maximumWithDefault _    xs = maximum xs
 
+{-@ type ListN a N = {v:[a] | len v = N} @-}
+{-@ type ListL a L = ListN a (len L) @-}
+
+{-@ safeZipWithError :: _ -> xs:[a] -> ListL b xs -> ListL (a,b) xs / [xs] @-}
 safeZipWithError msg (x:xs) (y:ys) = (x,y) : safeZipWithError msg xs ys
 safeZipWithError _   []     []     = []
 safeZipWithError msg _      _      = errorstar msg
