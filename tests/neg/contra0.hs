89137ea4c70baec5582dafbe3225529eeb17fd4d
nikivazou <nikivazou@gmail.com>
2014-11-20 16:46:27 -0800

    adding the test

--- tests/neg/contra0.hs
+++ tests/neg/contra0.hs
@@ -6,6 +6,10 @@ module Foo () where
 import Language.Haskell.Liquid.Prelude (liquidAssert)
 import Data.IORef
 
+
+{-@ data variance IO bivariant @-}
+{-@ data variance IORef bivariant @-}
+
 job :: IO () 
 job = do
   p <- newIORef (0 :: Int)
