8dfbfe3144d95ae0efdb975e42ccf8e4d335232b
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-11-21 09:31:46 -0500

    adding contra0 positive test too

---
+++ tests/pos/contra0.hs
@@ -0,0 +1,18 @@
+{-@ LIQUID "--no-termination" @-}
+{-@ LIQUID "--short-names"    @-}
+
+module Foo () where
+
+import Language.Haskell.Liquid.Prelude (liquidAssert)
+import Data.IORef
+
+
+{-@ data variance IO bivariant @-}
+{-@ data variance IORef bivariant @-}
+
+job :: IO () 
+job = do
+  p <- newIORef (0 :: Int)
+  writeIORef p 10
+  v <- readIORef p
+  liquidAssert (v >= 0) $ return ()
