9b9b32406b4757d5da950d0e0004bc0e8105b7db
nikivazou <nikivazou@gmail.com>
2014-11-24 12:13:38 -0800

    simplify prop measures

--- tests/todo/propmeasure.hs
+++ tests/todo/propmeasure.hs
@@ -7,23 +7,6 @@ module PropMeasure where
 myhead (x:_) = x
 
 {-@ measure nonEmpty @-}   
-nonEmpty (x:xs) = true 
-nonEmpty []     = false
-
--- | Perhaps its easiest to model `Prop` with a type like so-
-
-data Prop = TT | FF
-
-true :: Prop
-true = TT 
-
-false :: Prop
-false = FF 
-
-and, or :: Prop -> Prop -> Prop
-and = undefined
-or  = undefined
-
-not :: Prop -> Prop
-not = undefined
+nonEmpty (x:xs) = True 
+nonEmpty []     = False
 
