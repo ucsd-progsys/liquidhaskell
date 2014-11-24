1b1f111f66faf44da7a2685cb8e944d2be449ab7
nikivazou <nikivazou@gmail.com>
2014-11-23 18:25:26 -0800

    edit pairMeasure

--- tests/neg/PairMeasure0.hs
+++ tests/neg/PairMeasure0.hs
@@ -5,10 +5,14 @@ module Foo () where
   @-}
 
 {-@ type Pair a b   = {v0 : ({v:a | v = (getfst v0)}, b) | true } @-}
-{-@ type OPList a b = [(Pair a b)]<\h -> {v: (a, b) | (getfst v) >= (getfst h)}> @-}
+{-@ type OPList a b = [(Pair a b)]<\h -> {v: (Pair a b) | (getfst v) >= (getfst h)}> @-}
 {-@ type OList a    = [a]<\h -> {v: a | (v >= h)}> @-}
 
+
+-- This is Unsafe, as refinements in Predicate parameters (i.e., Pair a b)
+-- are lost, so application `getFsts` cannot be proven safe
 {-@ getFsts          :: OPList a b -> OList a @-}
+getFsts :: [(a, b)] -> [a]
 getFsts []           = [] 
 getFsts ((x,_) : xs) = x : getFsts xs
 
