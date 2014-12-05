593f6fed40c98d1b0819ff2942abcf4db6db0c20
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-12-05 13:28:54 -0800

    misc edits

---
+++ docs/tutorial/src/03-poly.hs
@@ -0,0 +1,29 @@
+Polymorphism
+------------
+
+\begin{comment}
+
+\begin{code}
+{-@ LIQUID "--short-names" @-}
+{-@ LIQUID "--no-termination" @-}
+module Intro where
+
+import Prelude hiding                   (abs)
+import Language.Haskell.Liquid.Prelude  (liquidAssert)
+
+main = undefined
+\end{code}
+
+\end{comment}
+
+
+Containers
+----------
+
++ Lists
+
+Higher-Order Functions
+----------------------
+
++ map
++ fold
