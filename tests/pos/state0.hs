191554974397fca5b214eb5d8cb77f1d7359d659
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-12-08 13:08:13 -0800

    lifting bug

--- tests/pos/state0.hs
+++ tests/pos/state0.hs
@@ -6,6 +6,8 @@ import State
 {-@ incr4 :: ST <{\v -> (v >= 0)}, {\xx v -> ((xx>=0) && (v>=0))}> Int Int @-}
 incr4 :: ST Int Int
 incr4 = fresh `bindST` \_ -> 
+        fresh `bindST` \_ -> 
+        fresh `bindST` \_ -> 
         fresh  
 
 
