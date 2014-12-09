078812cba39108c28c8a07a0dbaefbc0d7b85a95
nikivazou <nikivazou@gmail.com>
2014-12-08 20:54:24 -0800

    remove fixme0

--- tests/pos/fixme0.hs
+++ tests/pos/fixme0.hs
@@ -5,15 +5,15 @@ module Fixme where
 
 
 binderContainsV ::  Binder n -> Bool
-binderContainsV B = True
+binderContainsV B     = True
+binderContainsV (M x) = containsV x
 
-data Binder n = B
-data TT n = P Int Int (TT n) | V Int | Other | Bind Int (Binder n) (TT n)
+data Binder n = B | M (TT n)
+data TT n     = V Int | Other | Bind (Binder n) (TT n)
 
 containsV :: TT n -> Bool
-containsV (P nt n ty)     = containsV ty
 containsV (V i)           = True
-containsV (Bind n b body) = (binderContainsV b) || (containsV body)
+containsV (Bind b body) = (binderContainsV b) || (containsV body)
 -- containsV (App f arg)     = (containsV f) || (containsV arg)
 -- containsV (Proj tm i)     = containsV tm
 containsV _               = False
