2160940ba4f3f6e8d0b6ed5b1389d351a9a7f7b4
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-12-17 17:20:16 -0800

    Adding termination/lifting benchmark

--- tests/todo/TreeMap.hs
+++ tests/todo/TreeMap.hs
@@ -6,7 +6,7 @@ import Language.Haskell.Liquid.Prelude
 
 data Tree a = Leaf a | Node [Tree a] 
 
-{-@ data Tree a = Leaf (xx :: a) | Node (yy :: {vv : [{v:Tree a | size v < sizes vv}] | true}) @-}
+{-@ data Tree a = Leaf (xx :: a) | Node (yy :: {mickeymouse : [{v:Tree a | size v < sizes mickeymouse}] | true}) @-}
 
 {-@ measure size  @-}
 {-@ measure sizes  @-}
@@ -14,32 +14,40 @@ data Tree a = Leaf a | Node [Tree a]
 {-@ invariant {v: [Tree a] | sizes v >= 0  } @-}
 {-@ invariant {v: Tree a | size v >= 0  } @-}
 
-{-@ size           :: x:Tree a -> Nat / [size x, 0] @-}
+{-@ size           :: t:Tree a -> {v:Nat | v = size t} / [size t, 0] @-}
 size :: Tree a -> Int
 size (Leaf _)  = 1
 size (Node xs) = 1 + sizes xs
 
-{-@ sizes         :: xs:[Tree a] -> Nat / [sizes xs, len xs] @-}
+{-@ sizes         :: xs:[Tree a] -> {v:Nat | v = sizes xs} / [sizes xs, len xs] @-}
 sizes :: [Tree a ] -> Int
 sizes []      = 0
 sizes (t:ts)  = size t + sizes ts
 
 {- data Tree a [sizes] @-}
 
+foo tt = case tt of
+           Leaf x  -> () 
+           Node ts -> liquidAssert (1 + sizes ts == size tt) ()
+
 {-@ tmap :: _ -> tt:Tree a -> Tree a / [size tt] @-}
 tmap f tt = case tt of
-             Leaf x  -> Leaf (x)
-             Node ts -> Node (goo tt ts) -- [liquidAssert (size t < size tt) t | t <- ts]
-
-
+             Leaf x  -> Leaf x
+             Node ts -> Node (goo tt ts)
 
 {-@ goo :: tt:Tree a -> [{v: Tree a | size v < size tt}] -> [Tree a] @-}
 goo :: Tree a -> [Tree a] -> [Tree a]
 goo tt [] = []
 goo tt (t:ts) = t : goo tt ts
 
+
+             -- Node ts -> liquidAssert (sizes ts < size tt) (Node [])
+             -- Node ts -> Node [liquidAssert (size t < size tt) t | t <- ts]
+             -- Node ts -> Node [liquidAssert (size t < sizes ts) t | t <- ts]
+
+
 {- maps :: (a -> b) -> tt:Tree a -> ts:[{v:Tree a | size v < size tt}] -> [Tree b] / [size tt, len ts] @-} 
 -- maps _ _  []     = []
 -- maps f tt (t:ts) = tmap f t : maps f tt ts
 
-{-@ qualif SZ(v:Tree a, x:Tree a): size v < size x @-}
+{-@ qualif SZ(v:a, x:b): size v < size x @-}
