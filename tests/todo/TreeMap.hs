7535d025e3741975c4881f8f7295f8e296610664
nikivazou <nikivazou@gmail.com>
2014-12-17 17:11:47 -0800

    adding lemmasize

--- tests/todo/TreeMap.hs
+++ tests/todo/TreeMap.hs
@@ -11,16 +11,22 @@ data Tree a = Leaf a | Node [Tree a]
 {-@ measure size  @-}
 {-@ measure sizes  @-}
 
+{-@ invariant {v: [Tree a] | (sizes v) >= (size (head v))  } @-}
 {-@ invariant {v: [Tree a] | sizes v >= 0  } @-}
 {-@ invariant {v: Tree a | size v >= 0  } @-}
 
 
-{-@ size           :: x:Tree a -> Nat / [size x, 0] @-}
+
+{-@ measure head :: [a] -> a 
+    head (x:xs) = x
+  @-}
+
+{-@ size           :: x:Tree a -> {v:Nat | v = size x} / [size x, 0] @-}
 size :: Tree a -> Int
 size (Leaf _)  = 1
 size (Node xs) = 1 + sizes xs
 
-{-@ sizes         :: xs:[Tree a] -> Nat / [sizes xs, len xs] @-}
+{-@ sizes         :: xs:[Tree a] -> {v:Nat | v = sizes xs} / [sizes xs, len xs] @-}
 sizes :: [Tree a ] -> Int
 sizes []      = 0
 sizes (t:ts)  = size t + sizes ts
@@ -30,8 +36,12 @@ sizes (t:ts)  = size t + sizes ts
 {-@ tmap :: _ -> tt:Tree a -> Tree b / [size tt, 1, 0] @-}
 -- tmap f tt = case tt of
 tmap f (Leaf x) = Leaf (f x)
-tmap f tt@(Node ts) = Node (goo f tt ts) -- [liquidAssert (size t < size tt) t | t <- ts]
+tmap f tt@(Node ts) = Node (goo f (Node ts) (lemmasize (Node ts))) -- [liquidAssert (size t < size tt) t | t <- ts]
 
+{-@ Lazy lemmasize @-}
+{-@ lemmasize :: tt:Tree a -> [{v:Tree a | size v < size tt}] @-}
+lemmasize :: Tree a  -> [Tree a]
+lemmasize (Node (t:ts)) = t : lemmasize (Node ts)
 
 
 {-@ goo :: (a -> b) -> tt:Tree a -> ts:[{v: Tree a | size v < size tt}] -> [Tree b] / [size tt, 0, len ts] @-}
