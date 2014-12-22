748a922e7c1e6eabef039033dbecb158d961e062
nikivazou <nikivazou@gmail.com>
2014-12-22 15:19:07 -0800

    reasoning about state\'s value

--- tests/pos/StateConstraints.hs
+++ tests/pos/StateConstraints.hs
@@ -3,50 +3,76 @@ module Compose where
 
 
 
-data ST s = ST {runState :: s -> s}
+data ST s a = ST {runState :: s -> (a,s)}
 
-{-@ data ST s t <p :: s -> Prop, q :: s -> s -> Prop> = ST (runState :: x:s<p> -> s<q x>) @-}
+{-@ data ST s a <p :: s -> Prop, q :: s -> s -> Prop, r :: s -> a -> Prop> 
+  = ST (runState :: x:s<p> -> (a<r x>, s<q x>)) @-}
 
- {-@ runState :: forall <p :: s -> Prop, q :: s -> s -> Prop>. ST <p, q> s -> x:s<p> -> s<q x> @-}
+ {-@ runState :: forall <p :: s -> Prop, q :: s -> s -> Prop, r :: s -> a -> Prop>. ST <p, q, r> s a -> x:s<p> -> (a<r x>, s<q x>) @-}
 
 
 
-{-@ 
+{-
 cmp :: forall < pref :: s -> Prop, postf :: s -> s -> Prop
               , pre  :: s -> Prop, postg :: s -> s -> Prop
               , post :: s -> s -> Prop
+              , rg   :: s -> a -> Prop
+              , rf   :: s -> b -> Prop
+              , r    :: s -> b -> Prop
               >. 
        {xx:s<pre> -> w:s<postg xx> -> s<postf w> -> s<post xx>}
        {ww:s<pre> -> s<postg ww> -> s<pref>}
-       (ST <pref, postf> s)
-    -> (ST <pre, postg> s)
-    -> (ST <pre, post> s)
+       (ST <pre, postg, rg> s a)
+    -> (ST <pref, postf, rf> s b)
+    -> (ST <pre, post, r> s b)
 @-}
 
-cmp :: (ST s)
-    -> (ST s)
-    -> (ST s)
+cmp :: (ST s a)
+    -> (ST s b)
+    -> (ST s b)
 
-cmp (ST f) (ST g) = ST (\x -> f (g x))    
+cmp (ST g) (ST f) = ST (\x -> case g x of {(_, s) -> f s})    
+
+{-@ 
+bind :: forall < pref :: s -> Prop, postf :: s -> s -> Prop
+              , pre  :: s -> Prop, postg :: s -> s -> Prop
+              , post :: s -> s -> Prop
+              , rg   :: s -> a -> Prop
+              , rf   :: s -> b -> Prop
+              , r    :: s -> b -> Prop
+              , pref0 :: a -> Prop 
+              >. 
+       {x:s<pre> -> a<rg x> -> a<pref0>}      
+       {x:s<pre> -> y:s<postg x> -> b<rf y> -> b<r x>}
+       {xx:s<pre> -> w:s<postg xx> -> s<postf w> -> s<post xx>}
+       {ww:s<pre> -> s<postg ww> -> s<pref>}
+       (ST <pre, postg, rg> s a)
+    -> (a<pref0> -> ST <pref, postf, rf> s b)
+    -> (ST <pre, post, r> s b)
+@-}
+
+bind :: (ST s a)
+    -> (a -> ST s b)
+    -> (ST s b)
+
+bind (ST g) f = ST (\x -> case g x of {(y, s) -> (runState (f y)) s})    
+
+{-@ incr :: ST <{\x -> x >= 0}, {\x v -> v = x + 1}, {\x v -> v = x}>  Nat Nat @-}
+incr :: ST Int Int
+incr = ST $ \x ->  (x, x + 1)
+
+{-@ incr2 :: ST <{\x -> x >= 0}, {\x v -> v = x + 2}, {\x v -> v = x + 1}>  Nat Nat @-}
+incr2 :: ST Int Int
+incr2 = bind incr (\_ -> incr)
 
 
-
-{-@ incr :: ST <{\x -> x >= 0}, {\x v -> v = x + 1}>  Nat   @-}
-incr :: ST Int 
-incr = ST $ \x ->  x + 1
-
-
-{-@ incr2 :: ST <{\x -> x >= 0}, {\x v -> v = x + 2}>  Nat  @-}
-incr2 :: ST Int 
-incr2 = cmp incr incr
-
-{-@ incr3 :: ST <{\x -> x >= 0}, {\x v -> v = x + 3}>  Nat  @-}
-incr3 :: ST Int 
-incr3 = cmp (cmp incr incr) incr
+{-@ incr3 :: ST <{\x -> x >= 0}, {\x v -> v = x + 3}, {\x v -> v = x + 2}>  Nat Nat @-}
+incr3 :: ST Int Int
+incr3 = bind (bind incr (\_ -> incr)) (\_ -> incr)
 
 
-foo :: Int
-{-@ foo :: {v:Nat |  v = 3} @-}
+foo :: (Int, Int)
+{-@ foo :: ({v:Nat |  v = 2}, {v:Nat |  v = 3}) @-}
 foo = (runState incr3) 0
 
 
