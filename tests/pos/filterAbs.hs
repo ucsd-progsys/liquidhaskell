c964011552b4913cc87cdbf45aa3fc4d67564144
Ranjit Jhala <jhala@cs.ucsd.edu>
2014-12-22 20:09:22 -0800

    Update filterAbs.hs

--- tests/pos/filterAbs.hs
+++ tests/pos/filterAbs.hs
@@ -27,13 +27,18 @@ isNeg :: Int -> Bool
 isNeg n = n < 0
 
 
--- Now the below *should* work with
+-- | `positives` works by instantiating:
 -- p := \v   -> 0 < v
 -- q := \x v -> Prop v <=> 0 < 0
 
 
 {-@ positives :: [Int] -> [{v:Int | v > 0}] @-}
 positives xs = filter isPos xs
+
+
+-- | `negatives` works by instantiating:
+-- p := \v   -> 0 > v
+-- q := \x v -> Prop v <=> 0 > 0
 
 {-@ negatives :: [Int] -> [{v:Int | v < 0}] @-}
 negatives xs = filter isNeg xs
