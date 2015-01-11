50bc2c3eef9e5590bb2476652ebadb07dac290db
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-01-11 11:58:04 -0800

    would be nice to have partial measures

---
+++ tests/todo/partialmeasure.hs
@@ -0,0 +1,26 @@
+module Head where
+
+-- Note: `partialmeasureOld.hs` works fine 
+
+{-@ cons :: x:a -> _ -> {v:[a] | hd v = x} @-}
+cons x xs = x : xs
+
+{-@ test :: {v:_ | hd v = 0} @-}
+test :: [Int]
+test =  cons 0 [1,2,3,4]
+
+{-@ measure hd @-}
+hd       :: [a] -> a
+hd (x:_) = x
+hd []    = undefined
+
+-- Instead of rejecting, can we just default to "un-refined" constructors?
+
+-- Strengthened constructors
+--   data [a] where
+--     []  :: [a]    -- as before
+--     (:) :: x:a -> xs:[a] -> {v:[a] | hd v = x}
+
+
+
+
