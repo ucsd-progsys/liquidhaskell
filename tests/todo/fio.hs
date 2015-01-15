db6d24e1b32dbb89ca7ad168d6ce5368db9a3b7a
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-01-15 12:21:32 -0800

    add io example

---
+++ tests/todo/fio.hs
@@ -0,0 +1,47 @@
+module FIO where
+
+import Prelude hiding (read)
+
+{-@ data FIO a <pre :: World -> Prop, post :: World -> World -> a -> Prop> = FOO @-}
+data FIO a  = FOO
+
+data World  = W
+
+
+-- | forall m k v k'. sel (upd m k v) k' = if (k == k') then v else sel m k'
+
+{-@ measure sel :: World -> FilePath -> Int          @-}
+{-@ measure upd :: World -> FilePath -> Int -> World @-}
+
+
+{-@ open :: fp:FilePath -> FIO <{\w0 -> true}, {\w0 w1 r -> w1 = w0  && sel w0 fp = 1}> () @-}
+open :: FilePath -> FIO () 
+open = undefined
+
+{-@ read :: fp:FilePath -> FIO <{\w0 -> sel w0 fp = 1}, {\w0 w1 r -> w1 = w0}> String @-}
+read :: FilePath -> FIO String
+read = undefined
+
+--------------------------
+
+
+bind = undefined  -- TODO
+ret  = undefined  -- TODO
+
+
+ok1 f = open f
+
+ok2 f = open f `bind` \_ -> read f
+
+instance Monad FIO where
+  (>>=)  = bind
+  return = ret
+
+{-@ ok3   :: FilePath -> FIO () @-}
+ok3 f = do open f
+           read f
+
+{-@ fail1 :: FilePath -> FIO () @-}
+fail1 f   = read f
+
+
