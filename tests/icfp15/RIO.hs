ddddf06c14922667b544febeee07484e40208ee2
nikivazou <nikivazou@gmail.com>
2015-01-19 15:55:40 -0800

    fix return

--- tests/icfp15/RIO.hs
+++ tests/icfp15/RIO.hs
@@ -20,8 +20,9 @@ instance Monad RIO where
                , post :: World -> b -> World -> Prop>.
        {w:World<pre> -> x:a -> World<post1 w x> -> World<pre2>}        
        {w:World<pre> -> y:a -> w2:World<post1 w y> -> x:b -> World<post2 w2 x> -> World<post w x>}        
+       {w:World -> x:a -> w2:World<post1 w x> -> {v:a | v = x} -> a<p>}
        RIO <pre, post1> a
-    -> (a -> RIO <pre2, post2> b)
+    -> (a<p> -> RIO <pre2, post2> b)
     -> RIO <pre, post> b ;
  >>  :: forall < pre   :: World -> Prop 
                , pre2  :: World -> Prop 
