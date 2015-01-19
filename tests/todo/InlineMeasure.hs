3d7d13474f80d6fbb7c6b6028410d4ab4d003f94
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-01-19 14:37:38 -0800

    adding wtf test case

--- tests/todo/InlineMeasure.hs
+++ tests/todo/InlineMeasure.hs
@@ -2,15 +2,26 @@
 
 module InlineMeasures where
 
-mmax       :: Int -> Int -> Int
-mmax x y   = if x > y then x else y
+{-@ measure mmax @-}
+mmax       :: (Int, Int) -> Int
+mmax (x,y) = if x > y then x else y
 
 {-@ measure foo @-}
-foo :: [Int] -> Int
+foo        :: [Int] -> Int
 foo []     = 0
 foo (x:xs) = if (x > 0) then x else 0 
 
 {-@ measure bar @-}
-bar :: [Int] -> Int
-bar [] = 0
-bar (x:xs) = mmax x 0
+bar        :: [Int] -> Int
+bar []     = 0
+bar (x:xs) = y + z where y = x
+                         z = 10
+
+{-@ measure llen @-}
+llen        :: [a] -> Int
+llen []     = 0
+llen (x:xs) = 1 + z where z = llen xs
+
+{-@ goo :: _ -> xs:_ -> {v:_ | llen v = llen xs } @-}
+goo f []     = []
+goo f (x:xs) = f x : goo f xs 
