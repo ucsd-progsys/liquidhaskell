9a3f623d496c83e0b15454711dceda9158b5e5d4
nikivazou <nikivazou@gmail.com>
2015-01-19 13:56:14 -0800

    done with higher order

--- tests/icfp15/ex1.hs
+++ tests/icfp15/ex1.hs
@@ -10,27 +10,13 @@ ifM  :: forall < pre   :: World -> Prop
                  , post1 :: World -> () -> World -> Prop
                  , post :: World -> () -> World -> Prop>.
        {w:World<pre> -> y:() -> w2:World<pre> -> x:() -> World<post1 w2 x> -> World<post w x>}             
-       {w:World<pre> -> x:() -> {v:World | v = w} -> World<post1 w x>}
-
-       {w:World -> w2:World -> x:a -> World<post1 w2 x> -> World<post w x>}        
-       {w:World<pre> -> x:() -> {v:World | v = w} -> World<post w x>}
-       {w:World<pre> -> x:() -> World<post1 w x> -> World<pre>}        
-       {w:World<pre> -> y:() -> w2:World<post1 w y> -> x:() -> World<post w2 x> -> World<post w x>}        
-
-
-       {w:World -> w2:World -> x:a -> World<post w2 x> -> World<post w x>}        
-       {w:World<pre> -> x:() -> {v:World | v = w} -> World<post w x>}
-       {w:World<pre> -> x:() -> World<post w x> -> World<pre>}        
-       {w:World<pre> -> y:() -> w2:World<post w y> -> x:() -> World<post w2 x> -> World<post w x>}        
-
-
-
        RIO <pre, \w1 x -> {v:World<pre> | true}> Bool 
        -> RIO <pre, post1> () 
+       -> RIO <pre, post1> ()  
        -> RIO <pre, post> ()
 @-}
-ifM :: RIO Bool -> RIO () -> RIO ()
-ifM guard e1 = do {g <- guard ; if g then do {e1; ifM guard e1} else return ()}
+ifM :: RIO Bool -> RIO () -> RIO () -> RIO ()
+ifM guard e1 e2 = do {g <- guard ; if g then e1 else e2}
 
 
 
--- tests/icfp15/ex2.hs
+++ tests/icfp15/ex2.hs
@@ -6,31 +6,19 @@ import RIO
 
 
 {-@ 
-ifM  :: forall < pre   :: World -> Prop 
-                 , post1 :: World -> () -> World -> Prop
-                 , post :: World -> () -> World -> Prop>.
+whileM  :: forall < pre   :: World -> Prop 
+                  , post1 :: World -> () -> World -> Prop
+                  , post :: World -> () -> World -> Prop>.
        {w:World<pre> -> y:() -> w2:World<pre> -> x:() -> World<post1 w2 x> -> World<post w x>}             
        {w:World<pre> -> x:() -> {v:World | v = w} -> World<post1 w x>}
-
-       {w:World -> w2:World -> x:a -> World<post1 w2 x> -> World<post w x>}        
-       {w:World<pre> -> x:() -> {v:World | v = w} -> World<post w x>}
-       {w:World<pre> -> x:() -> World<post1 w x> -> World<pre>}        
-       {w:World<pre> -> y:() -> w2:World<post1 w y> -> x:() -> World<post w2 x> -> World<post w x>}        
-
-
-       {w:World -> w2:World -> x:a -> World<post w2 x> -> World<post w x>}        
        {w:World<pre> -> x:() -> {v:World | v = w} -> World<post w x>}
        {w:World<pre> -> x:() -> World<post w x> -> World<pre>}        
-       {w:World<pre> -> y:() -> w2:World<post w y> -> x:() -> World<post w2 x> -> World<post w x>}        
-
-
-
        RIO <pre, \w1 x -> {v:World<pre> | true}> Bool 
        -> RIO <pre, post1> () 
        -> RIO <pre, post> ()
 @-}
-ifM :: RIO Bool -> RIO () -> RIO ()
-ifM guard e1 = do {g <- guard ; if g then do {e1; ifM guard e1} else return ()}
+whileM :: RIO Bool -> RIO () -> RIO ()
+whileM guard e1 = do {g <- guard ; if g then do {e1; whileM guard e1} else return ()}
 
 
 
