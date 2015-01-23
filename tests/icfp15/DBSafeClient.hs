33af3454e1b6c92aff52e1d177966198b950646e
nikivazou <nikivazou@gmail.com>
2015-01-23 12:06:13 -0800

    more database

---
+++ tests/icfp15/DBSafeClient.hs
@@ -0,0 +1,28 @@
+module DBSafeClient where
+
+import DataBase
+
+import Prelude hiding (product)
+
+data Value = VA | VB | VC | VD
+
+data Tag = TA | TB | TC | TD   
+         deriving Eq
+
+rab :: Dict Tag Value
+{-@ rab :: Dict <{\v -> v = TA || v = TB }, {\i v -> (i == TA => v = VA) && (i == TB => v = VB)} > Tag Value @-}
+rab = extend TB VB $ extend TA VA empty
+
+rcd :: Dict Tag Value
+{-@ rcd :: Dict <{\v -> v = TC || v = TD }, {\i v -> (i == TC => v = VC) && (i == TD => v = VD)} > Tag Value @-}
+rcd = extend TC VC $ extend TD VD empty
+
+
+rabcd :: Dict Tag Value
+{-@ rabcd :: Dict <{\v -> v = TA || v = TB || v = TC || v = TD }, {\i v -> (i == TC => v = VC) && (i == TD => v = VD)} > Tag Value @-}
+rabcd = product rab rcd
+
+
+rfail :: Dict Tag Value
+rfail = undefined -- product rab rabcd
+
--- tests/icfp15/DataBase.hs
+++ tests/icfp15/DataBase.hs
@@ -2,13 +2,11 @@ module DataBase  where
 
 import qualified Data.Set
 
-data Value = VA | VB | VC | VD
+import Prelude hiding (product)
 
-data Tag = TA | TB | TC | TD   
-         deriving Eq
 
-data Dict key val = D {dom :: [key], dfun :: key -> val}         
-
+data Dict key val = D {dom :: [key], dfun :: key -> val}  
+ 
 {-@ dom :: forall <domain :: key -> Prop, range :: key -> val -> Prop>. 
            x:Dict <domain, range> key val  -> {v:[key<domain>] | v = dom x}
   @-}
@@ -20,9 +18,12 @@ data Dict key val = D {dom :: [key], dfun :: key -> val}
 
 {-@ data Dict key val <domain :: key -> Prop, range :: key -> val -> Prop>
   = D ( dom  :: [key<domain>])
-      ( dfun :: i:{v:key | Set_mem v (listElts dom)} -> val<range i>)      
+      ( dfun :: i:{v:key | Set_mem v (listElts dom)} -> val<range i>)     
   @-} 
 
+{-@ D :: forall <domain :: key -> Prop, range :: key -> val -> Prop>.
+         [key<domain>] -> (i:{v:key | Set_mem v (listElts dom)} -> val<range i>) -> Dict <domain, range> key val    
+  @-} 
 
 {-@ empty :: Dict <{\v -> false}, {\k v -> false}> key val @-}
 empty :: Dict key val
@@ -37,52 +38,50 @@ extend :: Eq key => key -> val -> Dict key val -> Dict key val
 extend k v (D ks f) = D (k:ks) (\i -> if i == k then v else f i)
 
 
+{-@ assume (Prelude.++) :: xs:[a] -> ys:[a] -> {v:[a] | listElts v = Set_cup (listElts xs) (listElts ys)} @-}
 
-rab :: Dict Tag Value
-{-@ rab :: Dict <{\v -> v = TA || v = TB }, {\i v -> (i == TA => v = VA) && (i == TB => v = VB)} > Tag Value @-}
-rab = extend TB VB $ extend TA VA empty
-
-rcd :: Dict Tag Value
-{-@ rcd :: Dict <{\v -> v = TC || v = TD }, {\i v -> (i == TC => v = VC) && (i == TD => v = VD)} > Tag Value @-}
-rcd = extend TC VC $ extend TD VD empty
+{-@ assume Prelude.elem :: x:a -> xs:[a] -> {v:Bool | Prop v <=> Set_mem x (listElts xs)} @-}
 
 
+product :: Eq key => Dict key val -> Dict key val -> Dict key val
+{-@ product :: forall <domain1 :: key -> Prop, 
+                       domain2 :: key -> Prop,
+                       domain  :: key -> Prop,
+                       range1  :: key -> val -> Prop,
+                       range2  :: key -> val -> Prop,
+                       range   :: key -> val -> Prop>.
+                       {key<domain1> -> key<domain>}
+                       {key<domain2> -> key<domain>}
+                       {k1: key<domain1> -> k2:key<domain2> -> {v:key | v = k1 && v = k2} -> {v:key | false}}
+                       {k:key<domain1> -> val<range1 k> -> val<range k> }
+                       {k:key<domain2> -> val<range2 k> -> val<range k> }
+               Dict <domain1, range1> key val 
+            -> Dict <domain2, range2> key val 
+            -> Dict <domain,  range > key val 
+  @-}
+product (D ks1 f1) (D ks2 f2) 
+  = let ks = ks1 ++ ks2 in 
+    -- ORDERING IN LETS IS IMPORTANT: ks should be in scope for f
+    let f i = if i `elem` ks1 then f1 (ensuredomain ks1 i) else f2 (ensuredomain ks2 i) in
+    D ks f 
 
+
+{-@ ensuredomain :: forall <p ::a -> Prop>. Eq a => xs:[a<p>] -> x:{v:a | Set_mem v (listElts xs)} -> {v:a<p> | Set_mem v (listElts xs) && v = x} @-}
+ensuredomain :: Eq a => [a] -> a -> a
+ensuredomain (y:ys) x | x == y    = y 
+                      | otherwise = ensuredomain ys x  
 
 
 -------------------------------------------------------------------------------
 -------------------------    HELPERS   ----------------------------------------
 -------------------------------------------------------------------------------
 
-{-@ measure tagToInt @-}
-tagToInt :: Tag -> Int
-tagToInt TA = 1
-tagToInt TB = 2
-tagToInt TC = 3
-tagToInt TD = 4
 
-
-{-@ measure valToInt @-}
-valToInt :: Value -> Int
-valToInt VA = 1
-valToInt VB = 2
-valToInt VC = 3
-valToInt VD = 4
 
 {-@ qual :: xs:[a] -> {v: a | Set_mem v (listElts xs)} @-}
 qual :: [a] -> a
 qual = undefined
 
-
-
-{-@ quall :: ww:Tag -> {v:Value | ww == TA => v = VA} @-}
-quall :: Tag -> Value
-quall = undefined
-
-
-
-
-
-{-@ liquidError :: {v:String | false} -> a @-}
-liquidError :: String -> a
-liquidError = error
+{-@ qual' :: forall <range :: key -> val -> Prop>. k:key -> val<range k> @-}
+qual' :: key -> val
+qual' = undefined
