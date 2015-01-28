aafae18c423dd50d5ce2fcda85adfce03107ade0
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-01-28 11:14:46 -0800

    Aasd

---
+++ tests/todo/coretologic.hs
@@ -0,0 +1,17 @@
+module CoreToLog where
+
+import Data.Set
+
+-- ISSUE: can we please allow things like `empty` to also
+-- appear in type and alias specifications, not just in
+-- measures as in `goo` below?
+
+{-@ type IsEmp a = {v:[a] | listElts v = empty} @-}
+
+{-@ foo :: IsEmp a @-}
+foo = []
+
+{-@ measure goo @-}
+goo        :: (Ord a) => [a] -> Set a
+goo []     = empty
+goo (x:xs) = (singleton x) `union` (goo xs)  
