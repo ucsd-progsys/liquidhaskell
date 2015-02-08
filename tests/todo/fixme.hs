520129e72211b5b92c9c8c7928f64065a1c9a153
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-02-07 18:33:39 -0800

    cannot use inline in data-definition?

--- tests/todo/fixme.hs
+++ tests/todo/fixme.hs
@@ -3,8 +3,9 @@ module InlineMeasure where
 data Zoo a = Z { elts :: [a], sz :: Int }
 
 -- | this is not ok (unbound symbol `boo`)
-{-@ data Zoo a = Z { elts :: [a], sz :: {v: Int | v = boo elts} } @-}
+{-@ data Zoo a = Z { elts :: [a], sz :: {v: Int | IsBoo v elts} } @-}
 
+{-@ predicate IsBoo V E = V = boo E @-}
 -- | this is ok
 
 {-@ type Moo a = {v:Zoo a | sz v = boo (elts v)} @-}
