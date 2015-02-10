ae19d2d055699cce421103442bdb859fa9873b0d
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-02-10 06:42:42 -0800

    set-mem qualification core2logic woes

---
+++ tests/todo/elems.hs
@@ -0,0 +1,13 @@
+module Goo where
+
+import qualified Data.Set as S
+
+data T a = T a
+
+{-@ measure elems @-}
+elems       :: T a -> S.Set a
+elems (T a) = S.singleton a
+
+{-@ member :: x:a -> t:T a -> {v:Bool | Prop v <=> S.member x (elems t)} @-}
+member :: a -> T a -> Bool
+member = undefined
