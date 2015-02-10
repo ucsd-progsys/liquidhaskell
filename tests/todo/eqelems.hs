4e29858c68945b056c6d3eaa0ddbbd2d54cdd4a3
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-02-10 06:58:35 -0800

    set-mem qualification core2logic woes

--- tests/todo/eqelems.hs
+++ tests/todo/eqelems.hs
@@ -8,8 +8,7 @@ data T a = T a
 elems       :: T a -> S.Set a
 elems (T a) = S.singleton a
 
-{-@ member :: x:a -> t:T a -> {v:Bool | Prop v <=> S.member x (elems t)} @-}
-member :: a -> T a -> Bool
-member = undefined
 
-        
+{-@ inline eqelems @-}
+eqelems s t = (elems s) == (elems t)
+         
