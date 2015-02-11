befaa351c7c79bbb2681b6b4c29d8bc02467f509
nikivazou <nikivazou@gmail.com>
2015-02-10 20:47:23 -0800

    update whileM

--- tests/icfp15/ex2.hs
+++ tests/icfp15/ex2.hs
@@ -6,18 +6,17 @@ import RIO
 
 
 
-
 -- THIS TYPE CHECKS, BUT HOW CAN WE ACTUALLY USE IT?
 {-@ 
 whileM  :: forall < pre   :: World -> Prop 
                   , post1 :: World -> () -> World -> Prop
                   , post2 :: World -> Bool -> World -> Prop
                   , post :: World -> () -> World -> Prop>.
-       {b::Bool, w::World<pre>, w2::World<pre>, x::() |- World<post1 w2 x> <: World<post w x>}             
+       {w::World<pre>, w2::World<pre>, x::() |- World<post1 w2 x> <: World<post w x>}             
        {w::World<pre>, x::() |- {v:World | v = w} <: World<post1 w x>}
        {w::World<pre>, x::() |- {v:World | v = w} <: World<post w x>}
-       {w::World<pre>, x::() |- World<post w x> <: World<pre>}
-       {w1::World, x::{v:Bool | Prop v} |- World<post2 w1 x> <: World<pre>}        
+       {w::World<pre>, x::(), xx::Bool |- {v:World<post w x> | Prop xx} <: World<pre>}
+       {w1::World, x::Bool |- World<post2 w1 x> <: World<pre>}        
        RIO <pre, post2> Bool 
        -> RIO <pre, post1> () 
        -> RIO <pre, post> ()
@@ -40,4 +39,4 @@ condM = undefined
 -- NV: TODO
 {- prop :: RIO <{\x -> true}, {\w1 x w2 -> counter1 w2 < counter2 w2}> () @-}
 prop :: RIO ()
-prop = whileM condM incr' 
+prop = whileM condM incr'  
