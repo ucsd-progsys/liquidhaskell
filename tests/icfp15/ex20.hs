dc329d8fd833e9f8abf09b9f80695e153377b619
nikivazou <nikivazou@gmail.com>
2015-02-12 11:05:07 -0800

    conditional brunchs do not work

--- tests/icfp15/ex20.hs
+++ tests/icfp15/ex20.hs
@@ -26,3 +26,25 @@ ifM  :: forall < pre   :: World -> Prop
 @-}
 ifM :: RIO Bool -> RIO () -> RIO () -> RIO ()
 ifM cond e1 e2  = do{ g <- cond; if g then e1 else e2}
+
+
+
+{-@ 
+ifM'  :: forall < pre   :: World -> Prop 
+                  , p :: World -> Bool -> World -> Prop
+                  , r1 :: World -> Prop
+                  , r2 :: World -> Prop
+                  , post1 :: World -> () -> World -> Prop
+                  , post2 :: World -> () -> World -> Prop
+                  , post :: World -> () -> World -> Prop>.                  
+       {x :: {v:Bool | Prop v }, w :: World<pre> |- World<p w x> <: World<r1> }
+       {x :: {v:Bool | not (Prop v) }, w :: World<pre> |- World<p w x> <: World<r2> }
+       {w1::World, y::(), w2::World, x::() |- World<post1 w2 x> <: World<post w1 x>}             
+       {w1::World, y::(), w2::World, x::() |- World<post2 w2 x> <: World<post w1 x>}             
+       RIO <pre, p> Bool 
+       -> RIO <r1, post1> ()
+       -> RIO <r2, post2> ()
+       -> RIO <pre, post> ()
+@-}
+ifM' :: RIO Bool -> RIO () -> RIO () -> RIO ()
+ifM' cond e1 e2  = do{ g <- cond; if g then e1 else e2}
