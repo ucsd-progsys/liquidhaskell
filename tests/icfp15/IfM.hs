ebadc647013c08ffe85b0a033a7ba0dd8b6777e4
nikivazou <nikivazou@gmail.com>
2015-02-13 15:49:44 -0800

    ifM is safe with another Abstract Refinement In State

---
+++ tests/icfp15/IfM.hs
@@ -0,0 +1,50 @@
+module Ex1 where
+
+{-@ LIQUID "--no-termination" @-}
+
+import Language.Haskell.Liquid.Prelude 
+
+{-@ data RIO a <pre :: World -> Prop, post :: World -> a -> World -> Prop, pp:: a -> Prop> 
+  = RIO (rs :: (x:World<pre> -> (a<pp>, World)<\w -> {v:World<post x w> | true}>))
+  @-}
+data RIO a  = RIO {runState :: World -> (a, World)}
+
+{-@ runState :: forall <pre :: World -> Prop, post :: World -> a -> World -> Prop, pp::a -> Prop>. 
+                RIO <pre, post, pp> a -> x:World<pre> -> (a<pp>, World)<\w -> {v:World<post x w> | true}> @-}
+
+data World  = W
+
+{-@ bind :: forall < pre   :: World -> Prop 
+               , pre2  :: World -> Prop 
+               , p     :: a -> Prop
+               , q     :: b -> Prop
+               , post1 :: World -> a -> World -> Prop
+               , post2 :: World -> b -> World -> Prop
+               , post :: World -> b -> World -> Prop>.
+       {w::World<pre>, x::a<p> |- World<post1 w x> <: World<pre2>}        
+       {w::World<pre>, y::a<p>, w2::World<post1 w y>, x::b |- World<post2 w2 x> <: World<post w x>}        
+       RIO <pre, post1, p> a
+    -> (a<p> -> RIO <pre2, post2, q> b)
+    -> RIO <pre, post, q> b @-}
+
+bind :: RIO a -> (a -> RIO b) -> RIO b
+bind (RIO g) f = RIO $ \x -> case g x of {(y, s) -> (runState (f y)) s} 
+
+
+{-@
+ifM  :: forall < pre   :: World -> Prop 
+               , p :: World -> Bool -> World -> Prop
+               , r :: World -> Prop
+               , q :: Bool -> Prop
+               , post1 :: World -> () -> World -> Prop
+               , post  :: World -> () -> World -> Prop>.                  
+       {b :: Bool<q>,   w :: World<pre>    |- World<p w b>      <: World<r>        } 
+       {w1::World<pre>, w2::World<r>, y::()|- World<post1 w2 y> <: World<post w1 y>}
+       RIO <pre, p, q> Bool 
+       -> RIO <r, post1, {\v -> true}> ()
+       -> RIO <r, post1, {\v -> true}> ()
+       -> RIO <pre, post, {\v -> true}> ()
+@-}
+ifM :: RIO Bool -> RIO () -> RIO () -> RIO ()
+ifM cond e1 e2
+  = cond `bind` \g -> if g then e1 else e2 
