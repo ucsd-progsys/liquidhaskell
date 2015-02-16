32572467cd50c68a5e0091d072bacc932070729f
nikivazou <nikivazou@gmail.com>
2015-02-15 21:40:34 -0800

    done with twiceM

--- tests/icfp15/TwiceM.hs
+++ tests/icfp15/TwiceM.hs
@@ -1,32 +1,35 @@
 module TwiceM where
 
+{-@ LIQUID "--short-names" @-}
+
 import RIO
 
-{-@ app :: forall <pre :: World -> Prop, post :: World -> b -> World -> Prop, p :: b -> Prop>.
+{-@ appM :: forall <pre :: World -> Prop, post :: World -> b -> World -> Prop, p :: b -> Prop>.
            (a -> RIO <pre, post, p> b) -> a -> RIO <pre, post, p> b @-}
-app :: (a -> RIO b) -> a -> RIO b
-app f x = f x
+appM :: (a -> RIO b) -> a -> RIO b
+appM f x = f x
 
 
 {-@
-twice  :: forall < pre   :: World -> Prop 
+twiceM  :: forall < pre   :: World -> Prop 
                  , post1 :: World -> a -> World -> Prop
                  , post :: World -> a -> World -> Prop
                  , p :: a -> Prop>.
-                 {w::World<pre> , x::a|- World<post1 w x> <: World<pre>}
+                 {w ::World<pre>, x::a|- World<post1 w x> <: World<pre>}
+                 {w1::World<pre>, y::a, w2::World<post1 w1 y>, w20::{v:World<pre> | v = w2}, x::a |- World<post1 w2 x> <: World<post w1 x>}
        (b -> RIO <pre, post1, p> a)  
      -> b -> RIO <pre, post, p> a 
 @-}
-twice :: (b -> RIO a) -> b -> RIO a
-twice f x = f x >> f x
+twiceM :: (b -> RIO a) -> b -> RIO a
+twiceM f w = let (RIO g) = f w in RIO $ \x -> case g x of {(y, s) -> let ff = \_ -> f w in (runState (ff y)) s} 
 
 
-{- measure counter :: World -> Int @-}
+{-@ measure counter :: World -> Int @-}
 
-{- incr :: RIO <{\x -> counter x >= 0}, {\w1 x w2 -> counter w2 = counter w1 + 1}>  Nat Nat @-}
+{-@ incr :: RIO <{\x -> counter x >= 0}, {\w1 x w2 -> counter w2 = counter w1 + 1}, {\x -> true}>  Nat Nat @-}
 incr :: RIO Int
 incr = undefined
 
-{- incr2 :: RIO <{\x -> counter x >= 0}, {\w1 x w2 -> counter w2 = counter w1 + 2}>  Nat Nat @-}
+{-@ incr2 :: RIO <{\x -> counter x >= 0}, {\w1 x w2 -> counter w2 = counter w1 + 2}, {\x -> true}> Nat Nat @-}
 incr2 :: RIO Int
-incr2 = twice (\_ -> incr) 0
+incr2 = twiceM (\_ -> incr) 0
