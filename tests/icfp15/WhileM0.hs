f7a739214cd2bcd48cd3a341d9ce6462fa2d9db2
nikivazou <nikivazou@gmail.com>
2015-02-16 14:01:18 -0800

    whileM type seems good enought

--- tests/icfp15/WhileM0.hs
+++ tests/icfp15/WhileM0.hs
@@ -14,13 +14,12 @@ whileM  :: forall < pre   :: World -> Prop
                , q :: Bool -> Prop
                , post1 :: World -> () -> World -> Prop
                , post  :: World -> () -> World -> Prop>. 
-       {b :: {v:Bool<q> | true}, w :: World<pre>, x::() |- World<p w b> <: World<post w x> } 
-       {x1::(), x2::(), w1::World, w2::World |- World<post w1 x2> <: World<post w2 x2> } 
-       {s1::World, b::{v:Bool<q> |      (Prop v)}, x :: () |- World<p s1 b> <: World<post s1 x> } 
+       {x::(), s1::World<pre>, b::Bool<q>, s2::World<p s1 b> |- World<post1 s2 x> <: World<pre>}
+       {x1::(), x2::(), s1::World, s3::World |- World<post s3 x2> <: World<post s1 x2> } 
        {s1::World, b::{v:Bool<q> |  not (Prop v)}, x :: () |- World<p s1 b> <: World<post s1 x> } 
-          RIO <{\x -> true}, p, q> Bool 
+          RIO <pre, p, q> Bool 
        -> RIO <{\x -> true}, post1, {\v -> true}> ()
-       -> RIO <{\x -> true}, post, {\v -> true}> ()
+       -> RIO <pre, post, {\v -> true}> ()
 @-}
 whileM :: RIO Bool -> RIO () -> RIO ()
 whileM (RIO cond) (RIO e) 
