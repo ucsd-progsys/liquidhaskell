11a56f3e5e41187f2a530584d8fed2671115d141
nikivazou <nikivazou@gmail.com>
2015-02-16 10:43:10 -0800

    removing papp4: is this the cause of fixpoint time\?

---
+++ src/Language/Haskell/Liquid/Simplify.hs
@@ -0,0 +1,32 @@
+module Language.Haskell.Liquid.Simplify (simplifyBounds) where
+
+import Language.Haskell.Liquid.Types
+
+import Language.Fixpoint.Types
+
+import Control.Applicative                 ((<$>))
+
+simplifyLen = 5
+
+simplifyBounds :: SpecType -> SpecType
+simplifyBounds = fmap go 
+  where
+  	go x = x{ur_reft = go' $ ur_reft x}
+
+  	go' (Reft (v, rs)) = Reft(v, filter (not . isBoundLike) rs)
+
+  	isBoundLike (RConc pred) = isBoundLikePred pred
+  	isBoundLike (RKvar _ _)  = False
+
+  	isBoundLikePred (PAnd ps)
+  	  = moreThan simplifyLen (isImp <$> ps)
+  	isBoundLikePred _ = False
+
+  	isImp (PImp _ _) = True
+  	isImp _          = False
+
+  	moreThan 0 _          = True
+  	moreThan _ []         = False
+  	moreThan i (True:xs)  = moreThan (i-1) xs
+  	moreThan i (False:xs) = moreThan i xs
+
