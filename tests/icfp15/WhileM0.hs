760bf0f6be6f6425703acf9e3f4613bf69c7b6b8
nikivazou <nikivazou@gmail.com>
2015-02-16 16:14:16 -0800

    another while example

--- tests/icfp15/WhileM0.hs
+++ tests/icfp15/WhileM0.hs
@@ -15,8 +15,8 @@ whileM  :: forall < pre   :: World -> Prop
                , post1 :: World -> () -> World -> Prop
                , post  :: World -> () -> World -> Prop>. 
        {x::(), s1::World<pre>, b::Bool<q>, s2::World<p s1 b> |- World<post1 s2 x> <: World<pre>}
-       {x1::(), x2::(), s1::World, s3::World |- World<post s3 x2> <: World<post s1 x2> } 
-       {s1::World, b::{v:Bool<q> |  not (Prop v)}, x :: () |- World<p s1 b> <: World<post s1 x> } 
+       {b::{v:Bool<q> | Prop v}, x1::(), x2::(), s1::World<pre>, s2::World<p s1 b>, s3::World<post1 s2 x1> |- World<post s3 x2> <: World<post s1 x2> } 
+       {s1::World, b::{v:Bool<q> | not (Prop v)}, x :: () |- World<p s1 b> <: World<post s1 x> } 
           RIO <pre, p, q> Bool 
        -> RIO <{\x -> true}, post1, {\v -> true}> ()
        -> RIO <pre, post, {\v -> true}> ()
