7e7a1f212aac87fffb234179219726ec0d9d83fc
nikivazou <nikivazou@gmail.com>
2015-04-01 18:30:20 -0700

    more tests

--- tests/pos/Strings.hs
+++ tests/pos/Strings.hs
@@ -6,3 +6,10 @@ import GHC.CString  -- This import interprets Strings as constants!
 bar :: () -> String 
 {-@ bar :: () -> {x:String | x = unpack "foo" && len x >= 0} @-}
 bar _ = "foo"
+
+
+{-@ prop :: {v:Bool | Prop v <=> true} @-}
+prop :: Bool
+prop = foo1 == foo2 
+  where foo1 = "foo"
+        foo2 = "foo"
