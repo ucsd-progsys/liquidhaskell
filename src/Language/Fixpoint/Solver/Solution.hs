2d57f09aaaaebeb5c3563fd3f1b45c25d90559c8
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-04-03 21:11:03 -0700

    twiddle with Solution.hs

--- src/Language/Fixpoint/Solver/Solution.hs
+++ src/Language/Fixpoint/Solver/Solution.hs
@@ -56,19 +56,45 @@ init _ fi = L.foldl' (refine fi qs) s0 ws
 
 refine :: F.FInfo a -> [F.Qualifier] -> Solution -> F.WfC a -> Solution
 refine fi qs s w   = refineK (wfEnv fi w) qs s (wfKvar w)
-  where
 
 refineK env qs s (v, t, k) = M.insert k eqs' s
   where
-    eqs'  = case M.lookup k s of
-              Nothing  -> instK env qs v t k
-              Just eqs -> [eq | eq <- eqs, okInst env v t eq]
+    eqs' = case M.lookup k s of
+             Nothing  -> instK env v t k qs
+             Just eqs -> [eq | eq <- eqs, okInst env v t eq]
 
-instK env qs v t k = error "TODO"
+instK :: F.SEnv F.SortedReft -> F.Symbol -> F.Sort -> F.Symbol -> [F.Qualifier] -> [EQual]
+instK env v t k = concatMap (instKQ env v t k)
 
-{- 1. generate ALL xts combinations
+instKQ :: F.SEnv F.SortedReft -> F.Symbol -> F.Sort -> F.Symbol -> F.Qualifier -> [EQual]
+instKQ env v t k q = error "TODO"
+
+{-
+
+  (qv, qt) : qvts = params q
+
+  bind0 = t1 t2 t3 t4    s1 s2 s3 s4
+
+  match :: [(Symbol, Sort)] -> [(Symbol, Sort)] -> [[Expr]]
+  match xts (vt : yts) = do
+    (th0, y0) <- candidates [(v,t)] qt
+    es        <- go th0 [Var y0] yts
+    return     $ reverse es
+
+    where
+      go th es ((y,t) : yts)
+        = do (th', x) <- candidates xts t
+             let th'' = th <> th'
+             let yts' = [(y, apply th t) | (y, t) <- yts]
+             go xts th'' (Var x : es) yts'
+      go th es []
+        = return es
+
+
+   1. generate ALL xts combinations
    2. for each combination, instantiate,
-   3.   check SORT if ok then return it. -}
+   3.   check SORT if ok then return it.
+-}
 
 wfKvar :: F.WfC a -> (F.Symbol, F.Sort, F.Symbol)
 wfKvar w@(F.WfC {F.wrft = sr})
@@ -76,7 +102,7 @@ wfKvar w@(F.WfC {F.wrft = sr})
   , F.isEmptySubst su = (v, F.sr_sort sr, k)
   | otherwise         = errorstar $ "wfKvar: malformed wfC " ++ show w
 
-wfEnv       :: F.FInfo a -> F.WfC a -> F.SEnv F.SortedReft
+wfEnv :: F.FInfo a -> F.WfC a -> F.SEnv F.SortedReft
 wfEnv fi w  = F.fromListSEnv xts
   where
     wbinds  = F.elemsIBindEnv $ F.wenv w
