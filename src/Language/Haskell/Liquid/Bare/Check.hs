c4210b7908ac3eb3025020db3e0c18e1ca220eb5
nikivazou <nikivazou@gmail.com>
2015-04-14 15:21:45 -0700

    remove more files

--- src/Language/Haskell/Liquid/Bare/Check.hs
+++ src/Language/Haskell/Liquid/Bare/Check.hs
@@ -374,9 +374,7 @@ checkMBody' emb sort γ body = case body of
   where
     psort = FApp propFTyCon []
     sty   = rTypeSortedReft emb sort' 
-    sort' = fromRTypeRep $ trep' { ty_vars  = [], ty_preds = [], ty_labels = []
-                                 , ty_binds = tail $ ty_binds trep'
-                                 , ty_args  = tail $ ty_args trep'
-                                 , ty_refts = tail $ ty_refts trep'            }
+    sort' = ty_res trep'
+    
     trep' = toRTypeRep sort
 
