1f9f8a757fea879b19b79e524c63b48baed1c768
Benjamin Cosman <bcosman@caltech.edu>
2015-04-17 10:26:54 -0700

    Eliminate.hs

--- src/Language/Fixpoint/Solver/Eliminate.hs
+++ src/Language/Fixpoint/Solver/Eliminate.hs
@@ -5,21 +5,34 @@ import qualified Language.Fixpoint.Types       as F
 import qualified Language.Fixpoint.Solver.Deps as D
 import qualified Language.Fixpoint.Visitor     as V
 
+import qualified Data.HashMap.Strict           as M
+import qualified Data.List                     as L
+
+
+instance F.Subable (F.SubC a) where
+  substa f                 = error "TODO"
+  substf f x               = error "TODO"
+  subst su x               = error "TODO"
+  syms x                   = error "TODO"
+
 
 eliminateAll :: F.FInfo a -> D.Deps -> F.FInfo a
 eliminateAll = error "TODO"
 
 eliminate :: F.FInfo a -> F.KVar -> F.FInfo a
-eliminate = error "TODO"
+eliminate fInfo kv = error "TODO"
+  where
+    (relevantSubCs, remainingSubCs) = L.partition (\subC -> kv `elem` (D.rhsKVars subC)) (M.elems (F.cm fInfo))
+    orPred = F.POr (map (foo kv fInfo) relevantSubCs)
 
 --TODO: ignores a constraint's sgrd, stag, and sinfo
 --Assume we are given the kvar corresponding to the constraint's RHS
 foo :: F.KVar -> F.FInfo a -> F.SubC a -> F.Pred
-foo kv fInfo subC = error "TODO"
+foo kv fInfo subC = pr'
   where
-    bindings = F.envCs (F.bs fInfo) (F.senv subC) --[(F.Symbol, F.SortedReft)]
-    pr = predAndSimpleEnvFromEnvAndLhs bindings (F.slhs subC)  --([(F.Symbol,F.Sort)],F.Pred)
-    pr' = projectNonWFVars (bar (F.ws fInfo) kv) pr
+    bindings = F.envCs (F.bs fInfo) (F.senv subC)
+    pr = predAndSimpleEnvFromEnvAndLhs bindings (F.slhs subC)
+    (_, pr') = projectNonWFVars (bar (F.ws fInfo) kv) pr --TODO: remove unused retval
 
 --TODO: ignores the WfC's env. also assumes will find exactly one wfc for a given kvar
 bar :: [F.WfC a] -> F.KVar -> [(F.Symbol,F.Sort)]
@@ -33,8 +46,8 @@ bar wfcs kv = [(sym, sort)]
 projectNonWFVars :: [(F.Symbol,F.Sort)] -> ([(F.Symbol,F.Sort)],F.Pred) -> ([(F.Symbol,F.Sort)],F.Pred)
 projectNonWFVars wfVars (vars, pr) = (vars', pr')
   where
-    vars' = [var | var <- vars, not (elem var wfVars)]
-    pr' = F.PExist [var | var <- vars, (elem var wfVars)] pr
+    vars' = [var | var <- vars, elem var wfVars]
+    pr' = F.PExist [var | var <- vars, not (elem var wfVars)] pr
 
 -- [ x : {v : int | v = 10}
 -- , y : {v : int | v = 20} ]
@@ -58,17 +71,13 @@ baz bindings = (bs, pr)
 -- ->
 -- (x = 10)
 blah :: (F.Symbol, F.SortedReft) -> F.Pred
-blah (sym, sr) = F.subst sub (F.reftPred reft)
+blah (sym, sr) = F.subst1 (F.reftPred reft) sub
   where
     reft = F.sr_reft sr
-    sub = F.mkSubst [((F.reftBind reft), (F.eVar sym))]
+    sub = ((F.reftBind reft), (F.eVar sym))
 
 
 {-
-eVar ::  Symbolic a => a -> Expr
-
-mkSubst  :: [(Symbol, Expr)] -> Subst
-
 data FInfo a = FI { cm    :: M.HashMap Integer (SubC a)
                   , ws    :: ![WfC a]
                   , bs    :: !BindEnv
