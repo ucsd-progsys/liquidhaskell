35f835674cbc8f34485db434d455f5ac17a83b21
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-05-02 15:17:15 -0700

    woot -- fixme.fq says Success (probably bogus though)

--- src/Language/Fixpoint/Solver/Solution.hs
+++ src/Language/Fixpoint/Solver/Solution.hs
@@ -95,7 +95,7 @@ update1 s (k, qs) = (change, M.insert k qs s)
 --------------------------------------------------------------------
 init :: Config -> F.FInfo a -> Solution
 --------------------------------------------------------------------
-init _ fi = tracepp "init solution" s
+init _ fi = {- tracepp "init solution" -} s
   where
     s     = L.foldl' (refine fi qs) s0 ws
     s0    = M.empty
@@ -153,7 +153,6 @@ instCands :: F.SEnv F.SortedReft -> [(F.Symbol, F.Sort)]
 instCands = filter isOk . F.toListSEnv . fmap F.sr_sort
   where
     isOk  = isNothing . F.functionSort . snd
-
 
 match :: [(F.Symbol, F.Sort)] -> [F.Symbol] -> [F.Sort] -> [[F.Symbol]]
 match xts xs (t : ts)
