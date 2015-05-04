f11e7d6870c74e1b4ac7d63ef76c5a738271d091
Benjamin Cosman <bcosman@caltech.edu>
2015-05-04 15:48:39 -0700

    Eliminate

--- src/Language/Fixpoint/Solver/Eliminate.hs
+++ src/Language/Fixpoint/Solver/Eliminate.hs
@@ -4,11 +4,12 @@ module Language.Fixpoint.Solver.Eliminate
 import           Language.Fixpoint.Types
 import qualified Language.Fixpoint.Solver.Deps as D
 import           Language.Fixpoint.Visitor (kvars, mapKVars)
-import           Language.Fixpoint.Names   (nonSymbol)
+import           Language.Fixpoint.Names   (nonSymbol, existSymbol)
 import           Language.Fixpoint.Misc    (errorstar)
 
 import qualified Data.HashMap.Strict           as M
 import           Data.List (partition, (\\))
+import           Data.Foldable (foldlM)
 import           Control.Monad.State
 
 
@@ -53,24 +54,25 @@ instance Elimable BindEnv where
   elimKVar kv pr = mapBindEnv (\(sym, sr) -> (sym, (elimKVar kv pr sr)))
 
 
+-- NOTE: assumes that eliminateAll is called only once for a given constraints file.
+--       if not, starting the fresh-variable counter again at 0 is bad
 eliminateAll :: FInfo a -> D.Deps -> FInfo a
-eliminateAll fInfo ds = foldl eliminate fInfo (D.depNonCuts ds)
+eliminateAll fInfo ds = evalState (foldlM eliminate fInfo (D.depNonCuts ds)) 0
 
---TODO: ignores the WfC's env
-eliminate :: FInfo a -> KVar -> FInfo a
-eliminate fInfo kv = elimKVar kv orPred (fInfo { cm = newSubCs , ws = remainingWs , bs = be' })
-  where
-    relevantSubCs  = M.filter (      (elem kv) . D.rhsKVars) (cm fInfo)
-    remainingSubCs = M.filter (not . (elem kv) . D.rhsKVars) (cm fInfo)
-    (kvWfC, remainingWs) = findWfC kv (ws fInfo)
-    predsAndBindings = map (extractPred kvWfC (bs fInfo)) (M.elems relevantSubCs)
-    preds    = map fst predsAndBindings
-    bindings = concatMap snd predsAndBindings
-    orPred = POr preds
+eliminate :: FInfo a -> KVar -> State Integer (FInfo a)
+eliminate fInfo kv = do
+  n <- get
+  let relevantSubCs  = M.filter (      (elem kv) . D.rhsKVars) (cm fInfo)
+  let remainingSubCs = M.filter (not . (elem kv) . D.rhsKVars) (cm fInfo)
+  let (kvWfC, remainingWs) = findWfC kv (ws fInfo)
+  let (bindingsList, (n', orPred)) = runState (mapM (extractPred kvWfC (bs fInfo)) (M.elems relevantSubCs)) (n, POr [])
+  let bindings = concat bindingsList
 
-    be = bs fInfo
-    (ids, be') = insertsBindEnv [(sym, trueSortedReft srt) | (sym, srt) <- bindings] be
-    newSubCs = M.map (\s -> s { senv = insertsIBindEnv ids (senv s)}) remainingSubCs
+  let be = bs fInfo
+  let (ids, be') = insertsBindEnv [(sym, trueSortedReft srt) | (sym, srt) <- bindings] be
+  let newSubCs = M.map (\s -> s { senv = insertsIBindEnv ids (senv s)}) remainingSubCs
+  put n'
+  return $ elimKVar kv orPred (fInfo { cm = newSubCs , ws = remainingWs , bs = be' })
 
 insertsBindEnv :: [(Symbol, SortedReft)] -> BindEnv -> ([BindId], BindEnv)
 insertsBindEnv bs = runState (mapM go bs)
@@ -87,8 +89,11 @@ findWfC kv ws = (w', ws')
     w' | [x] <- w  = x
        | otherwise = errorstar $ (show kv) ++ " needs exactly one wf constraint"
 
-extractPred :: WfC a -> BindEnv -> SubC a -> (Pred, [(Symbol, Sort)])
-extractPred wfc be subC = (PAnd $ [(blah (kVarVV, slhs subC)) , pr], vars)
+extractPred :: WfC a -> BindEnv -> SubC a -> State (Integer, Pred) [(Symbol, Sort)]
+extractPred wfc be subC = do (n, p@(POr preds)) <- get
+                             let (bs, (n', pr')) = runState (mapM renameVar vars) (n, PAnd $ pr : [(blah (kVarVV, slhs subC))])
+                             put (n', POr $ pr' : preds)
+                             return bs
   where
     wfcIBinds  = elemsIBindEnv $ wenv wfc
     subcIBinds = elemsIBindEnv $ senv subC
@@ -100,12 +105,14 @@ extractPred wfc be subC = (PAnd $ [(blah (kVarVV, slhs subC)) , pr], vars)
     kvSreft = wrft wfc
     kVarVV = reftBind $ sr_reft kvSreft
 
-    (vars,pr) = baz $ unmatchedBindings
+    (vars, pr) = baz unmatchedBindings
 
 
---trueSortedReft :: Sort -> SortedReft
---insertBindEnv :: Symbol -> SortedReft -> BindEnv -> (BindId, BindEnv)
-
+renameVar :: (Symbol, Sort) -> State (Integer, Pred) (Symbol, Sort)
+renameVar (sym, srt) = do (n, pr) <- get
+                          let sym' = existSymbol sym n
+                          put ((n+1), subst1 pr (sym, eVar sym'))
+                          return (sym', srt)
 
 subset :: (Eq a) => [a] -> [a] -> Bool
 subset xs ys = (xs \\ ys) == []
