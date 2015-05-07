c18ffa62cd6847712c644842b917da3b443fac89
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-05-07 14:10:29 -0700

    parse constants from prelude.hquals etc

--- src/Language/Haskell/Liquid/Constraint/ToFixpoint.hs
+++ src/Language/Haskell/Liquid/Constraint/ToFixpoint.hs
@@ -9,36 +9,55 @@ import Language.Haskell.Liquid.Constraint.Types
 
 import Language.Haskell.Liquid.Types hiding     ( binds )
 import Language.Fixpoint.Misc                   ( mapSnd )
-import Language.Fixpoint.Interface              ( parseQualifiers )
+import Language.Fixpoint.Interface              ( parseFInfo )
 
 -- import           Control.Applicative ((<$>))
 import qualified Data.HashMap.Strict            as M
+import           Data.Monoid
 
 import Language.Haskell.Liquid.Qualifier
 import Language.Haskell.Liquid.RefType          ( rTypeSortedReft )
 
-
 cgInfoFInfo :: GhcInfo -> CGInfo -> IO (F.FInfo Cinfo)
 cgInfoFInfo info cgi = do
-  qs    <- ghcQuals info
-  return F.FI { F.cm    = M.fromList $ F.addIds $ fixCs cgi
-              , F.ws    = fixWfs cgi
-              , F.bs    = binds cgi
-              , F.gs    = F.fromListSEnv . map mkSort $ meas spc
-              , F.lits  = lits cgi
-              , F.kuts  = kuts cgi
-              , F.quals = qs }
+  let tgtFI = targetFInfo info cgi
+  impFI    <- parseFInfo $ hqFiles info
+  return    $ tgtFI <> impFI
+
+--   qs    <- ghcQuals info
+--   return F.FI { F.cm    = M.fromList $ F.addIds $ fixCs cgi
+--               , F.ws    = fixWfs cgi
+--               , F.bs    = binds cgi
+--               , F.gs    = F.fromListSEnv . map mkSort $ meas spc
+--               , F.lits  = lits cgi
+--               , F.kuts  = kuts cgi
+--               , F.quals = qs }
+--    where
+--     spc    = spec info
+--     tce    = tcEmbeds spc
+--     mkSort = mapSnd (rTypeSortedReft tce . val)
+
+targetFInfo :: GhcInfo -> CGInfo -> F.FInfo Cinfo
+targetFInfo info cgi
+  = F.FI { F.cm    = M.fromList $ F.addIds $ fixCs cgi
+         , F.ws    = fixWfs cgi
+         , F.bs    = binds cgi
+         , F.gs    = F.fromListSEnv . map mkSort $ meas spc
+         , F.lits  = lits cgi
+         , F.kuts  = kuts cgi
+         , F.quals = targetQuals info }
    where
     spc    = spec info
     tce    = tcEmbeds spc
     mkSort = mapSnd (rTypeSortedReft tce . val)
 
-ghcQuals :: GhcInfo -> IO [F.Qualifier]
-ghcQuals info = do
-    let spcQs = qualifiers spc
-    let genQs = specificationQualifiers n info
-    impQs    <- parseQualifiers $ hqFiles info
-    return    $ impQs ++ spcQs ++ genQs
+targetQuals :: GhcInfo -> [F.Qualifier]
+targetQuals info = spcQs ++ genQs
   where
+    spcQs     = qualifiers spc
+    genQs     = specificationQualifiers n info
     n         = maxParams $ config spc
     spc       = spec info
+
+
+
