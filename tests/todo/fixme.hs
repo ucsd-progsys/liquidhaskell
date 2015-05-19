846f160268247fca6a6564f6ac20b225c1ef19d7
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-05-19 14:59:45 -0700

    aargh, what is this ghastly error

--- tests/todo/fixme.hs
+++ tests/todo/fixme.hs
@@ -7,3 +7,9 @@ keys :: (Ord k) => [(k, v)] -> S.Set k
 keys []       = S.empty
 keys (kv:kvs) = (S.singleton (fst kv)) `S.union` (keys kvs)
 
+-- this is fine
+
+{-@ measure okeys  :: [(a, b)] -> (S.Set a)
+    okeys ([])     = (Set_empty 0)
+    okeys (kv:kvs) = (Set_cup (Set_sng (fst kv)) (okeys kvs))
+  @-}
