8b703c8fda5a98af1bb939d843de1e7fb03057ca
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-06-04 18:15:08 -0700

    ok, stats works, see data in Dropbox/TODO.md

--- src/Language/Fixpoint/Interface.hs
+++ src/Language/Fixpoint/Interface.hs
@@ -53,10 +53,10 @@ import           Control.Monad (when)
 ---------------------------------------------------------------------------
 solve :: (Fixpoint a) => Config -> FInfo a -> IO (Result a)
 solve cfg x = do
-  when (statistics cfg) $ statistics cfg x
+  when (stats cfg) $ statistics cfg x
   case () of
-    _ | native cfg -> S.solve  cfg
-    _ | otherwise  -> solveExt cfg
+    _ | native cfg -> S.solve  cfg x
+    _              -> solveExt cfg x
 
 ---------------------------------------------------------------------------
 -- | Solve .fq File -------------------------------------------------------
@@ -93,6 +93,7 @@ solveExt cfg fi =   {-# SCC "Solve"  #-} execFq cfg fn fi
   where
     fn          = srcFile cfg
 
+execFq :: (Fixpoint a) => Config -> FilePath -> FInfo a -> IO ExitCode
 execFq cfg fn fi
   = do writeFile fq qstr
        withFile fq AppendMode (\h -> {-# SCC "HPrintDump" #-} hPutStr h (render d))
@@ -100,7 +101,7 @@ execFq cfg fn fi
     where
        fq   = extFileName Fq fn
        d    = {-# SCC "FixPointify" #-} toFixpoint cfg fi
-       qstr = render ((vcat $ toFix <$> quals fi) $$ text "\n")
+       qstr = render (vcat (toFix <$> quals fi) $$ text "\n")
 
 solveFile :: Config -> IO ExitCode
 solveFile cfg
@@ -115,6 +116,7 @@ fixCommand cfg fp z3 verbosity
   where
      rf  = if real cfg then realFlags else ""
 
+realFlags :: String
 realFlags =  "-no-uif-multiply "
           ++ "-no-uif-divide "
 
--- src/Language/Fixpoint/Statistics.hs
+++ src/Language/Fixpoint/Statistics.hs
@@ -7,56 +7,50 @@
 
 module Language.Fixpoint.Statistics (statistics) where
 
+import           Control.Arrow ((&&&))
 import           Control.Applicative                   ((<$>))
 import           GHC.Generics                          (Generic)
-import           Language.Fixpoint.Misc                (fst3, groupList)
+import           Language.Fixpoint.Misc                (applyNonNull, fst3, groupList)
 import           Language.Fixpoint.Solver.Deps
-import           Language.Fixpoint.Config       hiding (statistics)
+import           Language.Fixpoint.Config       -- hiding (statistics)
+import           Language.Fixpoint.PrettyPrint
 import qualified Language.Fixpoint.Types        as F
 import qualified Data.HashMap.Strict            as M
 import qualified Data.Graph                     as G
 import qualified Data.Tree                      as T
 import           Data.Hashable
+import           Data.List (sort,group)
 import           Data.Maybe (mapMaybe)
+import           Text.PrettyPrint.HughesPJ
 
 statistics :: Config -> F.FInfo a -> IO ()
-statistics _ fi = putStrLn $ "STATS: " ++ show info
+statistics _ fi = do putStrLn $ render   $ ppGraph es
+                     putStrLn $ render   $ pprint info
+                     error "DONE: statistics"
+                     -- putStrLn $ "COMPS: " ++ show css
+                     -- exitWith ExitSuccess
   where
-    css         = decompose fi
+    es          = kvEdges   fi
+    g           = kvGraph   es
+    css         = decompose g
     sizes       = fromIntegral . length <$> css
-    info        = stats sizes
+    info        = applyNonNull Nothing (Just . mkStats) sizes
 
-stats :: [Float] -> Maybe Stats
-stats [] = Nothing
-stats ns = Just Stats {
-    cSizes  = ns
-  , cTotal  = sum     ns
-  , cMean   = mean    ns
-  , cStdev  = stdDev  ns
-  , cMax    = maximum ns
-  }
+ppGraph :: [CEdge] -> Doc
+ppGraph es = text "GRAPH:" <+> vcat [pprint v <+> text "-->" <+> pprint v' | (v, v') <- es]
 
-stdDev :: [Float] -> Float
-stdDev xs   = sqrt (sum [(x - μ)^2 | x <- xs] / n)
-  where
-    μ       = mean   xs
-    n       = fromIntegral $ length xs
-
-mean :: [Float] -> Float
-mean ns  = sum ns / fromIntegral (length ns)
-
-
-data Stats = Stats { cSizes  :: [Float]
-                   , cTotal  :: Float
-                   , cMean   :: Float
-                   , cStdev  :: Float
-                   , cMax    :: Float
-                   } deriving (Show)
-
+-------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
 
 data CVertex = KVar F.KVar
              | Cstr Integer
                deriving (Eq, Ord, Show, Generic)
+
+instance PPrint CVertex where
+  pprint (KVar k) = pprint k
+  pprint (Cstr i) = text "id:" <+> pprint i
+
 
 instance Hashable CVertex
 
@@ -67,8 +61,13 @@ type Comps a  = [[a]]
 type KVComps  = Comps CVertex
 type SubComps = Comps Integer
 
-decompose :: F.FInfo a -> SubComps
-decompose = subComps . partition . kvGraph
+-------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
+
+
+decompose :: KVGraph -> SubComps
+decompose = subComps . partition
 
 subComps :: KVComps -> SubComps
 subComps = map $ mapMaybe cstr
@@ -77,24 +76,70 @@ subComps = map $ mapMaybe cstr
     cstr _        = Nothing
 
 partition :: KVGraph -> KVComps
-partition kg = map (fst3 . f) <$> vss
+partition kg = tracepp "flattened" $ map (fst3 . f) <$> vss
   where
     (g,f,_)  = G.graphFromEdges kg
     vss      = T.flatten <$> G.components g
 
+kvGraph :: [CEdge] -> KVGraph
+kvGraph es = [(v,v,vs) | (v, vs) <- groupList es ]
 
-kvGraph :: F.FInfo a -> KVGraph
-kvGraph fi = [(v,v,vs) | (v, vs) <- groupList es]
+kvEdges :: F.FInfo a -> [CEdge]
+kvEdges fi = selfes ++ concatMap (subcEdges bs) cs
   where
-    es     = concatMap (subcEdges bs) cs
     bs     = F.bs fi
     cs     = M.elems (F.cm fi)
-
-
+    selfes = [(Cstr i, Cstr i) | c <- cs, let i = F.subcId c]
 
 subcEdges :: F.BindEnv -> F.SubC a -> [CEdge]
 subcEdges bs c =  [(KVar k, Cstr i ) | k  <- lhsKVars bs c]
                ++ [(Cstr i, KVar k') | k' <- rhsKVars c ]
   where
     i          = F.subcId c
+
+-------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------
+
+data Stats = Stats { cSizes  :: [Float]
+                   , cFreq   :: [(Float, Int)]
+                   , cTotal  :: Float
+                   , cMean   :: Float
+                   , cMax    :: Float
+                   , cSpeed  :: Float
+                   } deriving (Show)
+
+instance PPrint Stats where
+  pprint s = vcat [ text "STAT: max/total = " <+> pprint (cMax   s) <+> text "/" <+> pprint (cTotal s)
+                  , text "STAT: freqs     = " <+> pprint (cFreq  s)
+                  , text "STAT: average   = " <+> pprint (cMean  s)
+                  , text "STAT: speed     = " <+> pprint (cSpeed s)
+                  ]
+
+mkStats :: [Float] -> Stats
+mkStats ns  = Stats {
+    cSizes  = ns
+  , cFreq   = frequency ns
+  , cTotal  = total
+  , cMean   = avg
+  , cMax    = max
+  , cSpeed  = total / max
+  }
+  where
+    max     = maximum ns
+    total   = sum  ns
+    avg     = mean ns
+
+frequency :: (Ord a) => [a] -> [(a, Int)]
+frequency = map (head &&& length) . group . sort
+
+stdDev :: [Float] -> Float
+stdDev xs   = sqrt (sum [(x - μ)^2 | x <- xs] / n)
+  where
+    μ       = mean   xs
+    n       = fromIntegral $ length xs
+
+mean :: [Float] -> Float
+mean ns  = sum ns / fromIntegral (length ns)
+
 
