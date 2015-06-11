07d7af566dd169bdff70585d7c5054a968dd0738
nikivazou <nikivazou@gmail.com>
2015-06-10 18:17:23 -0700

    make it compile and cleaning

--- src/Language/Haskell/Liquid/Constraint/Generate.hs
+++ src/Language/Haskell/Liquid/Constraint/Generate.hs
@@ -150,17 +150,17 @@ initEnv info
     makedcs      = map strengthenDataConType
     mapSndM f (x,y) = (x,) <$> f y
 
--- BEGIN: TO CLEAN
 makeAutoDecrDataCons dcts specenv dcs
   = (simplify invs, tys)
   where 
     (invs, tys) = unzip $ concatMap go tycons 
-    tycons = L.nub $ catMaybes $ map idTyCon dcs 
+    tycons      = L.nub $ catMaybes $ map idTyCon dcs 
+
     go tycon 
       | S.member tycon specenv =  zipWith (makeSizedDataCons dcts) (tyConDataCons tycon) [0..] 
-    go _ = [] 
-    idTyCon x = traceShow ("idTyCon of " ++ show x) 
-      (dataConTyCon <$> case idDetails x of {DataConWorkId d -> Just d; DataConWrapId d -> Just d; _ -> Nothing})  
+    go _ 
+      = [] 
+    idTyCon x = dataConTyCon <$> case idDetails x of {DataConWorkId d -> Just d; DataConWrapId d -> Just d; _ -> Nothing}  
 
     simplify invs = dummyLoc . (`strengthen` invariant) .  fmap (\_ -> mempty) <$> L.nub invs 
     invariant = U (F.Reft (F.vv_, F.Refa $ F.PAtom F.Ge (lenOf F.vv_) (F.ECon $ F.I 0)) ) mempty mempty
@@ -169,16 +169,16 @@ lenOf x = F.EApp lenLocSymbol [F.EVar x]
 
 makeSizedDataCons dcts x' n = (toRSort $ ty_res trep, (x, fromRTypeRep trep{ty_res = tres})) 
     where 
-      x = dataConWorkId x'
-      Just t = L.lookup x dcts 
-      trep = toRTypeRep t 
-      tres = ty_res trep `strengthen` U (F.Reft (F.vv_, F.Refa 
+      x      = dataConWorkId x'
+      t      = fromMaybe (errorstar "makeSizedDataCons: this should never happen") $ L.lookup x dcts 
+      trep   = toRTypeRep t 
+      tres   = ty_res trep `strengthen` U (F.Reft (F.vv_, F.Refa 
                               $ F.PAtom F.Eq (lenOf F.vv_) computelen)) mempty mempty
-      xs   = ty_binds trep 
-      as   = ty_vars  trep
-      recarguments = filter (\(t,x) -> (toRSort t == toRSort tres)) (zip (ty_args trep) (ty_binds trep))
-      computelen = foldr (F.EBin F.Plus) (F.ECon $ F.I n) (lenOf . snd <$> recarguments)
--- END: TO CLEAN
+      xs     = ty_binds trep 
+      as     = ty_vars  trep
+
+      recarguments = filter (\(t,_) -> (toRSort t == toRSort tres)) (zip (ty_args trep) (ty_binds trep))
+      computelen   = foldr (F.EBin F.Plus) (F.ECon $ F.I n) (lenOf . snd <$> recarguments)
 
 mergeDataConTypes xts yts = merge (L.sortBy f xts) (L.sortBy f yts)
   where
