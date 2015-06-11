f8a1b327afd9d2c4ea18014f351f7601ccb47ec3
nikivazou <nikivazou@gmail.com>
2015-06-10 20:13:06 -0700

    playing with interpreter

--- tests/todo/Interpreter.hs
+++ tests/todo/Interpreter.hs
@@ -4,13 +4,13 @@ module Interpreter where
 
 data BinOp a = Plus | Times
 
-data Exp   = EConst Int | EBinOp (BinOp Int) Exp Exp
+data Exp a  = EConst Int | EBinOp (BinOp a) (Exp a) (Exp a)
 
-data Instr = IConst Int | IBinOp (BinOp Int)
+data Instr a = IConst Int | IBinOp (BinOp a)
 
 data List a = Nil | Cons a (List a)
 
-type Prog  = List Instr
+type Prog  = List (Instr Int)
 type Stack = List Int
 
 {-@ measure binOpDenote @-}
@@ -18,19 +18,20 @@ binOpDenote :: Int -> Int -> BinOp a -> Int
 binOpDenote x y Plus  = (x+y)
 binOpDenote x y Times = (x*y)
 
-plus  = Plus 
-times = Times 
-
-{-
-expDenote :: Exp -> Int 
+{-@ measure expDenote @-}
+expDenote :: Exp a -> Int 
 expDenote (EConst n)       = n
 expDenote (EBinOp b e1 e2) = binOpDenote (expDenote e1) (expDenote e2) b
 
-instrDenote :: Stack -> Instr -> Maybe Stack
+
+{- HERE HERE -}
+{- measure instrDenote @-}
+instrDenote :: Stack -> Instr a -> Maybe Stack
 instrDenote s       (IConst n) = Just (cons n s)
 instrDenote (Cons x (Cons y s)) (IBinOp b) = Just (cons (binOpDenote x y b) s)
 instrDenote _        _         = Nothing
 
+{-
 {- measure progDenote :: Stack -> Prog -> Maybe Stack @-}
 progDenote :: Stack -> Prog -> Maybe Stack
 progDenote s Nil = Just s
@@ -43,7 +44,7 @@ compile :: Exp -> Prog
 compile (EConst n)       = single (IConst n)  
 compile (EBinOp b e1 e2) = compile e2 `append` compile e1 `append` (single $ IBinOp b) 
 
-
+-}
 -- List operations
 {-@ autosize Exp @-}
 {-@ autosize List @-}
@@ -54,4 +55,4 @@ Nil         `append` ys = ys
 emp      = Nil
 single x = Cons x Nil
 cons     = Cons 
--}
+
