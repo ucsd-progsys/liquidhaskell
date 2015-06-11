c711bab2d0ff541d5f95f481ef48c3bdeacb3658
nikivazou <nikivazou@gmail.com>
2015-06-10 18:25:34 -0700

    back to interpreter

--- tests/todo/Interpreter.hs
+++ tests/todo/Interpreter.hs
@@ -2,6 +2,7 @@ module Interpreter where
 {-@ LIQUID "--totality" @-}
 
 data BinOp = Plus | Times
+
 data Exp   = EConst Int | EBinOp BinOp Exp Exp
 
 data Instr = IConst Int | IBinOp BinOp
@@ -29,14 +30,16 @@ progDenote s Nil = Just s
 progDenote s (Cons x xs) | Just s' <- instrDenote s x = progDenote s' xs
                          | otherwise                  = Nothing
 
-{-@ compile :: e:Exp -> {v:Prog | (progDenote Nil v) == Nothing } @-}
-{- compile :: e:Exp -> {v:Prog | (progDenote Nil v) == Just (Cons (expDenote e) Nil)} @-}
+{- compile :: e:Exp -> {v:Prog | (progDenote Nil v) == Nothing } @-}
+{-@ compile :: e:Exp -> {v:Prog | (progDenote Nil v) == Just (Cons (expDenote e) Nil)} @-}
 compile :: Exp -> Prog
 compile (EConst n)       = single (IConst n)  
 compile (EBinOp b e1 e2) = compile e2 `append` compile e1 `append` (single $ IBinOp b) 
 
 
 -- List operations
+{-@ autosize Exp @-}
+{-@ autosize List @-}
 
 (Cons x xs) `append` ys = cons x $ append xs ys
 Nil         `append` ys = ys 
