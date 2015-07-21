daf0409645d4bfe8f822ea62149a57e5efbef8fc
nikivazou <nikivazou@gmail.com>
2015-07-21 14:29:14 -0700

    remove fixme

--- Liquid.hs
+++ Liquid.hs
@@ -103,6 +103,7 @@ solveCs cfg target cgi info dc
                        , FC.real    = real   cfg
                        , FC.native  = native cfg
                        , FC.srcFile = target
+                       , FC.cores   = fromInteger $ cores  cfg 
                        -- , FC.stats   = True
                        }
        ferr s r  = fmap (tidyError s) $ result $ sinfo <$> r
