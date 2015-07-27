f43742d9d57d9285acc26d9e19b2fa9fbeef2dd4
Benjamin Cosman <bcosman@caltech.edu>
2015-07-27 14:01:34 -0700

    Cleanup Deps

--- src/Language/Fixpoint/Solver/Deps.hs
+++ src/Language/Fixpoint/Solver/Deps.hs
@@ -4,16 +4,15 @@ module Language.Fixpoint.Solver.Deps (
 ) where
 
 import           Language.Fixpoint.Misc    (groupList)
-import           Language.Fixpoint.Names   (nonSymbol)
-import qualified Language.Fixpoint.Types   as F
-import qualified Language.Fixpoint.Visitor as V
-import qualified Data.HashMap.Strict       as M
+import           Language.Fixpoint.Types
+import           Language.Fixpoint.Visitor (lhsKVars, rhsKVars)
+import           Data.HashMap.Strict       (elems)
 import qualified Data.HashSet              as S
 import qualified Data.Graph                as G
-import           Control.Monad.State       (get, put, State, execState)
+import           Control.Monad.State       (get, put, execState)
 
-data Deps  = Deps { depCuts    :: ![F.KVar]
-                  , depNonCuts :: ![F.KVar]
+data Deps  = Deps { depCuts    :: ![KVar]
+                  , depNonCuts :: ![KVar]
                   }
              deriving (Eq, Ord, Show)
 
@@ -21,37 +20,35 @@ data Deps  = Deps { depCuts    :: ![F.KVar]
 -- | Compute Dependencies and Cuts ---------------------------
 --------------------------------------------------------------
 
-deps :: F.FInfo a -> Deps
-deps finfo = sccsToDeps sccs (F.kuts finfo)
+deps :: FInfo a -> Deps
+deps fi = sccsToDeps sccs (kuts fi)
   where
-    bs    = F.bs finfo
-    subCs = M.elems (F.cm finfo)
-    edges = concatMap (subcEdges bs) subCs
+    subCs = elems (cm fi)
+    edges = concatMap (subcEdges $ bs fi) subCs
     graph = [(k,k,ks) | (k, ks) <- groupList edges]
     sccs  = G.stronglyConnCompR graph
 
-sccsToDeps :: [G.SCC (F.KVar, F.KVar, [F.KVar])] -> F.Kuts -> Deps
-sccsToDeps xs ks = execState (mapM_ (bar ks) xs) (Deps [] [])
+sccsToDeps :: [G.SCC (KVar,KVar,[KVar])] -> Kuts -> Deps
+sccsToDeps xs ks = execState (mapM_ go xs) (Deps [] [])
   where
-    bar :: F.Kuts -> G.SCC (F.KVar, F.KVar,[F.KVar]) -> State Deps ()
-    bar _  (G.AcyclicSCC (v,_,_)) = do ds <- get
-                                       put ds {depNonCuts = v : depNonCuts ds}
-    bar ks (G.CyclicSCC vs)       = do let (v,vs') = chooseCut vs ks
-                                       ds <- get
-                                       put ds {depCuts = v : depCuts ds}
-                                       mapM_ (bar ks) (G.stronglyConnCompR vs')
+    go (G.AcyclicSCC (v,_,_)) = do ds <- get
+                                   put ds {depNonCuts = v : depNonCuts ds}
+    go (G.CyclicSCC vs)       = do let (v,vs') = chooseCut vs ks
+                                   ds <- get
+                                   put ds {depCuts = v : depCuts ds}
+                                   mapM_ go (G.stronglyConnCompR vs')
 
-chooseCut :: [(F.KVar, F.KVar, [F.KVar])] -> F.Kuts -> (F.KVar, [(F.KVar, F.KVar,[F.KVar])])
-chooseCut vs (F.KS ks) = (v, [x | x@(u,_,_) <- vs, u /= v])
+chooseCut :: [(KVar,KVar,[KVar])] -> Kuts -> (KVar, [(KVar,KVar,[KVar])])
+chooseCut vs (KS ks) = (v, [x | x@(u,_,_) <- vs, u /= v])
   where
     vs' = [x | (x,_,_) <- vs]
     is  = S.intersection (S.fromList vs') ks
     v   = head $ if S.null is then vs' else S.toList is
 
-subcEdges :: F.BindEnv -> F.SubC a -> [(F.KVar, F.KVar)]
-subcEdges bs c = [(k1, k2)        | k1 <- V.lhsKVars bs c
-                                  , k2 <- V.rhsKVars c    ]
-              ++ [(k2, F.KV nonSymbol) | k2 <- V.rhsKVars c]
+subcEdges :: BindEnv -> SubC a -> [(KVar, KVar)]
+subcEdges bs c = [(k1, k2)        | k1 <- lhsKVars bs c
+                                  , k2 <- rhsKVars c    ]
+              ++ [(k2, KV nonSymbol) | k2 <- rhsKVars c]
 -- this nonSymbol hack is one way to prevent nodes with potential
 -- outdegree 0 from getting pruned by stronglyConnCompR
 
