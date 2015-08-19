f8382d6cbf9d0c70d2faa672f2bf3980bc66998b
Benjamin Cosman <bcosman@caltech.edu>
2015-08-19 14:37:19 -0700

    Uniqify

--- src/Language/Fixpoint/Solver/Uniqify.hs
+++ src/Language/Fixpoint/Solver/Uniqify.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE DeriveGeneric              #-}
+{-# LANGUAGE DeriveGeneric #-}
 
 module Language.Fixpoint.Solver.Uniqify (renameAll) where
 
@@ -10,27 +10,27 @@ import qualified Data.HashSet            as S
 import           Data.List               ((\\), sort)
 import           Data.Maybe              (catMaybes)
 import           Data.Foldable           (foldlM)
-import           Control.Monad.State     (get, put, evalState, State)
-
 import           Data.Hashable
-import           GHC.Generics              (Generic)
+import           GHC.Generics            (Generic)
+import           Control.Monad.State     (evalState, State, state)
 
 --------------------------------------------------------------
 renameAll :: FInfo a -> FInfo a
-renameAll fi = renameVars fi $ M.toList $ invert $ mkIdMap fi
+renameAll fi = renameVars fi $ M.toList $ invertMap $ mkIdMap fi
 --------------------------------------------------------------
 
 data Ref = RB BindId | RI Integer
   deriving (Eq, Generic)
 instance Hashable Ref
 
--- stores for each constraint and each BindId the sets of other BindIds that
--- it references, i.e. the ones where it needs to know when their names gets changed
+-- stores for each constraint and BindId the set of other BindIds that it 
+-- references, i.e. those where it needs to know when their names gets changed
 type IdMap = M.HashMap Ref (S.HashSet BindId)
 type NameMap = M.HashMap Symbol BindId
 
-invert :: IdMap -> M.HashMap BindId (S.HashSet Ref)
-invert m = M.fromListWith S.union entries
+invertMap :: (Hashable k, Hashable v, Eq k, Eq v) => 
+  M.HashMap k (S.HashSet v) -> M.HashMap v (S.HashSet k)
+invertMap m = M.fromListWith S.union entries
   where 
     entries = [(v, S.singleton k) | (k, vs) <- M.toList m, v <- S.toList vs]
 
@@ -51,7 +51,7 @@ updateIdMap be m subcId s = M.insertWith S.union (RI subcId) refSet m'
 insertIdIdLinks :: BindEnv -> NameMap -> IdMap -> BindId -> IdMap
 insertIdIdLinks be nameMap m id = M.insertWith S.union (RB id) refSet m
   where
-    (_, sr) = lookupBindEnv id be
+    sr = snd $ lookupBindEnv id be
     symList = freeVars $ sr_reft sr
     refSet = S.fromList $ namesToIds symList nameMap
 
@@ -66,32 +66,30 @@ renameVars :: FInfo a -> [(BindId, S.HashSet Ref)] -> FInfo a
 renameVars fi xs = evalState (foldlM renameVarIfSeen fi xs) S.empty
 
 renameVarIfSeen :: FInfo a -> (BindId, S.HashSet Ref) -> State (S.HashSet Symbol) (FInfo a)
-renameVarIfSeen fi x@(id, _) =
-  do s <- get
-     let sym = fst $ lookupBindEnv id (bs fi)
-     let seen = sym `S.member` s
-     put (if seen then s else (S.insert sym s))
-     return (if seen then (renameVar fi x) else fi)
+renameVarIfSeen fi x@(id, _) = state (\s ->
+  let sym = fst $ lookupBindEnv id (bs fi) in
+  if sym `S.member` s then (renameVar fi x, s) else (fi, S.insert sym s))
 
 renameVar :: FInfo a -> (BindId, S.HashSet Ref) -> FInfo a
-renameVar fi (id, stuff) = elimKVar (blarg fi id sym sym') fi'' --TODO: optimize? (elimKVar separately on every rename is expensive)
+renameVar fi (id, refs) = elimKVar (updateKVars fi id sym sym') fi'' --TODO: optimize? (elimKVar separately on every rename is expensive)
   where
     sym = fst $ lookupBindEnv id (bs fi)
     sym' = renameSymbol sym id
     sub = (sym, eVar sym')
     go subst x = subst1 x subst
     fi' = fi { bs = adjustBindEnv (go sub) id (bs fi) }
-    fi'' = S.foldl' (foo sub) fi' stuff
+    fi'' = S.foldl' (applySub sub) fi' refs
 
-foo :: (Symbol, Expr) -> FInfo a -> Ref -> FInfo a
-foo sub fi (RB id) = fi { bs = adjustBindEnv (go sub) id (bs fi) }
-  where go sub (sym, sr) = (sym, subst1 sr sub)
-foo sub fi (RI id) = fi { cm = M.adjust (go sub) id (cm fi) }
-  where go sub c = c { slhs = subst1 (slhs c) sub ,
-                       srhs = subst1 (srhs c) sub }
+applySub :: (Symbol, Expr) -> FInfo a -> Ref -> FInfo a
+applySub sub fi (RB id) = fi { bs = adjustBindEnv go id (bs fi) }
+  where go (sym, sr) = (sym, subst1 sr sub)
+applySub sub fi (RI id) = fi { cm = M.adjust go id (cm fi) }
+  where go c = c { slhs = subst1 (slhs c) sub ,
+                   srhs = subst1 (srhs c) sub }
 
-blarg :: FInfo a -> BindId -> Symbol -> Symbol -> (KVar, Subst) -> Maybe Pred
-blarg fi id oldSym newSym (k, Su su) = if relevant then Just $ PKVar k $ mkSubst [(newSym, eVar oldSym)] else Nothing
+updateKVars :: FInfo a -> BindId -> Symbol -> Symbol -> (KVar, Subst) -> Maybe Pred
+updateKVars fi id oldSym newSym (k, Su su) = 
+  if relevant then Just $ PKVar k $ mkSubst [(newSym, eVar oldSym)] else Nothing
   where
     wfc = fst $ findWfC k (ws fi)
     relevant = (id `elem` (elemsIBindEnv $ wenv wfc)) && (oldSym `elem` (map fst su))
