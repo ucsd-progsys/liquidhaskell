5a918cbfe50c071b8ce4d3d3e099f5bb8f17047f
Eric Seidel <gridaphobe@gmail.com>
2015-08-27 23:22:29 -0700

    make devel/Paths_liquidhaskell.hs smarter

--- devel/Paths_liquidhaskell.hs
+++ devel/Paths_liquidhaskell.hs
@@ -1,4 +1,12 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Paths_liquidhaskell where
 
+import Language.Haskell.TH
+import System.Directory
+import System.FilePath
+
 getDataFileName :: FilePath -> IO FilePath
-getDataFileName = return
+getDataFileName f = do
+  let loc = $(do { loc <- location; f <- runIO (canonicalizePath (loc_filename loc)); litE (stringL f); })
+  let root = takeDirectory (takeDirectory loc)
+  return (root </> f)
