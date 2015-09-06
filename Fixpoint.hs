70dd6a9b46e3609e679369e655c34c0764f4e784
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-09-06 15:48:13 -0700

    so, whats going on in refine-sort

--- Fixpoint.hs
+++ Fixpoint.hs
@@ -3,10 +3,13 @@ import Language.Fixpoint.Config        (getOpts)
 import System.Exit
 import Language.Fixpoint.Misc (writeLoud)
 
+
+
+
 main :: IO ExitCode
 main = do
   cfg <- getOpts
-  writeLoud $ "Options: " ++ show cfg
+  writeLoud $  "Options: " ++ show cfg
   e <- solveFQ cfg
   putStrLn $ "EXIT: " ++ show e
   exitWith e
