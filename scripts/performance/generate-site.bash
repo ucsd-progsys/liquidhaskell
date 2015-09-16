85c7e6f9234c69ce0b0f56d1f81f39667a25e17d
Chris Tetreault <chris@doingmyprogramming.com>
2015-09-16 12:55:47 -0700

    wrote generate-site.bash

---
+++ scripts/performance/generate-site.bash
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPT_DIR=`pwd`;
+GIPEDA_DIR="$SCRIPT_DIR/gipeda";
+GIPEDA_SITE="$GIPEDA_DIR/site";
+GIPEDA_REPO="$GIPEDA_DIR/repository";
+
+function abort_if_failed {
+    local EXIT_CODE=$?;
+    if [ $EXIT_CODE != 0 ]
+    then
+        echo $1;
+        exit $EXIT_CODE;
+    fi
+}
+
+./generate-logs.bash;
+abort_if_failed "Failed to generate logs...";
+
+cd $GIPEDA_DIR;
+abort_if_failed "Unable to change to $GIPEDA_DIR..."; #You got problems
+
+./gipeda;
+abort_if_failed "Unable to generate report...";
