5f056dd3d4a0d806801ecd4b763055f41c68a4a1
Chris Tetreault <chris@doingmyprogramming.com>
2015-09-18 13:59:11 -0700

    some work done on log2csv

--- scripts/performance/log2csv.hs
+++ scripts/performance/log2csv.hs
@@ -1,1 +1,13 @@
-main = putStrLn "hello world"
+import Data.Attoparsec
+
+type Log = [(String, String)]
+
+printLog :: Log -> String
+printLog l = printLines $ fmap printLine l
+   where
+      printLines :: [String] -> String
+      printLines = concat
+      printLine :: (String, String) -> String
+      printLine (test, time) = test ++ ";" ++ time ++ "\n"
+
+main = undefined
