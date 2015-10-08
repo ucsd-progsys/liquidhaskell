39de9bbe785a5c2cc6fe9adfce5af1f85c6cfaf3
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-10-08 14:13:11 -0700

    gross, don't use upperCase names for qual-params

--- tests/pos/ListQSort-LType.hs
+++ tests/pos/ListQSort-LType.hs
@@ -2,9 +2,9 @@ module ListRange () where
 
 import Language.Haskell.Liquid.Prelude
 
-{-@  
-data List [llen] a <p :: x0:a -> x1:a -> Prop>  
-  = Nil 
+{-@
+data List [llen] a <p :: x0:a -> x1:a -> Prop>
+  = Nil
   | Cons (h :: a) (t :: List <p> (a <p h>))
 @-}
 
@@ -16,19 +16,20 @@ data List [llen] a <p :: x0:a -> x1:a -> Prop>
 {-@ invariant {v:List a | (llen v) >= 0} @-}
 
 {-@ qualif ZLLen(v:ListRange.List a) : (llen(v) >= 0)@-}
-{-@ qualif CmpLLen(v:ListRange.List a, A:ListRange.List b) : ((llen v) <= (llen A))@-}
+
+{-@ qualif CmpLLen(v:ListRange.List a, a:ListRange.List b) : (llen v <= llen a) @-}
 
 data List a = Nil | Cons a (List a)
 
 append k Nil         ys = Cons k ys
-append k (Cons x xs) ys = Cons x (append k xs ys) 
+append k (Cons x xs) ys = Cons x (append k xs ys)
 
 takeL x Nil         = Nil
 takeL x (Cons y ys) = if (y<x) then Cons y (takeL x ys) else takeL x ys
 
-{-@ takeGE :: Ord a 
-           => x:a 
-           -> xs:List a 
+{-@ takeGE :: Ord a
+           => x:a
+           -> xs:List a
            -> {v: (List {v:a | v >= x}) | ((llen v) <= (llen xs))}  @-}
 takeGE x Nil         = Nil
 takeGE x (Cons y ys) = if (y>=x) then Cons y (takeGE x ys) else takeGE x ys
@@ -38,13 +39,13 @@ quicksort (Cons x xs) = append x xsle xsge
   where xsle = quicksort (takeL x xs)
         xsge = quicksort (takeGE x xs)
 
-chk y = 
-  case y of 
+chk y =
+  case y of
    Nil -> True
-   Cons x1 xs -> case xs of 
+   Cons x1 xs -> case xs of
                  Nil -> True
                  Cons x2 xs2 -> liquidAssertB (x1 <= x2) && chk xs
-																	
+
 bar = quicksort $ mkList [1 .. 100]
 
 mkList :: Ord a => [a] -> List a
