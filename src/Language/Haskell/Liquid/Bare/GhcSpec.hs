8e888c2c237164b59cd483cf3c8929bdaab107d3
nikivazou <nikivazou@gmail.com>
2015-10-08 15:37:38 -0700

    append auto works

--- src/Language/Haskell/Liquid/Bare/GhcSpec.hs
+++ src/Language/Haskell/Liquid/Bare/GhcSpec.hs
@@ -144,9 +144,9 @@ makeAxioms :: [CoreBind] -> GhcSpec -> Ms.BareSpec -> BareM GhcSpec
 makeAxioms cbs spec sp 
   = do lmap          <- logicEnv <$> get
        (ms, tys, as) <- unzip3 <$> mapM (makeAxiom lmap cbs spec sp) (S.toList $ Ms.axioms sp)  
-       return $ spec { meas    = ms        ++  meas   spec 
-                     , asmSigs = tys       ++ asmSigs spec
-                     , axioms  = concat as ++ axioms spec } 
+       return $ spec { meas    = ms         ++  meas   spec 
+                     , asmSigs = concat tys ++ asmSigs spec
+                     , axioms  = concat as  ++ axioms spec } 
 
 emptySpec     :: Config -> GhcSpec
 emptySpec cfg = SP [] [] [] [] [] [] [] [] [] mempty [] [] [] [] mempty mempty mempty cfg mempty [] mempty mempty []
