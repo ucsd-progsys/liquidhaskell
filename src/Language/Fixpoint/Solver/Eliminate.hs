261aad2305caca3adca109fd9e43f9cd9a327f83
Benjamin Cosman <bcosman@caltech.edu>
2015-10-08 11:51:44 -0700

    Eliminate cleanup

--- src/Language/Fixpoint/Solver/Eliminate.hs
+++ src/Language/Fixpoint/Solver/Eliminate.hs
@@ -6,15 +6,13 @@ module Language.Fixpoint.Solver.Eliminate
        (eliminateAll, findWfC) where
 
 import           Language.Fixpoint.Types
-import           Language.Fixpoint.Solver.Deps (depNonCuts, deps)
-import           Language.Fixpoint.Visitor     (kvars, rhsKVars)
-import           Language.Fixpoint.Names       (existSymbol)
-import           Language.Fixpoint.Misc        (errorstar, snd3, thd3)
+import           Language.Fixpoint.Solver.Deps     (depNonCuts, deps)
+import           Language.Fixpoint.Visitor         (kvars)
+import           Language.Fixpoint.Misc            (errorstar)
 import           Language.Fixpoint.Solver.Solution (Solution, mkJVar)
 
 import qualified Data.HashMap.Strict as M
 import           Data.List           (partition, (\\), foldl')
-import           Control.Monad.State (get, put, runState, evalState, State, state)
 import           Control.Applicative ((<$>))
 import           Control.Arrow       (second)
 import           Control.DeepSeq     (($!!))
@@ -33,7 +31,9 @@ eliminate (!s, !fi) kv = (M.insert kv (mkJVar orPred) s, fi { cm = remainingCs ,
     relevantCs  = M.filter (   elem kv . kvars . crhs) (cm fi)
     remainingCs = M.filter (notElem kv . kvars . crhs) (cm fi)
     (kvWfC, remainingWs) = findWfC kv (ws fi)
-    orPred = {-# SCC "orPred" #-} POr $!! extractPred kvWfC fi <$> M.elems relevantCs
+    be = bs fi
+    kDom = domain be kvWfC
+    orPred = {-# SCC "orPred" #-} POr $!! extractPred kDom be <$> M.elems relevantCs
 
 findWfC :: KVar -> [WfC a] -> (WfC a, [WfC a])
 findWfC kv ws = (w', ws')
@@ -42,13 +42,13 @@ findWfC kv ws = (w', ws')
     w' | [x] <- w  = x
        | otherwise = errorstar $ show kv ++ " needs exactly one wf constraint"
 
-extractPred :: WfC a -> SInfo a -> SimpC a -> Pred
-extractPred wfc fi sc = projectNonWFVars be binds wfc $ PAnd (lhsPreds ++ suPreds)
+extractPred :: [Symbol] -> BindEnv -> SimpC a -> Pred
+extractPred kDom be sc = projectNonWFVars binds kDom $ PAnd (lhsPreds ++ suPreds)
   where
-    be = bs fi
-    binds = second sr_sort <$> envCs be (senv sc)
-    lhsPreds = bindPred <$> envCs be (senv sc)
-    suPreds = substPreds (usableDomain be wfc) $ crhs sc
+    env = envCs be (senv sc)
+    binds = second sr_sort <$> env
+    lhsPreds = bindPred <$> env
+    suPreds = substPreds (usableDomain be kDom) $ crhs sc
 
 -- x:{v:int|v=10} -> (x=10)
 bindPred :: (Symbol, SortedReft) -> Pred
@@ -63,22 +63,16 @@ substPreds dom (PKVar _ (Su subs)) = [PAtom Eq (eVar sym) expr | (sym, expr) <- 
 
 -- TODO: filtering out functions like this is a temporary hack - we shouldn't
 -- have function substitutions to begin with
-usableDomain :: BindEnv -> WfC a -> [Symbol]
-usableDomain be wfc = filter nonFunction $ domain be wfc
+usableDomain :: BindEnv -> [Symbol] -> [Symbol]
+usableDomain be kDom = filter nonFunction kDom
   where
     nonFunction sym = sym `notElem` functionsInBindEnv be
 
 functionsInBindEnv :: BindEnv -> [Symbol]
-functionsInBindEnv be = map snd3 fList
-  where
-    fList = filter (isFunctionSortedReft . thd3) $ bindEnvToList be
+functionsInBindEnv be = [sym | (_, sym, sr) <- bindEnvToList be, isFunctionSortedReft sr]
 
 domain :: BindEnv -> WfC a -> [Symbol]
 domain be wfc = reftBind (sr_reft $ wrft wfc) : map fst (envCs be $ wenv wfc)
 
-projectNonWFVars :: BindEnv -> [(Symbol,Sort)] -> WfC a -> Pred -> Pred
-projectNonWFVars be binds wf pr = PExist [v | v <- binds, not (elem (fst v) wfVars)] pr
-  where
-    wfVars = domain be wf
-    
-
+projectNonWFVars :: [(Symbol,Sort)] -> [Symbol] -> Pred -> Pred
+projectNonWFVars binds kDom pr = PExist [v | v <- binds, not (elem (fst v) kDom)] pr
