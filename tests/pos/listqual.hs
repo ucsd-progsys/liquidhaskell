d02b82f7d61a988129a849aca115e94ec1e5812e
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-10-20 20:59:55 -0700

    add [] to prims

--- tests/pos/listqual.hs
+++ tests/pos/listqual.hs
@@ -1,6 +1,12 @@
 module ListQual () where
 
-{-@ qualif Append(v:[a], xs:[a], ys:[a]): len v = len xs + len ys @-}
+{-@ qualif OkAppend(v:List a, xs:List a, ys:List a): len v = len xs + len ys @-}
+
+{-@ qualif BadAppend(v:[a], x:[a], ys:[a]): len v = len xs + len ys @-}
+
+{- qualAppend :: xs:_ -> ys:_ -> {v:_ | len v = len xs + len ys} @-}
+qualAppend :: [a] -> [a] -> [a]
+qualAppend = undefined
 
 append [] ys     = ys
 append (x:xs) ys = x : append xs ys
