46a05e02ef92b75b1ac241c95908379d776d94ee
Chris Tetreault <chris@doingmyprogramming.com>
2015-10-30 08:34:41 -0700

    handle empty file as input (build failure)

--- scripts/performance/log2csv.hs
+++ scripts/performance/log2csv.hs
@@ -18,9 +18,9 @@ readLog :: BS.ByteString -> IResult BS.ByteString Log
 readLog l = parse logFile l
 
 logFile :: Parser Log
-logFile = do
+logFile = (do
    anyChar `manyTill` endOfLine
-   logLine `manyTill` endOfInput
+   logLine `manyTill` endOfInput)
 
 logLine :: Parser (String, String)
 logLine = do
@@ -34,10 +34,15 @@ logLine = do
 main = do
    (i:_) <- getArgs
    i' <- BS.readFile i
-   logF <- return $ readLog i'
-   case (untilDone logF) of
-      Right r -> putStr $ printLog $ r
-      Left _ -> exitFailure
+   if i' == BS.empty
+      then
+      putStr ""
+      else
+      do
+         logF <- return $ readLog i'
+         case (untilDone logF) of
+            Right r -> putStr $ printLog $ r
+            Left _ -> exitFailure
    where
       untilDone (Done _ r) = Right r
       untilDone (Partial f) = untilDone (f $ BS.pack "")
