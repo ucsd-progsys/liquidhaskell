93a2c0f8b756ce638e52aab0e3da6c3732955209
nikivazou <nikivazou@gmail.com>
2015-11-19 12:09:01 -0800

    combining proofs

--- tests/equationalproofs/pos/AppendArrow.hs
+++ tests/equationalproofs/pos/AppendArrow.hs
@@ -23,12 +23,12 @@ data L a = N |  C a (L a)
 instance Eq a => Eq (L a) where
 
 {-@ axiomatize append @-}
+
 $(axiomatize
   [d| append :: L a -> L a -> L a
       append N xs        = xs
       append (C y ys) xs = C y (append ys xs)
     |])
-
 
 -- | Proof 1: N is neutral element
 
@@ -38,10 +38,12 @@ $(axiomatize
 -- | use a function called `use_axiom` to apply these axioms.
 
 -- prop_app_nil :: Eq a => L a -> Proof
+
 {-@ prop_app_nil :: ys:L a -> {v:Proof | append ys N == ys} @-}
 prop_app_nil :: (Eq a) => L a -> Proof
-prop_app_nil N        = auto 1 (append N N        == N     ) -- axiom_append_N N
+prop_app_nil N        =   auto 1 (append N N        == N     ) -- axiom_append_N N
 prop_app_nil (C x xs) = auto 1 (append (C x xs) N == C x xs)
+
 {-
 prop_app_nil (C x xs)
     = refl (append (C x xs) N)
@@ -49,8 +51,8 @@ prop_app_nil (C x xs)
       `by` (axiom_append_C N x xs)
                                       -- == C x (xs ++ N)
       `by` (prop_app_nil xs)
--}                                      -- == C x xs
-
+                                      -- == C x xs
+-}
 
 -- autoEq ::x:a -> y:a -> {v:a | v == y && x == y }
 
@@ -69,12 +71,15 @@ auto (e1 == e && e == e2)
 
 
 -- | Proof 2: append is associative
-
-{-@ prop_assoc :: xs:L a -> ys:L a -> zs:L a
-               -> {v:Proof | append (append xs ys) zs == append xs (append ys zs) } @-}
+{-
+{- prop_assoc :: xs:L a -> ys:L a -> zs:L a
+               -> {v:Proof | append (append xs ys) zs /= append xs (append ys zs) } @-}
 prop_assoc :: Eq a => L a -> L a -> L a -> Proof
 
-prop_assoc N ys zs        = auto 2 (append (append N ys) zs == append N (append ys zs))
+prop_assoc N ys zs     --    = auto 2 (append (append N ys) zs == append N (append ys zs))
+ =  axiom_append_N zs             -- == append ys zs
+-- f    `by` axiom_append_N (append ys zs) -- == append N (append ys zs)
+
 
 --   refl (append (append N ys) zs)
 --   `by` axiom_append_N ys             -- == append ys zs
@@ -82,10 +87,9 @@ prop_assoc N ys zs        = auto 2 (append (append N ys) zs == append N (append 
 
 
 prop_assoc (C x xs) ys zs
--- NV HERE: this takes too long
-    = auto 2 (append (append (C x xs) ys) zs == append (C x xs) (append ys zs))
---     = refl e1
---       `by` pr1 `by` pr2 `by` pr3 `by` pr4
+--      = auto 2 (append (append (C x xs) ys) zs == append (C x xs) (append ys zs))
+     = refl e1
+       `by` pr1 `by` pr2 `by` pr3 `by` pr4
   where
     e1  = append (append (C x xs) ys) zs
     pr1 = axiom_append_C ys x xs
@@ -96,7 +100,7 @@ prop_assoc (C x xs) ys zs
     e4  = C x (append xs (append ys zs))
     pr4 = axiom_append_C (append ys zs) x xs
     e5  = append (C x xs) (append ys zs)
-
+-}
 
 {-@ data L [llen] @-}
 {-@ invariant {v: L a | llen v >= 0} @-}
