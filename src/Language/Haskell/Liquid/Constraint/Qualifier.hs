03c680aa790e69ad047abe93e977304c2a73271d
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-12-07 14:05:34 -0800

    Asdasd

--- src/Language/Haskell/Liquid/Constraint/Qualifier.hs
+++ src/Language/Haskell/Liquid/Constraint/Qualifier.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE ViewPatterns          #-}
 {-# LANGUAGE PartialTypeSignatures #-}
 
-module Language.Haskell.Liquid.Qualifier (
+module Language.Haskell.Liquid.Constraint.Qualifier (
   specificationQualifiers
   ) where
 
@@ -22,14 +22,14 @@ import qualified Data.HashSet as S
 import Data.Bifunctor           (second)
 
 -----------------------------------------------------------------------------------
-specificationQualifiers :: Int -> GhcInfo -> [Qualifier]
+specificationQualifiers :: Int -> GhcInfo -> SEnv Sort -> [Qualifier]
 -----------------------------------------------------------------------------------
-specificationQualifiers k info
+specificationQualifiers k info lEnv
   = [ q | (x, t) <- (tySigs $ spec info) ++ (asmSigs $ spec info) ++ (ctors $ spec info)
         -- FIXME: this mines extra, useful qualifiers but causes a significant increase in running time
         -- , ((isClassOp x || isDataCon x) && x `S.member` (S.fromList $ impVars info ++ defVars info)) || x `S.member` (S.fromList $ defVars info)
         , x `S.member` (S.fromList $ defVars info)
-        , q <- refTypeQuals (getSourcePos x) (tcEmbeds $ spec info) (val t)
+        , q <- refTypeQuals lEnv (getSourcePos x) (tcEmbeds $ spec info) (val t)
         , length (q_params q) <= k + 1
     ]
 
@@ -52,16 +52,17 @@ specificationQualifiers k info
 
 -- TODO: rewrite using foldReft'
 -- refTypeQuals :: SpecType -> [Qualifier]
-refTypeQuals :: _ -> _ -> SpecType -> [Qualifier]
-refTypeQuals l tce t0        = go emptySEnv t0
+refTypeQuals :: SEnv Sort -> _ -> _ -> SpecType -> [Qualifier]
+refTypeQuals lEnv l tce t0    = go emptySEnv t0
   where
-    go γ t@(RVar _ _)         = refTopQuals l tce t0 γ t
+    scrape                    = refTopQuals lEnv l tce t0
+    go γ t@(RVar _ _)         = scrape γ t
     go γ (RAllT _ t)          = go γ t
     go γ (RAllP p t)          = go (insertSEnv (pname p) (rTypeSort tce $ (pvarRType p :: RSort)) γ) t
-    go γ t@(RAppTy t1 t2 _)   = go γ t1 ++ go γ t2 ++ refTopQuals l tce t0 γ t
+    go γ t@(RAppTy t1 t2 _)   = go γ t1 ++ go γ t2 ++ scrape γ t
     go γ (RFun x t t' _)      = (go γ t)
                                 ++ (go (insertSEnv x (rTypeSort tce t) γ) t')
-    go γ t@(RApp c ts rs _)   = (refTopQuals l tce t0 γ t)
+    go γ t@(RApp c ts rs _)   = (scrape γ t)
                                 ++ concatMap (go (insertSEnv (rTypeValueVar t) (rTypeSort tce t) γ)) ts
                                 ++ goRefs c (insertSEnv (rTypeValueVar t) (rTypeSort tce t) γ) rs
     go γ (RAllE x t t')       = (go γ t)
@@ -75,39 +76,44 @@ refTypeQuals l tce t0        = go emptySEnv t0
     goRef _ (RHProp _ _)  _   = errorstar "TODO: EFFECTS"
     insertsSEnv               = foldr (\(x, t) γ -> insertSEnv x (rTypeSort tce t) γ)
 
-refTopQuals l tce t0 γ t
-  = [ mkQual l t0 γ v so pa  | let (RR so (Reft (v, ra))) = rTypeSortedReft tce t
-                             , pa                        <- conjuncts ra
-                             , not $ isHole pa
-    ] ++
-    [ mkPQual l tce t0 γ s e | let (U _ (Pr ps) _) = fromMaybe (msg t) $ stripRTypeBase t
+refTopQuals lEnv l tce t0 γ t
+  = [ mkQ v so pa  | let (RR so (Reft (v, ra))) = rTypeSortedReft tce t
+                                  , pa                        <- conjuncts ra
+                                  , not $ isHole pa
+    ]
+    ++
+    [ mkP s e | let (U _ (Pr ps) _) = fromMaybe (msg t) $ stripRTypeBase t
                              , p <- findPVar (ty_preds $ toRTypeRep t0) <$> ps
                              , (s, _, e) <- pargs p
     ]
     where
+      mkQ   = mkQual lEnv l t0 γ
+      mkP   = mkPQual lEnv l tce t0 γ
       msg t = errorstar $ "Qualifier.refTopQuals: no typebase" ++ showpp t
 
-mkPQual l tce t0 γ t e = mkQual l t0 γ' v so pa
+mkPQual lEnv l tce t0 γ t e = mkQual lEnv l t0 γ' v so pa
   where
     v                  = "vv"
     so                 = rTypeSort tce t
     γ'                 = insertSEnv v so γ
     pa                 = PAtom Eq (EVar v) e
 
-mkQual l t0 γ v so p   = Q "Auto" ((v, so) : yts) p' l
+mkQual lEnv l t0 γ v so p   = Q "Auto" ((v, so) : yts) p' l
   where
-    yts                = [(y, lookupSort γ i x) | (x, i, y) <- xys ]
+    yts                = [(y, lookupSort l γ i x) | (x, i, y) <- xys ]
     p'                 = subst su p
     su                 = mkSubst [(x, EVar y) | (x, _, y) <- xys]
     xys                = zipWith (\x i -> (x, i, symbol ("~A" ++ show i))) xs [0..]
     -- xs                 = delete v $ orderedFreeVars γ p
-    xs                 = delete v $ syms p
+    xs                 = delete v $ orderedFreeVars lEnv p
 
-lookupSort γ i x = fromMaybe ai $ lookupSEnv x γ
+orderedFreeVars :: SEnv Sort -> Pred -> [Symbol]
+orderedFreeVars lEnv = nub . filter (not . (`memberSEnv` lEnv)) . syms
+
+lookupSort l γ i x = fromMaybe ai $ lookupSEnv x γ
   where
-    ai           = FVar i
+    ai           = fObj $ Loc l l $ tempSymbol "LHTV" i
 
-    -- orderedFreeVars γ = nub . filter (`memberSEnv` γ) . syms
 
 -- HEREHEREHERE
 -- lookupSort γ i x = fromMaybe (errorstar msg) $ lookupSEnv x γ
