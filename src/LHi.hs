8e3f004a663e91ce946b5433298aa92b338f8e09
Ranjit Jhala <jhala@cs.ucsd.edu>
2015-12-09 15:43:51 -0800

    asd

--- src/LHi.hs
+++ src/LHi.hs
@@ -1,6 +1,27 @@
-import System.Environment             (getArgs)
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+import           System.Environment      (getArgs)
+import           System.Daemon
+import           Control.Concurrent.MVar ( newMVar )
+import           Data.Default ( def )
+import           Language.Haskell.Liquid.Interactive.Types
+import qualified Language.Haskell.Liquid.Interactive.Handler as H
+
+
+daemonName :: String
+daemonName = "lhi"
 
 main :: IO ()
-main = lhi =<< getArgs
+main = do
+  st  <- newMVar H.init
+  cmd <- command
+  ensureDaemonRunning daemonName (options cmd) (H.handler st)
+  res <- client cmd
+  print res
 
-lhi _ = putStrLn "Hello, world!"
+options :: Command -> DaemonOptions
+options cmd = def { daemonPort = port cmd }
+
+client :: Command -> IO (Maybe Response)
+client cmd = runClient "localhost" (port cmd) cmd
