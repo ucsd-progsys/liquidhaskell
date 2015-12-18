c3d7001c52c41e10c9aadd9ac2bf87f94a4477f9
nikivazou <nikivazou@gmail.com>
2015-12-17 18:16:46 -0800

    adding todo steps

--- tests/equationalproofs/pos/MapFusion.hs
+++ tests/equationalproofs/pos/MapFusion.hs
@@ -16,6 +16,7 @@
 module Append where
 
 import Axiomatize
+import Equational 
 import Prelude hiding (map)
 
 data L a = N |  C a (L a)
@@ -44,10 +45,35 @@ $(axiomatize
       compose f g x = f (g x) 
     |])
 
-
 -- axiom_compose :: forall f g x. 
 
 
+{-
+TODO 
+ 1. when sig is on it is crashing
+ 2. when auto is on it is crashing
+ 3. define the Cons case 
+
+-}
+
+{- prop_fusion :: f:(a -> a) -> g:(a -> a) -> xs:L a
+                -> {v:Proof | map f (map g xs) ==  map (compose f g) xs }  @-}
+prop_fusion     :: Eq a => (a -> a) -> (a -> a) -> L a -> Proof
+prop_fusion f g N = 
+--    auto 2 (map f (map g N) == map (compose f g) N)
+   refl e1 `by` pr1 `by` pr2 `by` pr3
+  where
+    e1  = map f (map g N)
+    pr1 = axiom_map_N g
+    e2  = map f N
+    pr2 = axiom_map_N f
+    e3  = N
+    fg  = compose f g
+    pr3 = axiom_map_N (compose f g)
+    e4  = map fg N
+
+prop_fusion f g (C x xs) 
+  = undefined  
 
 
 
