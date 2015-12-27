bc1d16a16ef918b48fd9ad3060cda1aa26429c16
nikivazou <nikivazou@gmail.com>
2015-12-26 22:49:54 -0800

    new examples fixed

--- tests/equationalproofs/pos/MapAppend.hs
+++ tests/equationalproofs/pos/MapAppend.hs
@@ -3,7 +3,7 @@
 {-# LANGUAGE ExtendedDefaultRules #-}
 
 {-@ LIQUID "--autoproofs"      @-}
-{-@ LIQUID "--totality"        @-}
+{- LIQUID "--totality"        @-}
 {-@ LIQUID "--exact-data-cons" @-}
 
 module Append where
@@ -22,7 +22,7 @@ instance Eq a => Eq (L a) where
 $(axiomatize
   [d| map :: (a -> b) -> L a -> L b
       map f N        = N
-      map f (C x xs) = f x `C` map f xs 
+      map f (C x xs) = C (f x) (map f xs) 
     |])
 
 
@@ -34,8 +34,12 @@ $(axiomatize
     |])
 
 
+
 --   "map/append" forall f xs ys. map f (xs ++ ys) = map f xs ++ map f ys
-prop_map_append :: (a -> b) -> L a -> L a -> Prop 
+{-@ prop_map_append ::  f:(a -> a) -> xs:L a -> ys:L a 
+                    -> {v:Proof | map f (append xs ys) == append (map f xs) (map f ys) }
+  @-}
+prop_map_append :: Eq a => (a -> a) -> L a -> L a -> Proof
 prop_map_append f N ys = 
   auto 2 (map f (N `append` ys) == map f N `append` map f ys) 
 
@@ -46,7 +50,23 @@ prop_map_append f N ys =
 -}  
 
 prop_map_append f (C x xs) ys = 
-  auto 3 (map f ((C x xs) `append` ys) == map f (C x xs) `append` map f ys) 
+  auto 2  (map f (append (C x xs) ys) == {- <== -} append (map f (C x xs)) (map f ys))
+{-  
+  -- refl (append (map f (C x xs)) (map f ys))
+  --   `by` pr1 `by` pr2 `by` pr3 `by` pr4 `by` pr5 
+    where
+      e1  = append (map f (C x xs)) (map f ys)
+      pr1 = axiom_map_C f x xs
+      e2  = append (C (f x) (map f xs)) (map f ys)
+      pr2 = axiom_append_C  (map f ys) (f x) (map f xs)
+      e3  = C (f x) (append (map f xs) (map f ys))
+      pr3 = prop_map_append f xs ys
+      e4 = C (f x) (map f (append xs ys))
+      pr4 = axiom_map_C f x (append xs ys)
+      e5  = map f (C x (append xs ys))
+      pr5 = axiom_append_C ys x xs
+      e6  = map f (append (C x xs) ys)
+-}
 
 
 {-@ data L [llen] @-}
