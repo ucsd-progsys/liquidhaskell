194c07102b3567909dca75e86a507ba1343a8854
nikivazou <nikivazou@gmail.com>
2016-01-06 17:24:23 -0800

    fix more tests

--- benchmarks/icfp15/pos/TwiceM.hs
+++ benchmarks/icfp15/pos/TwiceM.hs
@@ -25,10 +25,10 @@ twiceM f w = let (RIO g) = f w in RIO $ \x -> case g x of {(y, s) -> let ff = \_
 
 {-@ measure counter :: World -> Int @-}
 
-{-@ incr :: RIO <{\x -> counter x >= 0}, {\w1 x w2 -> counter w2 = counter w1 + 1}>  Nat Nat @-}
+{-@ incr :: RIO <{\x -> counter x >= 0}, {\w1 x w2 -> counter w2 = counter w1 + 1}>  Nat @-}
 incr :: RIO Int
 incr = undefined
 
-{-@ incr2 :: RIO <{\x -> counter x >= 0}, {\w1 x w2 -> counter w2 = counter w1 + 2}> Nat Nat @-}
+{-@ incr2 :: RIO <{\x -> counter x >= 0}, {\w1 x w2 -> counter w2 = counter w1 + 2}> Nat @-}
 incr2 :: RIO Int
 incr2 = twiceM (\_ -> incr) 0
