603b8db9dc6690cfd18f4d1967546469610763dc
Rolph Recto <rolph.recto@gmail.com>
2016-01-20 16:07:46 -0500

    fixed possible exception bug in runSolverM

--- src/Language/Fixpoint/Solver/Monad.hs
+++ src/Language/Fixpoint/Solver/Monad.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 -- | This is a wrapper around IO that permits SMT queries
 
@@ -79,13 +80,16 @@ instance PTable Stats where
 runSolverM :: Config -> F.GInfo c b -> Int -> SolveM a -> IO a
 ---------------------------------------------------------------------------
 runSolverM cfg fi _ act = do
-  bracket
-    (makeContext (not $ real cfg) (solver cfg) file)
-    (cleanupContext)
-    (\ctx -> fst <$> runStateT (declare fi >> act) (SS ctx be $ stats0 fi))
+  bracket acquire release $ \ctx -> do
+    res <- runStateT (declare fi >> act) (SS ctx be $ stats0 fi)
+    smtWrite ctx "(exit)"
+    return $ fst res
+      
   where
-    be   = F.bs     fi
-    file = F.fileName fi -- (inFile cfg)
+    acquire = makeContext (not $ real cfg) (solver cfg) file
+    release = cleanupContext
+    be      = F.bs     fi
+    file    = F.fileName fi -- (inFile cfg)
 ---------------------------------------------------------------------------
 getBinds :: SolveM F.BindEnv
 ---------------------------------------------------------------------------
