ad9a689d42a4972eba37ed78d7f0fce9c7a968a6
Ranjit Jhala <jhala@cs.ucsd.edu>
2016-01-25 15:50:52 -0800

    de-monadize scc-deps computation

--- src/Language/Fixpoint/Solver/Deps.hs
+++ src/Language/Fixpoint/Solver/Deps.hs
@@ -17,12 +17,20 @@ data Deps = Deps { depCuts    :: !(S.HashSet KVar)
                  }
             deriving (Show)
 
---------------------------------------------------------------
--- | Compute Dependencies and Cuts ---------------------------
---------------------------------------------------------------
+instance Monoid Deps where
+  mempty                            = Deps S.empty S.empty
+  mappend (Deps d1 n1) (Deps d2 n2) = Deps (S.union d1 d2) (S.union n1 n2)
+
+dCut, dNonCut :: KVar -> Deps
+dNonCut v = Deps S.empty (S.singleton v)
+dCut    v = Deps (S.singleton v) S.empty
+
+--------------------------------------------------------------------------------
+-- | Compute Dependencies and Cuts ---------------------------------------------
+--------------------------------------------------------------------------------
 
 deps :: SInfo a -> Deps
-deps fi = sccsToDeps sccs (kuts fi)
+deps fi   = sccsToDeps (kuts fi) sccs
   where
     subCs = M.elems (cm fi)
     edges = concatMap (subcEdges $ bs fi) subCs
@@ -32,15 +40,30 @@ deps fi = sccsToDeps sccs (kuts fi)
     graph = [(k,k,ks) | (k, ks) <- groupList (edges ++ extraEdges)]
     sccs  = G.stronglyConnCompR graph
 
-sccsToDeps :: [G.SCC (KVar,KVar,[KVar])] -> Kuts -> Deps
+sccsToDeps :: Kuts -> [G.SCC (KVar, KVar, [KVar])] -> Deps
+sccsToDeps ks xs = mconcat $ sccDep ks <$> xs
+
+sccDep :: Kuts -> G.SCC (KVar, KVar, [KVar]) -> Deps
+sccDep _ (G.AcyclicSCC (v,_,_)) = dNonCut v
+sccDep ks (G.CyclicSCC vs)      = cycleDep ks vs
+
+cycleDep _ []  = mempty
+cycleDep ks vs = mconcat $ dCut v : (sccDep ks <$> sccs)
+  where
+    (v, vs')   = chooseCut vs ks
+    sccs       = G.stronglyConnCompR vs'
+
+{-
+sccsToDeps :: [G.SCC (KVar, KVar, [KVar])] -> Kuts -> Deps
 sccsToDeps xs ks = execState (mapM_ go xs) (Deps S.empty S.empty)
   where
     go (G.AcyclicSCC (v,_,_)) = do ds <- get
                                    put ds {depNonCuts = S.insert v $ depNonCuts ds}
-    go (G.CyclicSCC vs)       = do let (v,vs') = chooseCut vs ks
+    go (G.CyclicSCC vs)       = do let (v, vs') = chooseCut vs ks
                                    ds <- get
                                    put ds {depCuts = S.insert v $ depCuts ds}
                                    mapM_ go (G.stronglyConnCompR vs')
+-}
 
 chooseCut :: [(KVar,KVar,[KVar])] -> Kuts -> (KVar, [(KVar,KVar,[KVar])])
 chooseCut vs (KS ks) = (v, [x | x@(u,_,_) <- vs, u /= v])
