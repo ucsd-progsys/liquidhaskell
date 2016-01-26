b2d03053584a453f731eacc4a14059b45377a976
nikivazou <nikivazou@gmail.com>
2016-01-26 00:33:12 -0800

    instance loc

--- src/Language/Fixpoint/Types/Spans.hs
+++ src/Language/Fixpoint/Types/Spans.hs
@@ -50,6 +50,9 @@ import           Text.Printf
 class Loc a where
   srcSpan :: a -> SrcSpan
 
+instance Loc (Located a) where
+  srcSpan v = SS (loc v) (locE v)
+
 -----------------------------------------------------------------------
 -- | Retrofitting instances to SourcePos ------------------------------
 -----------------------------------------------------------------------
