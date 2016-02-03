25f4e520b32fbfa2267e1261e731d7e177da1403
nikivazou <nikivazou@gmail.com>
2016-02-03 15:39:39 -0800

    update fixpoint submodule and commit neg gradual test

--- tests/neg/Gradual.hs
+++ tests/neg/Gradual.hs
@@ -1,8 +1,10 @@
 module Gradual where
 
-{-@ safe :: {v:Int | ?? } -> (Int, Int) @-}
-safe :: Int -> (Int, Int)
-safe x = (bar1 x, bar2 x)
+
+{-@ unsafe :: {v:Int | ?? } -> Int  -> (Int, Int) @-}
+unsafe :: Int -> Int -> (Int, Int)
+unsafe _ x = (bar1 x, bar2 x)
+
 
 {-@ bar1 :: {v:Int | v < 0} -> Int @-}
 bar1 :: Int -> Int 
