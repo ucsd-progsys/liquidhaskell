8a2808a7af1cbacde400c50588f436c91ac10bf5
Ranjit Jhala <jhala@cs.ucsd.edu>
2016-03-29 11:29:48 +0200

    twiddle

--- tests/todo/predparams.hs
+++ tests/todo/predparams.hs
@@ -1,6 +1,8 @@
+
 -- ISSUE: this "crashes" without a decent source location
 -- You can fix this with the signature `ide :: forall <p :: a -> Prop>. a<p> -> a<p>`
 -- but it would be nice to have an error message that pinpoints the location.
+-- https://github.com/ucsd-progsys/liquidhaskell/issues/655
 
 module Ide where
 
