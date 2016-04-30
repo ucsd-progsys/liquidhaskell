7d2daebf04424b4ca813e6ccb92b29562073e6fc
Ranjit Jhala <jhala@cs.ucsd.edu>
2016-04-29 21:59:27 -0700

    use patternInline flag in ANF

--- src/Language/Fixpoint/Solver/Solution.hs
+++ src/Language/Fixpoint/Solver/Solution.hs
@@ -36,7 +36,7 @@ import           Prelude                        hiding (init, lookup)
 
 -- DEBUG
 -- import Text.Printf (printf)
-import           Debug.Trace (trace)
+-- import           Debug.Trace (trace)
 
 --------------------------------------------------------------------------------
 -- | Expanded or Instantiated Qualifier ----------------------------------------
@@ -199,7 +199,7 @@ bindExprs (_,be,_) i = [p `F.subst1` (v, F.eVar x) | F.Reft (v, p) <- rs ]
 applyExpr :: CombinedEnv -> Solution -> F.Expr -> ExprInfo
 applyExpr g s (F.PKVar k su)
   | kI == mempty =           (e, kI)
-  | otherwise    = trace msg (e, kI)
+  | otherwise    = {- trace msg -} (e, kI)
   where
     msg     = "applyKVar: " ++ show k ++ " info =" ++ show kI
     (e, kI) = applyKVar g s k su
