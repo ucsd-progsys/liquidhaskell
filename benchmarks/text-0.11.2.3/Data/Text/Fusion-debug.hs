eb37cbf3ef9cd3aedc91f4c700303ef7866a7a5a
Ranjit Jhala <jhala@cs.ucsd.edu>
2016-05-17 09:31:03 -0700

    print Packs

--- benchmarks/text-0.11.2.3/Data/Text/Fusion-debug.hs
+++ benchmarks/text-0.11.2.3/Data/Text/Fusion-debug.hs
@@ -30,12 +30,12 @@ mapAccumL f z0 (Stream next0 s0 len) = (nz, I.textP na 0 nl)
   where
     mlen          = upperBound 4 len
     (na, (nz,nl)) = runST
-                      (blob f next0 mlen z0 s0)
-                      -- ( do arr0      <- A.new mlen
-                           -- (marr, x) <- outerL f next0 arr0 mlen z0 s0 0
-                           -- arr       <- A.unsafeFreeze marr
-                           -- return (arr, x) )
-                      
+                      -- (blob f next0 mlen z0 s0)
+                       ( do arr0      <- A.new mlen
+                            (marr, x) <- outerL f next0 arr0 mlen z0 s0 0
+                            arr       <- A.unsafeFreeze marr
+                            return (arr, x) )
+
 blob f next0 mlen z0 s0 = do
   arr0      <- A.new mlen
   (marr, x) <- outerL f next0 arr0 mlen z0 s0 0
