0871bf3ee910b1855f6d734a40cae77d8c59a533
nikivazou <nikivazou@gmail.com>
2016-06-01 12:26:26 -0700

    foldr universal

--- benchmarks/haskell16/pos/FoldrUniversal.hs
+++ benchmarks/haskell16/pos/FoldrUniversal.hs
@@ -26,7 +26,7 @@ foldr f b xs
   -> e:b
   -> ys:L a
   -> base:{h Emp == e }
-  -> ih  : (x:a -> xs:L a -> {h (C x xs) == f x (h xs)})
+  -> step: (x:a -> xs:L a -> {h (C x xs) == f x (h xs)})
   -> { h ys == foldr f e ys }
   @-}
 foldrUniversal
@@ -37,41 +37,41 @@ foldrUniversal
     -> Proof
     -> (a -> L a -> Proof)
     -> Proof
-foldrUniversal f h e Emp base hi
+foldrUniversal f h e Emp base step
   =   h Emp
   ==! e               -- ? base
   ==! foldr f e Emp
   *** QED
-foldrUniversal f h e (C x xs) base ih
+foldrUniversal f h e (C x xs) base step
   =   h (C x xs)
-  ==! f x (h xs)         ? ih x xs
-  ==! f x (foldr f e xs) ? foldrUniversal f h e xs base ih
+  ==! f x (h xs)         ? step x xs
+  ==! f x (foldr f e xs) ? foldrUniversal f h e xs base step
   ==! foldr f e (C x xs)
   *** QED
 
 -- | foldrFunsion
 
 {-@ foldrFusion :: h:(b -> c) -> f:(a -> b -> b) -> g:(a -> c -> c) -> e:b -> ys:L a
-            -> p1:(x:a -> y:b -> {v:Proof | h (f x y) == g x (h y)})
+            -> fuse:(x:a -> y:b -> {h (f x y) == g x (h y)})
             -> { (compose h (foldr f e)) (ys) == foldr g (h e) ys }
   @-}
 foldrFusion :: (b -> c) -> (a -> b -> b) -> (a -> c -> c) -> b -> L a
              -> (a -> b -> Proof)
              -> Proof
-foldrFusion h f g e ys thm
+foldrFusion h f g e ys fuse
   = foldrUniversal g (compose h (foldr f e)) (h e) ys
-       (prop_base h f e)
-       (prop_ind h f e g thm)
+       (fuse_base h f e)
+       (fuse_step h f e g fuse)
 
-prop_ind :: (b -> c) -> (a -> b -> b) -> b -> (a -> c -> c)
+fuse_step :: (b -> c) -> (a -> b -> b) -> b -> (a -> c -> c)
          -> (a -> b -> Proof)
          -> a -> L a -> Proof
-{-@ prop_ind :: h:(b -> c) -> f:(a -> b -> b) -> e:b -> g:(a -> c -> c)
-         -> thm:(x:a -> y:b -> {v:Proof | h (f x y) == g x (h y)})
+{-@ fuse_step :: h:(b -> c) -> f:(a -> b -> b) -> e:b -> g:(a -> c -> c)
+         -> thm:(x:a -> y:b -> { h (f x y) == g x (h y)})
          -> x:a -> xs:L a
          -> {(compose h (foldr f e)) (C x xs) == g x ((compose h (foldr f e)) (xs))}
   @-}
-prop_ind h f e g thm x Emp
+fuse_step h f e g thm x Emp
   =   (compose h (foldr f e)) (C x Emp)
   ==! h (foldr f e (C x Emp))
   ==! h (f x (foldr f e Emp))
@@ -81,7 +81,7 @@ prop_ind h f e g thm x Emp
   ==! g x ((compose h (foldr f e)) Emp)
   *** QED
 
-prop_ind h f e g thm x (C y ys)
+fuse_step h f e g thm x (C y ys)
   =   (compose h (foldr f e)) (C x (C y ys))
   ==! h (foldr f e (C x (C y ys)))
   ==! h (f x (foldr f e (C y ys)))
@@ -92,16 +92,14 @@ prop_ind h f e g thm x (C y ys)
   ==! g x ((compose h (foldr f e)) (C y ys))
   *** QED
 
-prop_base :: (b->c) -> (a -> b -> b) -> b -> Proof
-{-@ prop_base :: h:(b->c) -> f:(a -> b -> b) -> e:b
+fuse_base :: (b->c) -> (a -> b -> b) -> b -> Proof
+{-@ fuse_base :: h:(b->c) -> f:(a -> b -> b) -> e:b
               -> { (compose h (foldr f e)) (Emp) == h e } @-}
-prop_base h f e
+fuse_base h f e
   =   (compose h (foldr f e)) Emp
   ==! h (foldr f e Emp)
   ==! h e
   *** QED
-
-
 
 {-@ axiomatize compose @-}
 compose :: (b -> c) -> (a -> b) ->  a -> c
