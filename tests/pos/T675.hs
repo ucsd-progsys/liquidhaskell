6138b7feeafede1cc658459efeb1a829b968c5fa
Ranjit Jhala <jhala@cs.ucsd.edu>
2016-06-10 16:40:46 -0700

    add new test from issue #675

--- tests/pos/T675.hs
+++ tests/pos/T675.hs
@@ -9,7 +9,11 @@ import Data.ByteString.Unsafe
 extract :: ByteString -> ByteString
 extract = unsafeTake 4 . unsafeDrop 96
 
--- We need these qualifiers, or --eliminate or --scrape-quals 
+{-@ extractETA :: ibs : { ibs : ByteString | bslen ibs >= 100 } -> { obs : ByteString | bslen obs == 4 } @-}
+extractETA :: ByteString -> ByteString
+extractETA ibs = unsafeTake 4 (unsafeDrop 96 ibs)
+
+-- We need these qualifiers, or --eliminate or --scrape-quals
 
 {- qualif Bs1(obs:ByteString, n:Int): (bslen obs >= n) @-}
 {- qualif Bs2(obs:ByteString, n:Int): (bslen obs == n) @-}
@@ -17,9 +21,9 @@ extract = unsafeTake 4 . unsafeDrop 96
 {- qualif Plus1(v:Int, x:Int, y:Int): (v == x + y) @-}
 
 {-@ ok :: x:Int -> {v:Int | v == x + 3} @-}
-ok :: Int -> Int 
+ok :: Int -> Int
 ok x = 2 + (1 + x)
 
 {-@ bad :: x:Int -> {v:Int | v == x + 3} @-}
-bad :: Int -> Int 
+bad :: Int -> Int
 bad = (2 +) . (1 +)
