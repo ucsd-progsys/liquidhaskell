c2438fea9140ab13300bb3275bf5709bd9278181
Niki Vazou <niki@awakenetworks.com>
2016-06-17 10:42:29 -0700

    add chunk example

---
+++ tests/pos/Chunks.hs
@@ -0,0 +1,22 @@
+module Foo where
+
+{-@ type Pos = {v:Int | 0 < v} @-}
+
+{-@ predicate ValidChunk V XS N 
+    = if len XS == 0 
+        then (len V == 0) 
+        else (((1 < len XS && 1 < N) => (len V  < len XS)) 
+          && ((len XS <= N ) => len V == 1))            
+  @-}
+
+{-@ chunks :: n:Pos -> xs:[a] -> {v:[[a]] | ValidChunk v xs n } / [len xs] @-}
+chunks :: Int -> [a] -> [[a]]
+chunks _ [] = []
+
+chunks n xs
+  | length xs <= n 
+  = let (x, xs') = splitAt n xs in x:chunks n xs'
+chunks n xs
+  | otherwise
+  = let (x, xs') = splitAt n xs in x:chunks n xs'
+
