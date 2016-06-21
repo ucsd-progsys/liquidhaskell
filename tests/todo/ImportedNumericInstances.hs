4d71624351a9a12beb04e5b94c2103f491f8d150
Niki Vazou <niki@awakenetworks.com>
2016-06-21 10:23:56 -0700

    no crash for used defined num instances

--- tests/todo/ImportedNumericInstances.hs
+++ tests/todo/ImportedNumericInstances.hs
@@ -1,15 +1,19 @@
 module ImportedNumericInstances where
 
+{- embed System.Posix.Types.Fd as int @-}
+
 import System.Posix.Types (Fd)
 -- Fd is a data type that is imported to be a num instance
 
-{- embed System.Posix.Types.Fd as int @-}
-
-extractSocketFd0 :: Maybe [a] -> Fd
-extractSocketFd0 = 
- maybe 0 (fromIntegral . getFoo)
+testToFix :: Maybe [a] -> Fd 
+testToFix =  maybe 0 (fromIntegral . getFoo)
 
 
+testOK :: Maybe [a] -> Boo 
+testOK =  maybe 0 (fromIntegral . getFoo)
+
+instance Num Boo where
+data Boo 
 
 {-@ getFoo :: {v:[a] | 0 <= len v}  -> Int @-} 
 getFoo :: [a] -> Int 
