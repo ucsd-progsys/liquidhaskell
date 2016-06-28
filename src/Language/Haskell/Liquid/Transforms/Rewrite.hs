2fa41c10c68cd7bc43aa588e7b495d89863d3307
Niki Vazou <niki@awakenetworks.com>
2016-06-27 18:28:41 -0700

    rename example

--- src/Language/Haskell/Liquid/Transforms/Rewrite.hs
+++ src/Language/Haskell/Liquid/Transforms/Rewrite.hs
@@ -49,7 +49,7 @@ import           Language.Haskell.Liquid.UX.Config  (Config, noSimplifyCore)
 --------------------------------------------------------------------------------
 rewriteBinds :: Config -> [CoreBind] -> [CoreBind]
 rewriteBinds cfg
-  | simplifyCore cfg = fmap (rewriteBindWith safeSimplifyPatTuple)
+  | simplifyCore cfg = fmap (rewriteBindWith simplifyPatTuple)
   | otherwise        = id
 
 simplifyCore :: Config -> Bool
@@ -150,8 +150,8 @@ rewriteWith tx           = go
 
 -}
 
-safeSimplifyPatTuple :: RewriteRule
-safeSimplifyPatTuple e 
+_safeSimplifyPatTuple :: RewriteRule
+_safeSimplifyPatTuple e 
   = find ((CoreUtils.exprType e ==) . CoreUtils.exprType) (simplifyPatTuple e)  
 
 --------------------------------------------------------------------------------
