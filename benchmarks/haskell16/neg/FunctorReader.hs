cde2c503fd0bfceb13f4b8c020274b7e85fa9af1
nikivazou <nikivazou@gmail.com>
2016-06-28 23:07:26 -0700

    functor reader id

--- benchmarks/haskell16/neg/FunctorReader.hs
+++ benchmarks/haskell16/neg/FunctorReader.hs
@@ -3,13 +3,15 @@
 {-@ LIQUID "--exact-data-cons" @-}
 {-@ LIQUID "--extensionality"  @-}
 
-{-# LANGUAGE IncoherentInstances #-}
-{-# LANGUAGE FlexibleContexts    #-}
+
+{-# LANGUAGE IncoherentInstances   #-}
+{-# LANGUAGE FlexibleContexts #-}
 module FunctorList where
 
 import Prelude hiding (fmap, id)
 
 import Proves
+import Helper
 
 -- | Functor Laws :
 -- | fmap-id fmap id ≡ id
@@ -30,42 +32,17 @@ id x = x
 compose :: (b -> c) -> (a -> b) -> a -> c
 compose f g x = f (g x)
 
--- | Identity 
-
 {-@ fmap_id :: xs:Reader r a -> { fmap id xs == id xs } @-}
-fmap_id :: Arg r => Reader r a  ->  Proof
+fmap_id :: Reader r a  ->  Proof
 fmap_id (Reader x) 
    =   fmap id (Reader x)
    ==! Reader (\r -> id (x r))
-   ==! Reader (\r -> x r)       ? fmap_id_helper x  
+   ==! Reader (\r -> x r)      
    ==! Reader x                
    ==! id (Reader x)
    *** QED
  
-
-{-@ fmap_id_helper ::  f:(r -> a)
-            -> { (\r:r -> (id (f r))) == (\r:r-> (f (r))) } @-}
-fmap_id_helper :: (Arg r) => (r -> a) ->  Proof
-fmap_id_helper f
-   =    ((\r -> id (f r)) 
-   =*=! (\r -> f r)) (fmap_id_helper_body f)
-   *** QED 
-
-
-{-@ fmap_id_helper_body 
-  :: f:(r -> a) -> r:r 
-  -> {(id (f r) == f r) 
-      && ((\r:r -> (id (f r))) (r) == id (f r))  
-      && ((\r:r-> (f r)) (r) == f r) 
-      } @-} 
-fmap_id_helper_body :: Arg r => (r -> a) -> r -> Proof 
-fmap_id_helper_body f r 
-  = id (f r) ==! f r *** QED 
-
-
-
-
-{- fmap_distrib :: f:(a -> a) -> g:(a -> a) -> xs:Reader r a
+{-@ fmap_distrib :: f:(a -> a) -> g:(a -> a) -> xs:Reader r a
                -> { fmap  (compose f g) xs == (compose (fmap f) (fmap g)) (xs) } @-}
 fmap_distrib :: (a -> a) -> (a -> a) -> Reader r a -> Proof
 fmap_distrib f g (Reader x)
@@ -77,8 +54,3 @@ fmap_distrib f g (Reader x)
   ==! fmap f (fmap g (Reader x))
   ==! (compose (fmap f) (fmap g)) (Reader x)
   *** QED
-
-
-{-@ qual :: f:(r -> a) -> {v:Reader r a | v == Reader f} @-}
-qual :: (r -> a) -> Reader r a 
-qual = Reader 
