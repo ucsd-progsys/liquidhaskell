c6f36afbe1d3d5c76f8b5dea59ed406ea675411e
nikivazou <nikivazou@gmail.com>
2016-06-30 02:53:11 -0700

    right identity

--- benchmarks/haskell16/pos/MonadReader.hs
+++ benchmarks/haskell16/pos/MonadReader.hs
@@ -35,6 +35,12 @@ bind (Reader x) f = Reader (\r -> fromReader (f (x r)) r)
 fromReader :: Reader r a -> r -> a 
 fromReader (Reader f) = f
 
+-- NV TODO the following is needed because Reader is not interpreted by
+-- Contraints.Generate.lamExpr
+
+{-@ axiomatize reader @-}
+reader x = Reader x 
+
  
 {-@ readerId :: f:(Reader r a) -> {f == Reader (fromReader f)} @-} 
 readerId :: (Reader r a) -> Proof 
@@ -55,19 +61,74 @@ left_identity x f
   ==! f x                       ? readerId (f x)
   *** QED 
 
-{- 
 -- | Right Identity
 
-{- right_identity :: x:Reader r a -> { bind x return == x } @-}
-right_identity :: Reader r a -> Proof
+
+{-@ right_identity :: x:Reader r a -> { bind x return == x }
+ @-}
+right_identity :: Arg r => Reader r a -> Proof
 right_identity (Reader x)
   =   bind (Reader x) return
   ==! Reader (\r -> fromReader (return (x r)) r)
-  ==! Reader (\r -> fromReader (Reader (\r' ->  (x r))) r)
-  ==! Reader (\r -> (\r' ->  (x r)) r)
-  ==! Reader (\r -> x r)
-  ==! Reader x
+  ==! Reader (\r -> fromReader (reader (\r' ->  (x r))) (r))
+       ? right_identity_helper x 
+  ==! Reader (\r -> (\r' ->  (x r)) (r)) 
+       ? right_identity_helper1 x 
+  ==! Reader (\r -> x r)           
+       -- ? right_identity_helper2 x       
+  ==! Reader x                           
+       ? lambda_expand x 
   *** QED 
+
+
+right_identity_helper1 :: Arg r => (r -> a) -> Proof 
+{-@ right_identity_helper1 :: Arg r => x:(r -> a) 
+  -> {(\r:r -> fromReader (reader (\r':r ->  (x r))) (r)) == (\r:r -> (\r':r ->  (x r)) (r))} @-}
+right_identity_helper1 x =
+  ((\r -> (\r' ->  (x r)) (r)) =*=! (\r -> fromReader (reader (\r' ->  (x r))) (r)))
+    (right_identity_helper1_body x) *** QED 
+
+
+right_identity_helper1_body :: Arg r => (r -> a) -> r -> Proof 
+{-@ right_identity_helper1_body :: Arg r => x:(r -> a) -> r:r 
+  -> {(fromReader (reader (\r':r ->  (x r))) (r) == (\r':r ->  (x r)) (r))
+    && ((\r:r -> fromReader (reader (\r':r ->  (x r))) (r)) (r) == (fromReader (reader (\r':r ->  (x r))) (r))) 
+    && ((\r:r -> (\r':r ->  (x r)) (r)) (r) == ((\r':r ->  (x r)) (r)))
+    } @-}
+right_identity_helper1_body x r
+  =  fromReader (reader (\r' -> (x r))) r 
+  ==! (\r' -> x r) r 
+  *** QED 
+
+
+right_identity_helper2 :: Arg r => (r -> a) -> Proof 
+{-@ right_identity_helper2 :: Arg r => x:(r -> a) 
+  -> { (\r:r -> (\r':r ->  (x r)) (r)) ==  (\r:r -> x r) } @-}
+right_identity_helper2 _ = simpleProof
+
+
+right_identity_helper :: Arg r => (r -> a) -> Proof 
+{-@ right_identity_helper :: Arg r => x:(r -> a) 
+  -> {(\r:r -> fromReader (return (x r)) r) == (\r:r -> fromReader (reader (\r':r ->  (x r))) (r))} @-}
+right_identity_helper x
+  =  (
+     (\r -> fromReader (return (x r)) r)
+     =*=! 
+     (\r -> fromReader (reader (\r' ->  (x r))) (r))
+     )  (right_identity_helper_body x) *** QED 
+
+right_identity_helper_body :: Arg r => (r -> a) -> r -> Proof 
+{-@ right_identity_helper_body :: Arg r => x:(r -> a) -> r:r
+  -> { (fromReader (return (x r)) r == fromReader (reader (\r':r ->  (x r))) (r))
+       && (((\r:r -> fromReader (return (x r)) r)) (r) == (fromReader (return (x r)) r))
+       && ((\r:r -> fromReader (reader (\r':r ->  (x r))) (r))(r) == (fromReader (reader (\r':r ->  (x r))) (r)))
+      } @-}
+right_identity_helper_body x r 
+  =   fromReader (return (x r)) r
+  ==! fromReader (reader (\r' ->  (x r))) r
+  *** QED 
+
+{- 
 
 -- | Associativity:	  (m >>= f) >>= g ≡	m >>= (\x -> f x >>= g)
 
@@ -101,8 +162,8 @@ associativity (Reader x) f g
   -- fold bind 
   ==! bind (Reader x) (\r4 ->(bind (f r4) g))
   *** QED  
+-}
 
 {-@ qual :: f:(r -> a) -> {v:Reader r a | v == Reader f} @-}
 qual :: (r -> a) -> Reader r a 
 qual = Reader 
--}
