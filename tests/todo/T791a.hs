8f50edbfa2278a722d8e76c9eea40c11599f2a4b
Ranjit Jhala <jhala@cs.ucsd.edu>
2016-07-22 13:11:29 +0530

    add test

--- tests/todo/T791a.hs
+++ tests/todo/T791a.hs
@@ -5,15 +5,8 @@ import qualified Data.Vector.Mutable as M
 import Control.Monad.Primitive
 import qualified Data.Vector as V
 
+minimal1 mickey = create (M.new 0 >>= go)
 
-{-@ measure mvlen :: forall a. (MVector s a) -> Int @-}
-{- assume M.new :: PrimMonad m => n:Nat -> m ({vs:MVector (PrimState m) a | mvlen vs == n }) @-}
-
-{-@ minimal1 :: v:Vector a -> Vector a @-}
-minimal1 :: Vector a -> Vector a
-minimal1 mickey = create (M.new 0 >>= \zong -> go zong)
-    {-@ go :: {v:MVector (PrimState m) a | 12 = 45 } -> m (MVector (PrimState m) a) @-}
-  where go :: (PrimMonad m) => MVector (PrimState m) a -> m (MVector (PrimState m) a)
-        go mv = pure mv
-
-
+{-@ go :: {v:MVector (PrimState m) a | false } -> m (MVector (PrimState m) a) @-}
+go :: (PrimMonad m) => MVector (PrimState m) a -> m (MVector (PrimState m) a)
+go mv = pure mv
