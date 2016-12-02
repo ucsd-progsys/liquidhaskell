337cc04eb71fa0c7e11af5cc322b210c1673cb35
Ranjit Jhala <jhala@cs.ucsd.edu>
2016-12-02 09:03:29 -0800

    issue is in simplify-core

---
+++ tests/pos/T866.hs
@@ -0,0 +1,11 @@
+-- see https://github.com/ucsd-progsys/liquidhaskell/issues/866
+
+module T866 where 
+
+data Body = Body Int Int 
+
+genBody :: [Int] -> Body
+genBody s   = Body x y 
+  where 
+    (x:y:_) = s
+
