667cb0640c7ecffcfe9fd0b1a1d0c54b9e1e6f65
Ranjit Jhala <jhala@cs.ucsd.edu>
2016-12-11 15:28:56 -0800

    yay tests/pos/bool0.hs works

---
+++ tests/pos/bool0.hs
@@ -0,0 +1,16 @@
+module PropMeasure where
+
+{-@ myhead :: {v:[a] | nonEmpty v} -> a @-}
+myhead (x:_) = x
+
+{-@ measure nonEmpty @-}   
+nonEmpty :: [a] -> Bool
+nonEmpty (x:xs) = True 
+nonEmpty []     = False
+
+
+
+
+
+
+
