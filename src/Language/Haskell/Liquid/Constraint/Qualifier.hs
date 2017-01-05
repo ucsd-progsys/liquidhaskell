c275779c8df53e88246903e9ad81ad5e1d277a78
Ranjit Jhala <jhala@cs.ucsd.edu>
2017-01-04 16:15:59 -0800

    flugh. where did the alsQuals go...

--- src/Language/Haskell/Liquid/Constraint/Qualifier.hs
+++ src/Language/Haskell/Liquid/Constraint/Qualifier.hs
@@ -30,7 +30,7 @@ qualifiers :: GhcInfo -> SEnv Sort -> [Qualifier]
 qualifiers info lEnv
   =  condNull (useSpcQuals info) (spcQualifiers info     )
   ++ condNull (useSigQuals info) (sigQualifiers info lEnv)
---  ++ condNull (useAlsQuals info) (alsQualifiers info lEnv)
+  ++ condNull (useAlsQuals info) (alsQualifiers info lEnv)
 
 -- --------------------------------------------------------------------------------
 -- qualifiers :: GhcInfo -> SEnv Sort -> [Qualifier]
