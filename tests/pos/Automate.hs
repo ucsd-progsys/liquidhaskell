1633aca173faa77eabb71067d57d749ff941cb64
nikivazou <nikivazou@gmail.com>
2017-01-07 20:40:59 +0200

    recursive call is required

--- tests/pos/Automate.hs
+++ tests/pos/Automate.hs
@@ -9,6 +9,11 @@ fibA :: Int -> Int
 fibA i | i <= 1 = i
       | otherwise = fibA (i-1) + fibA (i-2)
 
+fibUp :: Int -> Proof 
+{-@ fibUp :: i:Nat -> {fibA i <= fibA (i+1)} @-}
+fibUp i 
+ | i <= 2    = trivial
+ | otherwise = fibUp (i-1) &&& fibUp (i-2) *** QED 
 
 {-@ prop :: () -> {fibA 30 == 832040 } @-}
 prop :: () -> Proof 
