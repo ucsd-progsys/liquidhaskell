e46a2b56156e19b8b97bbe6163e7a0717c291e11
nikivazou <nikivazou@gmail.com>
2017-01-10 18:27:59 +0200

    fix for #914

---
+++ tests/pos/T914.hs
@@ -0,0 +1,12 @@
+{-@ LIQUID "--higherorder"     @-}
+
+module MapFusion where
+
+import Language.Haskell.Liquid.ProofCombinators
+
+{-@ inline compose @-}
+{-@ compose :: f:(b -> c) -> g:(a -> b) -> x:a -> c @-}
+compose :: (b -> c) -> (a -> b) -> a -> c
+compose f g x = f (g x)
+
+
