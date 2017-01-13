f51074523c1398f43f8aa50d1bd2c09cddb565db
Ranjit Jhala <jhala@cs.ucsd.edu>
2017-01-12 16:15:09 -0800

    add LSpec extension

--- src/Language/Fixpoint/Utils/Files.hs
+++ src/Language/Fixpoint/Utils/Files.hs
@@ -74,7 +74,7 @@ data Ext = Cgi      -- ^ Constraint Generation Information
          | Js       -- ^ JavaScript source
          | Ts       -- ^ Typescript source
          | Spec     -- ^ Spec file (e.g. include/Prelude.spec)
-         | LSpec    -- ^ Lifted-Spec file, containing automatically generated specifications 
+         | BinSpec  -- ^ Lifted-Spec file, containing automatically generated specifications 
          | Hquals   -- ^ Qualifiers file (e.g. include/Prelude.hquals)
          | Result   -- ^ Final result: SAFE/UNSAFE
          | Cst      -- ^ HTML file with templates?
@@ -116,7 +116,7 @@ extMap          = go
     go Mkdn     = ".markdown"
     go Json     = ".json"
     go Spec     = ".spec"
-    go LSpec    = ".lspec"
+    go BinSpec  = ".bspec"
     go Hquals   = ".hquals"
     go Result   = ".out"
     go Saved    = ".bak"
