a8ba33b104d28e4e6ede348dee093c8281fd38ef
nikivazou <nikivazou@gmail.com>
2017-01-17 17:13:19 +0200

    add overview

--- benchmarks/proofautomation/pos/MapFusion.hs
+++ benchmarks/proofautomation/pos/MapFusion.hs
@@ -22,9 +22,8 @@ compose f g x = f (g x)
 
 {-@ axiomatize map @-}
 map :: (a -> b) -> L a -> L b
-map f xs
-  | llen xs == 0 = N
-  | otherwise    = C (f (hd xs)) (map f (tl xs))
+map f N = N 
+map f (C x xs) = f x `C` map f xs 
 
 
 {-@ map_fusion :: f:(a -> a) -> g:(a -> a) -> xs:L a
@@ -37,10 +36,7 @@ map_fusion f g (C x xs) =  map_fusion f g xs
 data L a = N | C a (L a)
 {-@ data L [llen] a = N | C {headlist :: a, taillist :: L a }@-}
 
-{-@ measure nill @-}
-nill :: L a -> Bool
-nill N = True
-nill _ = False
+
 
 {-@ measure llen @-}
 llen :: L a -> Int
@@ -48,13 +44,3 @@ llen :: L a -> Int
 llen N        = 0
 llen (C _ xs) = 1 + llen xs
 
-{-@ measure hd @-}
-{-@ hd :: {v:L a | llen v > 0 } -> a @-}
-hd :: L a -> a
-hd (C x _) = x
-
-
-{-@ measure tl @-}
-{-@ tl :: xs:{v:L a | llen v > 0 } -> {v:L a | llen v == llen xs - 1 } @-}
-tl :: L a -> L a
-tl (C _ xs) = xs
