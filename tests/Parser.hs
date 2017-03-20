e13a78c46608729d7ba077891f80667ebafeb39f
Alan Zimmerman <alan.zimm@gmail.com>
2017-03-20 22:32:21 +0200

    Switch parser tests to use tasty
    1. The rest of the project uses tasty
    2. For the xml output for circle CI
    
    (I know hspec can do xml too)

--- tests/Parser.hs
+++ tests/Parser.hs
@@ -1,3 +1,10 @@
+{-# LANGUAGE DeriveDataTypeable  #-}
+{-# LANGUAGE DoAndIfThenElse     #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 -- | Simple test suite to test the parser.
 --
 -- Run as:
@@ -9,46 +16,52 @@ module Main where
 import           Data.Bifunctor
 import qualified Language.Haskell.Liquid.Parse as LH
 import qualified Language.Haskell.Liquid.Types as LH
-import           Test.Hspec
 import           Text.Parsec.Pos
+import           Test.Tasty
+import           Test.Tasty.HUnit
+import           Test.Tasty.Runners.AntXML
+
+-- ---------------------------------------------------------------------
 
 -- | Test suite entry point, returns exit failure if any test fails.
-main :: IO ()
-main = hspec spec
+main =  defaultMainWithIngredients (
+                antXMLRunner:defaultIngredients
+              ) tests
 
--- | Test suite.
-spec :: Spec
-spec = do
-  describe "Should succeed" succeeds
-  describe "Should fail" fails
+tests :: TestTree
+tests =
+  testGroup "Tests"
+    [
+      testSucceeds
+    , testFails
+    ]
 
--- | Tests for the parser.
-succeeds :: Spec
-succeeds = do
-  it
-    "Int"
-    (shouldBe
-       (parseSingleSpec "x :: Int")
+testSucceeds :: TestTree
+testSucceeds =
+  testGroup "Should succeed"
+    [ testCase "Int" $
+       (parseSingleSpec "x :: Int") @?=
        (Right
-          "Asrts ([\"x\" defined from: \"<test input>\" (line 1, column 1) to: \"<test input>\" (line 1, column 2)],(Int defined from: \"<test input>\" (line 1, column 6) to: \"<test input>\" (line 1, column 9),Nothing))"))
-  it
-    "k:Int -> Int"
-    (shouldBe
-       (parseSingleSpec "x :: k:Int -> Int")
+          "Asrts ([\"x\" defined from: \"<test input>\" (line 1, column 1) to: \"<test input>\" (line 1, column 2)],(Int defined from: \"<test input>\" (line 1, column 6) to: \"<test input>\" (line 1, column 9),Nothing))")
+    , testCase "k:Int -> Int" $
+       (parseSingleSpec "x :: k:Int -> Int") @?=
        (Right
-          "Asrts ([\"x\" defined from: \"<test input>\" (line 1, column 1) to: \"<test input>\" (line 1, column 2)],(k:Int -> Int defined from: \"<test input>\" (line 1, column 6) to: \"<test input>\" (line 1, column 18),Nothing))"))
+          "Asrts ([\"x\" defined from: \"<test input>\" (line 1, column 1) to: \"<test input>\" (line 1, column 2)],(k:Int -> Int defined from: \"<test input>\" (line 1, column 6) to: \"<test input>\" (line 1, column 18),Nothing))")
+    ]
 
-fails :: Spec
-fails = do
-  it
-    "Maybe k:Int -> Int"
-    (shouldBe
+testFails :: TestTree
+testFails =
+  testGroup "Should fail"
+    [ testCase "Maybe k:Int -> Int" $
        (bimap
           (unwords . words . show)
           show
-          (parseSingleSpec "x :: Maybe k:Int -> Int"))
+          (parseSingleSpec "x :: Maybe k:Int -> Int")) @?=
        (Left
-          "<test input>:1:13: Error: Cannot parse specification: Leftover while parsing"))
+          "<test input>:1:13: Error: Cannot parse specification: Leftover while parsing")
+    ]
+
+-- ---------------------------------------------------------------------
 
 -- | Parse a single type signature containing LH refinements. To be
 -- used in the REPL.
