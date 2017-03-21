c28c862b4759effb0c11ee09243fb3ba8aa9472f
Alan Zimmerman <alan.zimm@gmail.com>
2017-03-21 17:11:17 +0200

    Add tests for existing top level single spec parser

--- tests/Parser.hs
+++ tests/Parser.hs
@@ -13,13 +13,12 @@
 
 module Main where
 
-import           Data.Bifunctor
 import           Data.Data
 import           Data.Generics.Aliases
 import           Data.Generics.Schemes
 import           Language.Fixpoint.Types.Spans
 import qualified Language.Haskell.Liquid.Parse as LH
-import qualified Language.Haskell.Liquid.Types as LH
+-- import qualified Language.Haskell.Liquid.Types as LH
 import           Text.Parsec.Pos
 import           Test.Tasty
 import           Test.Tasty.HUnit
@@ -55,6 +54,150 @@ testSpecP =
     , testCase "assert" $
        parseSingleSpec "assert myabs :: Int -> PosInt" @?=
           "Asrt (\"myabs\" (dummyLoc),lq_tmp$db##0:Int -> PosInt (dummyLoc))"
+
+    , testCase "autosize" $
+       parseSingleSpec "autosize List" @?=
+          "ASize \"List\" (dummyLoc)"
+
+    , testCase "Local" $
+       parseSingleSpec "Local foo :: Nat -> Nat" @?=
+          "LAsrt (\"foo\" (dummyLoc),lq_tmp$db##0:Nat -> Nat (dummyLoc))"
+
+    , testCase "axiomatize" $
+       parseSingleSpec "axiomatize fibA" @?=
+          "Reflect \"fibA\" (dummyLoc)"
+
+    , testCase "reflect" $
+       parseSingleSpec "reflect map" @?=
+          "Reflect \"map\" (dummyLoc)"
+
+    , testCase "measure HMeas" $
+       parseSingleSpec "measure isAbs" @?=
+          "HMeas \"isAbs\" (dummyLoc)"
+
+    , testCase "measure Meas" $
+       parseSingleSpec "measure fv :: Expr -> (Set Bndr)" @?=
+          "Meas fv :: lq_tmp$db##0:Expr -> (Set Bndr)"
+
+    , testCase "define" $
+       parseSingleSpec "define $ceq = eqN" @?=
+          "Define (\"$ceq\" (dummyLoc),\"eqN\")"
+
+    , testCase "infixl" $
+       parseSingleSpec "infixl 9 +++" @?=
+          "BFix ()"
+
+    , testCase "infixr" $
+       parseSingleSpec "infixr 9 +++" @?=
+          "BFix ()"
+
+    , testCase "infix" $
+       parseSingleSpec "infix 9 +++" @?=
+          "BFix ()"
+
+    , testCase "defined" $ -- TODO:AZ synonym for "measure"?
+       parseSingleSpec "defined fv :: Expr -> (Set Bndr)" @?=
+          "Meas fv :: lq_tmp$db##0:Expr -> (Set Bndr)"
+
+    , testCase "inline" $
+       parseSingleSpec "inline eqelems" @?=
+          "Inline \"eqelems\" (dummyLoc)"
+
+    , testCase "bound PBound" $
+       parseSingleSpec "bound Foo = true" @?=
+          "PBound bound Foo forall [] . [] =  true"
+
+    , testCase "bound HBound" $
+       parseSingleSpec "bound step" @?=
+          "HBound \"step\" (dummyLoc)"
+
+    , testCase "class measure" $
+       parseSingleSpec "class measure sz :: forall a. a -> Int" @?=
+          "CMeas sz :: lq_tmp$db##0:a -> Int"
+
+    , testCase "instance measure" $
+       parseSingleSpec "instance measure sz :: MList a -> Int" @?=
+          "IMeas sz :: lq_tmp$db##0:(MList a) -> Int"
+
+    , testCase "instance" $
+       parseSingleSpec "instance VerifiedNum Int where\n  - :: x:Int -> y:Int -> OkInt {x - y} " @?=
+          "RInst (RI {riclass = VerifiedNum, ritype = [Int (dummyLoc)], risigs = [(\"-\" (dummyLoc),RISig x:Int -> y:Int -> (OkInt {x - y}) (dummyLoc))]})"
+
+    , testCase "class" $
+       parseSingleSpec "class Sized s where\n  size :: forall a. x:s a -> {v:Nat | v = sz x}" @?=
+          "Class (RClass {rcName = Sized, rcSupers = [], rcTyVars = [BTV \"s\"], rcMethods = [(\"size\" (dummyLoc),x:s a -> {v##0 : Nat | v##0 == sz x} (dummyLoc))]})"
+
+    , testCase "import" $
+       parseSingleSpec "import Foo" @?=
+          "Impt \"Foo\""
+
+    , testCase "data variance" $
+       parseSingleSpec "data variance IO bivariant" @?=
+          "Varia (\"IO\" (dummyLoc),[Bivariant])"
+
+    , testCase "data" $
+       parseSingleSpec "data Bob = B {foo :: Int}" @?=
+          "DDecl DataDecl: data = \"Bob\" (dummyLoc), tyvars = []"
+
+    , testCase "newtype" $
+       parseSingleSpec "newtype Foo = Bar {x :: Nat}" @?=
+          "NTDecl DataDecl: data = \"Foo\" (dummyLoc), tyvars = []"
+
+    , testCase "include" $
+       parseSingleSpec "include <listSet.hquals>" @?=
+          "Incl \"listSet.hquals\""
+
+    , testCase "invariant" $
+       parseSingleSpec "invariant {v:Tree a | 0 <= ht v}" @?=
+          "Invt {v##0 : (Tree a) | 0 <= ht v##0} (dummyLoc)"
+
+    , testCase "using" $
+       parseSingleSpec "using (Tree a) as  {v:Tree a   | 0 <= height v}" @?=
+          "IAlias ((Tree a) (dummyLoc),{v##0 : (Tree a) | 0 <= height v##0} (dummyLoc))"
+
+    , testCase "type" $
+       parseSingleSpec "type PosInt = {v: Int | v >= 0}" @?=
+          "Alias type PosInt   = {v##0 : Int | v##0 >= 0} -- defined at \"Fixpoint.Types.dummyLoc\" (line 0, column 0)"
+
+    , testCase "predicate" $
+       parseSingleSpec "predicate Pos X  = X > 0" @?=
+          "EAlias type Pos  \"X\" = PAtom Gt (EVar \"X\") (ECon (I 0)) -- defined at \"Fixpoint.Types.dummyLoc\" (line 0, column 0)"
+
+    , testCase "expression" $
+       parseSingleSpec "expression Avg Xs = ((sumD Xs) / (lenD Xs))" @?=
+          "EAlias type Avg  \"Xs\" = EBin Div (EApp (EVar \"sumD\") (EVar \"Xs\")) (EApp (EVar \"lenD\") (EVar \"Xs\")) -- defined at \"Fixpoint.Types.dummyLoc\" (line 0, column 0)"
+
+    , testCase "embed" $
+       parseSingleSpec "embed Set as Set_Set" @?=
+          "Embed (\"Set\" (dummyLoc),TC \"Set_Set\" (dummyLoc) (TCInfo {tc_isNum = False, tc_isReal = False, tc_isString = False}))"
+
+    , testCase "qualif" $
+       parseSingleSpec "qualif Foo(v:Int): v < 0" @?=
+          "Qualif (Q {qName = \"Foo\", qParams = [(\"v\",FInt)], qBody = PAtom Lt (EVar \"v\") (ECon (I 0)), qPos = \"Fixpoint.Types.dummyLoc\" (line 0, column 0)})"
+
+    , testCase "Decrease" $
+       parseSingleSpec "Decrease insert 3" @?=
+          "Decr (\"insert\" (dummyLoc),[2])"
+
+    , testCase "LAZYVAR" $
+       parseSingleSpec "LAZYVAR z" @?=
+          "LVars \"z\" (dummyLoc)"
+
+    , testCase "Strict" $
+       parseSingleSpec "Strict eval" @?=
+          "Lazy \"eval\" (dummyLoc)"
+
+    , testCase "Lazy" $
+       parseSingleSpec "Lazy eval" @?=
+          "Lazy \"eval\" (dummyLoc)"
+
+    , testCase "automatic-instances" $
+       parseSingleSpec "automatic-instances foo with 5" @?=
+          "Insts (\"foo\" (dummyLoc),Just 5)"
+
+    , testCase "LIQUID" $
+       parseSingleSpec "LIQUID \"--automatic-instances=liquidinstances\" " @?=
+          "Pragma \"--automatic-instances=liquidinstances\" (dummyLoc)"
     ]
 
 -- ---------------------------------------------------------------------
