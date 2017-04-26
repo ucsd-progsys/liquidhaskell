b0e5d7cd39315d66f6d5c033d4186a1f8a607c42
nikivazou <nikivazou@gmail.com>
2017-04-26 12:19:34 -0400

    make slicing optional

---
+++ tests/pos/Holes-Slicing.hs
@@ -0,0 +1,21 @@
+module Foo () where
+
+{-@ LIQUID "--savequery"      @-}
+{-@ LIQUID "--eliminate=none" @-}
+{-@ LIQUID "--noslice"        @-}
+
+{-@ measure isFoo :: A -> B -> Bool @-}
+{-@ isFoo :: a:A -> b:B -> {v:Bool | v <=> isFoo a b} @-}
+isFoo :: A -> B -> Bool
+isFoo a b = undefined
+
+{-@ prop1 :: u:A -> p:B -> {v : Int | isFoo u p <=> (v < 2) } @-}
+prop1 :: A -> B -> Int 
+prop1 = undefined 
+
+{-@ foo :: A -> B -> _ @-}
+foo :: A -> B -> Int 
+foo a b = if isFoo a b then 0 else 2 
+
+data A 
+data B 
