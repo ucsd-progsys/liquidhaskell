f78098d8500903b547745e09bac22e0764e04fab
Ranjit Jhala <jhala@cs.ucsd.edu>
2017-05-07 12:19:56 -0700

    fix for LH #1024 too

--- tests/pos/T1024.hs
+++ tests/pos/T1024.hs
@@ -2,10 +2,13 @@
 {-@ LIQUID "--higherorder" @-}
 {-@ LIQUID "--totality"    @-}
 
+
 module Bug where
+
 
 {-@ data Par1 p = Par1 { unPar1 :: p } @-}
 data Par1 p = Par1 { unPar1 :: p }
+
 
 {-@ reflect eqPar1 @-}
 eqPar1 :: (p -> p -> Bool) -> Par1 p -> Par1 p -> Bool
