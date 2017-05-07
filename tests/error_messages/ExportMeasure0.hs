dea271c90ea85e0761bec90f4440b00ae2eab806
Ranjit Jhala <jhala@cs.ucsd.edu>
2017-05-07 11:58:06 -0700

    added error messages for export reflects

--- tests/error_messages/ExportMeasure0.hs
+++ tests/error_messages/ExportMeasure0.hs
@@ -1,5 +1,7 @@
-module ExportMeasure () where
+-- FIX: module ExportMeasure (llen) where
+module ExportMeasure (llen) where
 
+{-@ data LL [llen] @-}
 data LL a = N | C a (LL a)
 
 {-@ invariant {v:LL a | (llen v) >= 0} @-}
@@ -9,6 +11,6 @@ llen :: (LL a) -> Int
 llen(N)      = 0
 llen(C x xs) = 1 + (llen xs)
 
-{-@ lmap :: (a -> b) -> xs:(L a) -> {v: L b | llen v = llen xs } @-} 
+{-@ lmap :: (a -> b) -> xs:(LL a) -> {v: LL b | llen v = llen xs } @-} 
 lmap f N = N
 lmap f (C x xs) = C (f x) (lmap f xs)
