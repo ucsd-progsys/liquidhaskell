e0f674a99f5bc9c0f7085a567a2e38de98c67485
Ranjit Jhala <jhala@cs.ucsd.edu>
2017-05-07 17:36:56 -0700

    add test cases for LH issue #1029

---
+++ tests/pos/ReflectLib7.hs
@@ -0,0 +1,9 @@
+{-@ LIQUID "--higherorder"    @-}
+{-@ LIQUID "--totality"       @-}
+{-@ LIQUID "--exactdc"        @-}
+
+module ReflectLib7 where
+
+{-@ reflect compose @-}
+compose :: (b -> c) -> (a -> b) -> a -> c
+compose f g x = f (g x)
