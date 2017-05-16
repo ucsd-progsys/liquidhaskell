ba99a9757ab65e95c64accd27fceb06a438910f1
Ranjit Jhala <jhala@cs.ucsd.edu>
2017-05-16 13:02:15 -0700

    remove tracepp

--- src/Language/Haskell/Liquid/Bare/ToBare.hs
+++ src/Language/Haskell/Liquid/Bare/ToBare.hs
@@ -36,13 +36,12 @@ measureToBare :: SpecMeasure -> BareMeasure
 --------------------------------------------------------------------------------
 measureToBare = bimap (fmap specToBare) dataConToBare
 
--- namedLocSymbol d = dropModuleNamesAndUnique . F.symbol <$> locNamedThing d
 dataConToBare :: DataCon -> LocSymbol
-dataConToBare d = F.tracepp msg ( ( dropModuleNames . F.symbol) <$> locNamedThing d) {- TODO:reflect-datacons namedLocSymbol d -}
+dataConToBare d = (dropModuleNames . F.symbol) <$> locNamedThing d
   where
-    msg  = "dataConToBare dc = " ++ show d ++ " v = " ++ show v ++ " vx = " ++ show vx
-    v    = dataConWorkId d
-    vx   = F.symbol v
+    _msg  = "dataConToBare dc = " ++ show d ++ " v = " ++ show v ++ " vx = " ++ show vx
+    v     = dataConWorkId d
+    vx    = F.symbol v
 
 specToBareTC :: RTyCon -> BTyCon
 specToBareTC = tyConBTyCon . rtc_tc
