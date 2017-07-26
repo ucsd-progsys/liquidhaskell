e2db70997c28efa0ec7ec36d7885e143377d227d
Ranjit Jhala <jhala@cs.ucsd.edu>
2017-07-26 06:39:18 -0700

    add default FuncSort to applySorts

--- src/Language/Fixpoint/SortCheck.hs
+++ src/Language/Fixpoint/SortCheck.hs
@@ -639,7 +639,7 @@ applySorts :: Vis.Visitable t => t -> [Sort]
 --------------------------------------------------------------------------------
 applySorts = (defs ++) . Vis.fold vis () []
   where
-    def    = [FFunc t1 t2 | t1 <- basicSorts, t2 <- basicSorts]
+    defs   = [FFunc t1 t2 | t1 <- basicSorts, t2 <- basicSorts]
     vis    = (Vis.defaultVisitor :: Vis.Visitor [KVar] t) { Vis.accExpr = go }
     go _ (EApp (ECst (EVar f) t) _)
            | f == applyName
