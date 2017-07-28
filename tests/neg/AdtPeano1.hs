5c4b0d1e377f8185775c17727fd72d8d1afbf149
Ranjit Jhala <jhala@cs.ucsd.edu>
2017-07-27 21:01:46 -0700

    makeHaskellDataDecls: TyCon -> LH.DataDecl

--- tests/neg/AdtPeano1.hs
+++ tests/neg/AdtPeano1.hs
@@ -5,7 +5,7 @@ module Peano where
 
 data Influx = Silly { goo :: Int }
 
-{-@ test1:: n:Int -> m:Int -> { v:() | Silly n == Silly m } -> { n == m } @-}
+{-@ test1:: n:Int -> m:Int -> { v:() | Silly n /= Silly m } -> { n == m } @-}
 test1 :: Int -> Int -> () -> Int
 test1 n m z = moo (Silly n) + moo (Silly m)
 
