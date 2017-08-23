78dbe587c73ec2562bd14742adb57339451c8cb7
Ranjit Jhala <jhala@cs.ucsd.edu>
2017-08-23 11:07:59 -0700

    add FIRST IndPred tests

---
+++ tests/todo/IndPred0.hs
@@ -0,0 +1,29 @@
+module Even where
+
+data Peano
+  = Z
+  | S Peano 
+
+data Even where
+  EZ  :: Even
+  ESS :: Peano -> Even -> Even
+
+{-@ data Even :: Peano -> Prop where
+      EZ  :: {v:Even | prop v = Even Z}
+      ESS :: n:Peano -> {v:Even | prop v = Even n} -> {v:Even | prop v = Even (S (S n)) }  
+  @-}
+
+-- LIQUID-ABBREV
+-- predicate Prop E = {prop v = E}
+
+-- data Even :: Peano -> Prop where
+--   EZ  :: Prop (Even Z)                                     
+--   ESS :: n:Nat -> Prop (Even n) -> Prop (Even (S (S n)))   
+
+{-@ test :: n:Peano -> {v:Even | prop v = Even (S (S n))} -> {v:Even | prop v = Even n} @-}
+test :: Peano -> Even -> Even
+test n p@EZ        = p      -- contra "..."
+test n p@(ESS m q) = q      -- G := p : {prop p  = Even (S (S n)) /\ prop p = Even (S (S m))}
+                            --        ; q : {prop q = Even m}
+                            --        ==> n = m
+                            --        ==> prop q = Even n
