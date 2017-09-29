8abefb4ae4d507849b485555ff1172c43aa427cd
nikivazou <nikivazou@gmail.com>
2017-09-29 13:57:40 -0400

    restore A

--- tests/todo/A.hs
+++ tests/todo/A.hs
@@ -2,11 +2,5 @@
 
 module A where 
 
-data Foo a b = Foo {fooA :: a, fooB :: b}  |  Bar
-
-
-
-{-@ measure isFoo @-}
-isFoo :: Foo a b -> Bool
-isFoo (Foo _ _)= True 
-isFoo Bar = False 
+data Foo a b= Foo {fooA :: a, fooB :: b} 
+{-@ data Foo a b = Foo {fooA :: a, fooB :: b} @-} 
