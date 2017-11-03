a16a9aebff8963dbb8bb515430391b771c3fb19c
Ranjit Jhala <jhala@cs.ucsd.edu>
2017-11-03 09:31:54 -0700

    move tests for #1123 into todo/

--- tests/todo/Foo.hs
+++ tests/todo/Foo.hs
@@ -1,31 +1,13 @@
-{-@ LIQUID "--short-names" @-}
-{-@ LIQUID "--no-termination" @-}
+{-@ LIQUID "--exact-data-cons" @-}
+{-@ LIQUID "--no-adt" @-}
 
-module Foo where
+module Step2 where
 
-import Prelude hiding (map, reverse)
+{-@ reflect sumListWith @-}
+sumListWith :: [Int] -> Int -> Int
+sumListWith []       c = c
+sumListWith (c : cs) _ = sumListWith cs c
 
-
-baz :: Int
-baz = 1
-
-{-@ poo :: ListN Int 1 @-}
-poo = [baz, baz]
-
-reverse :: [a] -> [a]
-
-{-@ measure size @-}
-size :: [a] -> Int
-size []     = 0
-size (_:xs) = size xs + 1
-
-{-@ type List  a    = [a]                    @-}
-{-@ type ListN a N  = {v:[a] | size v == N } @-}
-{-@ type ListX a Xs = ListN a {size Xs}      @-}
-
-{-@ reverse :: xs:List a -> ListN a {size xs} @-}
-reverse           = go []
-  where
-    {-@ go :: acc:_ -> xs:_ -> ListN a {1 + size acc + size xs} @-}
-    go acc []     = acc
-    go acc (x:xs) = go (x:acc) xs
+{-@ reflect sumList @-}
+sumList :: [Int] -> Int
+sumList cs = sumListWith cs 0
