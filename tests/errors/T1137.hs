beb2f51e4dfa59c58a55fafb6d3044f94bcd81c1
Ranjit Jhala <jhala@cs.ucsd.edu>
2017-12-05 23:45:37 -0800

    add test for #1137

---
+++ tests/errors/T1137.hs
@@ -0,0 +1,20 @@
+import Data.List
+
+{-@ type WeekDayNum = { i:Int | 0 < i && i <= 7 } @-}
+type WeekDayNum = Int -- Mon == 1, ..., Sun == 7
+
+data WeekDay = Mon | Tue | Wed | Thu | Fri | Sat | Sun
+  deriving (Read, Show, Eq, Bounded)
+
+{-@ weekdays :: { wd:[WeekDay] | len wd == 7 } @-}
+weekdays :: [WeekDay]
+weekdays = [Mon, Tue, Wed, Thu, Fri, Sat, Sun]
+
+{-@ fromWeekDayNum :: WeekDayNum -> WeekDay @-}
+fromWeekDayNum :: WeekDayNum -> WeekDay
+fromWeekDayNum i = weekdays !! (i-1)
+
+{-@ fromWeekDayNum :: WeekDay -> WeekDayNum @-}
+toWeekDayNum :: WeekDay -> WeekDayNum
+toWeekDayNum wd = case wd `elemIndex` weekdays of
+		    Just i -> i + 1
