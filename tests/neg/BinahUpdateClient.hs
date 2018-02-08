7f97f7de179ed20e36de66e792580feeb542f799
Ranjit Jhala <jhala@cs.ucsd.edu>
2018-02-08 12:13:03 -0800

    generalize CoSub to work on sorts

---
+++ tests/neg/BinahUpdateClient.hs
@@ -0,0 +1,14 @@
+{-@ LIQUID "--no-adt" 	                           @-}
+{-@ LIQUID "--exact-data-con"                      @-}
+{-@ LIQUID "--higherorder"                         @-}
+{-@ LIQUID "--no-termination"                      @-}
+{-@ LIQUID "--ple" @-} 
+
+{-# LANGUAGE ExistentialQuantification, KindSignatures, TypeFamilies, GADTs #-}
+
+module BinahUpdateClient where 
+
+import BinahUpdateLib 
+
+testUpdateQuery :: () -> Update Blob Int
+testUpdateQuery () = createUpdate BlobXVal 8  -- toggle to 80 to be SAFE 
