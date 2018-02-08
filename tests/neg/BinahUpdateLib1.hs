c78962781e83cc3608377fd2b65253b6b0f662c5
Ranjit Jhala <jhala@cs.ucsd.edu>
2018-02-08 15:40:51 -0800

    aha, new tests to reproduce wierd ctor-sort bug

---
+++ tests/neg/BinahUpdateLib1.hs
@@ -0,0 +1,38 @@
+{-@ LIQUID "--no-adt" 	                           @-}
+{-@ LIQUID "--exact-data-con"                      @-}
+{-@ LIQUID "--higherorder"                         @-}
+{-@ LIQUID "--no-termination"                      @-}
+{-@ LIQUID "--ple" @-}
+
+{-# LANGUAGE ExistentialQuantification, KindSignatures, TypeFamilies, GADTs #-}
+
+module BinahUpdateLib where
+
+class PersistEntity record where
+    data EntityField record :: * -> *
+
+instance PersistEntity Blob where
+    {-@ data EntityField Blob typ where
+          BinahUpdateLib.BlobYVal :: EntityField Blob {v:_ | True}
+        | BinahUpdateLib.BlobXVal :: EntityField Blob {v:_ | v >= 10}
+      @-}
+   data EntityField Blob typ
+      = typ ~ Int => BlobXVal |
+	typ ~ Int => BlobYVal
+
+{-@ data Blob  = B { xVal :: {v:Int | v >= 0}, yVal :: Int } @-}
+data Blob  = B { xVal :: Int, yVal :: Int }
+
+data Update record typ = Update
+    { updateField :: EntityField record typ
+    , updateValue :: typ
+    }
+
+createUpdate :: EntityField record a -> a -> Update record a
+createUpdate field value = Update {
+      updateField = field
+    , updateValue = value
+}
+
+testUpdateQuery :: () -> Update Blob Int
+testUpdateQuery () = createUpdate BlobXVal 8  -- toggle to 80 to be SAFE
