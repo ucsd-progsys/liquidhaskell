34b04fac5ec566a1847fc8abfeac3d744550aa19
Ranjit Jhala <jhala@cs.ucsd.edu>
2018-02-21 11:41:39 -0800

    update singleton-list test

--- tests/pos/SingletonLists.hs
+++ tests/pos/SingletonLists.hs
@@ -2,6 +2,8 @@ module SingletonLists where
 
 {-@ LIQUID "--higherorder" @-}
 
+{-@ infix : @-}
+
 empList :: ([a] -> a) -> ()
 {-@ empList :: f:([a] -> a) -> {f [] == f []} @-}
 empList _ = ()
@@ -10,3 +12,8 @@ empList _ = ()
 singletonList :: ([a] -> a) -> a -> ()
 {-@ singletonList :: f:([a] -> a) -> x:a -> {f [x] == f [x]} @-}
 singletonList _ _ = ()
+
+singletonListC :: ([a] -> a) -> a -> ()
+{-@ singletonListC :: f:([a] -> a) -> x:a -> {f (x:[]) == f [x]} @-}
+singletonListC _ _ = ()
+
