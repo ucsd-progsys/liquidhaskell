6a35909f30f248fb7649950bbcbdeb9a68855daa
Ranjit Jhala <jhala@cs.ucsd.edu>
2018-02-24 22:21:41 -0800

    add tests

---
+++ tests/errors/BadDataConType1.hs
@@ -0,0 +1,5 @@
+module Boo where
+
+{-@ data T = C { fldX :: Int, fldY :: String } @-}
+
+data T = C { fldX :: Int, fldY :: Int }
---
+++ tests/errors/BadDataConType2.hs
@@ -0,0 +1,5 @@
+module Boo where
+
+{-@ data T = C { fldX :: Int } @-}
+
+data T = C { fldX :: Int, fldY :: Int }
