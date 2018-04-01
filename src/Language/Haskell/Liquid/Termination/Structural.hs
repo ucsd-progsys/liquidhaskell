17b2319f62acf1db757223f72b91faf2ba46465c
Joachim Breitner <mail@joachim-breitner.de>
2018-04-01 19:01:50 -0400

    A rewrite of the  termination checker (to address #1248)
    The main improvement over the previous code are
    
      * It allows more than one argument. This is ok, because a function
        with to arguments like
    
    	len [] a = a
    	len (x:xs) a = len xs (a + 1)
    
        is equivalent to
    
    	len []     = \a -> a
    	len (x:xs) = \x -> len xs (a + 1)
    
        and should be treated equivalently.
    
      * It handles mutual recursion:
    
            isEven [] = True
            isEven (x:xs) = not $ isOdd xs
            isOdd [] = True
            isOdd (x:xs) = not $ isEven xs
    
      * Detects (and complains about) unsaturated recursive calls.
        Previously, this was accepted
    
            isBogus xs = map isBogus [xs]
    
      * Is sound in the presence of shadowing (GHC Core does not guarantee
        shadowing! It is hard to produce it in practice, but a termination
        checker should take it into account.) Without paying attention
        to shadowing, these non-terminating functions would have been accepted:
    
            foo [] = 0
            foo (x:xs) = let xs = [x] in foo xs
    
            bar [] = 0
            bar (x:xs) = case reverse (inits (xs ++ xs) of (xs:_) -> bar xs
    
        and this function would be needlessly rejected:
    
            foo [] = 0
            foo (x:xs) = foo xs + let foo = const 0 in foo (x:xs)

--- src/Language/Haskell/Liquid/Termination/Structural.hs
+++ src/Language/Haskell/Liquid/Termination/Structural.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 module Language.Haskell.Liquid.Termination.Structural (
 
   terminationCheck
@@ -7,15 +9,16 @@ module Language.Haskell.Liquid.Termination.Structural (
 import Language.Haskell.Liquid.Types hiding (terminationCheck)
 import Language.Fixpoint.Types.Errors
 import Language.Haskell.Liquid.GHC.Misc (showPpr)
-import Language.Haskell.Liquid.Types.Visitors 
 
 import CoreSyn
 import Var
 import Name (getSrcSpan)
+import VarSet
+import UniqSet (nonDetEltsUniqSet)
+import Data.Monoid ((<>))
 
-import Text.PrettyPrint.HughesPJ
+import Text.PrettyPrint.HughesPJ hiding ((<>))
 import qualified Data.HashSet                           as S
-import qualified Data.List                              as L
 
 
 terminationCheck :: GhcInfo -> Output Doc 
@@ -34,62 +37,104 @@ resultToDoc :: Result -> Output Doc
 resultToDoc OK        = mempty
 resultToDoc (Error x) = mempty {o_result = Unsafe x }
 
-checkBind :: [CoreBind] -> Var -> Result  
-checkBind cbs x = maybe OK isInductive (findRecBind cbs x)
+checkBind :: [CoreBind] -> Var -> Result
+checkBind cbs x = maybe OK isStructurallyRecursiveGroup (findRecBind cbs x)
 
 
 findRecBind :: [CoreBind] -> Var -> Maybe [(Var,CoreExpr)]
-findRecBind [] _ = Nothing 
-findRecBind (NonRec x _:_) y | x == y 
-  = Nothing 
-findRecBind (Rec xes:_)    y | y `elem` (fst <$> xes) 
-  = Just xes 
+findRecBind [] _ = Nothing
+findRecBind (NonRec x _:_) y | x == y
+  = Nothing
+findRecBind (Rec xes:_)    y | y `elem` (fst <$> xes)
+  = Just xes
 findRecBind (_:xes) y
   = findRecBind xes y
 
-isInductive :: [(Var,CoreExpr)] -> Result
-isInductive xes = mconcat (checkInductive <$> xes)
+isStructurallyRecursiveGroup :: [(Var,CoreExpr)] -> Result
+isStructurallyRecursiveGroup xes = mconcat (isStructurallyRecursive funs <$> xes)
+  where funs = mkVarSet (map fst xes)
 
-checkInductive :: (Var, CoreExpr) -> Result
-checkInductive (x, e) 
-  | length xs /= 1
-  = err (text  "check inductive on function with many arguments!")
-  | Case _ _ _ alts <- e'
-  , (bs, is)        <- L.partition (isBaseCase x) alts 
-  , is'             <- filter (not . null) (map (nonInductiveCalls x (length ts)) is)
-  = if null bs then err (text "The definition has no base case.")
-    else if not (null is') then err (text "The definition contains non inductive calls: " <+> (text $ showPpr is'))
-    else OK
-  | otherwise 
-  = err (text "The definition should be a function followed by case analysis.")
+isStructurallyRecursive :: VarSet -> (Var, CoreExpr) -> Result
+isStructurallyRecursive funs (fun, rhs)
+  | null xs
+  = mkError fun (text "The definition has no value parameters.")
+  | otherwise
+  = check (Env (mkError fun) funs (mkVarSet xs) emptyVarSet) body
   where
-    (ts, xs, e') = collectTyAndValBinders e
-    err expl = Error [ErrStTerm (getSrcSpan x) (text $ showPpr x) expl]
+    (_ts, xs, body) = collectTyAndValBinders rhs
 
+mkError :: Var -> Doc -> Result
+mkError fun expl = Error [ErrStTerm (getSrcSpan fun) (text $ showPpr fun) expl]
 
-nonInductiveCalls :: Var -> Int -> (AltCon, [Var], CoreExpr) -> [CoreExpr] 
-nonInductiveCalls f i (_, xs, e) = go e 
+data Env = Env
+    { retErr   :: Doc -> Result -- ^ How to signal erros
+    , funs     :: VarSet        -- ^ Which functions are interesting
+    , params   :: VarSet        -- ^ Variables bound to the first parameter of the current function
+    , subterms :: VarSet        -- ^ Variables bound to immediate subterms of these parameters
+    }
+
+shadow :: Env -> [Var] -> Env
+shadow (Env retErr funs params subterms) vs
+    = Env retErr (funs `delVarSetList` vs) (params `delVarSetList` vs) (subterms `delVarSetList` vs)
+
+envAddSubterms :: Env -> [Var] -> Env
+envAddSubterms env vs = env { subterms = subterms env `extendVarSetList` vs }
+
+check :: Env -> CoreExpr -> Result
+check env = \case
+    e@Var{}    -> checkCall env (collectArgs e)
+    e@App{}    -> checkCall env (collectArgs e)
+    Lam v e    -> check (env `shadow` [v]) e
+    Let (NonRec v rhs) body -> check env rhs <> check (env `shadow` [v]) body
+    Let (Rec pairs) body -> foldMap (check (env `shadow` vs)) (body : rhss)
+      where (vs,rhss) = unzip pairs
+
+    Case scrut bndr _ alts -> mconcat $
+        [ check env scrut ] ++
+        [ let env' | isParam env scrut = env `shadow` (bndr:pats) `envAddSubterms` pats
+                   | otherwise         = env `shadow` (bndr:pats)
+          in check env' rhs
+        | (_, pats, rhs) <- alts]
+
+    -- Boring transparent cases
+    Cast e _   -> check env e
+    Tick _ e   -> check env e
+    -- Boring base cases
+    Lit{}      -> mempty
+    Coercion{} -> mempty
+    Type{}     -> mempty
+
+isParam :: Env -> CoreExpr -> Bool
+isParam env (Var v)    = v `elemVarSet` params env
+isParam env (Cast e _) = isParam env e
+isParam env (Tick _ e) = isParam env e
+isParam _   _          = False
+
+isGoodArg :: Env -> CoreExpr -> Bool
+isGoodArg env (Var v)    = v `elemVarSet` subterms env
+isGoodArg env (Cast e _) = isGoodArg env e
+isGoodArg env (Tick _ e) = isGoodArg env e
+isGoodArg _   _          = False
+
+checkCall :: Env -> (CoreExpr, [CoreExpr]) -> Result
+checkCall env (Var fun, args)
+    | not (fun `elemVarSet` funs env)
+    = checkArgs
+    | null args
+    = retErr env $ text "Unsaturated call to" <+> (text $ showPpr fun)
+    | isGoodArg env firstArg
+    = checkArgs
+    | otherwise =
+    retErr env $ text "Non-structural recursion in call" <+> (text $ showPpr (App (Var fun) firstArg)) $$ subTermsHelpMsg
   where
-    go (App g e) | isRecCall i g , not (isSmallerArg e) = [App g e]
-    go (Let (NonRec _ ex) e) = go ex ++ go e
-    go (Let (Rec xes) e)     = concatMap go (e:(snd <$> xes)) ++ go e
-    go (Tick _ e)            = go e 
-    go (App e1 e2)           = go e1 ++ go e2 
-    go (Lam _ e)             = go e 
-    go (Cast e _)            = go e 
-    go (Case e _ _ alt)      = go e ++ concat [go e' | (_, _, e') <- alt]
-    go _ = []    
+    valArgs   = dropWhile isTypeArg args
+    firstArg  = head valArgs
+    checkArgs = foldMap (check env) args
 
-    isRecCall 0 (Var g)    = f == g
-    isRecCall i (Tick _ e) = isRecCall i e 
-    isRecCall i (App f _)  = isRecCall (i-1) f
-    isRecCall _ _          = False 
+    subTermsHelpMsg | isEmptyVarSet (subterms env) = text "No valid arguments are in scope."
+                    | otherwise = text "Valid arguments are: " <+> (hcat $ punctuate comma $ map (text . showPpr) $ nonDetEltsUniqSet (subterms env))
 
-    isSmallerArg (Var x)    = x `elem` xs 
-    isSmallerArg (Tick _ e) = isSmallerArg e 
-    isSmallerArg _          = False 
+-- Not a variable in the function position
+checkCall env (e, args) = foldMap (check env) (e : args)
 
-
-isBaseCase :: Var -> (a, b, CoreExpr) -> Bool 
-isBaseCase x (_, _, e) = (not (x `elem` freeVars mempty e))
 
