17262325cde7c83802382ce50e0fad94a5ab965c
Anish Tondwalkar <atondwal@ucsd.edu>
2018-04-17 17:18:21 -0700

    removed splitApp testcase

--- tests/pos/splitApp.hs
+++
@@ -1,9 +0,0 @@
-module SplitApp where
-
-{-@ f :: u:Int -> w:Int -> {v:Int|v=u+w} @-}
-f :: Int -> Int -> Int
-f a b = a + b
-
-{-@ g :: j:Int -> Int -> Int -> {v:Int|v=j} @-}
-g :: Int -> Int -> Int -> Int
-g x y z = f x y - y
