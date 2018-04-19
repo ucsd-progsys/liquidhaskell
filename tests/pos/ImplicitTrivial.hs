9a75a4d839515fec7b8fa02df0d05a2458200e09
Anish Tondwalkar <atondwal@ucsd.edu>
2018-04-19 09:34:11 -0700

    add ImplicitTrivial.hs
    let's first make sure that adding implicits doesn't break stuff, before
    we actually make them work themselves

---
+++ tests/pos/ImplicitTrivial.hs
@@ -0,0 +1,12 @@
+module ImplicitTrivial where
+
+-- This module has implicits but doesn't acutally use them for verification
+
+{-@ foo :: n:Int ~> m:Int -> {v:_|v=m+1} @-}
+foo :: Int -> Int
+foo moo = 1 + moo
+
+{-@ bar :: m:Int -> {v:_|v=m+1} @-}
+bar :: Int -> Int
+bar moo = foo moo
+ 
