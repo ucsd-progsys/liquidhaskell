9f85ed4fc30cb5a471fec39ec77b43a2fdff5b9f
Anish Tondwalkar <anish@archlinux.info>
2018-06-06 19:32:36 -0700

    fix for gp commit

--- tests/pos/tachio-d2.hs
+++ tests/pos/tachio-d2.hs
@@ -1,13 +1,11 @@
 module Tachio (main) where
 
 {-@ chaos :: { True } @-}
--- setting chaos = undefined makes the IF look like dead code.
-chaos = False
+chaos = undefined
 
 {-@ app :: a:Int ~> (v:{v>=a} -> ()) -> {v:Int | v=a} -> () @-}
 app :: (Int -> ()) -> Int -> ()
--- app f x = if chaos then app f (x + 1) else f x
-app f x = f x
+app f x = if chaos then app f (x + 1) else f x
 
 {-@ check :: x:Int -> y:{x <= y} -> () @-}
 check :: Int -> Int -> ()
