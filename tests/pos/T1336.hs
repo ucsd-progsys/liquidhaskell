88a1d5e4c9799920fc66a94590c6db1eb6222403
Ranjit Jhala <jhala@cs.ucsd.edu>
2018-06-26 13:19:19 -0700

    done with lh

--- tests/pos/T1336.hs
+++ tests/pos/T1336.hs
@@ -4,6 +4,12 @@ import Data.Ratio
 
 {-@ embed Ratio * as int @-}
 
+-- | You can also write: 
+--
+--     embed Ratio * as real 
+--
+--   if for some reason you need to treat the values thus.
+
 {-@ foo :: {v:Ratio Int | v /= 0} -> Bool @-}
 foo :: Ratio Int -> Bool
 foo x = y == y 
