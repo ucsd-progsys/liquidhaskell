7c80103a61b93d6f4588e3af203c5f87902aec9e
Ranjit Jhala <jhala@cs.ucsd.edu>
2018-08-01 21:31:04 -0700

    see failing Data02.hs

---
+++ tests/datacon/pos/Data02.hs
@@ -0,0 +1,12 @@
+module Data00 where 
+
+{-@ data Pair = P { pX :: Nat, pY :: {v:Nat | pX < v} } @-}
+data Pair = P { pX :: Int, pY :: Int }
+
+{-@ test1 :: Pair -> TT @-}
+test1 (P a b) =  a < b 
+
+{-@ test2 :: Nat -> Pair @-}
+test2 x = P x (x + 1)
+
+
