ad9d8ac5227e4fa32ffbe3cfb566f3122be08dc7
Ranjit Jhala <jhala@cs.ucsd.edu>
2018-08-30 14:40:45 -0700

    add two tests

---
+++ tests/names/pos/Ord.hs
@@ -0,0 +1,6 @@
+module Ord where 
+
+{-@ bigger :: x:_ -> y:_ -> {v:_ | v >= x && v >= y} @-}
+bigger :: (Ord a) => a -> a -> a 
+bigger x y | x `compare` y == GT = x 
+           | otherwise           = y
--- tests/pos/stateInvarint.hs
+++ tests/pos/stateInvarint.hs
@@ -4,7 +4,7 @@ import Prelude hiding (return, (>>=))
 
 data ST s a = S (s -> (a, s))
 {-@ data ST s a <p :: s -> Bool> 
-     = S (x::(f:s<p> -> (a, s<p>))) 
+     = S { x:: (f:s<p> -> (a, s<p>)) } 
   @-}
 
 {-@ foo :: (Int, {v:Int|v >=0})@-}
@@ -22,30 +22,18 @@ action
 act1 :: ST Int Int
 act1 = S (\n -> (n, n+1))
 
-
 act2 :: ST Int Int
 act2 = S (\n -> (n, n+9))
 
-
-
-{-@
-apply :: forall <p :: s -> Bool>.
-          ST <p> s a -> f:s<p> -> (a, s <p>)
-  @-}
+{-@ apply :: forall <p :: s -> Bool>. ST <p> s a -> f:s<p> -> (a, s <p>) @-}
 apply :: ST s a -> s -> (a, s)
 apply (S f) x = f x
 
-{-@
-return :: forall <p:: s -> Bool>.
-          x:a -> ST <p> s {v:a|v=x}
-  @-}
+{-@ return :: forall <p:: s -> Bool>. x:a -> ST <p> s {v:a|v=x} @-}
 return ::  a -> ST s a
 return x = S $ \s -> (x, s)
 
-{-@
-comp :: forall < p :: s -> Bool>.
-    ST <p> s a -> (a -> ST <p> s b) -> ST <p> s b
-@-}
+{-@ comp :: forall < p :: s -> Bool>. ST <p> s a -> (a -> ST <p> s b) -> ST <p> s b @-}
 comp :: ST s a -> (a -> ST s b) -> ST s b
 (S m) `comp` k 
   = S $ \s -> case (m s) of { (r, new_s) -> 
