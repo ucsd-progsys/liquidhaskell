dc1172bd4855c387ed8255512080ebcacf0cf01d
Ranjit Jhala <jhala@cs.ucsd.edu>
2018-09-07 22:05:22 -0700

    dont qualify measure binders

--- tests/import/lib/T1096_Types.hs
+++ tests/import/lib/T1096_Types.hs
@@ -7,7 +7,7 @@ data Foo = A Foo | B
 size :: Foo -> Integer 
 
 {-@ measure size @-}
-{-@ invariant {t:Foo | 0 <= size t} @-}
+{- invariant {t:Foo | 0 <= size t} @-}
 {-@ size :: Foo -> {v:Integer |  0 <= v }  @-}
 size (A x) = 1 + size x
 size B     = 0 
