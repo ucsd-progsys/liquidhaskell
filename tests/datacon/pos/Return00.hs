d65e9e60496196fb270023f8465d218699d8c91c
Ranjit Jhala <jhala@cs.ucsd.edu>
2018-09-14 15:00:52 -0700

    move 'return' stuff into 'pattern' directory

--- tests/datacon/pos/Return00.hs
+++ tests/datacon/pos/Return00.hs
@@ -1,6 +1,13 @@
-import Control.Monad.Primitive
+-- import Control.Monad.Primitive
 
-{-@ silly :: PrimMonad m => m () @-}
-silly :: PrimMonad m => m () 
+{- silly :: PrimMonad m => m () @-}
+-- silly :: PrimMonad m => m () 
+--
+{-@ silly :: (Monad m) => m () @-}
+silly :: (Monad m) => m ()
 silly = return () 
+
+{-@ billy :: Int -> () @-}
+billy :: Int -> ()
+billy _ = () 
 
--- tests/pattern/pos/MultipleInvariants.hs
+++ tests/pattern/pos/MultipleInvariants.hs
@@ -1,5 +1,3 @@
-{-@ LIQUID "--reflection" @-}
-
 -- TAG: invariant 
 
 module Blank where
@@ -7,22 +5,16 @@ module Blank where
 import Data.Word
 import GHC.Ptr
 
-
-{- measure sizeOf :: forall a . Ptr a -> Int @-}
-{-@ reflect sizeOf @-}
-sizeOf :: Ptr a -> Int 
-sizeOf = undefined 
-
+{-@ measure sizeOf :: forall a . Ptr a -> Int @-}
 
 {-@ invariant {v:Ptr Word16 | sizeOf v = 2} @-}
 {-@ invariant {v:Ptr Word32 | sizeOf v = 4} @-}
 
-
-{-@ bar :: p:_ -> {v:_ | sizeOf p == 4 }@-}
+{-@ bar :: p:_ -> {v:_ | sizeOf p == 4 } @-}
 bar :: Ptr Word32 -> ()
 bar (Ptr _) = ()
 
-{-@ foo :: p:_ -> {v:_ | sizeOf p == 2 }@-}
+{-@ foo :: p:_ -> {v:_ | sizeOf p == 2 } @-}
 foo :: Ptr Word16 -> ()
 foo (Ptr _) = ()
 
