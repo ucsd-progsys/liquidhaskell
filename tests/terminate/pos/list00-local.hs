1057ec7f5951b5c671934cbb777b38158f428840
Ranjit Jhala <jhala@cs.ucsd.edu>
2018-09-14 14:15:16 -0700

    hmm termination micro tests pass

---
+++ tests/terminate/pos/list00-local.hs
@@ -0,0 +1,8 @@
+
+lrev :: [a] -> [a]
+lrev = go [] 
+  where  
+    {-@ go :: _ -> xs:_ -> _ / [len xs] @-}
+    go :: [a] -> [a] -> [a]
+    go acc []     = acc 
+    go acc (x:xs) = go (x:acc) xs 
---
+++ tests/terminate/pos/list04-local.hs
@@ -0,0 +1,17 @@
+
+{-@ data L [llen] @-}
+data L a = N | C a (L a)
+
+{-@ measure llen @-}
+{-@ llen :: (L a) -> Nat @-}
+llen :: L a -> Int
+llen N        = 0
+llen (C x xs) = 1 + llen xs 
+
+rev               = go N 
+  where 
+    {-@ go :: _ -> xs:_ -> _ / [llen xs] @-}  
+    go :: L a -> L a -> L a  ----------------- >>> We need this GHC-tysig, maybe because the CORE is wierd otherwise?
+    go acc N        = acc
+    go acc (C x xs) = go (C x acc) xs
+
