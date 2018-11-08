7233aeb3015e217640ad5b6a5186bf4340b1ba7e
Ranjit Jhala <jhala@cs.ucsd.edu>
2018-11-07 19:52:57 -0800

    add test

---
+++ tests/ple/pos/NegNormalForm.hs
@@ -0,0 +1,96 @@
+{-@ LIQUID "--reflection" @-}
+{-@ LIQUID "--ple"        @-}
+
+module Tutorial6 where
+
+import Prelude hiding (not, lookup)
+
+-------------------------------------------------------------------------------
+-- | General datatype for Predicates 
+-------------------------------------------------------------------------------
+
+data Pred 
+  = Var Int
+  | Not Pred 
+  | Or  Pred Pred 
+  | And Pred Pred 
+
+-------------------------------------------------------------------------------
+-- | Define NNF as a Refinement of @Pred@ 
+-------------------------------------------------------------------------------
+
+{-@ type NNF = {v:Pred | isNNF v} @-}
+
+{-@ measure isVar @-}
+isVar :: Pred -> Bool 
+isVar (Var _) = True 
+isVar _       = False 
+
+{-@ measure isNNF @-}
+isNNF :: Pred -> Bool 
+isNNF (Var _)   = True 
+isNNF (And p q) = isNNF p && isNNF q 
+isNNF (Or  p q) = isNNF p && isNNF q 
+isNNF (Not p)   = isVar p 
+
+
+-------------------------------------------------------------------------------
+-- | Define semantics of @Pred@ 
+-------------------------------------------------------------------------------
+
+{-@ reflect lookup @-}
+lookup :: [a] -> a -> Int -> a
+lookup []     def _ = def
+lookup (v:_)  _   0 = v 
+lookup (_:vs) def k = lookup vs def (k-1) 
+
+{-@ reflect eval @-}
+eval :: [Bool] -> Pred -> Bool
+eval s (Var i)   = lookup s False i
+eval s (And f g) = (eval s f) && (eval s g)
+eval s (Or  f g) = (eval s f) || (eval s g)
+eval s (Not f)   = not (eval s f)
+
+
+-------------------------------------------------------------------------------
+-- | NNF Conversion 
+-------------------------------------------------------------------------------
+
+{-@ reflect nnfP @-}
+{-@ nnfP :: Pred -> NNF @-} 
+nnfP :: Pred -> Pred 
+nnfP (Var i)    = Var i 
+nnfP (Not p)    = nnfN p  
+nnfP (And p q)  = And (nnfP p) (nnfP q) 
+nnfP (Or  p q)  = Or  (nnfP p) (nnfP q) 
+
+{-@ reflect nnfN @-}
+{-@ nnfN :: Pred -> NNF @-} 
+nnfN :: Pred -> Pred 
+nnfN (Var i)    = Not (Var i) 
+nnfN (Not p)    = nnfP p  
+nnfN (And p q)  = Or  (nnfN p) (nnfN q) 
+nnfN (Or  p q)  = And (nnfN p) (nnfN q) 
+
+-------------------------------------------------------------------------------
+-- | NNF Conversion Correctness 
+-------------------------------------------------------------------------------
+
+{-@ thmP :: s:_ -> p:_ -> { eval s p = eval s (nnfP p) } @-}
+thmP :: [Bool] -> Pred -> Bool
+thmP s (Var i)   = True
+thmP s (And p q) = thmP s p && thmP s q
+thmP s (Or  p q) = thmP s p && thmP s q
+thmP s (Not p)   = thmN s p 
+
+{-@ thmN :: s:_  -> p:_ -> { eval s p = not (eval s (nnfN p)) } @-}
+thmN :: [Bool] -> Pred -> Bool
+thmN s (Var i)   = True
+thmN s (And p q) = thmN s p && thmN s q
+thmN s (Or  p q) = thmN s p && thmN s q
+thmN s (Not p)   = thmP s p 
+
+{-@ reflect not @-}
+not :: Bool -> Bool 
+not True = False 
+not False = True 
