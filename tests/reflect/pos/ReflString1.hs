061b2cc0fcfe126b7c83a530a8f6cfad190b0f40
Ranjit Jhala <jhala@cs.ucsd.edu>
2018-12-17 14:27:06 -0800

    add tests for reflect-string #1044

---
+++ tests/reflect/pos/ReflString1.hs
@@ -0,0 +1,20 @@
+
+-- cf https://github.com/ucsd-progsys/liquidhaskell/issues/1044
+
+{-@ LIQUID "--reflection" @-}
+
+module ReflectString where 
+
+data Vname = V String 
+
+{-@ reflect xVar @-}
+xVar :: Vname 
+xVar = V "x"
+
+{-@ reflect yVar @-}
+yVar :: Vname 
+yVar = V "x"
+
+{-@ pf :: _ -> { xVar = yVar } @-}
+pf () = ()
+
