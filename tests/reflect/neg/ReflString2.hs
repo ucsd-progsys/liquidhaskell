1b5da951ee424241eca82dbb49121b55674af51c
Ranjit Jhala <jhala@cs.ucsd.edu>
2018-12-18 21:02:36 -0800

    remove debug

---
+++ tests/reflect/neg/ReflString2.hs
@@ -0,0 +1,17 @@
+{-@ LIQUID "--reflection" @-}
+
+{-@ reflect flob @-}
+flob :: String -> Int 
+flob x = if x == "cat" then 1 else 2
+
+{-@ check1:: () -> {v:Int | v = flob "cod" } @-}
+check1 :: () -> Int
+check1() = flob "cow"
+
+{-@ check2:: () -> {v:Int | pcow v } @-}
+check2:: () -> Int
+check2() = flob "cow"
+
+{-@ inline pcow @-}
+pcow :: Int -> Bool 
+pcow v = v == flob "cow"
