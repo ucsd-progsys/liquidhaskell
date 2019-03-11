596bca7d8fa159d138c7f3e7d13b1ba01893caa3
nikivazou <nikivazou@gmail.com>
2019-03-11 09:23:30 +0100

    more normalization for Labels to go throught

--- src/Language/Haskell/Liquid/LawInstances.hs
+++ src/Language/Haskell/Liquid/LawInstances.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Language.Haskell.Liquid.LawInstances ( checkLawInstances ) where
 
 import qualified Data.List                                  as L
@@ -39,23 +40,37 @@ checkOneLaw c (x, t) li
   = [mkError (text "The instance for the law" <+> pprint x <+> text "is not defined")]
   where 
     lix = L.lookup (Left x) (lilEqus li)
-    mkError = ErrILaw (lilPos li) (pprint c) (pprint $ lilTyArgs li)
+    mkError = ErrILaw (lilPos li) (pprint c) (pprintXs $ lilTyArgs li)
+    pprintXs [l] = pprint l 
+    pprintXs xs  = pprint xs 
 
 unify :: (Doc -> Error) -> Class -> LawInstance -> LocSpecType -> LocSpecType -> [Error]
 unify mkError c li t1 t2 
-  = go t12 t22 
+  = if t11 =*= t22 then [] else err
   where 
+    err = [mkError (pprint t1 <+> text "\nis different than\n" <+> pprint t2
+      <+> text "\nesubt1 = " <+> pprint esubst1  
+      <+> text "\nesubt = " <+> pprint esubst  
+      <+> text "\ncompared\n" <+> pprint t11 <+> text "\nWITH\n" <+> pprint t22 
+           )]
+
     t22 = fromRTypeRep (trep2 {ty_vars = [], ty_binds = fst <$> args2, ty_args = snd <$> args2, ty_refts = drop (length tc2) (ty_refts trep2)})
+    t11 = fromRTypeRep (trep1 { ty_vars = []
+                              , ty_binds = fst <$> args2
+                              , ty_args = (tx . snd) <$> args1
+                              , ty_refts = F.subst esubst <$> drop (length tc1) (ty_refts trep1)
+                              , ty_res = tx $ ty_res trep1})
+    tx = subtsSpec tsubst . F.subst esubst
     subtsSpec = subts :: ([(TyVar, Type)] -> SpecType -> SpecType)
-    t12 = F.subst esubst t11
-    t11 = subtsSpec tsubst $ fromRTypeRep (trep1 {ty_vars = [], ty_binds = fst <$> args1, ty_args = snd <$> args1, ty_refts = drop (length tc1) (ty_refts trep1)})
+
     trep1 = toRTypeRep $ val t1 
     trep2 = toRTypeRep $ val t2 
     (tc1, args1) = splitTypeConstraints $ zip (ty_binds trep1) (ty_args trep1)
     (tc2, args2) = splitTypeConstraints $ zip (ty_binds trep2) (ty_args trep2)
-    esubst = F.mkSubst ((zip  (fst <$> args1) ((F.EVar . fst) <$> args2))
+    esubst = F.mkSubst (esubst1
                  ++  [(F.symbol x, F.EVar (F.symbol y)) | (Left x, (Left y, _)) <- lilEqus li]
                      )
+    esubst1 = zip  (fst <$> args1) ((F.EVar . fst) <$> args2)
 
     tsubst = reverse $ zip ((\(RTV v) -> v) <$> (findTyVars tc1 ++ (ty_var_value <$> concat argVars)))
                  (toType <$> (argBds ++ (((`RVar` mempty) . ty_var_value) <$>ty_vars trep2)))
@@ -70,10 +85,6 @@ unify mkError c li t1 t2
     findTyVars (_:ts) = findTyVars ts 
     findTyVars [] = [] 
 
-    go tt1 tt2 =  if tt1 == tt2 then ok else err
-    
-    err = [mkError (pprint t1 <+> text "is different than" <+> pprint t2)]
-    ok  = []  
 
 splitTypeConstraints :: [(F.Symbol, SpecType)] -> ([(F.Symbol, SpecType)], [(F.Symbol, SpecType)])
 splitTypeConstraints = go []  
@@ -81,4 +92,4 @@ splitTypeConstraints = go []
     go cs (b@(x,RApp c _ _ _):ts) 
       | isClass c
       = go (b:cs) ts 
-    go cs r = (reverse cs, r)
+    go cs r = (reverse cs, map (\(x, t) -> (x, shiftVV t x)) r)
