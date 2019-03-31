674933d49c879073cf30d90956b75b201de6ca7d
Vassil Keremidchiev <varosi@gmail.com>
2019-03-31 17:14:37 +0300

    Batch script was not running on AppVeyor

--- appveyor-copy.bat
+++ appveyor-copy.bat
@@ -3,5 +3,7 @@ rem Copy runtime DLLs
 echo "" | stack exec -- where libstdc++-6.dll > lib.txt
 echo "" | stack exec -- where libgcc_s_seh-1.dll >> lib.txt
 echo "" | stack exec -- where libwinpthread-1.dll >> lib.txt
-for /F %i IN (lib.txt) do copy /Y "%i" .\
+
+for /F %i IN (lib.txt) do copy /Y '%i' .\
+
 del /q lib.txt
