be9a83aaf59d1339028971491fddb75f61071d87
pbougou <panagiotis.bougoulias@imdea.org>
2019-05-17 21:49:42 +0200

    RFun.

--- src/Language/Haskell/Liquid/Synthesize.hs
+++ src/Language/Haskell/Liquid/Synthesize.hs
@@ -16,38 +16,66 @@ import qualified Data.HashMap.Strict as M
 import           Data.Default 
 import qualified Data.Text as T
 import           Data.Maybe
-import Debug.Trace 
+import           Debug.Trace 
 
 type SSEnv = M.HashMap Symbol SpecType
 
 synthesize :: FilePath -> F.Config -> CGInfo -> SSEnv -> SpecType -> IO [SExpr]
 synthesize tgt fcfg cgi ctx t = (:[]) <$> evalSM (go t) tgt fcfg cgi ctx
   where 
+
+    -- Is it good to push and pop here or in (1)??
+    pushContext :: SpecType -> StateT SState IO SExpr
+    pushContext t = do
+      ctx <- sContext <$> get
+      liftIO $ SMT.smtPush ctx
+      sexpr <- go t
+      liftIO $ SMT.smtPop ctx
+      return sexpr
+
+
+    go :: SpecType -> StateT SState IO SExpr
     -- Type Variable
     go (RVar α _)        = (`synthesizeRVar` α) <$> getSEnv
     -- Function 
-    go (RFun x tx t _)   = do y <- freshVar 
-                              addEnv y tx 
-                              SFun y <$> go (t `subst1` (x, EVar y))
+    go (RFun x tx t _)   = 
+        do  let RR s (Reft (pn, in_rr)) = rTypeSortedReft (tyConEmbed cgi) tx  
+            y <- freshVar 
+            addEnv y tx 
+
+            ctx <- sContext <$> get
+            -- (1)
+            liftIO $ SMT.smtPush ctx
+            liftIO $ SMT.smtDecl ctx y s
+            liftIO $ SMT.smtAssert ctx (substInFExpr pn y in_rr)
+            -- liftIO $ SMT.smtCheckSat ctx in_rr
+            sexpr <- go (t `subst1` (x, EVar y))
+            liftIO $ SMT.smtPop ctx
+            return $ SFun y sexpr 
+
     -- Type Abstraction 
     go (RAllT _ t)       = go t
     -- Data Type, e.g., c = Int and ts = [] or c = List and ts = [a] 
-    go t@(RApp c _ts _ r)  | R.isNumeric (tyConEmbed cgi) c = do 
-        let RR s (Reft(x,rr)) = rTypeSortedReft (tyConEmbed cgi) t 
-        ctx <- sContext <$> get 
-        liftIO $ SMT.smtPush ctx
-        liftIO $ SMT.smtDecl ctx x s 
-        liftIO $ SMT.smtCheckSat ctx rr 
-        SMT.Model modelBinds <- (liftIO $ SMT.smtGetModel ctx) 
-        -- TODO: get model and parse the value for x 
-        let xNotFound = error ("Symbol " ++ show x ++ "not found.")
-        let smtVal = T.unpack (fromMaybe xNotFound $ lookup x modelBinds)
-        liftIO $ SMT.smtPop ctx
-        return $ tracepp ("numeric with " ++ show r) (SVar $ symbol smtVal)
+    go t@(RApp c _ts _ r)  
+      | R.isNumeric (tyConEmbed cgi) c = 
+          do  let RR s (Reft(x,rr)) = rTypeSortedReft (tyConEmbed cgi) t 
+              ctx <- sContext <$> get 
+              liftIO $ SMT.smtPush ctx
+              liftIO $ SMT.smtDecl ctx x s
+              liftIO $ SMT.smtCheckSat ctx rr 
+              -- Get model and parse the value of x
+              SMT.Model modelBinds <- liftIO $ SMT.smtGetModel ctx
+              
+              let xNotFound = error $ "Symbol " ++ show x ++ "not found."
+                  smtVal = T.unpack (fromMaybe xNotFound $ lookup x modelBinds)
+
+              liftIO (SMT.smtPop ctx)
+              return $ tracepp ("numeric with " ++ show r) (SVar $ symbol smtVal)
+              -- return def
     go (RApp _c _ts _ _) 
       = return def 
     -- Type Application, e.g, m a 
-    go (RAppTy _ _ _)  = return def 
+    go RAppTy{} = return def 
 
 
     -- Fancy Liquid Types to be ignored for now since they do not map to haskell types
@@ -60,6 +88,40 @@ synthesize tgt fcfg cgi ctx t = (:[]) <$> evalSM (go t) tgt fcfg cgi ctx
     go (RExprArg _)    = return def 
     go (RHole _)       = return def 
 
+------------------------------------------------------------------------------
+-- Handle dependent arguments
+------------------------------------------------------------------------------
+-- * Arithmetic refinement expressions: 
+--    > All constants right, all variables left
+------------------------------------------------------------------------------
+-- depsSort :: Expr -> Expr 
+-- depsSort e = 
+--   case e of 
+--     PAnd exprs -> PAnd (map depsSort exprs)
+--       -- error "PAnd not implemented yet"
+--     PAtom brel e1 e2 -> PAtom brel (depsSort e1)
+--       -- error ("e1 = " ++ show e1)
+--       -- error "PAtom not implemented yet"
+--     ESym _symConst -> error "ESym not implemented yet" 
+--     constant@(ECon _c) -> constant 
+--     EVar _symbol -> error "EVar not implemented yet"
+--     EApp _expr1 _expr2 -> error "EVar not implemented yet"
+--     ENeg _expr -> error "ENeg not implemented yet"
+--     EBin _bop _expr1 _expr2 -> error "EBin not implemented yet"
+--     EIte _expr1 _expr2 _expr3 -> error "EIte not implemented yet"
+--     ECst _expr _sort -> error "ECst not implemented yet"
+--     ELam _targ _expr -> error "ELam not implemented yet"
+--     ETApp _expr _sort -> error "ETApp not implemented yet"
+--     ETAbs _expr _symbol -> error "ETAbs not implemented yet"
+--     POr _exprLst -> error "POr not implemented yet" 
+--     PNot _expr -> error "PNot not implemented yet"
+--     PImp _expr1 _expr2 -> error "PImp not implemented yet"
+--     PIff _expr1 _expr2 -> error "PIff not implemented yet"
+--     PKVar _kvar _subst -> error "PKVar not implemented yet"
+--     PAll _ _expr -> error "PAll not implemented yet"
+--     PExist _ _expr -> error "PExist not implemented yet" 
+--     PGrad _kvar _subst _gradInfo _expr -> error "PGrad not implemented yet"
+--     ECoerc _sort1 _sort2 _expr -> error "ECoerc not implemented yet"
 
 synthesizeRVar :: SSEnv -> RTyVar -> SExpr
 synthesizeRVar ctx α = case M.keys $ M.filter isMyVar ctx of
@@ -126,3 +188,36 @@ incrSM :: SM Int
 incrSM = do s <- get 
             put s{ssIdx = ssIdx s + 1}
             return (ssIdx s)
+
+-- The rest will be added when needed.
+-- Replaces    | old w   | new  | symbol name in expr.
+substInFExpr :: Symbol -> Symbol -> Expr -> Expr
+substInFExpr pn nn e = 
+  case e of 
+    PAnd exprs -> PAnd (map (substInFExpr pn nn) exprs)
+    PAtom brel e1 e2 -> 
+      PAtom brel (substInFExpr pn nn e1) (substInFExpr pn nn e2) 
+    ESym _symConst -> error "ESym not implemented yet"
+    constant@ECon{} -> constant
+    var@(EVar symb) ->
+      if symb == pn 
+        then EVar nn
+        else var
+    EApp e1 e2 -> EApp (substInFExpr pn nn e1) (substInFExpr pn nn e2)
+    ENeg e -> ENeg (substInFExpr pn nn e) 
+    EBin bop e1 e2 -> EBin bop (substInFExpr pn nn e1) (substInFExpr pn nn e2) 
+    EIte e1 e2 e3 -> 
+      EIte (substInFExpr pn nn e1) (substInFExpr pn nn e2) (substInFExpr pn nn e3)
+    ECst e s -> ECst (substInFExpr pn nn e) s
+    ELam targ e -> ELam targ (substInFExpr pn nn e)
+    ETApp e s -> ETApp (substInFExpr pn nn e) s 
+    ETAbs e sym -> ETAbs (substInFExpr pn nn e) sym
+    POr _exprLst -> error "POr not implemented yet" 
+    PNot _expr -> error "PNot not implemented yet"
+    PImp _expr1 _expr2 -> error "PImp not implemented yet"
+    PIff _expr1 _expr2 -> error "PIff not implemented yet"
+    PKVar _kvar _subst -> error "PKVar not implemented yet"
+    PAll _ _expr -> error "PAll not implemented yet"
+    PExist _ _expr -> error "PExist not implemented yet" 
+    PGrad _kvar _subst _gradInfo _expr -> error "PGrad not implemented yet"
+    ECoerc _sort1 _sort2 _expr -> error "ECoerc not implemented yet"  
