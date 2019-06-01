5aebfbfbbd7159968523c3b3ac2b57c927ec2afe
nikivazou <nikivazou@gmail.com>
2019-06-01 18:18:24 +0200

    fix build

--- tests/synth/fortytwo.hs
+++ tests/synth/fortytwo.hs
@@ -1,3 +1,5 @@
+{-@ LIQUID "--typed-holes" @-}
+
 {-@ fortytwo :: { i:Int | i > 41 } @-}
 fortytwo :: Int
 fortytwo = _fortytwo
--- tests/synth/greater.hs
+++ tests/synth/greater.hs
@@ -1,3 +1,6 @@
+{-@ LIQUID "--typed-holes" @-}
+
+
 {-@ greater :: { k:Int | k > 1 } -> i:Int -> { j:Int | j - k > 0 } @-}
 greater :: Int -> Int -> Int 
 greater = _greater
