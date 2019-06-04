562b86f3c149e2224cc91e69f487729c42f9b387
James Parker <dev@jamesparker.me>
2019-06-04 13:32:56 -0400

    let 42 hole test

--- tests/synth/fortytwo.hs
+++ tests/synth/fortytwo.hs
@@ -3,3 +3,9 @@
 {-@ fortytwo :: { i:Int | i > 41 } @-}
 fortytwo :: Int
 fortytwo = _fortytwo
+
+{-@ letfortytwo :: { i:Int | i > 41 } @-}
+letfortytwo :: Int
+letfortytwo =
+    let x = _x in
+    x + x
