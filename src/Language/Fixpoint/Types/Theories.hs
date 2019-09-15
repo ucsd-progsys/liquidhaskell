dc99207582ce53fc3017da6177888b9b888b932d
nikivazou <nikivazou@gmail.com>
2019-09-15 13:06:47 +0200

    cleaning up

--- src/Language/Fixpoint/Types/Theories.hs
+++ src/Language/Fixpoint/Types/Theories.hs
@@ -237,9 +237,9 @@ instance B.Binary SmtSort
 --   'smtSort False msg t' serializes a sort 't' using 'Int' instead of tyvars.
 
 sortSmtSort :: Bool -> SEnv DataDecl -> Sort -> SmtSort
-sortSmtSort poly env t  = (if poly then tracepp ("sortSmtSort: " ++ showpp t) else id) $  go . unAbs $ t
+sortSmtSort poly env t  = {- tracepp ("sortSmtSort: " ++ showpp t) else id) $ -}  go . unAbs $ t
   where
-    m = tracepp ("sortAbs for " ++ showpp t ) $ sortAbs t 
+    m = sortAbs t 
     go (FFunc _ _)    = SInt
     go FInt           = SInt
     go FReal          = SReal
@@ -247,8 +247,8 @@ sortSmtSort poly env t  = (if poly then tracepp ("sortSmtSort: " ++ showpp t) el
       | t == boolSort = SBool
       | isString t    = SString 
     go (FVar i)
-      | poly, i <= m  = tracepp ("FVAR i = " ++ showpp i ++ " m = " ++ showpp m) $ SVar i
-      | otherwise     = tracepp ("otherwise i = " ++ showpp i ++ " m = " ++ showpp m) $ SInt
+      | poly, i < m   = SVar i
+      | otherwise     = SInt
     go t              = fappSmtSort poly env ct ts where (ct:ts) = unFApp t
 
 fappSmtSort :: Bool -> SEnv DataDecl -> Sort -> [Sort] -> SmtSort
--- tests/test.hs
+++ tests/test.hs
@@ -74,6 +74,7 @@ unitTests
     , testGroup "elim-neg"   <$> dirTests elimCmd   "tests/neg"    ["float.fq"]   (ExitFailure 1)
     , testGroup "elim-crash" <$> dirTests elimCmd   "tests/crash"  []             (ExitFailure 2)
     , testGroup "proof"      <$> dirTests elimCmd   "tests/proof"     []          ExitSuccess
+    , testGroup "rankN"      <$> dirTests elimCmd   "tests/rankNTypes" []         ExitSuccess
 
     , testGroup "horn-pos"   <$> dirTests elimCmd   "tests/horn/pos"  []          ExitSuccess
     , testGroup "horn-neg"   <$> dirTests elimCmd   "tests/horn/neg"  []          (ExitFailure 1)
