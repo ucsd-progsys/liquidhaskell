95823dc1879ec76a80fe4c01fb7e50255926fed4
Panagiotis Bougoulias <panagiotis.bougoulias@imdea.org>
2019-09-23 15:27:20 +0200

    RAllT case.

--- src/Language/Haskell/Liquid/Synthesize/Termination.hs
+++ src/Language/Haskell/Liquid/Synthesize/Termination.hs
@@ -12,7 +12,7 @@ import qualified Language.Fixpoint.Types        as F
 
 
 import Var 
-
+import Debug.Trace
 
 decrType :: Var -> SpecType -> [Var] -> [(F.Symbol, SpecType)] -> SpecType
 decrType x ti xs xts = {- F.tracepp ("Decr type for " ++ showpp x ++ " on arguments " ++ showpp xs) $ -} go [] [] xs ti 
@@ -21,4 +21,5 @@ decrType x ti xs xts = {- F.tracepp ("Decr type for " ++ showpp x ++ " on argume
       | isDecreasing mempty mempty tx  = let (x', tx') = R.makeDecrType mempty (zip (v:accvs) ((x,tx):accxts)) 
                                          in RFun x' tx' t r 
     go accvs accxts (v:vs) (RFun x tx t r) = RFun x tx (go (v:accvs) ((x,tx):accxts) vs t) r
+    go accvs accxts (v:vs) (RAllT a t) = RAllT a $ go accvs accxts vs t
     go _     _       _     t = t 
