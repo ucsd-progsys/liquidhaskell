6d226b645607aee4b945b74c736f1d4203571ca8
nikivazou <nikivazou@gmail.com>
2019-09-26 05:59:29 +0200

    add extentionality to the test

--- tests/pos/T1548.hs
+++ tests/pos/T1548.hs
@@ -6,6 +6,18 @@ module T1548 where
 import Language.Haskell.Liquid.Equational 
 
 
+example :: (b -> d) -> a -> b -> Proof
+{-@ example :: g:(b -> d) -> a:a -> b:b -> { snd (second g (a,b)) == snd (a,g b) } @-}
+example g a b  
+  =   snd (second g (a,b)) 
+  ==. snd (a,g b)
+  *** QED 
+
+hFun :: (b -> d)  -> Proof
+{-@ hFun :: g:(b -> d) -> { snd . second g == g . snd } @-}
+hFun g = extensionality (snd . second g) (g . snd) (h g)
+
+
 h :: (b -> d) -> (a,b) -> Proof
 {-@ h :: g:(b -> d) -> p:(a,b) -> { (snd . second g) (p) == (g . snd) (p) } @-}
 h g p =   (snd . second g) p
@@ -28,3 +40,9 @@ sndSecond g (a,b)
   ==. g b
   ==. g (snd (a,b))
   *** QED
+
+
+
+extensionality :: (a -> b) -> (a -> b) -> (a -> ()) -> ()  
+{-@ assume extensionality :: f:(a -> b) -> g:(a -> b) -> (x:a -> {f x == g x}) ->  {f == g}  @-}
+extensionality _ _ _ = ()
