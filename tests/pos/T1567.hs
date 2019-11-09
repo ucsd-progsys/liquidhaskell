5896548c4da52ef1042b0aad544a669a29b3566c
nikivazou <nikivazou@gmail.com>
2019-11-09 09:07:29 +0100

    add docs

--- tests/pos/T1567.hs
+++ tests/pos/T1567.hs
@@ -1,15 +1,12 @@
 {-# LANGUAGE RankNTypes    #-}
 {-# LANGUAGE TypeOperators #-}
 
--- Natural transformation 
-
  {-@ infixr 1 ==> @-} 
  infixr 1 ==> 
  type f ==> g = forall z. f z -> g z 
   
 
 
- naturality :: (Functor f, Functor g) => (f ==> g) -> (q -> r) -> f q -> () 
- {-@ naturality :: g:(f ==> g) -> h:(q -> r) -> p:f q 
-                       -> { true } @-} 
- naturality _ _ _ = () 
+ test ::  (f ==> g) -> f x -> f y -> ()
+ {-@ test :: g:(f ==> g) -> f x -> f y -> ()  @-} 
+ test _ _ _ = () 
