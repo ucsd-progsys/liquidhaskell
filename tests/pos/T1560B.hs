4d964de78219086b0494045d8e94652ace40a851
Niki Vazou <nikivazou@gmail.com>
2019-11-28 11:30:01 +0100

    update test and FP

---
+++ tests/pos/T1560B.hs
@@ -0,0 +1,23 @@
+{-@ LIQUID "--ple"        @-}
+{-@ LIQUID "--reflection" @-}
+
+module T1560 where 
+
+k :: a -> b -> b
+k _ b = b
+
+{-@ reflect f @-}
+f :: Int -> Int
+f x = x + 1
+
+{-@ reflect g @-}
+g :: Int -> Int -> Int
+g x = f
+
+{-@ fgeqxy :: x:Int -> y:Int -> {f y = g x y} @-}
+fgeqxy :: Int -> Int -> ()
+fgeqxy x y = g x y `k` f y `k` ()
+
+{-@ fgeqx :: x:Int -> {f = g x} @-}
+fgeqx :: Int -> ()
+fgeqx x = f `k` g x `k` ()
