eb61fbbdba1f0c66ac06954800f7678d52848e48
nikivazou <nikivazou@gmail.com>
2020-01-28 01:40:30 +0100

    fix for #T1593

---
+++ tests/pos/T1593.hs
@@ -0,0 +1,16 @@
+{-@ LIQUID "--reflection"     @-}
+
+-- not ok
+data Thing0 m = Thing
+  { t0 :: (m Int -> Int) -> Int
+  }
+
+-- not ok
+data Thing1 m = Thing1
+  { t1 :: (Int -> m Int) -> Int
+  }
+
+-- ok
+data Thing2 m = Thing2
+  { t2 :: Int -> m Int -> Int
+  }
