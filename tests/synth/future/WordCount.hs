a9c86ed917b51aa43d409d154a59008b7c0e1a27
Panagiotis Bougoulias <panagiotis.bougoulias@imdea.org>
2020-02-06 14:47:44 +0100

    +tests

---
+++ tests/synth/future/WordCount.hs
@@ -0,0 +1,82 @@
+{-@ LIQUID "--typed-holes" @-}
+{-@ LIQUID "--reflection" @-}
+{-@ LIQUID "--ple"        @-}
+
+module WordCount where
+
+-- import Data.String
+
+-----------------------------------------------------------------------------------------------------
+-------------------------------------------DEFINITIONS-----------------------------------------------
+-----------------------------------------------------------------------------------------------------
+
+data List a = N | Cons a (List a)
+  deriving Eq 
+
+{-@ data List [length'] a <p :: a -> a -> Bool> = N | Cons {hd :: a, tl :: (List (a<p hd>))} @-}
+
+{-@ measure length' @-}
+{-@ length' :: List a -> Nat @-}
+length' :: List a -> Int
+length' N = 0
+length' (Cons _ xs) = 1 + length' xs
+
+{-@ type LPair a = (xs::List (List a)<{\h t -> h == t}>, { ys: Nat | ys == length' xs }) @-}
+type LPair a = (List (List a), Int)
+
+{-@ measure fst'' @-}
+{-@ fst'' :: LPair a -> List (List a) @-}
+fst'' :: LPair a -> List (List a)
+fst'' (x, _) = x
+
+{-@ measure snd'' @-}
+{-@ snd'' :: LPair a -> Nat @-}
+snd'' :: LPair a -> Int
+snd'' (_, x) = x
+
+-----------------------------------------------------------------------------------------------------
+----------------------------------------END OF DEFINITIONS-------------------------------------------
+-----------------------------------------------------------------------------------------------------
+
+{-@ span'' :: f:(a -> Bool) -> x:List a -> 
+      { p:( List <{\h t -> f h && f t}> { v: a | f v }, { r: List a | length' r <= length' x } ) | length' (fst p) + length' (snd p) == length' x }
+  @-}
+span'' :: (a -> Bool) -> List a -> (List a, List a)
+span'' _ N = (N, N)
+span'' p (Cons x xs') = if p x then  let  (ys, zs) = span'' p xs' 
+                                     in   (Cons x ys, zs)
+                               else  let  xs = Cons x xs' in (N, xs)
+
+{-@ groupBy'' :: p: (a -> a -> Bool) -> List a
+      -> [ List <{\h t -> p h t}> a ] 
+  @-} 
+groupBy''  :: (a -> a -> Bool) -> List a -> [ List a ] 
+groupBy'' _ N           = []
+groupBy'' f (Cons x xs) = 
+    let (ys, zs) = span'' (f x) xs 
+    in  Cons x ys : groupBy'' f zs 
+
+{-@ zipLen :: [ List (List a)<{\h t -> h == t}> ] -> [ LPair a ] @-} 
+zipLen :: Eq a => [ List (List a) ] -> [ LPair a ]
+zipLen xs = zipF' length' xs 
+
+{-@ zipF' :: f:(a -> b) -> x:[a] 
+      -> [(y::a, { z:b | z == f y })]
+  @-}
+zipF' :: (a -> b) -> [a] -> [(a, b)]
+zipF' _ []       = []
+zipF' f (x : xs) = (x, f x) : zipF' f xs
+
+{-@ zip' :: xs:List a -> {ys: List b | length' ys == length' xs } 
+              -> { v: List (a, b) | length' v == length' xs } 
+  @-}
+zip' :: List a -> List b -> List (a, b) 
+zip' N      N                = N
+zip' (Cons x xs) (Cons y ys) = Cons (x, y) (zip' xs ys)
+
+{-@ goal :: List (List a) -> [ LPair a ] @-}
+goal :: Eq a => List (List a) -> [ LPair a ]
+goal xs 
+    = _hole
+    -- = zipLen (groupBy'' (==) xs) 
+
--- tests/synth/target/Data3.hs
+++ tests/synth/target/Data3.hs
@@ -1,6 +1,6 @@
 {-@ LIQUID "--typed-holes" @-}
 
-module Data2 where
+module Data3 where
 
 {-@ data L [length'] a = N | C {x :: a, xs :: (L a)} @-}
 data L a = C a (L a) | N
@@ -11,11 +11,15 @@ length' :: L a -> Int
 length' N        = 0
 length' (C _ xs) = 1 + length' xs
 
-{-@ appendL :: x: L a -> y: L a -> { v: L a | length' v == length' x + length' y } @-}
+
+{-@ appendL :: x: L a -> y: L a -> 
+    { v: L a | length' v == length' x + length' y } 
+  @-}
 appendL N        y = y
 appendL (C x xs) y = C x (appendL xs y)
 
-{-@ ex1 :: xs:(L a) -> {v: (L a) | 2 * length' xs ==  length' v} @-}
-ex1 :: L a -> L a 
-ex1 = _hole
-
+{-@ append :: xs: L a -> ys: L a -> 
+    { v: L a | length' v == length' xs + length' ys } 
+  @-}
+append :: L a -> L a -> L a
+append = _goal
