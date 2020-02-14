c576700f04983d7abcbe5ff587fb63d737b4e68f
Panagiotis Bougoulias <panagiotis.bougoulias@imdea.org>
2020-02-14 14:56:23 +0100

    ListZipWith works.

--- tests/synth/target/ListZip.hs
+++ tests/synth/target/ListZip.hs
@@ -2,7 +2,24 @@
 
 module ListZip where 
 
+
+
+{-@ myerror :: {v:String | false } -> a @-} 
+myerror :: String -> a 
+myerror = error 
+
+dc :: String 
+dc = "dead code"
+
 {-@ zip' :: xs: [a] -> {ys:[b] | len ys == len xs} -> {v:[(a, b)] | len v == len xs} @-}
 zip' :: [a] -> [b] -> [(a, b)]
 zip' = _goal
 
+-- zip' xs ys = 
+--     case xs of 
+--         []     ->   case ys of
+--                         []      -> []
+--                         (x1:x2) -> error " len mismatch "
+--         x0:xs0 ->   case ys of
+--                         [] -> error " len mismatch "
+--                         (y0:ys0) -> (x0, y0) : zip' xs0 ys0
