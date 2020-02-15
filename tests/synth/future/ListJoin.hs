3ca9898c5fa142e36c1b21dafdf62c1fb8e1e9d0
Panagiotis Bougoulias <panagiotis.bougoulias@imdea.org>
2020-02-15 20:29:25 +0100

    TODO: Variable introduction.

---
+++ tests/synth/future/ListJoin.hs
@@ -0,0 +1,16 @@
+{-@ LIQUID "--typed-holes" @-}
+
+module ListJoin where
+
+-- Variable introduction for map's function
+{-@ map' :: (a -> b) -> x: [a] -> { v: [b] | len v == len x } @-}
+map' :: (a -> b) -> [a] -> [b]
+map' = map
+
+{-@ append' :: x: [a] -> y: [a] -> { v: [a] | len v == len x + len y } @-}
+append' :: [a] -> [a] -> [a]
+append' xs ys = xs ++ ys
+
+{-@ join' :: x: [a] -> y: [b] -> { v: [(a, b)] | len v == len x * len y } @-}
+join' :: [a] -> [b] -> [(a, b)]
+join' = _goal
