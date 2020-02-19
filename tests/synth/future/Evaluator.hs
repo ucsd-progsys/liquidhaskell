4208377e7311e464fb7e3992ba4c447d763471b5
Panagiotis Bougoulias <panagiotis.bougoulias@imdea.org>
2020-02-19 18:35:05 +0100

    Remove some tests.

---
+++ tests/synth/future/Evaluator.hs
@@ -0,0 +1,90 @@
+{-@ LIQUID "--typed-holes=5" @-}
+
+{-@ err :: { v: Int | false } -> a @-}
+err :: Int -> a
+err s = undefined
+
+{-@ zero :: {v: Int | v == 0} @-}
+zero :: Int 
+zero = 0
+
+{-@ one :: {v: Int | v == 1} @-}
+one :: Int 
+one = 1
+
+{-@ two :: {v: Int | v == 2} @-}
+two :: Int 
+two = 2
+
+data AST = Zero | One | Two | PlusNode AST AST | MinusNode AST AST | ProductNode AST AST
+
+{-@ measure size @-}
+{-@ size :: AST -> Nat @-}
+size :: AST -> Int
+size Zero 
+    = 1
+size One  
+    = 1
+size Two  
+    = 1
+size (PlusNode l r) 
+    = 1 + size l + size r
+size (MinusNode l r) 
+    = 1 + size l + size r
+size (ProductNode l r) 
+    = 1 + size l + size r
+
+{-@ measure result @-}
+{-@ result :: AST -> Int @-}
+result :: AST -> Int
+result Zero 
+    = 0
+result One 
+    = 1
+result Two
+    = 2
+result (PlusNode l r)
+    = result l + result r
+result (MinusNode l r)
+    = result l - result r
+result (ProductNode l r)
+    = result l * result r
+
+{-@ type OpCode = { v: Int | v >= 0 && v <= 2 } @-}
+type OpCode = Int
+
+{-@ data PAST = IntNode { x :: Int } | OpNode { op :: OpCode, l :: PAST, r :: PAST } @-}
+data PAST = IntNode { x :: Int } | OpNode { op :: OpCode, l :: PAST, r :: PAST }
+
+{-@ sizeP :: PAST -> Nat @-}
+sizeP :: PAST -> Int
+sizeP (IntNode x)
+    = 1
+sizeP (OpNode op l r)
+    = 1 + sizeP l + sizeP r
+
+{-@ measure resultP @-}
+{-@ resultP :: PAST -> Int @-}
+resultP :: PAST -> Int
+resultP (IntNode x)
+    = x
+resultP (OpNode op l r)
+    = if op == 0
+        then resultP l + resultP r
+        else if op == 1 
+                then resultP l - resultP r
+                else resultP l * resultP r
+
+{-@ transform :: x: AST -> { v: PAST | resultP v == result x } @-}
+transform :: AST -> PAST
+transform = _goal
+-- transform x 
+--     = case x of
+--         Zero                -> IntNode zero
+--         One                 -> IntNode one
+--         Two                 -> IntNode two
+--         PlusNode x6 x7      -> OpNode zero (transform x6) (transform x7)
+--         MinusNode x15 x16   -> OpNode one (transform x15) (transform x16)
+--         ProductNode x24 x25 -> OpNode two (transform x24) (transform x25)
+
+    
---
+++ tests/synth/future/ListSplit.hs
@@ -0,0 +1,26 @@
+{-@ LIQUID "--typed-holes=10" @-}
+
+module ListSplit where
+
+import qualified Data.Set as S
+
+{-@ err :: { v: Int | false } -> a @-}
+err :: Int -> a
+err s = undefined
+
+-- Case split on expressions 
+{-@ split :: xs: [a] -> 
+    { v: ( [a], [a] ) | len xs == len (fst v) + len (snd v) && 
+                        listElts xs == S.union (listElts (fst v)) (listElts (snd v)) }    
+ @-}
+split :: [a] -> ([a], [a])
+split = _goal
+-- split xs = 
+--     case xs of 
+--         [] -> (xs, xs)
+--         x5:x6 -> 
+--             case x6 of
+--                 [] -> (x6, xs)
+--                 x11:x12 ->
+--                     case split x12 of
+--                         (x16, x17) -> (x11:x16, x5:x17)
