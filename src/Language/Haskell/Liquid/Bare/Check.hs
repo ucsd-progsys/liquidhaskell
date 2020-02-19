758e9702186b3aa05ec409f4d14f080deee9e72a
Alfredo Di Napoli <alfredo@well-typed.com>
2020-02-19 15:19:46 +0100

    Add the tyConRealArity function
    This commits adds the 'tyConRealArity', where for 8.6.5 this is simply
    aliased to 'tyConArity'. This was done following a breaking change in
    the result of this function for GHC releases after 8.6.5.

--- src/Language/Haskell/Liquid/Bare/Check.hs
+++ src/Language/Haskell/Liquid/Bare/Check.hs
@@ -433,7 +433,7 @@ checkTcArity (RTyCon { rtc_tc = tc }) givenArity
   | otherwise
     = Nothing
   where
-    expectedArity = tyConArity tc
+    expectedArity = tyConRealArity tc
 
 
 checkAbstractRefs
