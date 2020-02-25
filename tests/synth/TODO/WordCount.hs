0decb2422f67276d60512e2db59622a6de5c4132
Panagiotis Bougoulias <panagiotis.bougoulias@imdea.org>
2020-02-25 17:16:38 +0100

    Progress on specification.

--- tests/synth/TODO/WordCount.hs
+++ tests/synth/TODO/WordCount.hs
@@ -21,7 +21,7 @@ length' :: List a -> Int
 length' N = 0
 length' (Cons _ xs) = 1 + length' xs
 
-{-@ type LPair a = (xs::List (List a)<{\h t -> h == t}>, { ys: Nat | ys == length' xs }) @-}
+{-@ type LPair a = (xs::List <{\h t -> h == t}> (List a), { ys: Nat | ys == length' xs }) @-}
 type LPair a = (List (List a), Int)
 
 {-@ measure fst'' @-}
@@ -47,8 +47,8 @@ span'' p (Cons x xs') = if p x then  let  (ys, zs) = span'' p xs'
                                      in   (Cons x ys, zs)
                                else  let  xs = Cons x xs' in (N, xs)
 
-{-@ groupBy'' :: p: (a -> a -> Bool) -> List a
-      -> [ List <{\h t -> p h t}> a ] 
+{-@ groupBy'' :: p: (a -> a -> Bool) -> xs: List a
+      -> { v: [ List <{\h t -> p h t}> a ] | length' xs == sLen v }
   @-} 
 groupBy''  :: (a -> a -> Bool) -> List a -> [ List a ] 
 groupBy'' _ N           = []
@@ -56,16 +56,20 @@ groupBy'' f (Cons x xs) =
     let (ys, zs) = span'' (f x) xs 
     in  Cons x ys : groupBy'' f zs 
 
-{-@ zipLen :: [ List (List a)<{\h t -> h == t}> ] -> [ LPair a ] @-} 
-zipLen :: Eq a => [ List (List a) ] -> [ LPair a ]
-zipLen xs = zipF' length' xs 
+-- {-@ zipLen :: xs: [ List (List a)<{\h t -> h == t}> ] -> { v: [ LPair a ] | sumLen v == sLen' xs } @-} 
+-- zipLen :: Eq a => [ List (List a) ] -> [ LPair a ]
+-- zipLen xs = zipF' length'' xs 
 
-{-@ zipF' :: f:(a -> b) -> x:[a] 
-      -> [(y::a, { z:b | z == f y })]
+{-@ zipLen0 :: xs: [ List <{\h t -> h == t}> (List a) ] -> { v: [ (xs::List <{\h t -> h == t}> (List a), Nat) ] | sumLen v == sLen xs } @-} 
+zipLen0 :: Eq a => [ List (List a) ] -> [ (List (List a), Int) ]
+zipLen0 xs = zipF'' length' xs 
+
+{-@ zipF'' :: f: (List (List a) -> b) -> x: [ List (List a) ]
+                -> { v: [ (y::List (List a), { z: b | z == f y}) ] | sumLen v == sLen x }
   @-}
-zipF' :: (a -> b) -> [a] -> [(a, b)]
-zipF' _ []       = []
-zipF' f (x : xs) = (x, f x) : zipF' f xs
+zipF'' :: (List (List a) -> b) -> [List (List a)] -> [(List (List a), b)]
+zipF'' _ []       = []
+zipF'' f (x : xs) = (x, f x) : zipF'' f xs
 
 {-@ zip' :: xs:List a -> {ys: List b | length' ys == length' xs } 
               -> { v: List (a, b) | length' v == length' xs } 
@@ -74,8 +78,20 @@ zip' :: List a -> List b -> List (a, b)
 zip' N      N                = N
 zip' (Cons x xs) (Cons y ys) = Cons (x, y) (zip' xs ys)
 
-{-@ goal :: List (List a) -> [ LPair a ] @-}
-goal :: Eq a => List (List a) -> [ LPair a ]
--- goal = _hole
-goal xs = zipLen (groupBy'' (==) xs) 
+{-@ measure sumLen @-}
+{-@ sumLen :: [ (List (List a), b) ] -> Nat @-}
+sumLen :: [ (List (List a), b) ] -> Int
+sumLen []          = 0
+sumLen ((x, l):xs) = length' x + sumLen xs
+
+{-@ measure sLen @-}
+{-@ sLen :: [ List a ] -> Nat @-}
+sLen :: [ List a ] -> Int
+sLen [ ]    = 0
+sLen (x:xs) = length' x + sLen xs
+
+-- {-@ goal :: xs: List (List a) -> { v: [ LPair a ] | length' xs == sumLen v } @-}
+-- goal :: Eq a => List (List a) -> [ () ]
+-- -- -- goal = _hole
+-- goal xs = zipLen0 (groupBy'' (==) xs) 
 
