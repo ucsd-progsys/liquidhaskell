9d65d03c1622569539280d8cc3688886ff12ac45
Panagiotis Bougoulias <panagiotis.bougoulias@imdea.org>
2020-03-06 16:48:37 +0100

    Tests for merge.

--- tests/synth/ints/fortytwo.hs
+++
@@ -1,6 +0,0 @@
-{-@ LIQUID "--typed-holes" @-}
-
-{-@ fortytwo :: { i:Int | i > 41 } @-}
-fortytwo :: Int
-fortytwo = _fortytwo
-
--- tests/synth/ints/greater.hs
+++
@@ -1,6 +0,0 @@
-{-@ LIQUID "--typed-holes" @-}
-
-
-{-@ greater :: { k:Int | k > 1 } -> i:Int -> { j:Int | j - k > 0 } @-}
-greater :: Int -> Int -> Int 
-greater = _greater
