d186790e223742aa8f399c9717b3c7b579884e66
nikivazou <nikivazou@gmail.com>
2020-04-02 10:14:38 +0200

    Do not substitute bound variables on qualification

---
+++ tests/pos/T1636.hs
@@ -0,0 +1,25 @@
+{-@ LIQUID "--reflection" @-}
+{-@ LIQUID "--ple"        @-}
+
+module Qualify where 
+
+data RA a  = RA  
+
+
+{-@ type RRA a E1 E2 = {v:RA a | E1 == E2} @-}
+
+
+{-@ assume eqRTCtx :: f:a -> g:a  -> RRA a {f} {g} -> ctx:(a -> b) -> RRA b {ctx f} {ctx g}  @-}
+eqRTCtx ::  a -> a -> RA a -> (a -> b) -> RA b
+eqRTCtx _f _g _pf _ctx = undefined  
+
+fEqG0BadDomain :: Integer -> RA Integer
+fEqG0BadDomain x = RA
+
+{-@ reflect f @-}
+f :: Integer -> Integer 
+f x = x + 1 
+
+{-@ reflect g @-}
+g :: Integer -> Integer 
+g x = x + 1 
