1087bdcc6a9de09b9110b1684f682ef6c32b5c18
Zack Grannan <zgrannan@gmail.com>
2020-04-16 13:04:32 +0800

    Add failing test

---
+++ tests/neg/ReWrite4.hs
@@ -0,0 +1,31 @@
+module ReWrite where 
+
+{-@ LIQUID "--reflection" @-}
+{-@ LIQUID "--ple" @-}
+{-@ infix ++ @-}
+
+import Prelude hiding ((++), drop, length)
+
+{-@ measure length @-}
+{-@ length :: [a] -> Int @-}
+length :: [a] -> Int
+length []     = 0
+length (_:xs) = 1 + length xs
+
+{-@ reflect drop @-}
+{-@ drop :: Int -> [a] -> [a] @-}
+drop :: Int -> [a] -> [a]
+drop n (x:xs) = drop (n - 1) xs
+drop _ []     = []
+drop 0 xs     = xs
+
+{-@ dropProof :: n : Int -> { xs : [a] | length xs >= n } -> { drop n xs = [] } @-}
+dropProof :: Int -> [a] -> ()
+dropProof _ []     = ()
+dropProof n (_:xs) = dropProof (n - 1) xs
+
+{-@ rewriteWith dropProof' dropProof  @-}
+{-@ dropProof' :: n : Int -> xs : [a] -> { drop n xs = [] } @-}
+dropProof' :: Int -> [a] -> ()
+dropProof' _ _ = ()
+
