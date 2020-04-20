4bdff5e6840deabc7731720f7c59f2a4e9c4048d
Zack Grannan <zgrannan@gmail.com>
2020-04-20 11:48:20 +0800

    Also add a sort check

---
+++ tests/neg/ReWrite8.hs
@@ -0,0 +1,28 @@
+module ReWrite8 where
+
+{-@ LIQUID "--savequery" @-}
+{-@ LIQUID "--reflection" @-}
+{-@ LIQUID "--ple" @-}
+{-@ infix ++ @-}
+
+import Prelude hiding ((++))
+
+data MyList a = MyNil | Cons a (MyList a)
+
+{-@ reflect ++ @-}
+(++)::MyList a -> MyList a -> MyList a
+MyNil       ++ ys = ys
+(Cons x xs) ++ ys = Cons x (xs ++ ys)
+
+
+{-@ concatIdent :: xs : MyList a -> { xs = xs ++ MyNil } @-}
+concatIdent :: MyList a -> ()
+concatIdent MyNil       = ()
+concatIdent (Cons _ xs) = concatIdent xs
+
+{-@ rewriteWith concatIdent' concatIdent @-}
+{-@ concatIdent' :: xs : MyList a -> { xs = xs ++ MyNil } @-}
+concatIdent' :: MyList a -> ()
+concatIdent' _ = ()
+
+
