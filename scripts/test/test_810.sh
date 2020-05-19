424ba3577e53fb212b4cc258c21b62be2e6e4d74
Alfredo Di Napoli <alfredo@well-typed.com>
2020-05-19 11:04:31 +0200

    Hide more packages

--- scripts/test/test_810.sh
+++ scripts/test/test_810.sh
@@ -2,6 +2,9 @@
 
 TASTY_GLOB_PATTERN=$1
 
+RUNNER="\"stack exec -- liquid -v0 -hide-package ghc-prim -hide-package containers -hide-package vector -hide-package bytestring\""
+
 stack build --fast --test --no-run-tests && 
     stack test --fast -j1 liquidhaskell:test \
-    --ta="-p /$TASTY_GLOB_PATTERN/"
+    --ta="-p /$TASTY_GLOB_PATTERN/" \
+    --ta="--liquid-runner $RUNNER"
--- scripts/test/test_865.sh
+++ scripts/test/test_865.sh
@@ -2,8 +2,8 @@
 
 TASTY_GLOB_PATTERN=$1
 
-stack build --stack-yaml=stack-8.6.5.yaml --fast liquidhaskell:exe:liquid-legacy && \
+stack build --test --no-run-tests --stack-yaml=stack-8.6.5.yaml --fast liquidhaskell:exe:liquid-legacy && \
     stack test --stack-yaml=stack-8.6.5.yaml -j1 liquidhaskell:test \
-    --flag liquidhaskell:include --flag liquidhaskell:devel \
-    --ta="--liquid-runner \"stack --stack-yaml=$PWD/stack-8.6.5.yaml exec -- liquid \"" \
+    --flag liquidhaskell:include --flag liquidhaskell:devel --flag liquidhaskell:-testNewExecutable \
+    --ta="--liquid-runner \"stack --stack-yaml=$PWD/stack-8.6.5.yaml exec -- liquid-legacy\"" \
     --ta="-p /$TASTY_GLOB_PATTERN/" --fast
