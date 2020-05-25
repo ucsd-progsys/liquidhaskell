b0a2774f12a6eafca9617b2ace5581428d051942
Alfredo Di Napoli <alfredo@well-typed.com>
2020-05-25 11:38:46 +0200

    Remove use of --fast from CI

--- scripts/test/test_810.sh
+++ scripts/test/test_810.sh
@@ -4,7 +4,7 @@ TASTY_GLOB_PATTERN=$1
 
 RUNNER="\"stack --silent exec -- liquid -v0 \""
 
-stack build --fast --test --no-run-tests && 
-    stack test --fast -j1 liquidhaskell:test \
+stack build --test --no-run-tests && 
+    stack test -j1 liquidhaskell:test \
     --ta="-p /$TASTY_GLOB_PATTERN/" \
     --ta="--liquid-runner $RUNNER"
--- scripts/test/test_865.sh
+++ scripts/test/test_865.sh
@@ -2,8 +2,8 @@
 
 TASTY_GLOB_PATTERN=$1
 
-stack build --test --no-run-tests --stack-yaml=stack-8.6.5.yaml --fast && \
+stack build --test --no-run-tests --stack-yaml=stack-8.6.5.yaml && \
     stack test --stack-yaml=stack-8.6.5.yaml -j1 liquidhaskell:test \
     --flag liquidhaskell:include --flag liquidhaskell:-testNewExecutable \
     --ta="--liquid-runner \"stack --stack-yaml=$PWD/stack-8.6.5.yaml exec -- liquid-legacy\"" \
-    --ta="-p /$TASTY_GLOB_PATTERN/" --fast
+    --ta="-p /$TASTY_GLOB_PATTERN/"
