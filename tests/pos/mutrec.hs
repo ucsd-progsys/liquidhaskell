5852ddb9926ef38085ad35b9b4635ebe8ef38a86
James Parker <dev@jamesparker.me>
2020-06-05 21:12:30 -0400

    Improve documentation and test case for mutual recursion.

--- tests/pos/mutrec.hs
+++ tests/pos/mutrec.hs
@@ -1,13 +1,13 @@
 module MutRec () where
 
-{-@ isEven :: n:Nat -> z:{v:Int | v = 0} -> Bool / [n, z] @-}
-isEven :: Int -> Int -> Bool
-isEven 0 _ = True
-isEven n _ = isOdd (n-1) 1
+{-@ isEven :: n:Nat -> Bool / [n, 0] @-}
+isEven :: Int -> Bool
+isEven 0 = True
+isEven n = isOdd (n-1)
 
-{-@ isOdd :: n:Nat -> z:{v:Int | v = 1} -> Bool / [n, z] @-}
-isOdd :: Int -> Int -> Bool
-isOdd  n _ = not $ isEven n 0
+{-@ isOdd :: n:Nat -> Bool / [n, 1] @-}
+isOdd :: Int -> Bool
+isOdd  n = not $ isEven n
 
 
 {- decrease isEven 1 2 -}
