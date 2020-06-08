7e670f0dfd2c320e810bcd7bc389a1a292a4bec9
Alfredo Di Napoli <alfredo@well-typed.com>
2020-06-08 16:51:24 +0200

    Use CPP to selectively export the plugin

--- src/LiquidHaskell.hs
+++ src/LiquidHaskell.hs
@@ -1,10 +1,20 @@
+{-# LANGUAGE CPP #-}
+
 module LiquidHaskell (
     -- * LiquidHaskell Specification QuasiQuoter
     lq
+#ifdef MIN_VERSION_GLASGOW_HASKELL
+#if MIN_VERSION_GLASGOW_HASKELL(8,10,0,0)
     -- * LiquidHaskell as a compiler plugin
   , plugin
+#endif
+#endif
   ) where
 
 import Language.Haskell.Liquid.UX.QuasiQuoter
-import Language.Haskell.Liquid.GHC.Plugin (plugin)
 
+#ifdef MIN_VERSION_GLASGOW_HASKELL
+#if MIN_VERSION_GLASGOW_HASKELL(8,10,0,0)
+import Language.Haskell.Liquid.GHC.Plugin (plugin)
+#endif
+#endif
