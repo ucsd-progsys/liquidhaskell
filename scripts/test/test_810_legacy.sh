a8924a1bc40a743c4ed3594a908ac96d86124574
Alfredo Di Napoli <alfredo@well-typed.com>
2020-06-29 15:33:25 +0200

    Do not pass extra args to stack build in CI

--- scripts/test/test_810_legacy.sh
+++ scripts/test/test_810_legacy.sh
@@ -4,7 +4,7 @@ TASTY_GLOB_PATTERN=$1
 
 RUNNER="\"stack --silent exec -- liquid-legacy\""
 
-stack build --flag liquidhaskell:no-plugin --flag liquid-platform:legacy --test --no-run-tests &&
+stack build --test --no-run-tests &&
     stack test -j1 liquidhaskell:test --flag liquidhaskell:include \
     --flag liquid-platform:legacy --flag liquid-platform:devel --flag liquidhaskell:no-plugin \
     --ta="-p /$TASTY_GLOB_PATTERN/" \
