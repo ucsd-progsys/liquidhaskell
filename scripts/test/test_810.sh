6aef1f6ac5e0371720a36775347898e80ee86582
Alfredo Di Napoli <alfredo@well-typed.com>
2020-06-30 11:28:27 +0200

    Revert back to old naming scheme

--- scripts/test/test_810.sh
+++ scripts/test/test_810.sh
@@ -2,9 +2,9 @@
 
 TASTY_GLOB_PATTERN=$1
 
-RUNNER="\"stack --silent exec -- liquid -v0 \""
+RUNNER="\"stack --silent exec -- liquidhaskell -v0 \""
 
-stack build --test --no-run-tests && 
+stack build --test --no-run-tests &&
     stack test -j1 liquidhaskell:test \
     --ta="-p /$TASTY_GLOB_PATTERN/" \
     --ta="--liquid-runner $RUNNER"
--- scripts/test/test_810_legacy.sh
+++ scripts/test/test_810_legacy.sh
@@ -4,8 +4,7 @@ TASTY_GLOB_PATTERN=$1
 
 RUNNER="\"stack --silent exec -- liquid-legacy\""
 
-stack build --flag liquidhaskell:no-plugin --test --no-run-tests &&
-    stack test -j1 liquidhaskell:test --flag liquidhaskell:include \
-    --flag liquid-platform:legacy --flag liquid-platform:devel --flag liquidhaskell:no-plugin \
+stack test -j1 liquidhaskell:test --flag liquidhaskell:include \
+    --flag liquid-platform:devel --flag liquidhaskell:no-plugin \
     --ta="-p /$TASTY_GLOB_PATTERN/" \
     --ta="--liquid-runner $RUNNER"
