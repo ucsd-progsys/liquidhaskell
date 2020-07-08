0a5b41e1428cc30e797a2d2a73222d2af09091fe
Alfredo Di Napoli <alfredo@well-typed.com>
2020-07-08 12:39:41 +0200

    Mirror the parallel package.

---
+++ liquid-parallel/src/Control/Parallel.hs
@@ -0,0 +1,3 @@
+module Control.Parallel (module Exports) where
+
+import "parallel" Control.Parallel as Exports
---
+++ liquid-parallel/src/Control/Seq.hs
@@ -0,0 +1,3 @@
+module Control.Seq (module Exports) where
+
+import "parallel" Control.Seq as Exports
