6962f29b1230da3e440472342bfc997c709a19e0
Alfredo Di Napoli <alfredo@well-typed.com>
2020-07-20 09:07:09 +0200

    Refactoring of dev mode hooks
    This commit factors out the content of the custom 'Setup.hs', which was
    the same for each and every module into a liquidhaskell module we can
    then reuse by using the 'setup-depends' directive.

--- liquid-bytestring/Setup.hs
+++ liquid-bytestring/Setup.hs
@@ -1,16 +1,6 @@
 module Main where
 
-import Distribution.Simple
-import System.Environment
+import Language.Haskell.Liquid.Cabal (liquidHaskellMain)
 
 main :: IO ()
-main = do
-  libHooks <- customHooks
-  defaultMainWithHooks libHooks
-
-customHooks :: IO UserHooks
-customHooks = do
-  mbDevMode <- lookupEnv "LIQUID_DEV_MODE"
-  case mbDevMode of
-    Nothing -> pure simpleUserHooks
-    Just x  -> pure simpleUserHooks { buildHook = \_ _ _ _ -> return () }
+main = liquidHaskellMain
