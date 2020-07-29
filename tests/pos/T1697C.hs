4440f7803205fc9a097e6ecfc6b99a379b43b111
Niki Vazou <nikivazou@gmail.com>
2020-07-29 17:15:14 +0200

    add two other examples

--- tests/pos/T1697C.hs
+++ tests/pos/T1697C.hs
@@ -1,6 +1,8 @@
-{-@ LIQUID "--no-pattern-inline" @-}
+-------------------------------------------------------------------------------------------
+-- | Polymorphic user but defining bind and return as normal functions (WORKING)
+-------------------------------------------------------------------------------------------
 
-module B where
+module C where
 
 data User = U
 
@@ -10,25 +12,17 @@ data User = U
 data TaggedT user m a = TaggedT { unTag :: m a }
 {-@ data variance TaggedT invariant invariant covariant contravariant covariant @-}
 
-instance Functor m => Functor (TaggedT user m) where
-  fmap f = TaggedT . fmap f . unTag
-
-instance Applicative m => Applicative (TaggedT user m) where
-  pure = TaggedT . pure
-  f <*> x = TaggedT $ unTag f <*> unTag x
-
 {-@
-instance Monad m => Monad (TaggedT user m) where
-  >>= :: forall < p :: user -> Bool
-                , q :: user -> Bool
-                , r :: user -> Bool
-                , s :: user -> Bool
-                , t :: user -> Bool
-                , u :: user -> Bool
-                , rx :: a -> Bool
-                , rf :: a -> b -> Bool
-                , ro :: b -> Bool
-                >.
+assume bindT :: forall < p :: user -> Bool
+                       , q :: user -> Bool
+                       , r :: user -> Bool
+                       , s :: user -> Bool
+                       , t :: user -> Bool
+                       , u :: user -> Bool
+                       , rx :: a -> Bool
+                       , rf :: a -> b -> Bool
+                       , ro :: b -> Bool
+                       >.
     {content :: a<rx> |- b<rf content> <: b<ro>}
     {content :: a<rx> |- b<ro> <: b<rf content>}
     {{v : (user<s>) | True} <: {v : (user<p>) | True}}
@@ -38,13 +32,15 @@ instance Monad m => Monad (TaggedT user m) where
     {{v : (user<s>) | True} <: {v : (user<u>) | True}}
     x:TaggedT<p, q> user m (a<rx>)
     -> (y:a -> TaggedT<r, s> user m (b<rf y>))
-    -> TaggedT<t, u> user m (b<ro>);
-
-  return :: a -> TaggedT<{\_ -> True}, {\_ -> False}> user m a
+    -> TaggedT<t, u> user m (b<ro>)
 @-}
-instance Monad m =>  Monad (TaggedT user m) where
-  x >>= f = TaggedT $ unTag x >>= \y -> unTag (f y)
-  
+bindT :: Monad m => TaggedT user m a -> (a -> TaggedT user m b) -> TaggedT user m b
+bindT x f = TaggedT $ unTag x >>= \y -> unTag (f y)
+
+{-@ assume returnT :: a -> TaggedT<{\_ -> True}, {\_ -> False}> user m a @-}
+returnT :: Monad m => a -> TaggedT user m a
+returnT x = TaggedT (return x)
+
 -------------------------------------------------------------------------------------------
 -- | Test
 -------------------------------------------------------------------------------------------
@@ -55,4 +51,4 @@ respondT = undefined
 
 {-@ test :: TaggedT<{\_ -> True}, {\v -> v == currentUser}> User m () @-}
 test :: Monad m => TaggedT User m ()
-test = return "a" >>= respondT
+test = returnT "a" `bindT` respondT
