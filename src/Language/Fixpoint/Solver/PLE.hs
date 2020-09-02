54017d0ff32ea2f3b4765bb3bca50e1c6c11797f
Niki Vazou <nikivazou@gmail.com>
2020-09-02 19:21:36 +0300

    remove contra optimization

--- src/Language/Fixpoint/Solver/PLE.hs
+++ src/Language/Fixpoint/Solver/PLE.hs
@@ -680,7 +680,7 @@ reWriteDDecl ddecl = concatMap go (ddCtors ddecl)
 
 askSMT :: Config -> SMT.Context -> [(Symbol, Sort)] -> Expr -> IO Bool
 askSMT cfg ctx bs e
-  | isContraPred e     = return False 
+--   | isContraPred e     = return False 
   | isTautoPred  e     = return True
   | null (Vis.kvars e) = SMT.checkValidWithContext ctx [] PTrue e'
   | otherwise          = return False
