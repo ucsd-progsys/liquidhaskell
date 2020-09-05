003b8c5b4f40a5da52f8f49359533a7503a89849
PLR <51248199+plredmond@users.noreply.github.com>
2020-09-05 14:56:47 -0700

    document things a bit

--- default.nix
+++ default.nix
@@ -1,3 +1,17 @@
+/*
+ * to build all of the packages in the project
+ *
+ *    nix-build
+ *
+ * to get an environment for development of one package in the project
+ *
+ *    nix-shell --argstr envFor liquidhaskell
+ *
+ * to extend and override in a new nix expression
+ *
+ *    Eg. for liquid-fixpoint, which lives in its own repo and shouldn't need to repeat everything
+ *    TODO: import this repo's pins.nix and override the haskellPackages it contains
+ */
 { envFor ? null, config ? { allowBroken = true; }, ... }:
 let
   pins = import ./pins.nix { inherit config; };
