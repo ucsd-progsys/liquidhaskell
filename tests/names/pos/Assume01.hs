053a356494d23fb69027522669fe4ec2463458b6
Ranjit Jhala <jhala@cs.ucsd.edu>
2020-10-31 09:57:32 -0700

    asd

--- tests/names/pos/Assume01.hs
+++ tests/names/pos/Assume01.hs
@@ -1,6 +1,6 @@
 module OverWrite where
 
-import qualified Data.Set
+import qualified Data.Set as S
 
 {-@ type UList a = {v:[a] | ListUnique v} @-}
 
@@ -12,11 +12,11 @@ import qualified Data.Set
 
 {-@ predicate EqElts X Y    = ((listElts X) = (listElts Y)) @-}
 
-{-@
-  measure listDup :: [a] -> (Data.Set.Set a)
-  listDup([])   = {v | Set_emp v }
-  listDup(x:xs) = {v | v = if (Set_mem x (listElts xs)) then (Set_cup (Set_sng x) (listDup xs)) else (listDup xs) }
+{-@ measure listDup :: [a] -> (Data.Set.Set a)
+      listDup []     = {v | Set_emp v }
+      listDup (x:xs) = {v | v = if (Set_mem x (listElts xs)) then (Set_cup (Set_sng x) (listDup xs)) else (listDup xs) }
   @-}
+
 
 {-@ foo :: xs:(UList a) -> {v: UList a | EqElts v xs} @-}
 foo  = reverse 
