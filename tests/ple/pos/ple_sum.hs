7d32b27ebd14f2e25c3b34796ab89f91b90c978c
Ranjit Jhala <jhala@cs.ucsd.edu>
2021-01-31 19:23:40 -0800

    add tests

---
+++ tests/ple/pos/ple_sum.hs
@@ -0,0 +1,13 @@
+
+{-@ LIQUID "--reflection" @-}
+{-@ LIQUID "--ple" @-}
+{-@ LIQUID "--fuel=5" @-}
+
+{-@ reflect sumTo @-}
+sumTo :: Int -> Int 
+sumTo n = if n <= 0 then 0 else n + sumTo (n-1)
+
+{-@ test :: () -> { sumTo 5 == 15 } @-}
+test :: () -> () 
+test _ = ()
+
