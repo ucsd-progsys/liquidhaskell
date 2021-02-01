ce7986b112ebeb3f02925f62292746b234a4cc64
Niki Vazou <nikivazou@gmail.com>
2021-02-01 12:33:32 +0100

    add example and better error message

---
+++ tests/errors/ReflectLam.hs
@@ -0,0 +1,15 @@
+{-@ LIQUID "--reflection" @-}
+
+
+module ReflectLam where 
+
+
+
+{-@ reflect bar @-}
+bar :: [Integer]
+bar = mymap (\x -> x + 1) [1,2,3]
+
+{-@ reflect mymap @-}
+mymap :: (a -> b) -> [a] -> [b]
+mymap f [] = [] 
+mymap f (x:xs) = f x : mymap f xs 
