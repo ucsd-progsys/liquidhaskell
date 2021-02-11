843cc8dfefa807c6db0a6047f8d95703b65970bb
Niki Vazou <nikivazou@gmail.com>
2021-02-11 13:35:28 +0100

    replicate the crash

--- tests/import/client/CString.hs
+++ tests/import/client/CString.hs
@@ -4,5 +4,5 @@ module CString where
 import LString 
 
 bar :: [a] -> () 
-{-@ bar :: xs:{[a] | 0 < len xs} -> {myhead xs == True } @-} 
-bar xs@(_:_) = ()
+{-@ bar :: xs:{[a] | true } -> {myhead xs} @-} 
+bar xs = ()
