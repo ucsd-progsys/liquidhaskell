68ddfdda106950107be9449301dba80483dc04c0
PLR <51248199+plredmond@users.noreply.github.com>
2021-03-11 22:56:01 -0800

    update to fixed liquid-fixpoint; merge namespaced-outputs and unnamespaced outputs by namspacing unnamespaced outputs at lookup time

--- flake.nix
+++ flake.nix
@@ -14,11 +14,11 @@
 
   outputs = { self, nixpkgs, flake-utils, liquid-fixpoint }:
     let
-      namespaced-outputs = system:
+      mkOutputs = system:
         let
           pkgs = import nixpkgs {
             inherit system;
-            overlays = liquid-fixpoint.overlays ++ self.overlays;
+            overlays = [ liquid-fixpoint.overlay.${system} ] ++ self.overlays.${system};
           };
         in
         {
@@ -45,54 +45,52 @@
 
           devShell = self.defaultPackage.${system}.env;
 
-        };
-      unnamespaced-outputs = {
 
-        overlays = [
-          # add th-compat to haskellPackages
-          (final: prev: {
-            haskellPackages = prev.haskellPackages.override {
-              overrides = selfH: superH: {
-                th-compat = selfH.callHackage "th-compat" "0.1" { };
-              };
-            };
-          })
-          # overlay to add our own package
-          (final: prev:
-            let
-              callCabal2nix = prev.haskellPackages.callCabal2nix;
-              source-ignores = [ "*.nix" "result" ];
-              source = path: prev.nix-gitignore.gitignoreSource source-ignores path;
-            in
-            {
+          overlays = [
+            # add th-compat to haskellPackages
+            (final: prev: {
               haskellPackages = prev.haskellPackages.override {
-                overrides = selfH: superH: with prev.haskell.lib; {
-                  # LH without tests
-                  liquidhaskell =
-                    let src = prev.nix-gitignore.gitignoreSource ([ "liquid-*" ] ++ source-ignores) ./.;
-                    in dontCheck (callCabal2nix "liquidhaskell" src { });
-                  ## LH spec/shadow packages
-                  liquid-base = dontHaddock (callCabal2nix "liquid-base" (source ./liquid-base) { });
-                  liquid-bytestring = dontHaddock (callCabal2nix "liquid-bytestring" (source ./liquid-bytestring) { });
-                  liquid-containers = (callCabal2nix "liquid-containers" (source ./liquid-containers) { });
-                  liquid-ghc-prim = dontHaddock (callCabal2nix "liquid-ghc-prim" (source ./liquid-ghc-prim) { });
-                  liquid-parallel = (callCabal2nix "liquid-parallel" (source ./liquid-parallel) { });
-                  liquid-vector = (callCabal2nix "liquid-vector" (source ./liquid-vector) { });
-                  ## LH bundles
-                  liquid-platform = (callCabal2nix "liquid-platform" (source ./liquid-platform) { });
-                  liquid-prelude = dontHaddock (callCabal2nix "liquid-prelude" (source ./liquid-prelude) { });
-                  ## LH with tests
-                  liquidhaskell_with_tests = overrideCabal selfH.liquidhaskell (old: {
-                    testDepends = old.testDepends or [ ] ++ [ prev.hostname ];
-                    #testHaskellDepends = old.testHaskellDepends ++ projectPackages; # TODO use the packages list somehow
-                    preCheck = ''export TASTY_LIQUID_RUNNER="liquidhaskell -v0"'';
-                  });
+                overrides = selfH: superH: {
+                  th-compat = selfH.callHackage "th-compat" "0.1" { };
                 };
               };
             })
-        ];
+            # overlay to add our own package
+            (final: prev:
+              let
+                callCabal2nix = prev.haskellPackages.callCabal2nix;
+                source-ignores = [ "*.nix" "result" ];
+                source = path: prev.nix-gitignore.gitignoreSource source-ignores path;
+              in
+              {
+                haskellPackages = prev.haskellPackages.override {
+                  overrides = selfH: superH: with prev.haskell.lib; {
+                    # LH without tests
+                    liquidhaskell =
+                      let src = prev.nix-gitignore.gitignoreSource ([ "liquid-*" ] ++ source-ignores) ./.;
+                      in dontCheck (callCabal2nix "liquidhaskell" src { });
+                    ## LH spec/shadow packages
+                    liquid-base = dontHaddock (callCabal2nix "liquid-base" (source ./liquid-base) { });
+                    liquid-bytestring = dontHaddock (callCabal2nix "liquid-bytestring" (source ./liquid-bytestring) { });
+                    liquid-containers = (callCabal2nix "liquid-containers" (source ./liquid-containers) { });
+                    liquid-ghc-prim = dontHaddock (callCabal2nix "liquid-ghc-prim" (source ./liquid-ghc-prim) { });
+                    liquid-parallel = (callCabal2nix "liquid-parallel" (source ./liquid-parallel) { });
+                    liquid-vector = (callCabal2nix "liquid-vector" (source ./liquid-vector) { });
+                    ## LH bundles
+                    liquid-platform = (callCabal2nix "liquid-platform" (source ./liquid-platform) { });
+                    liquid-prelude = dontHaddock (callCabal2nix "liquid-prelude" (source ./liquid-prelude) { });
+                    ## LH with tests
+                    liquidhaskell_with_tests = overrideCabal selfH.liquidhaskell (old: {
+                      testDepends = old.testDepends or [ ] ++ [ prev.hostname ];
+                      #testHaskellDepends = old.testHaskellDepends ++ projectPackages; # TODO use the packages list somehow
+                      preCheck = ''export TASTY_LIQUID_RUNNER="liquidhaskell -v0"'';
+                    });
+                  };
+                };
+              })
+          ];
 
-      };
+        };
     in
-    flake-utils.lib.eachDefaultSystem namespaced-outputs // unnamespaced-outputs;
+    flake-utils.lib.eachDefaultSystem mkOutputs;
 }
