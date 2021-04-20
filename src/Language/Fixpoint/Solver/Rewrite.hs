5c8f394bea3d78b1ca2626253d65ec5360c1ec58
Zack Grannan <zgrannan@protonmail.com>
2021-04-20 16:06:39 -0700

    WIP

--- src/Language/Fixpoint/Solver/Rewrite.hs
+++ src/Language/Fixpoint/Solver/Rewrite.hs
@@ -23,171 +23,21 @@ import qualified Data.Maybe           as Mb
 import           Language.Fixpoint.Types hiding (simplify)
 import qualified Data.Text as TX
 import Text.PrettyPrint (text)
+import qualified Language.REST.Types as RT
+import           Language.REST.Op
 
-type Op = Symbol
-type OpOrdering = [Symbol]
-data Term = Term Symbol [Term] deriving (Eq, Generic)
-instance Hashable Term
-
-termSym :: Term -> Symbol
-termSym (Term s _) = s
-
-instance Show Term where
-  show (Term op [])   = TX.unpack $ symbolText op
-  show (Term op args) =
-    TX.unpack (symbolText op) ++ "(" ++ L.intercalate ", " (map show args) ++ ")"
-
-data SCDir =
-    SCUp
-  | SCEq 
-  | SCDown
-  deriving (Eq, Ord, Show, Generic)
-
-instance Hashable SCDir
-
-type SCPath = ((Op, Int), (Op, Int), [SCDir])
 type SubExpr = (Expr, Expr -> Expr)
 
-data SCEntry = SCEntry {
-    from :: (Op, Int)
-  , to   :: (Op, Int)
-  , dir  :: SCDir
-} deriving (Eq, Ord, Show, Generic)
-
-instance Hashable SCEntry
-
-getDir :: OpOrdering -> Term -> Term -> SCDir
-getDir o from to =
-  case (synGTE o from to, synGTE o to from) of
-      (True, True)  -> SCEq
-      (True, False) -> SCDown
-      (False, _)    -> SCUp
-
-getSC :: OpOrdering -> Term -> Term -> S.HashSet SCEntry
-getSC o (Term op ts) (Term op' us) = 
-  S.fromList $ do
-    (i, from) <- zip [0..] ts
-    (j, to)   <- zip [0..] us
-    return $ SCEntry (op, i) (op', j) (getDir o from to)
-
-scp :: OpOrdering -> [Term] -> S.HashSet SCPath
-scp _ []       = S.empty
-scp _ [_]      = S.empty
-scp o [t1, t2] = S.fromList $ do
-  (SCEntry a b d) <- S.toList $ getSC o t1 t2
-  return (a, b, [d])
-scp o (t1:t2:trms) = S.fromList $ do
-  (SCEntry a b' d) <- S.toList $ getSC o t1 t2
-  (a', b, ds)      <- S.toList $ scp o (t2:trms)
-  guard $ b' == a'
-  return (a, b, d:ds)
-
-synEQ :: OpOrdering -> Term -> Term -> Bool
-synEQ o l r = synGTE o l r && synGTE o r l
-
-opGT :: OpOrdering -> Op -> Op -> Bool
-opGT ordering op1 op2 = case (L.elemIndex op1 ordering, L.elemIndex op2 ordering) of
-  (Just index1, Just index2) -> index1 < index2
-  (Just _, Nothing)          -> True
-  _                          -> False
-
-removeSynEQs :: OpOrdering -> [Term] -> [Term] -> ([Term], [Term])
-removeSynEQs _ [] ys      = ([], ys)
-removeSynEQs ordering (x:xs) ys
-  | Just yIndex <- L.findIndex (synEQ ordering x) ys
-  = removeSynEQs ordering xs $ take yIndex ys ++ drop (yIndex + 1) ys
-  | otherwise =
-    let
-      (xs', ys') = removeSynEQs ordering xs ys
-    in
-      (x:xs', ys')
-
-synGTEM :: OpOrdering -> [Term] -> [Term] -> Bool
-synGTEM ordering xs ys =     
-  case removeSynEQs ordering xs ys of
-    (_   , []) -> True
-    (xs', ys') -> any (\x -> all (synGT ordering x) ys') xs'
-    
-synGT :: OpOrdering -> Term -> Term -> Bool
-synGT o t1 t2 = synGTE o t1 t2 && not (synGTE o t2 t1)
-
-synGTM :: OpOrdering -> [Term] -> [Term] -> Bool
-synGTM o t1 t2 = synGTEM o t1 t2 && not (synGTEM o t2 t1)
-
-synGTE :: OpOrdering -> Term -> Term -> Bool
-synGTE ordering t1@(Term x tms) t2@(Term y tms') =
-  if opGT ordering x y then
-    synGTM ordering [t1] tms'
-  else if opGT ordering y x then
-    synGTEM ordering tms [t2]
-  else
-    synGTEM ordering tms tms'
-
-subsequencesOfSize :: Int -> [a] -> [[a]]
-subsequencesOfSize n xs = let l = length xs
-                          in if n>l then [] else subsequencesBySize xs !! (l-n)
- where
-   subsequencesBySize [] = [[[]]]
-   subsequencesBySize (x:xs) = let next = subsequencesBySize xs
-                             in zipWith (++) ([]:next) (map (map (x:)) next ++ [[]])
-
-data TermOrigin = PLE | RW OpOrdering deriving (Show, Eq)
+data TermOrigin = PLE | RW deriving (Show, Eq)
 
 instance PPrint TermOrigin where
   pprintTidy _ = text . show
 
-data DivergeResult = Diverging | NotDiverging OpOrdering
+data DivergeResult = Diverging | NotDiverging
 
 fromRW :: TermOrigin -> Bool
-fromRW (RW _) = True
-fromRW PLE    = False
-
-getOrdering :: TermOrigin -> Maybe OpOrdering
-getOrdering (RW o) = Just o
-getOrdering PLE    = Nothing
-
-diverges :: Maybe Int -> [(Term, TermOrigin)] -> Term -> DivergeResult
-diverges maxOrderingConstraints path term = go 0
-  where
-   path' = map fst path ++ [term]
-   go n |    n > length syms'
-          || n > Mb.fromMaybe (length syms') maxOrderingConstraints = Diverging
-   go n = case L.find (not . diverges') (orderings' n) of
-     Just ordering -> NotDiverging ordering
-     Nothing       -> go (n + 1)
-   ops (Term o xs) = o:concatMap ops xs
-   syms'           = L.nub $ concatMap ops path'
-   suggestedOrderings :: [OpOrdering]
-   suggestedOrderings =
-     reverse $ Mb.catMaybes $ map (getOrdering . snd) path
-   orderings' n    =
-     suggestedOrderings ++ concatMap L.permutations ((subsequencesOfSize n) syms')
-   diverges' o     = divergesFor o path term
-
-divergesFor :: OpOrdering -> [(Term, TermOrigin)] -> Term -> Bool
-divergesFor o path term = any diverges' terms'
-  where
-    terms = map fst path ++ [term]
-    lastRWIndex =
-      Mb.fromMaybe 0 (fmap fst $ L.find (fromRW . snd . snd) $ reverse $ zip [1..] path) 
-    okTerms    = take lastRWIndex terms
-    checkTerms = drop lastRWIndex terms
-    terms' = L.subsequences checkTerms ++ do
-      firstpart  <- L.tails okTerms
-      secondpart <- L.inits checkTerms
-      return $ firstpart ++ secondpart
-    diverges' :: [Term] -> Bool
-    diverges' trms' =
-      if length trms' <= 1 || termSym (head trms') /= termSym (last trms') then
-        False
-      else
-        any ascending (scp o trms') && all (not . descending) (scp o trms')
-      
-descending :: SCPath -> Bool
-descending (a, b, ds) = a == b && L.elem SCDown ds && L.notElem SCUp ds
-
-ascending :: SCPath -> Bool
-ascending  (a, b, ds) = a == b && L.elem SCUp ds
+fromRW RW  = True
+fromRW PLE = False
 
 data RWTerminationOpts =
     RWTerminationCheckEnabled (Maybe Int) -- # Of constraints to consider
@@ -217,19 +67,19 @@ getRewrite rwArgs path (subE, toE) (AutoRewrite args lhs rhs) =
       RWTerminationCheckDisabled -> return (expr', RW [])
   where
     
-    convert (EIte i t e) = Term "$ite" $ map convert [i,t,e]
+    convert (EIte i t e) = RT.App "$ite" $ map convert [i,t,e]
     convert (EApp (EVar s) (EVar var))
       | dcPrefix `isPrefixOfSym` s
-      = Term (symbol $ TX.concat [symbolText s, "$", symbolText var]) []
+      = RT.App (Op $ TX.unpack $ TX.concat [symbolText s, "$", symbolText var]) []
      
     convert e@(EApp{})    | (EVar fName, terms) <- splitEApp e
-                          = Term fName $ map convert terms
-    convert (EVar s)      = Term s []                  
-    convert (PAnd es)     = Term "$and" $ map convert es
-    convert (POr es)      = Term "$or" $ map convert es
-    convert (PAtom s l r) = Term (symbol $ "$atom" ++ show s) [convert l, convert r]
-    convert (EBin o l r)  = Term (symbol $ "$ebin" ++ show o) [convert l, convert r]
-    convert (ECon c)      = Term (symbol $ "$econ" ++ show c) []
+                          = RT.App (Op (show fName)) $ map convert terms
+    convert (EVar s)      = RT.App (Op (show s)) []
+    convert (PAnd es)     = RT.App "$and" $ map convert es
+    convert (POr es)      = RT.App "$or" $ map convert es
+    convert (PAtom s l r) = RT.App (Op $ "$atom" ++ show s) [convert l, convert r]
+    convert (EBin o l r)  = RT.App (Op $ "$ebin" ++ show o) [convert l, convert r]
+    convert (ECon c)      = RT.App (Op $ "$econ" ++ show c) []
     convert e             = error (show e)
     
     
