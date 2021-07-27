6812a791520d7218dce4d8ea9fa68fdffe99f7b6
Facundo Domínguez <facundo.dominguez@tweag.io>
2021-07-27 13:09:01 -0300

    Add hostname to shell-stack.nix

--- shell-stack.nix
+++ shell-stack.nix
@@ -6,7 +6,7 @@ with pkgs;
 
 haskell.lib.buildStackProject ({
   name = "liquidhaskell-stack";
-  buildInputs = [ git z3 ];
+  buildInputs = [ git z3 hostname ];
   ghc = ghc;
   LANG = "en_US.utf8";
 })
