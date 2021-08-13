7f0a7e0e3f3cb605e574dcad35f3f1ebe377d21f
Facundo Domínguez <facundo.dominguez@tweag.io>
2021-08-13 19:26:22 -0300

    Implement shortening of names in the prettified output

--- src/Language/Fixpoint/Solver/Prettify.hs
+++ src/Language/Fixpoint/Solver/Prettify.hs
@@ -14,6 +14,8 @@ import qualified Data.HashMap.Lazy as HashMap
 import           Data.HashSet (HashSet)
 import qualified Data.HashSet as HashSet
 import           Data.List (intersperse)
+import           Data.Maybe (fromMaybe)
+import qualified Data.Text as Text
 import           Language.Fixpoint.Misc (ensurePath)
 import           Language.Fixpoint.Solver.EnvironmentReduction
   ( dropLikelyIrrelevantBindings
@@ -27,10 +29,29 @@ import           Language.Fixpoint.Types.Config (Config, queryFile)
 import           Language.Fixpoint.Types.Constraints
 import           Language.Fixpoint.Types.Environments
   (BindEnv, elemsIBindEnv, lookupBindEnv)
-import           Language.Fixpoint.Types.Names (Symbol)
+import           Language.Fixpoint.Types.Names
+  ( Symbol
+  , dropPrefixOfSym
+  , prefixOfSym
+  , suffixOfSym
+  , suffixSymbol
+  , symbol
+  , symbolText
+  )
 import           Language.Fixpoint.Types.PrettyPrint
 import           Language.Fixpoint.Types.Refinements
-  (SortedReft(..), conjuncts, reftBind, reftPred, sortedReftSymbols)
+  ( Expr(..)
+  , Reft
+  , SortedReft(..)
+  , conjuncts
+  , reft
+  , reftBind
+  , reftPred
+  , sortedReftSymbols
+  , substf
+  , substSortInExpr
+  )
+import           Language.Fixpoint.Types.Sorts (Sort(..), substSort)
 import           Language.Fixpoint.Types.Substitutions (exprSymbolsSet)
 import qualified Language.Fixpoint.Utils.Files as Files
 import           System.FilePath (addExtension)
@@ -68,19 +89,20 @@ prettyConstraint aenv bindEnv c =
 
       simplifiedLhs = inlineInSortedReft 100 boolSimplEnv (slhs c)
       simplifiedRhs = inlineInSortedReft 100 boolSimplEnv (srhs c)
-      prunedPrettyEnv =
-        eraseUnusedBindings constraintSymbols $
-        dropLikelyIrrelevantBindings aenv constraintSymbols $
+
+      prunedEnv =
+        dropLikelyIrrelevantBindings aenv (constraintSymbols simplifiedLhs simplifiedRhs) $
         HashMap.map snd boolSimplEnv
-      constraintSymbols =
-        HashSet.union (sortedReftSymbols simplifiedLhs) (sortedReftSymbols simplifiedRhs)
+      (renamedEnv, c') =
+        shortenVarNames prunedEnv c { slhs = simplifiedLhs, srhs = simplifiedRhs }
+      prettyEnv = eraseUnusedBindings (constraintSymbols (slhs c') (srhs c')) renamedEnv
    in hang (text "\n\nconstraint:") 2 $
-          text "lhs" <+> toFix simplifiedLhs
-      $+$ text "rhs" <+> toFix simplifiedRhs
-      $+$ (pprId (sid c) <+> text "tag" <+> toFix (stag c))
-      $+$ toFixMeta (text "simpleConstraint" <+> pprId (sid c)) (toFix (sinfo c))
+          text "lhs" <+> toFix (slhs c')
+      $+$ text "rhs" <+> toFix (srhs c')
+      $+$ pprId (sid c) <+> text "tag" <+> toFix (stag c)
+      $+$ toFixMeta (text "constraint" <+> pprId (sid c)) (toFix (sinfo c))
       $+$ hang (text "environment:") 2
-            (vcat $ intersperse "" $ elidedMessage : map prettyBind prunedPrettyEnv)
+            (vcat $ intersperse "" $ elidedMessage : map prettyBind prettyEnv)
   where
     prettyBind (ms, sr) = sep [maybe "_" toFix ms <+> ":", nest 2 $ prettySortedRef sr]
     prettySortedRef sr = braces $ sep
@@ -89,6 +111,9 @@ prettyConstraint aenv bindEnv c =
       ]
 
     elidedMessage = "// elided some likely irrelevant bindings"
+
+    constraintSymbols sr0 sr1 =
+      HashSet.union (sortedReftSymbols sr0) (sortedReftSymbols sr1)
 
 pprId :: Show a => Maybe a -> Doc
 pprId (Just i)  = "id" <+> text (show i)
@@ -103,17 +128,111 @@ toFixMeta k v = text "// META" <+> k <+> text ":" <+> v
 -- Erasure is accomplished by replacing the symbol with @Nothing@ in the
 -- bindings.
 eraseUnusedBindings
-  :: HashSet Symbol -> HashMap Symbol SortedReft -> [(Maybe Symbol, SortedReft)]
-eraseUnusedBindings ss env = map (first reachable) $ HashMap.toList env
+  :: HashSet Symbol -> [(Symbol, SortedReft)] -> [(Maybe Symbol, SortedReft)]
+eraseUnusedBindings ss env = map (first reachable) env
   where
     allSymbols = HashSet.union ss envSymbols
     envSymbols =
       HashSet.unions $
-      map (exprSymbolsSet . reftPred . sr_reft) $
-      HashMap.elems env
+      map (exprSymbolsSet . reftPred . sr_reft . snd) env
 
     reachable s =
       if s `HashSet.member` allSymbols then
         Just s
       else
         Nothing
+
+-- | Shortens the names of refinements types in a given environment
+-- and constraint
+shortenVarNames
+  :: HashMap Symbol SortedReft
+  -> SubC a
+  -> ([(Symbol, SortedReft)], SubC a)
+shortenVarNames env c =
+  let bindsRenameMap = proposeRenamings $ HashMap.keys env
+      env' = map (renameBind bindsRenameMap) (HashMap.toList env)
+   in
+      (env', renameSubC bindsRenameMap c)
+  where
+    renameSubC :: HashMap Symbol Symbol -> SubC a -> SubC a
+    renameSubC symMap c =
+      mkSubC
+        (senv c)
+        (renameSortedReft symMap $ slhs c)
+        (renameSortedReft symMap $ srhs c)
+        (sid c)
+        (stag c)
+        (sinfo c)
+
+    renameBind
+      :: HashMap Symbol Symbol -> (Symbol, SortedReft) -> (Symbol, SortedReft)
+    renameBind symMap (s, sr) =
+      (at symMap s, renameSortedReft symMap sr)
+
+    renameSortedReft
+      :: HashMap Symbol Symbol -> SortedReft -> SortedReft
+    renameSortedReft symMap (RR t r) =
+      let sortSubst = FObj . (at symMap)
+       in RR (substSort sortSubst t) (renameReft symMap r)
+
+    renameReft :: HashMap Symbol Symbol -> Reft -> Reft
+    renameReft symMap r =
+      let m = HashMap.insert (reftBind r) (prefixOfSym $ reftBind r) symMap
+          sortSubst = FObj . (at symMap)
+       in reft (at m (reftBind r)) $
+            substSortInExpr sortSubst $
+            (substf (EVar . (at m)) $ reftPred r)
+
+    at :: HashMap Symbol Symbol -> Symbol -> Symbol
+    at m k = fromMaybe k $ HashMap.lookup k m
+
+-- | Given a list of symbols with no duplicates, it proposes shorter
+-- names to use for them.
+--
+-- All proposals preserve the prefix of the original name. A prefix is
+-- the longest substring containing the initial character and no separator
+-- ## ('symSepName').
+--
+-- Suffixes are preserved, except when symbols with a given prefix always
+-- use the same suffix.
+--
+proposeRenamings :: [Symbol] -> HashMap Symbol Symbol
+proposeRenamings = toSymMap . toPrefixSuffixMap
+
+-- | Indexes symbols by prefix and then by suffix. If the prefix
+-- equals the symbol, then the suffix is the empty symbol.
+--
+-- For instance,
+--
+-- > toPrefixSuffixMap ["a##b##c"] ! "a" ! "b" == ["a##b##c"]
+-- > toPrefixSuffixMap ["a##b"] ! "a" ! "" == ["a##b"]
+--
+toPrefixSuffixMap :: [Symbol] -> HashMap Symbol (HashMap Symbol [Symbol])
+toPrefixSuffixMap xs = HashMap.fromListWith (HashMap.unionWith (++))
+  [ (pfx, HashMap.singleton sfx [s])
+  | s <- xs
+  , let pfx = prefixOfSym s
+        sfx = suffixOfSym $ dropPrefixOfSym s
+  ]
+
+-- | Suggests renamings for every symbol in a given prefix/suffix map.
+toSymMap :: HashMap Symbol (HashMap Symbol [Symbol]) -> HashMap Symbol Symbol
+toSymMap prefixMap = HashMap.fromList
+  [ r
+  | (pfx, h) <- HashMap.toList prefixMap
+  , r <- renameWithSuffixes pfx (HashMap.toList h)
+  ]
+  where
+    renameWithSuffixes pfx = \case
+      [(_sfx, ss)] -> renameWithAppendages pfx ("", ss)
+      sfxs -> concatMap (renameWithAppendages pfx) sfxs
+
+    renameWithAppendages pfx (sfx, ss) = zip ss $ case ss of
+      [_s] -> [pfx `suffixIfNotNull` sfx]
+      ss -> zipWith (rename pfx sfx) [1..] ss
+
+    rename pfx sfx i _s =
+      pfx `suffixIfNotNull` sfx `suffixSymbol` symbol (show i)
+
+    suffixIfNotNull a b =
+      if Text.null (symbolText b) then a else a `suffixSymbol` b
