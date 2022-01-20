c60be0c32ff566ce3406d4a1b9412c9e67ffcd3c
Niki Vazou <nikivazou@gmail.com>
2022-01-20 09:26:14 +0100

    add documentation

--- src/Language/Haskell/Liquid/GHC/Play.hs
+++ src/Language/Haskell/Liquid/GHC/Play.hs
@@ -22,45 +22,62 @@ import Language.Haskell.Liquid.Types.Errors
 -------------------------------------------------------------------------------
 
 getNonPositivesTyCon :: [TyCon] -> [(TyCon, [DataCon])]
-getNonPositivesTyCon tcs = Mb.catMaybes $ map go (M.toList $ makeAppearences tcs)
+getNonPositivesTyCon tcs = Mb.catMaybes $ map go (M.toList $ makeOccurrences tcs)
   where 
-    go (tc,dcocs) = case filter (\(_,occ) -> elem tc (negA occ)) dcocs of  
+    go (tc,dcocs) = case filter (\(_,occ) -> elem tc (negOcc occ)) dcocs of  
                       [] -> Nothing
                       xs -> Just (tc, fst <$> xs) 
 
-data TyConAppearance = TyConA {posA :: [TyCon], negA :: [TyCon] }
+
+-- OccurrenceMap maps type constructors to their TyConOccurrence. 
+-- for each of their data constructor. For example, for the below data definition
+-- data T a = P (T a) | N (T a -> Int) | Both (T a -> T a) | None 
+-- the entry below should get generated
+--  OccurrenceMap 
+-- = T |-> [(P, [
+--          (P,    TyConOcc [T] [])
+--          (N,    TyConOcc [Int] [T])
+--          (Both, TyConOcc [T] [T])
+--          (None, TyConOcc [] [])
+--         ])] 
+-- For positivity check, ultimately we only care about self occurences, 
+-- but we keep track of all the TyCons for the mutually inductive data types. 
+-- We separate the occurences per data constructor only to provide better error messages. 
+type OccurrenceMap = M.HashMap TyCon [(DataCon, TyConOccurrence)] 
+
+data TyConOccurrence 
+    = TyConOcc { posOcc :: [TyCon] -- TyCons that occur in positive positions
+               , negOcc :: [TyCon] -- TyCons that occur in negative positions
+               }
   deriving Eq
 
-instance Monoid TyConAppearance where 
-  mempty = TyConA mempty mempty 
-instance Semigroup TyConAppearance where 
-  TyConA p1 n1 <> TyConA p2 n2 = TyConA (L.nub (p1 <> p2)) (L.nub (n1 <> n2))
-instance Outputable TyConAppearance where
-  ppr (TyConA pos neg) = text "pos" <+> ppr pos <+>  text "neg" <+> ppr neg
+instance Monoid TyConOccurrence where 
+  mempty = TyConOcc mempty mempty 
+instance Semigroup TyConOccurrence where 
+  TyConOcc p1 n1 <> TyConOcc p2 n2 = TyConOcc (L.nub (p1 <> p2)) (L.nub (n1 <> n2))
+instance Outputable TyConOccurrence where
+  ppr (TyConOcc pos neg) = text "pos" <+> ppr pos <+>  text "neg" <+> ppr neg
 
 
--- Keep track of appearances of each DataCon for a better error message
-type ApprearanceMap = M.HashMap TyCon [(DataCon, TyConAppearance)] 
-
-instance Outputable ApprearanceMap where
+instance Outputable OccurrenceMap where
   ppr m = ppr (M.toList m)
 
 
-makeAppearences :: [TyCon] -> ApprearanceMap 
-makeAppearences tycons 
-  = let m0 = M.fromList [(tc, map (\dc -> (dc, makeAppearence (dctypes dc))) (tyConDataCons tc)) 
+makeOccurrences :: [TyCon] -> OccurrenceMap 
+makeOccurrences tycons 
+  = let m0 = M.fromList [(tc, map (\dc -> (dc, makeOccurrence (dctypes dc))) (tyConDataCons tc)) 
                         | tc <- tycons']
-    -- fixpoint to find appearanced of mutually recursive data definitons
+    -- fixpoint to find occurrences of mutually recursive data definitons
     in fix (\m -> foldl merge m tycons') m0 
   where 
     fix f x = let x' = f x in if x == x' then x else fix f x' 
     merge m tc = M.update (mergeList m) tc m 
     mergeList m xs = Just [(dc, mergeApp m am) | (dc,am) <- xs]
-    mergeApp m (TyConA pos neg) = 
-        let TyConA pospos posneg = mconcat (findAppearence m <$> pos)
-            TyConA negpos negneg = mconcat (findAppearence m <$> neg) 
+    mergeApp m (TyConOcc pos neg) = 
+        let TyConOcc pospos posneg = mconcat (findOccurrence m <$> pos)
+            TyConOcc negpos negneg = mconcat (findOccurrence m <$> neg) 
         -- Keep positive, flip negative 
-        in TyConA (L.nub (pos <> pospos <> negneg)) (L.nub (neg <> negpos <> posneg))
+        in TyConOcc (L.nub (pos <> pospos <> negneg)) (L.nub (neg <> negpos <> posneg))
 
 
     tycontypes tc = concatMap dctypes $ tyConDataCons tc
@@ -78,11 +95,11 @@ makeAppearences tycons
     tcs (CastTy _ _)      = [] 
     tcs (CoercionTy _)    = []  
 
-makeAppearence :: [Type] -> TyConAppearance 
-makeAppearence ts = foldl (go 1) mempty ts 
+makeOccurrence :: [Type] -> TyConOccurrence 
+makeOccurrence ts = foldl (go 1) mempty ts 
   where 
-    go :: Int -> TyConAppearance -> Type -> TyConAppearance
-    go p m (TyConApp tc ts)  = addAppearence p tc $ foldl (go p) m ts 
+    go :: Int -> TyConOccurrence -> Type -> TyConOccurrence
+    go p m (TyConApp tc ts)  = addOccurrence p tc $ foldl (go p) m ts 
     go _ m (TyVarTy _ )      = m 
     go _ m (AppTy t1 t2)     = go 0 (go 0 m t1) t2  
     go p m (ForAllTy _ t)    = go p m t 
@@ -91,13 +108,13 @@ makeAppearence ts = foldl (go 1) mempty ts
     go _ m (CastTy _ _)      = m  
     go _ m (CoercionTy _)    = m   
 
-    addAppearence p tc (TyConA pos neg)  
-      | p > 0     = TyConA (L.nub (tc:pos)) neg  
-      | p < 0     = TyConA pos (L.nub (tc:neg))  
-      | otherwise = TyConA (L.nub (tc:pos)) (L.nub (tc:neg))  
+    addOccurrence p tc (TyConOcc pos neg)  
+      | p > 0     = TyConOcc (L.nub (tc:pos)) neg  
+      | p < 0     = TyConOcc pos (L.nub (tc:neg))  
+      | otherwise = TyConOcc (L.nub (tc:pos)) (L.nub (tc:neg))  
 
-findAppearence :: ApprearanceMap -> TyCon -> TyConAppearance
-findAppearence m tc = mconcat (snd <$> M.lookupDefault mempty tc m)
+findOccurrence :: OccurrenceMap -> TyCon -> TyConOccurrence
+findOccurrence m tc = mconcat (snd <$> M.lookupDefault mempty tc m)
 
 
 
