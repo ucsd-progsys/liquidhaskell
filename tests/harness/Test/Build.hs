329b70dee8e680b8518d99da0d4009d24110d7c8
Matt Walker <matthew.walker@tweag.io>
2022-05-31 07:30:08 -0400

    Write modifyErrors temporarily into the test-driver

--- tests/harness/Test/Build.hs
+++ tests/harness/Test/Build.hs
@@ -31,6 +31,7 @@ import Control.Monad (void)
 import Data.String.AnsiEscapeCodes.Strip.Text (stripAnsiEscapeCodes)
 import System.Environment
 import Data.Foldable (for_)
+import Data.Function ((&))
 
 -- | Whether or not we want to only build the dependencies of a library (to help
 -- sanitize the compiler output)
@@ -159,7 +160,8 @@ stackErrorStripper = cabalErrorStripper . stripStackExtraneousMessages . stripSt
 
 -- | For building `main`; provided so that we don't repeat outselves in
 -- "Driver_cabal.hs" and "Driver_stack.hs"
-program :: Sh () -- ^ test that the environent is okay
+program
+  :: Sh () -- ^ test that the environent is okay
   -> (Text -> Text) -- ^ stripper for the "stdout"
   -> (Text -> Text) -- ^ stripper for the "stderr"
   -> (OnlyDeps -> TestGroupName -> IO (ExitCode, Text, Text)) -- ^ builder function
@@ -227,3 +229,12 @@ simpleProgram testEnv runner (Options testGroups False) = do
     Just testGroupsSelected -> do
       let selectedTestGroups = if null testGroupsSelected then snd <$> M.toList allTestGroupsMap else testGroupsSelected
       simpleBuild runner selectedTestGroups >>= exitWith
+
+modifyErrors :: IO ()
+modifyErrors = do
+  let msgs = errorMsgs2 & M.toList
+  for_ msgs $ \(fp, msg) -> Sh.shelly $ do
+    fContents <- Sh.readfile fp
+    Sh.writefile fp $ "{-@ LIQUID \"--expect-error-containing=\\\"" <> msg <> "\\\"\" @-}" <> fContents
+
+      
