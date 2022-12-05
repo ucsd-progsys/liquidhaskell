aa9949d8cbc3cdc2ee50b06ffdc4ffbd508d738c
Lisa Vasilenko <vasilliza@gmail.com>
2022-12-05 18:53:26 +0100

    add generated unary spec

--- tests/relational/pos/IncrLet.hs
+++ tests/relational/pos/IncrLet.hs
@@ -1,7 +1,15 @@
 module Fixme where
 
+{-@ reflect incr @-}
 incr :: Int -> Int
-incr = let one = 1 in (+ one)
+incr = (+ 1)
 
-{-@ relational incr ~ incr :: {x1:Int -> Int ~ x2:Int -> Int
-                           | x1 < x2 :=> r1 x1 < r2 x2    }  @-}
+{-@ relational incr ~ incr :: { x1:Int -> Int 
+                              ~ x2:Int -> Int
+                              | x1 < x2 :=> r1 x1 < r2 x2 } @-}
+
+{-@ relIncrIncr_rg9 :: x1:GHC.Types.Int 
+                    -> x2:{VV : GHC.Types.Int | x1 < x2} 
+                    -> {VV : () | incr x1 < incr x2} @-}
+relIncrIncr_rg9 :: Int -> Int-> ()
+relIncrIncr_rg9 _ _ = ()
