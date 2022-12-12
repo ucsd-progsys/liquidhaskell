25909259e162ac8ba85de8b5e50b97b095c7d895
Lisa Vasilenko <vasilliza@gmail.com>
2022-12-12 13:02:43 +0100

    include generated proofs in the examples

--- tests/relational/pos/IncrLet.hs
+++ tests/relational/pos/IncrLet.hs
@@ -1,15 +1,20 @@
 module Fixme where
 
+import GHC.Types
+
+{-@ reflect plus @-}
+plus :: Int -> Int -> Int 
+plus x y = x + y
+
 {-@ reflect incr @-}
 incr :: Int -> Int
-incr = (+ 1)
+incr = plus 1
 
 {-@ relational incr ~ incr :: { x1:Int -> Int 
                               ~ x2:Int -> Int
                               | x1 < x2 :=> r1 x1 < r2 x2 } @-}
 
-{-@ relIncrIncr_rg9 :: x1:GHC.Types.Int 
-                    -> x2:{VV : GHC.Types.Int | x1 < x2} 
-                    -> {VV : () | incr x1 < incr x2} @-}
-relIncrIncr_rg9 :: Int -> Int-> ()
-relIncrIncr_rg9 _ _ = ()
+-- {-@ relIncrIncr_rtt :: x1:GHC.Types.Int -> x2:{VV : GHC.Types.Int | x1 < x2} -> {VV : () | Fixme.incr x1 < Fixme.incr x2} @-}
+-- relIncrIncr_rtt :: GHC.Types.Int -> GHC.Types.Int -> ()
+-- relIncrIncr_rtt = () (() () ()) (() () ())
+
--- tests/relational/todo/Map.hs
+++ tests/relational/todo/Map.hs
@@ -9,9 +9,32 @@ diff (x : xs) (y : ys) | x == y = diff xs ys
 diff (x : xs) (y : ys) | x /= y = 1 + diff xs ys
 diff _ _                        = 0
 
+{-@ reflect map @-}
 map :: (Int -> Int) -> [Int] -> [Int]
 map _ []       = []
 map f (x : xs) = f x : map f xs
 
 {-@ relational map ~ map :: {f1:(x1:_ -> _) -> xs1:_ -> _ ~ f2:(x2:_ -> _) -> xs2:_ -> _ 
                          | true :=> len xs1 = len xs2 && f1 = f2 :=> Fixme.diff xs1 xs2 >= Fixme.diff (r1 f1 xs1) (r2 f2 xs2)} @-}
+
+{-@ relMapMap_rga :: f1:(x1:Int -> Int) -> f2:(x2:Int -> Int) -> xs1:[Int] -> xs2:{VV : [Int] | f1 == f2 && len xs1 == len xs2} 
+                  -> {VV : () | Fixme.diff xs1 xs2 >= Fixme.diff (Fixme.map f1 xs1) (Fixme.map f2 xs2)} @-}
+relMapMap_rga :: (Int -> Int)
+                -> (Int -> Int)
+                -> [Int]
+                -> [Int]
+                -> ()
+relMapMap_rga ds_d1mml ds_d1mm ds_d1mnl ds_d1mn =
+    case ds_d1mnl of
+        [] ->
+            case ds_d1mn of
+                [] -> ()
+                (:) x xs -> ()
+
+        (:) xl xsl ->
+            case ds_d1mn of
+                [] -> relMapMap_rga ds_d1mml ds_d1mm xsl [] `const` relMapMap_rga ds_d1mml ds_d1mm xsl []
+                (:) x xs -> -- (\ _ _ _ _ -> ()) ((\ _ _ -> ()) xl x) ((\ _ _ -> ()) xl x) 
+                            -- (
+                                relMapMap_rga ds_d1mml ds_d1mm xsl xs `const` relMapMap_rga ds_d1mml ds_d1mm xsl xs
+
