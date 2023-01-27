3660b8cddaa4258ee355522dcb39e9a0af7f1fb9
Facundo Domínguez <facundo.dominguez@tweag.io>
2023-01-27 10:45:33 -0300

    Collect comments from match groups

--- src-ghc/Liquid/GHC/GhcMonadLike.hs
+++ src-ghc/Liquid/GHC/GhcMonadLike.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeSynonymInstances #-}
 {-# LANGUAGE FlexibleInstances #-}
 -- | This module introduces a \"lighter\" "GhcMonad" typeclass which doesn't require an instance of
@@ -304,7 +305,8 @@ apiComments pm =
     let hs = unLoc (pm_parsed_source pm)
         importComments = map (epAnnComments . ann . getLoc) (hsmodImports hs)
         modComments = epAnnComments (hsmodAnn hs)
-        declComments = map (epAnnComments . ann . getLoc) (hsmodDecls hs)
+        declComments = concat $ concat
+          [ [funBindMatchComments d, [ epAnnComments (ann sp)]] | L sp d <- hsmodDecls hs ]
      in
         mapMaybe (tokComment . toRealSrc) $
           concatMap priorComments $
@@ -317,3 +319,7 @@ apiComments pm =
     -- TODO: take into account anchor_op, which only matters if the source was
     -- pre-processed by an exact-print-aware tool.
     toRealSrc (L a e) = L (RealSrcSpan (anchor a) Nothing) e
+
+    funBindMatchComments (ValD _ fb@FunBind{}) =
+      map (epAnnComments . ann . getLoc) $ unLoc $ mg_alts $ fun_matches fb
+    funBindMatchComments _ = []
