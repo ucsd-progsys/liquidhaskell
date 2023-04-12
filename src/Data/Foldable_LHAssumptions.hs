021b5066d1cfd2a217dac89c496e20fcf5a1471f
Facundo Domínguez <facundo.dominguez@tweag.io>
2023-04-12 09:34:14 -0300

    Tweak quantifiers of Data.Foldable.null

--- src/Data/Foldable_LHAssumptions.hs
+++ src/Data/Foldable_LHAssumptions.hs
@@ -4,5 +4,5 @@ import GHC.Types_LHAssumptions()
 
 {-@
 assume Data.Foldable.length :: Data.Foldable.Foldable f => forall a. xs:f a -> {v:Nat | v = len xs}
-assume Data.Foldable.null   :: Data.Foldable.Foldable f => v:(f a) -> {b:Bool | (b <=> len v = 0) && (not b <=> len v > 0)}
+assume Data.Foldable.null   :: Data.Foldable.Foldable f => forall a. v:(f a) -> {b:Bool | (b <=> len v = 0) && (not b <=> len v > 0)}
 @-}
