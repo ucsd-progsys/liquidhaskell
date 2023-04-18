4426cbd7d5b53acb2ceb5afb8b32f869eece4522
Facundo Domínguez <facundo.dominguez@tweag.io>
2023-04-18 10:27:23 -0300

    Rewrite the liquid-parallel package to use LHAssumptions modules

--- liquid-parallel/src/Control/Parallel/Strategies_LHAssumptions.hs
+++ liquid-parallel/src/Control/Parallel/Strategies_LHAssumptions.hs
@@ -1,3 +1,8 @@
-module spec Control.Parallel.Strategies where
+{-# OPTIONS_GHC -Wno-unused-imports #-}
+module Control.Parallel.Strategies_LHAssumptions where
 
+import Control.Parallel.Strategies
+
+{-@
 assume Control.Parallel.Strategies.withStrategy :: Control.Parallel.Strategies.Strategy a -> x:a -> {v:a | v == x}
+@-}
