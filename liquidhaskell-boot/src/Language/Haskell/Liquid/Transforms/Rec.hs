7cf94b6153ebb444178d70a48c3e7cef179e0629
Facundo Domínguez <facundo.dominguez@tweag.io>
2024-09-13 21:36:48 +0000

    Apply inlineLoopBreaker to local bindings

--- liquidhaskell-boot/src/Language/Haskell/Liquid/Transforms/Rec.hs
+++ liquidhaskell-boot/src/Language/Haskell/Liquid/Transforms/Rec.hs
@@ -7,10 +7,7 @@ module Language.Haskell.Liquid.Transforms.Rec (
      transformRecExpr
      ) where
 
-import qualified Data.HashMap.Strict                  as M
-import           Data.Maybe (mapMaybe)
 import           Liquid.GHC.API      as Ghc hiding (panic)
-import           Language.Haskell.Liquid.GHC.Play
 import           Language.Fixpoint.Misc               (mapSnd) -- , traceShow)
 import           Language.Haskell.Liquid.Types.Errors
 import           Prelude                              hiding (error)
@@ -19,56 +16,7 @@ import qualified Data.List                            as L
 
 
 transformRecExpr :: CoreProgram -> CoreProgram
-transformRecExpr cbs = pg
-  where
-    pg     = inlineFailCases pg0
-    pg0    = map inlineLoopBreaker cbs
-
-
--- | Changes top level bindings of the form
---
--- > v = \x1...xn ->
--- >   letrec v0 = \y0...ym -> e0
--- >       in v0 xj..xn
---
--- to
---
--- > v = \x1...xj y0...ym ->
--- >   e0 [ v0 := v x1...xj y0...ym ]
---
-inlineLoopBreaker :: Bind Id -> Bind Id
-inlineLoopBreaker (NonRec x e)
-    | Just (lbx, lbe, lbargs) <- hasLoopBreaker be =
-       let lbe' = sub (M.singleton lbx ecall) lbe
-           mkLams ex = foldr Lam ex (αs ++ take (length as - length lbargs) as)
-           mkLets ex = foldr Let ex nrbinds
-        in Rec [(x, mkLams (mkLets lbe'))]
-  where
-    (αs, as, e') = collectTyAndValBinders e
-    (nrbinds, be) = collectNonRecLets e'
-
-    ecall = L.foldl' App (L.foldl' App (Var x) (Type . TyVarTy <$> αs)) (Var <$> as)
-
-    hasLoopBreaker :: CoreExpr -> Maybe (Var, CoreExpr, [CoreExpr])
-    hasLoopBreaker (Let (Rec [(x1, e1)]) e2)
-      | (Var x2, args) <- collectArgs e2
-      , isLoopBreaker x1
-      , x1 == x2
-      , all isVar args
-      , L.isSuffixOf (mapMaybe getVar args) as
-      = Just (x1, e1, args)
-    hasLoopBreaker _ = Nothing
-
-    isLoopBreaker =  isStrongLoopBreaker . occInfo . idInfo
-
-    getVar (Var x) = Just x
-    getVar _ = Nothing
-
-    isVar (Var _) = True
-    isVar _ = False
-
-inlineLoopBreaker bs
-  = bs
+transformRecExpr = inlineFailCases
 
 -- | Inlines bindings of the form
 --
@@ -103,15 +51,10 @@ inlineFailCases = (go [] <$>)
 
     goalt su (Alt c xs e)   = Alt c xs (go' su e)
 
-    isFailId x  = isLocalId x && isSystemName (varName x) && L.isPrefixOf "fail" (show x)
+    isFailId x  = isLocalId x && isSystemName (varName x) && L.isPrefixOf "fail" (getOccString x)
     getFailExpr = L.lookup
 
     addFailExpr x (Lam v e) su
       | not (elemVarSet v $ exprFreeVars e)  = (x, e):su
     addFailExpr _ _         _  = impossible Nothing "internal error" -- this cannot happen
 
-
-collectNonRecLets :: Expr t -> ([Bind t], Expr t)
-collectNonRecLets = go []
-  where go bs (Let b@(NonRec _ _) e') = go (b:bs) e'
-        go bs e'                      = (reverse bs, e')
