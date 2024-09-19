6ecac3bf668911f5b34504eeebdf7876364d320f
Facundo Domínguez <facundo.dominguez@tweag.io>
2024-09-19 20:08:48 +0000

    Please hlint

--- liquidhaskell-boot/src/Language/Haskell/Liquid/Bare/Elaborate.hs
+++ liquidhaskell-boot/src/Language/Haskell/Liquid/Bare/Elaborate.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE ExplicitForAll            #-}
 {-# LANGUAGE TypeFamilies              #-}
 {-# LANGUAGE DeriveFunctor             #-}
-{-# LANGUAGE TypeApplications          #-}
 {-# LANGUAGE LambdaCase                #-}
 {-# LANGUAGE OverloadedStrings         #-}
 
