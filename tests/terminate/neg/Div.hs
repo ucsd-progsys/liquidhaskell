8977522a5c97f67e2a4a182abaef42360b1af358
Roland Coeurjoly <rolandcoeurjoly@gmail.com>
2024-09-22 13:06:57 +0200

    Add non-termination test for Div

---
+++ tests/terminate/neg/Div.hs
@@ -0,0 +1,18 @@
+{-@ LIQUID "--expect-error-containing=Liquid Type Mismatch" @-}
+module Div where
+
+iterateTo0 :: Int -> Int
+iterateTo0 0 = 0
+iterateTo0 n = iterateTo0 (div n 2)
+
+{-@ test :: {v:Int | v == 0} @-}
+test = iterateTo0 (-1)
+
+{-@ test2 :: {v:Int | v == 0} @-}
+test2 = iterateTo0 1024
+
+{-@ test3 :: {v:Int | v == 0} @-}
+test3 = iterateTo0 9
+
+{-@ test4 :: {v:Int | v == 0} @-}
+test4 = iterateTo0 0
