fd8512975eef541db7b0eaa009496710e6a44475
Facundo Domínguez <facundo.dominguez@tweag.io>
2024-11-26 09:23:15 +0000

    Accomodate change of XBndrWildCard GhcPs

--- liquidhaskell-boot/src/Language/Haskell/Liquid/Bare/Elaborate.hs
+++ liquidhaskell-boot/src/Language/Haskell/Liquid/Bare/Elaborate.hs
@@ -712,6 +712,6 @@ specTypeToLHsType = \case
       impossible Nothing "RExprArg should not appear here"
     RAppTy t t' _ -> nlHsFunTy (specTypeToLHsType t) (specTypeToLHsType t')
     RRTy _ _ _ t -> specTypeToLHsType t
-    RHole _ -> noLocA $ HsWildCardTy Ghc.noExtField
+    RHole _ -> noLocA $ HsWildCardTy Ghc.noAnn
     RExprArg _ ->
       todo Nothing "Oops, specTypeToLHsType doesn't know how to handle RExprArg"
