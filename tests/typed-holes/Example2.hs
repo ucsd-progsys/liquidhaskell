45456af94d20a453a03caf7103018650f7312b98
Matheus de Sousa Bernardo <matheussbernardo@gmail.com>
2025-02-25 10:13:28 +0100

    Adding more examples to the warn on term holes tests

---
+++ tests/typed-holes/Example2.hs
@@ -0,0 +1,28 @@
+{-@ LIQUID "--expect-error-containing=Hole Found" @-}
+{-@ LIQUID "--exact-data-cons" @-}
+{-@ LIQUID "--warn-on-term-holes" @-}
+-- Based on https://ucsd-progsys.github.io/liquidhaskell-blog/2016/10/06/structural-induction.lhs/
+
+module Example2 where
+    import Prelude hiding ((<>))
+    import Language.Haskell.Liquid.ProofCombinators ((===), (***), QED(QED), Proof)
+    
+    hole = undefined
+
+    {-@ reflect empty @-}
+    empty  :: [a]
+    empty  = []
+
+    {-@ infix <> @-}
+    {-@ reflect <> @-}
+    (<>) :: [a] -> [a] -> [a]
+    [] <> xs = xs
+    (x:xs) <> ys = x : (xs <> ys)
+
+    {-@ leftId  :: x:[a] -> { (empty <> x) == x } @-}
+    leftId :: [a] -> Proof
+    leftId x
+        =   empty <> x
+        === hole
+        === x
+        *** QED
