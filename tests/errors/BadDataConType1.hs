c4d30b39a56f799d109088a8e55ce854a0e92409
Niki Vazou <nikivazou@gmail.com>
2025-05-21 11:00:32 +0200

    cleaning

--- tests/errors/BadDataConType1.hs
+++ tests/errors/BadDataConType1.hs
@@ -1,4 +1,5 @@
-{-@ LIQUID "--expect-error-containing=Specified type does not refine Haskell type for `BadDataConType1.C`" @-}
+{-@ LIQUID "--expect-error-containing=Specified type does not refine Haskell type for `BadDataConType1.fldY`" @-}
+{-@ LIQUID "--expect-error-containing=Illegal type specification for `BadDataConType1.fldY`" @-}
 module BadDataConType1 where
 
 {-@ data T = C { fldX :: Int, fldY :: String } @-}
