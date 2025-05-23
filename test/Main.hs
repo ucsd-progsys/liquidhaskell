2cc9c9c014d7904d2a3d25f609ba75fa79e15bbe
Xavier Góngora <ixbalanque@protonmail.ch>
2025-05-23 11:37:13 -0600

    alternative quot and mod implementation test fails

--- test/Main.hs
+++ test/Main.hs
@@ -2,6 +2,22 @@
 
 module Main where
 
-import QuotRem ()
+import Control.Monad (unless)
+import QuotRem (prop_quotRem, prop_quotRemDivision)
+import System.Exit (exitFailure)
+import Test.QuickCheck (quickCheck, verboseCheck, quickCheckResult, isSuccess)
 
-main = putStrLn "hello from the liquidhaskell-test suite"
+main :: IO ()
+main = do
+        verboseCheck prop_quotRemDivision
+        verboseCheck prop_quotRem
+
+-- | Throws error if a test fails.
+main' :: IO ()
+main' = do
+  let tests =
+        [ quickCheckResult prop_quotRemDivision
+        , quickCheckResult prop_quotRem
+        ]
+  success <- fmap (all isSuccess) . sequence $ tests
+  unless success exitFailure
--- test/QuotRem.hs
+++ test/QuotRem.hs
@@ -1,12 +1,20 @@
 -- |
 
-module QuotRem where
+module QuotRem (prop_quotRem, prop_quotRemDivision) where
+import Test.QuickCheck
 
-{-@ predicate EqSign X Y = if X >= 0 then Y >= 0 else Y < 0 @-}
+prop_quotRem :: Integral a => a -> a -> Property
+prop_quotRem x y = y /= 0 ==> quotRem' x y == quotRem x y
 
-{-@ quotRemProps :: x:a
-                    -> {y:a | y /= 0 && EqSign x y}
-                    -> {z : (a,a) |
-                         (EqSign x y => fst z = x / y && snd z = x mod y)}  @-}
-quotRemProps :: Integral a => a -> a -> (a,a)
-quotRemProps x y = (div x y, mod x y)
+{-@quotRem':: x:a -> {y:a | y /= 0} -> {z:(a,a) | fst z = quot x y && snd z = rem x y }@-}
+-- | This is a definition of `quotRem` in terms of `div` and `mod`
+-- used at @liquidhaskell/src/GHC/Real_LHAssumptions.hs@
+-- to implement both 'quot' and 'rem' in the refinement logic.
+quotRem' :: Integral a => a -> a -> (a,a)
+quotRem' x y | signum x == signum y || abs x == abs y = (div x y, mod x y)
+             | abs x > abs y = (- (div x y), x + y * div x y)
+             | otherwise = (0, x)
+
+prop_quotRemDivision :: Integral a => a -> a -> Property
+prop_quotRemDivision x y = y/= 0 ==> x == q * y + r
+  where (q,r) = quotRem' x y
