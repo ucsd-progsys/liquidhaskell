bb6c1f0ccfbcc283fff78f6047a3fd544ca67959
Xavier Góngora <ixbalanque@protonmail.ch>
2025-06-17 10:41:28 -0600

    update test to include unresolved qualified name

--- tests/names/neg/QualifiedAliases.hs
+++ tests/names/neg/QualifiedAliases.hs
@@ -1,14 +1,20 @@
-{-@ LIQUID "--expect-error-containing=Multiple definitions of Type Alias" @-}
+{-@ LIQUID "--expect-any-error" @-}
 
 -- | This is an instance of LiquidHaskell having a flat import namespace
--- for logic names. Here, both Nat modules export the same type alias 'INat',
--- producing an error even though we explicitly qualify it to avoid ambiguity.
+-- for logic names. Here, both @Nat@ modules export a type alias with
+-- the same name @INat@. With current behavior this module produces a
+-- _Multiple definition of Type Alias_ error when using the alias unqualified,
+-- and an _Unknown type constructor_ error with the qualified alias.
+-- NOTE: This test will be moved to @names-pos@ after fixing issue #2841
 module QualifiedAliases where
 
 import qualified Nat1 as N
 import Nat2
 
 {-@ llength :: [a] -> Nat @-}
-llength :: [a] -> Int
 llength [] = 0
 llength (x : xs) = 1 + length xs
+
+{-@ llength' :: [a] -> N.Nat @-}
+llength' [] = 0
+llength' (x : xs) = 1 + length xs
