b2625c9ccc97c85780e712eba636940f1b846ce4
Alessio Ferrarini <alessio.ferrarini@imdea.org>
2025-10-31 10:08:17 +0100

    Add more tests about tuple parsing

--- tests/names/pos/TupleParse.hs
+++ tests/names/pos/TupleParse.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE GADTs #-}
+{-@ LIQUID "--reflection" @-}
+{-@ LIQUID "--ple"        @-}
 
 module TupleParse where
 
@@ -9,3 +11,13 @@ import Language.Haskell.Liquid.ProofCombinators
 data Foo where
   {-@ MkFoo :: x:Int -> Ix Foo (x, x) @-}
   MkFoo :: Int -> Foo
+
+
+{-@ type Alias a B = { v:a | B = B } @-}
+{-@ tuples :: Alias (a, b) 1 -> Alias (b, a) 1 @-}
+tuples :: (a, b) -> (b, a)
+tuples (x, y) = (y, x)
+
+{-@ goldbachsconjecture  :: { (1, 2) /= (2, 1) } @-}
+goldbachsconjecture :: Proof
+goldbachsconjecture = trivial
