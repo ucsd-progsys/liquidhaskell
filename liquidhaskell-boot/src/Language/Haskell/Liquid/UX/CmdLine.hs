c01a24fc40c5e83275ff3c8dbeb863a1e97ac2e2
SpyTanu <tanukoshual@gmail.com>
2026-03-20 21:43:10 +0530

    Update CmdLine.hs

--- liquidhaskell-boot/src/Language/Haskell/Liquid/UX/CmdLine.hs
+++ liquidhaskell-boot/src/Language/Haskell/Liquid/UX/CmdLine.hs
@@ -494,7 +494,7 @@ envCfg = do
                          return (c <> c')
                      ) defConfig (words s)
   where
-    envLoc  = Loc l l
+    envLoc  = Loc l l x
     l       = safeSourcePos "ENVIRONMENT" 1 1
 
 copyright :: String
