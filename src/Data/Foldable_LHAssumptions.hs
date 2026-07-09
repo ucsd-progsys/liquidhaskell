5efe0a187629bcbd9cc0933e40061cbd510c6525
Xavier Góngora <xavier.gongora@tweag.io>
2026-07-09 16:26:54 -0600

    Add specification for `Data.Foldable.find`

--- src/Data/Foldable_LHAssumptions.hs
+++ src/Data/Foldable_LHAssumptions.hs
@@ -9,4 +9,5 @@ import Prelude hiding (length, null)
 {-@
 assume length :: Foldable f => forall a. xs:f a -> {v:Nat | v = len xs}
 assume null   :: Foldable f => forall a. v:(f a) -> {b:Bool | (b <=> len v = 0) && (not b <=> len v > 0)}
+assume find   :: Foldable f =>  pred:(a -> Bool) -> f a -> Maybe {x:a | pred x}
 @-}
